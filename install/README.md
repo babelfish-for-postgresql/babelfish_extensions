56ade4ead6f57bd6def890146ede437593825017
Xavier Sierra <xaviersb@ongres.com>
2021-12-10 18:42:55 +0100

    feat: quick install scripts
    Signed-off-by: Xavier Sierra <xaviersb@ongres.com>

---
+++ install/README.md
@@ -0,0 +1,24 @@
+# Quick install scripts
+
+This directory contains scripts for installing Babelfish for PostgreSQL. 
+
+The installation process consist in two steps, one for for installting the pre-requisites and another for downloading and compiling babelfish. 
+
+At this point we only have quickstart scripts for Centos8 and Ubuntu 20. 
+
+## Installing for Centos 8 
+
+For centos8 you need to run the following commands:
+``` sh
+sudo sh centos.centos8.prerequisites.sh
+sudo sh install.sh
+```
+
+## Installing for Ubuntu 20.04
+
+For Ubuntu 20 you need to run the following commands:
+``` sh
+sudo sh ubuntu.20.04.prerequisites.sh
+sudo sh install.sh
+```
+
---
+++ install/install.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+
+export BABELFISH_CODE_USER="${BABELFISH_CODE_USER:-babelfish-compiler}"
+export BABELFISH_CODE_PATH="${BABELFISH_CODE_PATH:-/opt/babelfish-code}"
+export BABELFISH_INSTALLATION_PATH="${BABELFISH_INSTALLATION_PATH:-/usr/local/pgsql-13.4}"
+export BABELFISH_DATA_PATH=/usr/local/pgsql/data
+export PG_SRC="$BABELFISH_CODE_PATH/postgresql_modified_for_babelfish"
+export EXTENSIONS_SOURCE_CODE_PATH="$BABELFISH_CODE_PATH/babelfish_extensions"
+export PG_CONFIG="$BABELFISH_INSTALLATION_PATH/bin/pg_config"
+export cmake=/usr/bin/cmake
+
+if ! id "$BABELFISH_CODE_USER" > /dev/null
+then
+  useradd "$BABELFISH_CODE_USER" 
+fi
+
+mkdir -p "$BABELFISH_CODE_PATH"
+
+cd "$BABELFISH_CODE_PATH" || exit 1
+
+## Downloading latest babelfish engine source code
+wget https://github.com/babelfish-for-postgresql/postgresql_modified_for_babelfish/archive/refs/heads/BABEL_1_X_DEV__13_4.zip
+  
+unzip BABEL_1_X_DEV__13_4.zip 
+
+mv postgresql_modified_for_babelfish-BABEL_1_X_DEV__13_4 "$PG_SRC"
+
+rm BABEL_1_X_DEV__13_4.zip
+
+chown -R "$BABELFISH_CODE_USER:$BABELFISH_CODE_USER" "$PG_SRC"
+
+## Downloading latest babelbish extension source code
+
+wget https://github.com/babelfish-for-postgresql/babelfish_extensions/archive/refs/heads/BABEL_1_X_DEV.zip
+
+unzip BABEL_1_X_DEV.zip
+
+mv babelfish_extensions-BABEL_1_X_DEV $EXTENSIONS_SOURCE_CODE_PATH
+
+rm BABEL_1_X_DEV.zip
+
+chown -R "$BABELFISH_CODE_USER:$BABELFISH_CODE_USER" "$EXTENSIONS_SOURCE_CODE_PATH"
+
+cd "$PG_SRC" || exit 1
+runuser -u "$BABELFISH_CODE_USER" -- ./configure CFLAGS="-ggdb" \
+  --prefix="$BABELFISH_INSTALLATION_PATH" \
+  --enable-debug \
+  --with-libxml \
+  --with-uuid=ossp \
+  --with-icu \
+  --with-extra-version=" Babelfish for PostgreSQL"
+
+mkdir -p "$BABELFISH_INSTALLATION_PATH"
+
+chown -R "$BABELFISH_CODE_USER:$BABELFISH_CODE_USER" "$BABELFISH_INSTALLATION_PATH"
+
+#Compilining babelfish engine
+cd "$BABELFISH_CODE_PATH/postgresql_modified_for_babelfish" || exit 1
+runuser -u "$BABELFISH_CODE_USER" make # Compiles the Babefish for PostgreSQL engine
+
+cd "$BABELFISH_CODE_PATH/postgresql_modified_for_babelfish/contrib" || exit 1
+runuser -u "$BABELFISH_CODE_USER" make # Compiles the PostgreSQL default extensions
+
+cd "$BABELFISH_CODE_PATH/postgresql_modified_for_babelfish" || exit 1
+
+runuser -u "$BABELFISH_CODE_USER" make install # Installs the Babelfish for PostgreSQL engine
+cd "$BABELFISH_CODE_PATH/postgresql_modified_for_babelfish/contrib" || exit 1
+
+runuser -u "$BABELFISH_CODE_USER" make install # Installs the PostgreSQL default extensions
+
+# Dowloads the compressed Antlr4 Runtime sources on /opt/antlr4-cpp-runtime-4.9.2-source.zip 
+curl https://www.antlr.org/download/antlr4-cpp-runtime-4.9.2-source.zip \
+  --output /opt/antlr4-cpp-runtime-4.9.2-source.zip 
+
+# Uncompress the source into /opt/antlr4
+unzip -d /opt/antlr4 /opt/antlr4-cpp-runtime-4.9.2-source.zip
+
+mkdir /opt/antlr4/build 
+cd /opt/antlr4/build || exit 1
+
+# Generates the make files for the build
+cmake .. -DANTLR_JAR_LOCATION="$EXTENSIONS_SOURCE_CODE_PATH/contrib/babelfishpg_tsql/antlr/thirdparty/antlr/antlr-4.9.2-complete.jar" \
+         -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_DEMO=True
+# Compiles and install
+make
+make install
+
+cp /usr/local/lib/libantlr4-runtime.so.4.9.2 "$BABELFISH_INSTALLATION_PATH/lib"
+
+# Install babelfishpg_money extension
+cd "$EXTENSIONS_SOURCE_CODE_PATH/contrib/babelfishpg_money" || exit 1
+runuser -u "$BABELFISH_CODE_USER" -- make
+runuser -u "$BABELFISH_CODE_USER" -- make install
+
+# Install babelfishpg_common extension
+cd "$EXTENSIONS_SOURCE_CODE_PATH/contrib/babelfishpg_common" || exit 1
+runuser -u "$BABELFISH_CODE_USER" -- make
+runuser -u "$BABELFISH_CODE_USER" -- make install
+
+# Install babelfishpg_tds extension
+cd "$EXTENSIONS_SOURCE_CODE_PATH/contrib/babelfishpg_tds" || exit 1
+runuser -u "$BABELFISH_CODE_USER" -- make
+runuser -u "$BABELFISH_CODE_USER" -- make install
+
+# Installs the babelfishpg_tsql extension
+cd "$EXTENSIONS_SOURCE_CODE_PATH/contrib/babelfishpg_tsql" || exit 1
+runuser -u "$BABELFISH_CODE_USER" -- make
+runuser -u "$BABELFISH_CODE_USER" -- make install
+
+mkdir -p "$BABELFISH_DATA_PATH"
+
+useradd postgres
+
+chown -R postgres:postgres "$BABELFISH_INSTALLATION_PATH"
+chown -R postgres:postgres "$BABELFISH_DATA_PATH"
+
+runuser -u "postgres" -- "$BABELFISH_INSTALLATION_PATH/bin/initdb" -D "$BABELFISH_DATA_PATH"
+
+echo "listen_addresses = '*'" >> "$BABELFISH_DATA_PATH/postgresql.conf"
+echo "shared_preload_libraries = 'babelfishpg_tds'" >> "$BABELFISH_DATA_PATH/postgresql.conf"
+
+cd "$BABELFISH_INSTALLATION_PATH" || exit 1
+
+runuser -u "postgres" -- "$BABELFISH_INSTALLATION_PATH/bin/pg_ctl" -D "$BABELFISH_DATA_PATH" start
