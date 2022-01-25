c73e75e7e1e5b70ac591936380909ec04ef4087a
Avantika Dasgupta <davantik@amazon.com>
2022-01-25 13:55:25 -0500

    Support sys.columns system catalog view
    This commit updates the sys.columns view to get the values for following columns:
    precision, scale, is_identity, is_computed and is_ansi_padded
    
    These columns are being retrieved from PostgreSQL's information_schema.columns view but there seems to be some issue while calling information_schema views in tsql dialect.  This commit creates an internal function named sys.columns_internal with the plpgsql language which can safely call information_schema in psql dialect. This function is being used to get all the values for the sys.columns view.
    
    Task: BABEL-2497
    Author: Rishabh Tanwar <ritanwar@amazon.com>
    Signed-off-by: Avantika Dasgupta <davantik@amazon.com>

---
+++ test/JDBC/input/BABEL-SYS_COLUMNS.sql
@@ -0,0 +1,24 @@
+-- Tests for sys.columns catalog view
+-- Test precision and scale for all numeric datatypes
+create table t1(a int, b float, c bigint, d numeric, e smallint, f tinyint, g decimal, h money, i smallmoney);
+go
+select name, column_id, precision, scale from sys.columns where object_id=OBJECT_ID('t1');
+go
+
+-- Test identity and computed columns
+create table t2(a int, b int IDENTITY(1,1), c as a * b);
+go
+select name, column_id, is_identity, is_computed from sys.columns where object_id=OBJECT_ID('t2');
+go
+
+-- Test ansi padded columns
+create table t3(a char(10), b nchar(10), c binary(10));
+go
+select name, column_id, is_ansi_padded from sys.columns where object_id=OBJECT_ID('t3');
+go
+
+-- Cleanup
+drop table t1;
+drop table t2;
+drop table t3;
+go
