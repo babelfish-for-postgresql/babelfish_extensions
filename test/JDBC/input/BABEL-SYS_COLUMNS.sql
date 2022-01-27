424f1b86a8a7130c08a1048f94e91169f992d44c
Avantika Dasgupta <davantik@amazon.com>
2022-01-27 15:52:10 -0800

    sys.columns.collation_name shows incorrect collation
    This commit fixes a bug where sys.columns view returns the collation 
    name as "default" even for columns with non-default collation.
    
    The issue was in the sys.columns view where view pg_collation was 
    joined with pg_type on typcollation but instead it should be joined with 
    pg_attribute on attcollation column.
    
    Task: BABEL-2660
    Author: Rishabh Tanwar <ritanwar@amazon.com>
    Signed-off-by: Avantika Dasgupta <davantik@amazon.com>

--- test/JDBC/input/BABEL-SYS_COLUMNS.sql
+++ test/JDBC/input/BABEL-SYS_COLUMNS.sql
@@ -17,8 +17,21 @@ go
 select name, column_id, is_ansi_padded from sys.columns where object_id=OBJECT_ID('t3') order by name;
 go
 
+-- Test collation name
+create table t4(
+	c1 char(10) COLLATE SQL_LATIN1_GENERAL_CP1_CI_AI,
+	c2 char(10) COLLATE SQL_LATIN1_GENERAL_CP1_CI_AS,
+	c3 char(10) COLLATE SQL_LATIN1_GENERAL_CP1_CS_AI,
+	c4 char(10) COLLATE SQL_LATIN1_GENERAL_CP1_CS_AS,
+	c5 char(10) COLLATE SQL_LATIN1_GENERAL_CP1250_CI_AS
+);
+go
+select name, column_id, collation_name from sys.columns where object_id=OBJECT_ID('t4') order by name;
+go
+
 -- Cleanup
 drop table t1;
 drop table t2;
 drop table t3;
+drop table t4;
 go
