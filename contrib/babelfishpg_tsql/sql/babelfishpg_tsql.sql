2bc106e8706d9b79cae540a1060b062629a4e188
Sharu Goel <30777678+thephantomthief@users.noreply.github.com>
2022-01-29 04:39:21 +0530

    Fix sp_tablecollations_100 procedure trying to search for string @object
    This commit fixes an issue where sp_tablecollations_100 searched 
    for “@object” instead of the parameter @object.
    
    Author: Kushaal Shroff <kushaal@amazon.com>
    Signed-off-by: Sharu Goel <goelshar@amazon.com>

--- contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
+++ contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
@@ -610,7 +610,7 @@ BEGIN
     from
         sys.spt_tablecollations_view s_tcv
     where
-        s_tcv.object_id = sys.object_id("@object") AND
+        s_tcv.object_id = sys.object_id(@object) AND
         s_tcv.name NOT IN ('cmin', 'cmax', 'xmin', 'xmax', 'ctid', 'tableoid')
     order by colid;
 END;
