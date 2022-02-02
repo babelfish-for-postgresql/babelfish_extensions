d12363588b698b493247c04ef9ffa42c498db322
Dipesh Dhameliya <dddhamel@amazon.com>
2022-02-02 19:53:18 +0530

    [BABEL-2890] Use logical schema name to write table name in column metadata instead of physical schema name.
    Previously, Physical schema name i.e., master_dbo, temp_dbo, etc was being written as part of column metadata while
    sending the response.But logical schema should be written as far as Babelfish concerned.
    
    In this commit, I introduced two rendezvous variables pltsql_get_logical_schema_name and pltsql_is_shared_schema which
    would be initialised by babelfishpg_tsql extension to get_logical_schema_name() and is_shared_schema() respectively.
    
    Variable pltsql_is_shared_schema would be used by tds extension to determine if given physical schema name needs to be
    translated to logical schema name or not. And variable pltsql_get_logical_schema_name would be used to retrieve the
    logical schema name given physical schema name if required.

--- contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
@@ -1697,8 +1697,9 @@ PrepareRowDescription(TupleDesc typeinfo, List *targetlist, int16 *formats,
 			/* if not found, add one */
 			if (!found)
 			{
-				Relation 				rel;
-				TdsRelationMetaDataInfo relMetaDataInfo;
+				Relation		rel;
+				TdsRelationMetaDataInfo	relMetaDataInfo;
+				const char		*physical_schema_name;
 
 				relMetaDataInfo = (TdsRelationMetaDataInfo) palloc(sizeof(TdsRelationMetaDataInfoData));
 				tableNum++;
@@ -1719,7 +1720,16 @@ PrepareRowDescription(TupleDesc typeinfo, List *targetlist, int16 *formats,
 
 				/* fetch the relation name, schema name */
 				relMetaDataInfo->partName[0] = RelationGetRelationName(rel);
-				relMetaDataInfo->partName[1] = get_namespace_name(RelationGetNamespace(rel));
+				physical_schema_name = get_namespace_name(RelationGetNamespace(rel));
+
+				/* logical schema name should be sent as part of response as far as Babelfish is concerned. */
+				if (pltsql_plugin_handler_ptr && 
+					pltsql_plugin_handler_ptr->pltsql_get_logical_schema_name)
+					/* Its very unlikely that logical schema name could not be found for any other cases. */
+					relMetaDataInfo->partName[1] = pltsql_plugin_handler_ptr->pltsql_get_logical_schema_name(physical_schema_name);
+
+				if (physical_schema_name)
+					pfree(physical_schema_name);
 
 				relation_close(rel, AccessShareLock);
 
