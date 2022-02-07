97b25dfdb5a8daa1e92b2678137b1e271fe835fb
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2022-02-07 18:28:42 +0530

    Information about objects created outside of the current logical database should not be returned for sys.default_constraints, sys.index_columns, and sys.computed_columns
    Information about objects created outside of the current logical database should not be returned for sys.default_constraints, sys.index_columns, and sys.computed_columns
    
    Previously, Babelfish was returning details about all objects created under all Babelfish databases for most of the sys views which was wrong.
    
    Ideally only information about objects created under the current logical database should be returned with any sys views.
    
    This commit fixes above mentioned issue for sys.default_constraints, sys.index_columns, sys.computed_columns by using sys.schemas to filter the returned information.
    
    Task: BABEL-2709
    Author: Dipesh Dhameliya dddhamel@amazon.com
    Signed-off-by: Shalini Lohia lshalini@amazon.com

--- test/JDBC/input/views/sys-index_columns.sql
+++ test/JDBC/input/views/sys-index_columns.sql
@@ -14,8 +14,53 @@ GO
 SELECT COUNT(*) FROM sys.index_columns WHERE object_id = OBJECT_ID('sys_index_columns')
 GO
 
-SELECT COUNT(*) FROM sys.all_columns WHERE object_id = object_id('sys.index_columns')
+DROP TABLE IF EXISTS sys_index_columns
 GO
 
-DROP TABLE IF EXISTS sys_index_columns
+CREATE DATABASE db1;
+GO
+
+USE db1
+GO
+
+CREATE TABLE rand_name1(rand_col1 int DEFAULT 1);
+GO
+
+CREATE INDEX idx_rand_name1 ON rand_name1(rand_col1);
+GO
+
+SELECT count(*) FROM  sys.index_columns idx JOIN sys.tables tab ON idx.object_id = tab.object_id WHERE tab.name = 'rand_name1';
+GO
+
+USE master;
+GO
+
+SELECT count(*) FROM  sys.index_columns idx JOIN sys.tables tab ON idx.object_id = tab.object_id WHERE tab.name = 'rand_name1';
+GO
+
+CREATE TABLE rand_name2(rand_col2 int DEFAULT 1);
+GO
+
+CREATE INDEX idx_rand_name2 ON rand_name2(rand_col2);
+GO
+
+SELECT count(*) FROM  sys.index_columns idx JOIN sys.tables tab ON idx.object_id = tab.object_id WHERE tab.name = 'rand_name2';
+GO
+
+USE db1
+GO
+
+SELECT count(*) FROM  sys.index_columns idx JOIN sys.tables tab ON idx.object_id = tab.object_id WHERE tab.name = 'rand_name2';
+GO
+
+DROP TABLE rand_name1;
+GO
+
+USE master
+GO
+
+DROP TABLE rand_name2;
+GO
+
+DROP DATABASE db1;
 GO
