f9a50a71726efea6850b5760add38947eef24fc9
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2022-02-07 18:54:54 +0530

    Information about objects created outside of the current logical database should not be returned for sys.foreign_keys, sys.key_constraints, sys.procedures and sys.objects
    Information about objects created outside of the current logical database should not be returned for sys.foreign_keys, sys.key_constraints, sys.procedures, and sys.objects
    
    Previously, Babelfish was returning details about all objects created under all Babelfish databases for most of the sys views which was wrong.
    
    Ideally, only information about objects created under the current logical database should be returned with any sys views.
    
    This commit fixes sys.foreign_keys, sys.key_constraints, sys.procedures and sys.objects views with following changes:
    1. Fixed sys.foreign_keys, sys.key_constraints, sys.procedures to return correct information from sys.schemas.
    2. Incorporated sys.default_constraints objects in sys.objects views.
    
    Task: BABEL-2710
    Author: Dipesh Dhameliya dddhamel@amazon.com
    Signed-off-by: Shalini Lohia lshalini@amazon.com

---
+++ test/JDBC/input/views/sys-key_constraints.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE db1;
+GO
+
+USE db1
+GO
+
+create table uq_1 (a int not null unique)
+GO
+
+select count(*) from sys.key_constraints where parent_object_id = object_id('uq_1');
+GO
+
+USE master
+GO
+
+select count(*) from sys.key_constraints where parent_object_id = object_id('uq_1');
+GO
+
+create table uq_2 (a int not null unique)
+GO
+
+select count(*) from sys.key_constraints where parent_object_id = object_id('uq_2');
+GO
+
+USE db1
+GO
+
+select count(*) from sys.key_constraints where parent_object_id = object_id('uq_2');
+GO
+
+drop table uq_1;
+GO
+
+USE master
+GO
+
+drop table uq_2;
+GO
+
+DROP DATABASE db1
+GO
