c918fd4a9a4f17c176e97dc3f888d6bbbac7c1a6
Kushaal Shroff <51415286+KushaalShroff@users.noreply.github.com>
2022-02-09 15:55:51 +0530

    Casting HASHBYTES() result to shortened datatype causes TDS error
    sys.varbinary is created as a domain over sys.bbf_varbinary in Babelfish. The sys.hashbytes() function returns sys.bbf_varbinary currently and there was no explicit cast from sys.bbf_varbinary to sys.varbinary available. Neither typmod cast for sys.bbf_varbinary was available.
    
    This resulted in typmod not being handled properly while casting sys.bbf_varbinary to sys.varbinary. This resulted in a protocol violation on the TDS side.
    
    This changes fixes this issue by creating a typmod cast for sys.BBF_VARBINARY that will handle typmod correctly.
    
    Task: BABEL-2557
    Author: Dipesh Dhameliya dddhamel@amazon.com
    Signed-off-by: Kushaal Shroff kushaal@amazon.com

--- contrib/babelfishpg_common/sql/varbinary.sql
+++ contrib/babelfishpg_common/sql/varbinary.sql
@@ -46,6 +46,15 @@ CREATE TYPE sys.BBF_VARBINARY (
     COLLATABLE     = false
 );
 
+CREATE OR REPLACE FUNCTION sys.bbfvarbinary(sys.BBF_VARBINARY, integer, boolean)
+RETURNS sys.BBF_VARBINARY
+AS 'babelfishpg_common', 'varbinary'
+LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+-- typmod cast for sys.BBF_VARBINARY
+CREATE CAST (sys.BBF_VARBINARY AS sys.BBF_VARBINARY)
+WITH FUNCTION sys.bbfvarbinary(sys.BBF_VARBINARY, integer, BOOLEAN) AS ASSIGNMENT;
+
 CREATE OR REPLACE FUNCTION sys.byteavarbinary(pg_catalog.BYTEA, integer, boolean)
 RETURNS sys.BBF_VARBINARY
 AS 'babelfishpg_common', 'byteavarbinary'
