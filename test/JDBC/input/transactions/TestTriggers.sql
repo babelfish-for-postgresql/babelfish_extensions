76c32aa175cb03c76eff62e9df0f6dbe66b63926
Dipesh Dhameliya <dipeshdhameliya125@gmail.com>
2022-02-10 21:05:14 +0530

    Added more test cases for Triggers
    This commit adds tests cases for Trigger with error being raised from different level inside the trigger.
    
    Task: BABEL-2845
    Author: Dipesh Dhameliya dddhamel@amazon.com
    Signed-off-by: Dipesh Dhameliya dddhamel@amazon.com

--- test/JDBC/input/transactions/TestTriggers.sql
+++ test/JDBC/input/transactions/TestTriggers.sql
@@ -1,10 +1,10 @@
-create table triggerTab1(c1 int, c2 varchar(30))
+create table triggerTab1(c1 int, c2 varchar(30), check (c1 < 5))
 go
 
-create table triggerTab2(c1 int)
+create table triggerTab2(c1 int, check (c1 < 5))
 go
 
-create table triggerTab3(c1 int)
+create table triggerTab3(c1 int, check (c1 < 5))
 go
 
 insert into triggerTab1 values(1, 'first')
@@ -46,6 +46,85 @@ go
 insert into triggerTab1 values(3, 'third');
 go
 commit;
+go
+
+update triggerTab2 set c1 = 6;
+GO
+
+begin tran
+go
+update triggerTab2 set c1 = 6;
+go
+if (@@trancount > 0) rollback;
+go
+
+begin tran
+go
+insert into triggerTab1 values(6, 'six');
+go
+if (@@trancount > 0) rollback;
+go
+
+drop trigger txnTrig1;
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+begin tran;
+update triggerTab2 set c1 = 6;
+commit;
+select * from triggerTab2 order by c1;
+select * from inserted;
+go
+
+insert into triggerTab1 values(2, 'second');
+go
+
+begin tran;
+go
+insert into triggerTab1 values(3, 'third');
+go
+if (@@trancount > 0) rollback;
+go
+
+drop trigger txnTrig1;
+go
+
+drop trigger txnTrig3;
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+begin tran;
+update triggerTab2 set c1 = 2;
+commit;
+select * from triggerTab2 order by c1;
+select * from inserted;
+go
+
+create trigger txnTrig3 on triggerTab3 for delete as
+select * from triggerTab3 order by c1;
+insert into triggerTab3 values(6);
+select * from deleted;
+rollback tran sp2;
+go
+
+insert into triggerTab1 values(2, 'second');
+go
+
+begin tran;
+go
+insert into triggerTab1 values(3, 'third');
+go
+if (@@trancount > 0) rollback;
+go
+
+drop trigger txnTrig3;
+go
+
+create trigger txnTrig3 on triggerTab3 for delete as
+select * from triggerTab3 order by c1;
+insert into triggerTab3 values(1);
+select * from deleted;
+rollback tran sp2;
 go
 
 drop trigger txnTrig1;
@@ -98,6 +177,72 @@ commit;
 go
 
 exec triggerProc2;
+go
+
+drop procedure triggerProc2;
+go
+
+drop trigger txnTrig1
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+commit;
+update triggerTab2 set c1 = 6;
+select * from triggerTab2 order by c1;
+go
+
+create procedure triggerProc2 as
+begin tran;
+exec triggerProc1;
+insert into triggerTab1 values(6, 'six');
+commit;
+go
+
+exec triggerProc2;
+go
+
+if (@@trancount>0) commit;
+go
+
+drop trigger txnTrig1;
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+begin tran;
+update triggerTab2 set c1 = 2;
+commit;
+select * from triggerTab2 order by c1;
+select * from inserted;
+go
+
+drop procedure triggerProc1;
+go
+drop procedure triggerProc2;
+go
+
+create table tmp__1 (a int not null);
+go
+
+insert into tmp__1 values (1)
+go
+
+create procedure triggerProc1 as
+save tran sp1;
+insert into tmp__1 values (null);
+rollback tran sp1;
+go
+
+create procedure triggerProc2 as
+begin tran;
+exec triggerProc1;
+insert into triggerTab1 values(3, 'third');
+commit;
+go
+
+exec triggerProc2;
+go
+
+drop table tmp__1 
 go
 
 create table triggerErrorTab(c1 int not null);
@@ -194,4 +339,220 @@ go
 drop table triggerTab3;
 go
 drop table triggerErrorTab
+go
+
+create table triggerTab1(c1 int, c2 varchar(30), check (c1 < 5))
+go
+
+create table triggerTab2(c1 int, check (c1 < 5))
+go
+
+create table triggerTab3(c1 int, check (c1 < 5))
+go
+
+insert into triggerTab1 values(1, 'first')
+go
+
+insert into triggerTab2 values(1)
+go
+
+insert into triggerTab3 values(1)
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+insert into triggerTab2 values (2);
+go
+
+create trigger txnTrig2 on triggerTab2 for insert as
+insert into triggerTab3 values (2);
+go
+
+create trigger txnTrig3 on triggerTab3 for insert as
+select 'nest level 3'
+go
+
+insert into triggerTab1 values(2, 'two')
+go
+
+begin tran
+go
+insert into triggerTab1 values(6, 'six')
+go
+if (@@trancount > 0) rollback tran;
+go
+
+select * from triggerTab1;
+GO
+
+select * from triggerTab2;
+GO
+
+select * from triggerTab3;
+GO
+
+drop trigger txnTrig1;
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+insert into triggerTab2 values (6);
+go
+
+insert into triggerTab2 values(2)
+go
+
+begin tran
+go
+insert into triggerTab2 values(6)
+go
+if (@@trancount > 0) rollback tran;
+go
+
+begin tran
+go
+insert into triggerTab1 values(3, 'three')
+go
+if (@@trancount > 0) rollback tran;
+go
+
+select * from triggerTab1;
+GO
+
+select * from triggerTab2;
+GO
+
+select * from triggerTab3;
+GO
+
+drop trigger txnTrig1;
+go
+drop trigger txnTrig2;
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+insert into triggerTab2 values (2);
+go
+
+create trigger txnTrig2 on triggerTab2 for insert as
+insert into triggerTab3 values (6);
+go
+
+insert into triggerTab3 values(2)
+go
+
+begin tran
+go
+insert into triggerTab3 values(6)
+go
+if (@@trancount > 0) rollback tran;
+go
+
+begin tran
+go
+insert into triggerTab1 values(3, 'three')
+go
+if (@@trancount > 0) rollback tran;
+go
+
+select * from triggerTab1;
+GO
+select * from triggerTab2;
+GO
+select * from triggerTab3;
+GO
+
+drop table triggerTab1;
+go
+drop table triggerTab2;
+go
+drop table triggerTab3;
+go
+
+create table triggerTab1(c1 int not null)
+go
+
+create table triggerTab2(c1 int not null)
+go
+
+create table triggerTab3(c1 int not null)
+go
+
+insert into triggerTab1 values(1)
+go
+
+insert into triggerTab2 values(1)
+go
+
+insert into triggerTab3 values(1)
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+select 'nest level 1'
+insert into triggerTab2 values (2);
+go
+
+create trigger txnTrig2 on triggerTab2 for insert as
+select 'nest level 2'
+insert into triggerTab3 values (null);
+go
+
+create trigger txnTrig3 on triggerTab3 for insert as
+select 'nest level 3'
+go
+
+begin tran
+go
+insert into triggerTab1 values(2)
+go
+if (@@trancount > 0) rollback tran;
+go
+
+begin tran
+go
+insert into triggerTab1 values(NULL)
+go
+if (@@trancount > 0) rollback tran;
+go
+
+select * from triggerTab1;
+go
+select * from triggerTab2;
+go
+select * from triggerTab3;
+go
+
+drop trigger txnTrig1;
+go
+
+drop trigger txnTrig2;
+go
+
+create trigger txnTrig1 on triggerTab1 for insert as
+select 'nest level 1'
+insert into triggerTab2 values (NULL);
+go
+
+create trigger txnTrig2 on triggerTab2 for insert as
+select 'nest level 2'
+insert into triggerTab3 values (3);
+go
+
+begin tran
+go
+insert into triggerTab1 values(3)
+go
+if (@@trancount > 0) rollback tran;
+go
+
+select * from triggerTab1;
+go
+select * from triggerTab2;
+go
+select * from triggerTab3;
+go
+
+drop table triggerTab1;
+go
+drop table triggerTab2;
+go
+drop table triggerTab3;
 go
