03a557fe07b667cb426fb6bd9b45bcc0ce7ef412
Dipesh Dhameliya <dipeshdhameliya125@gmail.com>
2022-02-10 18:51:46 +0530

    Fix inconsistent JDBC tests
    These tests confirm that the views we created are working fine and
    ensures that no error occurs when we SELECT from the views. Previously
    these two tests would fail from time to time because we specify the
    select result to be more than 1, while in some cases there can be only
    one result. This commit fixes this issue.
    
    Task: BABEL-2417/BABEL-2596
    Author: Xiaohui Fanhe hexiaohu@amazon.com
    Signed-off-by: Dipesh Dhameliya dddhamel@amazon.com

--- test/JDBC/input/BABEL-2596.sql
+++ test/JDBC/input/BABEL-2596.sql
@@ -1,40 +1,40 @@
  -- Only need to scan the entire table and ensure no error is raised
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.system_objects
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
 
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.all_columns
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
 
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.all_objects
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
 
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.all_views
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
 
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.columns
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
 
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.identity_columns
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
 
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.views
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
 
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.tables
-SELECT (CASE WHEN @a > 1 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
 go
