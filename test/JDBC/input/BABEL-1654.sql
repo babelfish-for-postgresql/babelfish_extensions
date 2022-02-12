987c9c99febaf6aab1a9af05168cd04e31d7f7bc
jusjosj <96209028+jusjosj@users.noreply.github.com>
2022-02-11 16:45:03 -0800

    Fix issue where calling UPDATE and UPDATE_COLUMN function causes crash
    Calling the UPDATE and UPDATE_COLUMN function outside of a trigger may crash 
    Babelfish and PostgreSQL. This commit prevents the crash.
    
    Task: BABEL-2879
    Author: Zhibai Song <szh@amazon.com>
    Signed-off-by: Justin Jossick <jusjosj@amazon.com>

--- test/JDBC/input/BABEL-1654.sql
+++ test/JDBC/input/BABEL-1654.sql
@@ -1,3 +1,9 @@
+if update(x) select 1;
+GO
+
+select COLUMNS_UPDATED();
+GO
+
 CREATE TABLE employeeData( ID INT IDENTITY (1,1) PRIMARY KEY,Emp_First_name VARCHAR (50),Emp_Last_name VARCHAR (50),Emp_Salary INT,
 a varchar (50), b varchar(50), c varchar(50), d varchar(50), e varchar(50), f varchar(50))
 GO
