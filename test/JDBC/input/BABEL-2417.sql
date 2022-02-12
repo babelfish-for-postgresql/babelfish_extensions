02c0e1f61e9e84920f4ad75f5d0a2f7cf6208c59
Xiaohui <xiaohui.fanhe@gmail.com>
2022-02-11 16:46:48 -0800

    Fix JDBC tests for intermittent failure in pipeline
    No longer checks whether the sys views have context, only check if the 
    select process finished without error.
    
    Task: BABEL-2417/2596
    Author: Xiaohui Fanhe <hexiaohu@amazon.com>
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

--- test/JDBC/input/BABEL-2417.sql
+++ test/JDBC/input/BABEL-2417.sql
@@ -1,5 +1,5 @@
 -- Only need to scan the entire table and ensure no error is raised
 DECLARE @a INT
 SELECT @a = COUNT(*) FROM sys.objects
-SELECT (CASE WHEN @a > 0 THEN 'true' ELSE 'false' END) AS result
+SELECT (CASE WHEN @a >= 0 THEN 'true' ELSE 'false' END) AS result
 go
