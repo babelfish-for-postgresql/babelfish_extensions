9d5ad653acbc5bf8c1569c70177459280ba59325
Xiaohui <xiaohui.fanhe@gmail.com>
2022-02-14 17:08:04 -0800

    UDF return types related to char datatype(case 3)
    Implemented a generic function to retrieve the correct typmod for a datatype 
    when it is not provided by engine.  Previously, the return typmod was -1 for 
    datatypes with unspecified datalength as PostgreSQL functions generally ignore 
    the typmod of both the argument and return data type.
    
    This commit introduces TdsGetGenericTypmod(), which looks up the return type 
    typmod from a persistent store using function oid.
    
    Task: BABEL-1631
    Author: Satarupa Biswas <satarupb@amazon.com>
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

---
+++ test/JDBC/input/BABEL-1631.sql
@@ -0,0 +1,30 @@
+-- Check if exact input typmod is retrieved on TDS side for char/nchar datatypes
+-- when engine provided typmod = -1
+
+DROP FUNCTION IF EXISTS custom_f3;
+go
+CREATE FUNCTION custom_f3(@one CHAR(10)) RETURNS CHAR(6) AS BEGIN RETURN @one; END;
+go
+SELECT custom_f3('abcdef');
+go
+
+DROP FUNCTION IF EXISTS custom_f4;
+go
+CREATE FUNCTION custom_f4(@one NCHAR(10)) RETURNS NCHAR(6) AS BEGIN RETURN @one; END;
+go
+SELECT custom_f4('abc');
+go
+
+DROP FUNCTION IF EXISTS custom_f5;
+go
+CREATE FUNCTION custom_f5(@one CHAR(20)) RETURNS CHAR(8) AS BEGIN RETURN @one; END;
+go
+SELECT custom_f5('abcdefghij');
+go
+
+DROP FUNCTION IF EXISTS custom_f6;
+go
+CREATE FUNCTION custom_f6(@one CHAR(10)) RETURNS CHAR AS BEGIN RETURN @one; END;
+go
+SELECT custom_f6('abcdef');
+go
