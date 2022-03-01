0a86f912c6c54071b9741ffa491653f31864f61c
Rishabh Tanwar <33982749+rishabhtanwar29@users.noreply.github.com>
2022-03-01 19:30:42 +0530

    Apply rounding logic to (small) money on excessive fractional digit
    Input that includes excessive fractional digits (like $123.12345) should be calculated to $123.1235. This commit applies "round half away from zero" logic to excessive fractional digits.
    
    Task: BABEL-3005
    Author: Sangil Song sonsangi@amazon.com
    Signed-off-by: Rishabh Tanwar ritanwar@amazon.com

---
+++ test/JDBC/input/BABEL-3005.sql
@@ -0,0 +1,74 @@
+use master;
+go
+
+declare @v money = 123.00014;
+select @v;
+go
+
+declare @v money = 123.000144;
+select @v;
+go
+
+declare @v money = 123.000149;
+select @v;
+go
+
+declare @v money = 123.00015;
+select @v;
+go
+
+declare @v money = 123.000151;
+select @v;
+go
+
+declare @v money = 123.000155;
+select @v;
+go
+
+declare @v money = 123.00016;
+select @v;
+go
+
+declare @v money = 123.00019;
+select @v;
+go
+
+declare @v money = 123.00025;
+select @v;
+go
+
+declare @v money = -123.00014;
+select @v;
+go
+
+declare @v money = -123.000144;
+select @v;
+go
+
+declare @v money = -123.000149;
+select @v;
+go
+
+declare @v money = -123.00015;
+select @v;
+go
+
+declare @v money = -123.000151;
+select @v;
+go
+
+declare @v money = -123.000155;
+select @v;
+go
+
+declare @v money = -123.00016;
+select @v;
+go
+
+declare @v money = -123.00019;
+select @v;
+go
+
+declare @v money = -123.00025;
+select @v;
+go
