c7174289dba1c62c604235aacc9a0058820a7896
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2022-03-02 18:18:18 +0530

    Support the xp_qv procedure
    This commit adds the xp_qv procedure to master.dbo schema; the procedure is accessed by applications in master.dbo schema.
    
    Task: BABEL-2900
    Author: Leszek Kurzyna lkurzyna@amazon.com
    Signed-off-by: Shalini Lohia lshalini@amazon.com

--- test/JDBC/input/storedProcedures/Test-xp_qv.sql
+++ test/JDBC/input/storedProcedures/Test-xp_qv.sql
@@ -15,3 +15,17 @@ set @param1 = N'NameofService'
 EXECUTE @retVal = xp_qv @param1, @param2
 select @retVal;
 go
+
+-- Test master.dbo access
+DECLARE @retVal INT
+
+EXECUTE @retVal = master.dbo.xp_qv N'87651903210', N'SERVICENAME'
+select @retVal;
+go
+
+-- Test dbo access
+DECLARE @retVal INT
+
+EXECUTE @retVal = dbo.xp_qv N'87651903210', N'SERVICENAME'
+select @retVal;
+go
