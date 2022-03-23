b4fa305eebb74aa7cbef6ed81394d50a4b300449
Kushaal Shroff <kushaal@amazon.com>
2022-03-23 18:32:10 +0530

    Babel-217: Add Support for TDS Columnmetadata flags.
    Earlier we were sending generic flags for each of the columns.
    With this commit we have added support to send computed and identity
    flags for their respective columns.

--- contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
@@ -28,6 +28,7 @@
 #include "parser/parse_coerce.h"
 #include "utils/fmgroids.h"
 #include "utils/lsyscache.h"
+#include "utils/syscache.h"
 #include "utils/memdebug.h"
 #include "utils/numeric.h"
 #include "utils/portal.h"
@@ -125,8 +126,7 @@ static void FillTabNameWithNumParts(StringInfo buf, uint8 numParts, TdsRelationM
 static void FillTabNameWithoutNumParts(StringInfo buf, uint8 numParts, TdsRelationMetaDataInfo relMetaDataInfo);
 static void SetTdsEstateErrorData(void);
 static void ResetTdsEstateErrorData(void);
-static bool get_attnotnull(Oid relid, AttrNumber attnum);
-static char get_attidentity(Oid relid, AttrNumber attnum);
+static void SetAttributesForColmetada(TdsColumnMetaData *col);
 
 static inline void
 SendPendingDone(bool more)
@@ -1364,14 +1364,7 @@ PrepareRowDescription(TupleDesc typeinfo, List *targetlist, int16 *formats,
 			col->attrNum = 0;
 		}
 
-		col->attNotNull = get_attnotnull(col->relOid, col->attrNum);
-		{
-			char attidentity = get_attidentity(col->relOid, col->attrNum);
-			if (attidentity != '\0')
-				col->attidentity = true;
-			else
-				col->attidentity = false;
-		}
+		SetAttributesForColmetada(col);
 
 		switch (finfo->sendFuncId)
 		{
@@ -2920,58 +2913,34 @@ GetTdsEstateErrorData(int *number, int *severity, int *state)
 }
 
 /*
- * get_attnotnull
- *		Given the relation id and the attribute number,
- *		return the "attnotnull" field from the attribute relation.
+ * Using the relation id and the attribute number, set the attributes
+ * required in the TDS Column Metadata from the attributes relation.
  */
-static bool
-get_attnotnull(Oid relid, AttrNumber attnum)
-{
-	HeapTuple	  tp;
-	Form_pg_attribute att_tup;
-
-	tp = SearchSysCache2(ATTNUM,
-			ObjectIdGetDatum(relid),
-			Int16GetDatum(attnum));
-
-	if (HeapTupleIsValid(tp))
-	{
-		bool result;
-
-		att_tup = (Form_pg_attribute) GETSTRUCT(tp);
-		result = att_tup->attnotnull;
-		ReleaseSysCache(tp);
-
-		return result;
-	}
-	/* Assume att is nullable if no valid heap tuple is found */
-	return false;
-}
-
-/*
- * get_attidentity
- *		Given the relation id and the attribute number,
- *		return the "attidentity" field from the attribute relation.
- */
-static char
-get_attidentity(Oid relid, AttrNumber attnum)
+static void
+SetAttributesForColmetada(TdsColumnMetaData *col)
 {
 	HeapTuple	  tp;
 	Form_pg_attribute att_tup;
 
 	tp = SearchSysCache2(ATTNUM, 
-			ObjectIdGetDatum(relid),
-			Int16GetDatum(attnum));
+			ObjectIdGetDatum(col->relOid),
+			Int16GetDatum(col->attrNum));
+
+	/* Initialise to false if no valid heap tuple is found. */
+	col->attNotNull = false;
+	col->attidentity = false;
+	col->attgenerated = false;
 
 	if (HeapTupleIsValid(tp))
 	{
-		char result;
 		att_tup = (Form_pg_attribute) GETSTRUCT(tp);
-		result = att_tup->attidentity;
-		ReleaseSysCache(tp);
+		col->attNotNull = att_tup->attnotnull;
+		if (att_tup->attgenerated != '\0')
+			col->attgenerated = true;
 
-		return result;
+		if (att_tup->attidentity != '\0')
+			col->attidentity = true;
+
+		ReleaseSysCache(tp);
 	}
-	/* return '\0' if no valid heap tuple is found */
-	return '\0';
 }
