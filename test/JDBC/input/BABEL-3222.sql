7c221fa282780a9aac5825119603da9cc944e9d4
Harsh Lunagariya <lunharsh@amazon.com>
2022-04-15 09:43:45 +0000

    Add pltsql_ruleutils.c and pltsql_ruleutils.h
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>
    
    Added tsql_constraintdef for check constraint definition
    
    Fix the build issues to merge it into REL2_X_DEV
    
    Remove few nodetag from isSimpleNode which are not needed for check constraint
    
    Code clean up and basic tests
    
    Signed-off-by: Shalini Lohia <lshalini@amazon.com>
    
    Fix the empty spaces in test output
    
    Signed-off-by: Shalini Lohia <lshalini@amazon.com>

---
+++ test/JDBC/input/BABEL-3222.sql
@@ -0,0 +1,43 @@
+create table test_tsql_const(
+	c_bit sys.bit check(c_bit <> cast(1 as sys.bit)),
+	c_int int check(c_int < 10),
+);
+go
+
+create table test_tsql_cast(
+	c_float float check(c_float < cast(133.230182309832423 as float) and c_float < cast(133.230182309832423 as float(7))),
+	c_real real check(c_real < cast(133.230182309832423 as real)),
+	c_decimal decimal check(c_decimal < cast(133.230182309832423 as decimal) and c_decimal < cast(133.230182309832423 as decimal(5,3))),
+	c_numeric numeric check(c_numeric < cast(133.230182309832423 as numeric) and c_numeric < cast(133.230182309832423 as numeric(5,3))),
+);
+go
+
+create table test_tsql_collate(
+	c_varchar varchar check(c_varchar <> cast('sflkjasdlkfjf' as varchar(12)) COLLATE bbf_unicode_cp1_ci_as),
+	c_char char check(c_char <> cast('sflkjasdlkfjf' as char(7)) COLLATE bbf_unicode_cp1_ci_as),
+	c_nchar nchar check(cast(c_nchar as nchar(7)) <> cast('sflkjasdlkfjf' as nchar(7)) COLLATE bbf_unicode_cp1_ci_as),
+);
+go
+
+create table test_null(a int, check(a IS NOT NULL));
+go
+
+select tsql_get_constraintdef(oid) from pg_constraint where contype='c' and conrelid = (select oid from pg_class where relname='test_null');
+go
+
+select tsql_get_constraintdef(oid) from pg_constraint where contype='c' and conrelid = (select oid from pg_class where relname='test_tsql_const');
+go
+
+select tsql_get_constraintdef(oid) from pg_constraint where contype='c' and conrelid = (select oid from pg_class where relname='test_tsql_collate');
+go
+
+select tsql_get_constraintdef(oid) from pg_constraint where contype='c' and conrelid = (select oid from pg_class where relname='test_tsql_cast');
+go
+
+drop table test_tsql_const
+drop table test_null
+drop table test_tsql_collate
+drop table test_tsql_cast
+go
+
+
