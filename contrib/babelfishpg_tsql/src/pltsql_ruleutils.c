317e376a7f260f221210af3b875cbb1ec3228bf3
Harsh Lunagariya <lunharsh@amazon.com>
2022-04-15 09:43:45 +0000

    Add pltsql_ruleutils.c and pltsql_ruleutils.h
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>

---
+++ contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
@@ -0,0 +1,7 @@
+/*
+ *
+ * pltsql_ruleutils.c
+ *    Functions to de-parse TSQL object definition from
+ *    stored expressions/querytrees
+ *
+ */
---
+++ contrib/babelfishpg_tsql/src/pltsql_ruleutils.h
@@ -0,0 +1,11 @@
+/*
+ *
+ * pltsql_ruleutils.h
+ *	  Declarations for pltsql_ruleutils.h
+ *
+ */
+
+#ifndef PLTSQL_RULEUTILS_H
+#define PLTSQL_RULEUTILS_H
+
+#endif							/* PLTSQL_RULEUTILS_H */
