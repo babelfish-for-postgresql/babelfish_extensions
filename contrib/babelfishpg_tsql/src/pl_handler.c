bea21f3700e3dd5b8c4ae6e3815d884de45f4d4a
Dipesh Dhameliya <dipeshdhameliya125@gmail.com>
2022-05-02 13:34:46 +0530

    Use logical schema name to write table name in column metadata instead of physical schema name
    Previously, we introduced rendezvous variable pltsql_get_logical_schema_name babelfishpg_tsql extension to get_logical_schema_name_from_physical_schema_name().
        
    With these changes, we are assuming that we must have received the valid schema name from engine while sending column metadata on wire. So updated the rendezvous variable pltsql_get_logical_schema_name to initialize to get_logical_schema_name(). We first try to get the logical schema name given physical schema name; and if we could not find the logical schema name then we can say that received schema name is shared schema and it does not need to be translated to logical schema name.
    
    Task: BABEL-2890
    Signed-off-by: Dipesh Dhameliya <dddhamel@amazon.com>

--- contrib/babelfishpg_tsql/src/pl_handler.c
+++ contrib/babelfishpg_tsql/src/pl_handler.c
@@ -3255,7 +3255,7 @@ _PG_init(void)
 		(*pltsql_protocol_plugin_ptr)->pltsql_get_login_default_db = &get_login_default_db;
 		(*pltsql_protocol_plugin_ptr)->pltsql_is_login = &is_login;
 		(*pltsql_protocol_plugin_ptr)->pltsql_get_generic_typmod = &probin_read_ret_typmod;
-		(*pltsql_protocol_plugin_ptr)->pltsql_get_logical_schema_name = &get_logical_schema_name_from_physical_schema_name;
+		(*pltsql_protocol_plugin_ptr)->pltsql_get_logical_schema_name = &get_logical_schema_name;
 	}
 
 	*pltsql_config_ptr = &myConfig;
