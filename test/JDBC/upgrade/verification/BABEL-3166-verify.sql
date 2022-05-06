9ebafd00cc82aa23f4848f24f3ae817ce84ed064
Rishabh Tanwar <33982749+rishabhtanwar29@users.noreply.github.com>
2022-05-06 19:27:45 +0530

    Add github action to test and verify major version upgrade
    This commit adds github action to verify babelfish extension
    related objects after major version upgrade.
    
    Additionally added two new directories in JDBC test framework:
    1. upgrade/preparation: here we can put sql files to create
       objects in the source database.
    2. upgrade/verification: here we can put sql files which
       will get executed on upgraded database to test/verify
       the objects created during preparation.
    
    Task: BABEL-3191
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>

---
+++ test/JDBC/upgrade/verification/BABEL-3166-verify.sql
@@ -0,0 +1,27 @@
+USE db_babel_3166;
+go
+
+-- Look at function's probin for typmod information
+SELECT proname, probin FROM pg_proc WHERE proname = 'babel_3166_func';
+go
+
+SELECT babel_3166_func(1.2, 'abc', 'abcd', 'abcdefgh', 0x12bcfe);
+go
+
+DROP FUNCTION babel_3166_func;
+
+-- Look at procedures's probin for typmod information
+SELECT proname, probin FROM pg_proc WHERE proname = 'babel_3166_proc';
+go
+
+EXEC babel_3166_proc 1.2, 'abc', 'abcd', 'abcdefgh', 0x12bcfe;
+go
+
+DROP PROCEDURE babel_3166_proc;
+go
+
+USE master;
+go
+
+DROP DATABASE db_babel_3166;
+go
