5e8b85d8a2fd45e90f04e95edf624e1e866afa22
Satarupa Biswas <satarupabiswas89@gmail.com>
2022-05-13 02:46:43 +0530

    NUMERIC overflow causes TDS error (#178)
    * NUMERIC overflow causes TDS error
    
    Fix includes sending default numeric precision(i.e., 38) on wire whenever we encounter aggregate function SUM() and AVG(); and setting resultant scale = max(6, input scale) for aggregate function AVG()
    
    Task: BABEL-3157
    Signed-off-by: Satarupa Biswas <satarupb@amazon.com>
    
    * NUMERIC overflow causes TDS error for aggregate functions
    
    Fix includes sending default numeric precision(i.e., 38) on wire whenever we encounter aggregate function SUM() and AVG(); and setting resultant scale = max(6, input scale) for aggregate function AVG()
    
    Task: BABEL-3157
    Signed-off-by: Satarupa Biswas satarupb@amazon.com
    
    * NUMERIC overflow causes TDS error for aggregate functions
    
    Before fix, expected resultant precision for aggregate function SUM() was always same as individual precision which caused numeric overflow error even when #digits in sum became 1 more than precision of individual operand.
    
    The fix includes sending default numeric precision(i.e., 38) on wire whenever we encounter aggregate function SUM() and AVG(); and setting resultant scale = max(6, input scale) for aggregate function AVG()
    
    Task: BABEL-3157
    Signed-off-by: Satarupa Biswas satarupb@amazon.com
    
    Co-authored-by: Satarupa Biswas <satarupb@amazon.com>

--- contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
@@ -660,11 +660,22 @@ resolve_numeric_typmod_from_exp(Node *expr)
 
 			/*
 			 * [BABEL-3074] NUMERIC overflow causes TDS error for aggregate
-			 * function sum(); resultant precision should be individual precision + 1
+			 * function sum(); resultant precision should be tds_default_numeric_precision 
 			 */
 			if (aggFuncName && strlen(aggFuncName) == 3 &&
 					(strncmp(aggFuncName, "sum", 3) == 0))
-				precision++;
+				precision = tds_default_numeric_precision;
+
+			/*
+			 * For aggregate function avg(); resultant precision should be
+			 * tds_default_numeric_precision and resultant scale = max(input scale, 6)
+			 */
+			if (aggFuncName && strlen(aggFuncName) == 3 &&
+					(strncmp(aggFuncName, "avg", 3) == 0))
+			{
+				precision = tds_default_numeric_precision;
+				scale = Max(scale, 6);
+			}
 
 			pfree(aggFuncName);
 			return ((precision << 16) | scale) + VARHDRSZ;
