092e3a5e985596251fb5813904bdabde9ba9ac92
shalinilohia50 <shalinilohia50@gmail.com>
2022-05-13 08:16:21 +0000

    sysadmin should be a row in sys.server_principals
    In SQL Server, sysadmin is a server-level role,
    and it is the only server-level role which we currently implement in Babelfish.
    Therefore, the role should be present in the emulated catalog
    sys.server_principals, but no such rows is currently in this catalog.
    
    Task: BABEL-2472
    Signed-off-by: Shalini Lohia <lshalini@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.0.0--2.1.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.0.0--2.1.0.sql
@@ -1362,18 +1362,25 @@ CAST(Base.rolname AS sys.SYSNAME) AS name,
 CAST(Base.oid As INT) AS principal_id,
 CAST(CAST(Base.oid as INT) as sys.varbinary(85)) AS sid,
 CAST(Ext.type AS CHAR(1)) as type,
-CAST(CASE WHEN Ext.type = 'S' THEN 'SQL_LOGIN' ELSE NULL END AS NVARCHAR(60)) AS type_desc,
+CAST(CASE WHEN Ext.type = 'S' THEN 'SQL_LOGIN' 
+WHEN Ext.type = 'R' THEN 'SERVER_ROLE'
+ELSE NULL END AS NVARCHAR(60)) AS type_desc,
 CAST(Ext.is_disabled AS INT) AS is_disabled,
 CAST(Ext.create_date AS SYS.DATETIME) AS create_date,
 CAST(Ext.modify_date AS SYS.DATETIME) AS modify_date,
-CAST(Ext.default_database_name AS SYS.SYSNAME) AS default_database_name,
+CAST(CASE WHEN Ext.type = 'R' THEN NULL ELSE Ext.default_database_name END AS SYS.SYSNAME) AS default_database_name,
 CAST(Ext.default_language_name AS SYS.SYSNAME) AS default_language_name,
-CAST(Ext.credential_id AS INT) AS credential_id,
-CAST(Ext.owning_principal_id AS INT) AS owning_principal_id,
+CAST(CASE WHEN Ext.type = 'R' THEN NULL ELSE Ext.credential_id END AS INT) AS credential_id,
+CAST(CASE WHEN Ext.type = 'R' THEN 1 ELSE Ext.owning_principal_id END AS INT) AS owning_principal_id,
 CAST(Ext.is_fixed_role AS sys.BIT) AS is_fixed_role
 FROM pg_catalog.pg_authid AS Base INNER JOIN sys.babelfish_authid_login_ext AS Ext ON Base.rolname = Ext.rolname;
 
 GRANT SELECT ON sys.server_principals TO PUBLIC;
+
+-- We have initialized 'sysadmin' role during initialize_babelfish().
+-- Need to initialize it here again for the old users who are upgrading to new
+-- babelfish version should have this role in the server_principles view.
+CALL sys.babel_initialize_logins('sysadmin');
 
 -- Drop the deprecated view if there isn't any dependent object
 CALL sys.babelfish_drop_deprecated_view('sys', 'server_principals_deprecated');
