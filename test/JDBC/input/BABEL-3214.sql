92b82f0ea33bec60d1673284cdeac9663f68b5dc
Sharu Goel <goelshar@amazon.com>
2022-05-13 09:47:13 +0000

    Reflect T-SQL isolation level in sys.dm_exec_sessions view
    We reflect the PG default_transaction_isolation GUC in the
    transaction_isolation_level column in sys.dm_exec_sessions. However,
    this is not correct and we should be reflecting the T-SQL transaction
    isolation level in the view.
    
    This commit adds changes to reflect the T-SQL transaction isolation
    level in the view even if internally the PG transaction isolation level
    might be different.
    
    Task: BABEL-3214
    Signed-off-by: Sharu Goel <goelshar@amazon.com>

---
+++ test/JDBC/input/BABEL-3214.sql
@@ -0,0 +1,42 @@
+/*
+ * T-SQL transaction isolation level mapping is as follows for dm_exec_sessions:
+ *
+ * 1 = READ UNCOMMITTED
+ * 2 = READ COMMITTED
+ * 3 = REPEATABLE READ
+ * 4 = SERIALIZABLE
+ * 5 = SNAPSHOT
+ */
+
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+SELECT CAST(current_setting('transaction_isolation') AS VARCHAR);
+SELECT transaction_isolation_level from sys.dm_exec_sessions WHERE session_id = @@SPID;
+GO
+
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SELECT CAST(current_setting('transaction_isolation') AS VARCHAR);
+SELECT transaction_isolation_level from sys.dm_exec_sessions WHERE session_id = @@SPID;
+GO
+
+-- Isolation level not supported so will reflect same value as before
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+GO
+SELECT CAST(current_setting('transaction_isolation') AS VARCHAR);
+SELECT transaction_isolation_level from sys.dm_exec_sessions WHERE session_id = @@SPID;
+GO
+
+-- Isolation level not supported so will reflect same value as before
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+GO
+SELECT CAST(current_setting('transaction_isolation') AS VARCHAR);
+SELECT transaction_isolation_level from sys.dm_exec_sessions WHERE session_id = @@SPID;
+GO
+
+-- Isolation level internally mapped to PG repeatable read so
+-- current_setting will show "repeatable read" but
+-- dm_exec_sessions will show code for snapshot isolation
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+SELECT CAST(current_setting('transaction_isolation') AS VARCHAR);
+SELECT transaction_isolation_level from sys.dm_exec_sessions WHERE session_id = @@SPID;
+GO
