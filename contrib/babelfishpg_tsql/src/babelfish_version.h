d37e09741f16ceebcf62c2b0fe5d572c14445d4a
Xiaohui Fanhe <hexiaohu@amazon.com>
2022-05-18 17:54:03 +0000

    Bump Babelfish version to 1.3.0
    Task: BABEL-3184
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,8 +8,8 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "1.2.1"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 13.6.0.2"
+#define BABELFISH_VERSION_STR "1.3.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 13.7.0.1"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
 
