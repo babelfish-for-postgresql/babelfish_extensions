ef4fa6cfdd59bd4779a17da9fba0e3e5acf29d96
jungkook <2jungkook@users.noreply.github.com>
2022-05-30 17:41:27 -0700

    Fix + and / operators during pg_upgrade (#19)
    [Engine]
    
    In Babelfish v1.2, we had to re-define some operators to fix issues.
    However, we were not able to apply the fix in the upgrade script because
    customer-defined object may have depended on those operators.
    
    This commit introduces a fix by doing it during pg_upgrade.
    
    [Extension]
    
    As this issue can only be tested when a user initiates Major Version
    Upgrade from Babelfish v1.0 or v1.1, added a temporary test case
    manipulating pg_operator directly. Once we have a Github action that can
    test MVU from v1.0, we should remove this test case to avoid
    side-effects to other test cases.
    
    Task: BABEL-3112
    Signed-off-by: Jungkook Lee <jungkook@amazon.com>

---
+++ test/JDBC/upgrade/verification/BABEL-3112-verify.sql
@@ -0,0 +1,8 @@
+select cast('abc' as text) + cast('def' as text);
+go
+select CAST(2.56 as bigint) / CAST(3.60 as money);
+go
+select CAST(2.56 as int) / CAST(3.60 as money);
+go
+select CAST(2.56 as smallint) / CAST(3.60 as money);
+go
