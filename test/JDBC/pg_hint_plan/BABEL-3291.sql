3b792dd8aa0688402778f1b1aaf529fa74f36788
gattania <106396849+gattania@users.noreply.github.com>
2022-06-09 16:23:33 -0700

    Create a Github action that can test pg_hint_plan (#254)
    Currently Babelfish does not support query hints. To add support for query hints, we will be using the third-part library pg_hint_plan. This pull request has the following changes:
    
    Creating a Github action that can build and install the specified release of pg_hint_plan
    Add a separate Github workflow to test pg_hint_plan as part of Github CI
    Unit tests to verify if the query hints are getting applied
    
    Task: BABEL-3291
    Signed-off-by: Akshay Gattani <gattania@amazon.com>

---
+++ test/JDBC/pg_hint_plan/BABEL-3291.sql
@@ -0,0 +1,31 @@
+drop table if exists babel_3291_t1
+go
+
+create table babel_3291_t1(a1 int PRIMARY KEY, b1 int)
+go
+
+set babelfish_showplan_all on
+go
+
+/*
+ * Run a SELECT query without any hints to ensure that un-hinted queries still work.
+ * This also ensures that when the SELECT query is not hinted it produces a different plan(index scan)
+ * than the sequential scan that we're hinting in the query below. This verifies that the next test is actually valid.
+ * If the planner was going to choose a sequential scan anyway, the next test wouldn't actually prove that hints were working.
+ */
+select * from babel_3291_t1 where a1 = 1
+go
+
+/*
+ * Run a SELECT query and give the hint to follow a sequential scan. 
+ * The query plan should now use a sequential scan instead of the index scan it uses in the un-hinted test above.
+ */
+select /*+SeqScan(babel_3291_t1)*/ * from babel_3291_t1 where a1 = 1
+go
+
+set babelfish_showplan_all off
+go
+
+-- cleanup
+drop table babel_3291_t1
+go
