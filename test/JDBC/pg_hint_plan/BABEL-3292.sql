d4c1376cc0f4b8412ef88cc9676c1aa5dab6f650
Akshay Gattani <gattania@amazon.com>
2022-06-21 20:45:12 +0000

    Added tests for INSERT, UPDATE and DELETE queries

--- test/JDBC/pg_hint_plan/BABEL-3292.sql
+++ test/JDBC/pg_hint_plan/BABEL-3292.sql
@@ -13,7 +13,7 @@ go
 create index index_babel_3292_t1_c1 on babel_3292_t1(c1)
 go
 
-create table babel_3292_t2(a2 int PRIMARY KEY, b2 int)
+create table babel_3292_t2(a2 int PRIMARY KEY, b2 int, c2 int)
 go
 
 create index index_babel_3292_t2_b2 on babel_3292_t2(b2)
@@ -25,6 +25,7 @@ go
 set babelfish_showplan_all on
 go
 
+-- Test SELECT queries with and without hints
 /*
  * Run a SELECT query without any hints to ensure that un-hinted queries still work.
  * This also ensures that when the SELECT query is not hinted it produces a different plan(bitmap heap scan and bitmap index scan)
@@ -53,9 +54,7 @@ go
 select * from babel_3292_t1 where b1=1 option(table hint(babel_3292_t1, index(index_babel_3292_t1_b1)))
 go
 
-/*
- * Test with multiple index hints
- */
+-- Test with multiple index hints
 select * from babel_3292_t1 where b1 = 1 and c1 = 1
 go
 
@@ -65,9 +64,7 @@ go
 select * from babel_3292_t1 where b1 = 1 and c1 = 1 option(table hint(babel_3292_t1, index(index_babel_3292_t1_b1), index(index_babel_3292_t1_c1)))
 go
 
-/*
- * Test with multiple tables
- */
+-- Test with multiple tables
 select * from babel_3292_t1, babel_3292_t2 where b1 = 1 and b2 = 1
 go
 
@@ -78,6 +75,37 @@ select * from babel_3292_t1 with(index=index_babel_3292_t1_b1), babel_3292_t2 wi
 go
 
 select * from babel_3292_t1, babel_3292_t2 where b1 = 1 and b2 = 1 option(table hint(babel_3292_t1, index(index_babel_3292_t1_b1)), table hint(babel_3292_t2, index(index_babel_3292_t2_b2)))
+go
+
+-- Test INSERT queries with and without hints
+insert into babel_3292_t2 select * from babel_3292_t1 where b1 = 1
+go
+
+insert into babel_3292_t2 select * from babel_3292_t1 with(index(index_babel_3292_t1_b1)) where b1 = 1
+go
+
+-- Test UPDATE queries with and without hints
+update babel_3292_t1 set a1 = 1 where b1 = 1
+go
+
+update babel_3292_t1 with(index(index_babel_3292_t1_b1)) set a1 = 1 where b1 = 1
+go
+
+-- Test DELETE queries with and without hints
+delete from babel_3292_t1 where b1 = 1
+go
+
+delete from babel_3292_t1 with(index(index_babel_3292_t1_b1)) where b1 = 1
+go
+
+-- Test UNION queries with and without hints
+select * from babel_3292_t1 where b1 = 1 UNION select * from babel_3292_t2 where b2 = 1 -- None of the queries have a hint
+go
+
+select * from babel_3292_t1 where b1 = 1 UNION select * from babel_3292_t2 with(index=index_babel_3292_t2_b2) where b2 = 1 -- Only one query has a hint
+go
+
+select * from babel_3292_t1 with(index=index_babel_3292_t1_b1) where b1 = 1 UNION select * from babel_3292_t2 with(index=index_babel_3292_t2_b2) where b2 = 1 -- Both queries have a hint
 go
 
 set babelfish_showplan_all off
