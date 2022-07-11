6a082803532790e6808364dc63b9e8419389e33a
Sai Rohan Basa <bsrohan@amazon.com>
2022-07-11 13:23:17 +0000

    Used default DB instead of own DB in objectpropertyex upgrade tests
    Signed-off-by: Sai Rohan Basa <bsrohan@amazon.com>

--- test/JDBC/input/functions/objectpropertyex-vu-prepare.sql
+++ test/JDBC/input/functions/objectpropertyex-vu-prepare.sql
@@ -1,26 +1,20 @@
--- Global setup for tests
-CREATE DATABASE db2
-GO
-USE db2
-GO
-
 -- Setup
-CREATE SCHEMA ownerid_schema
+CREATE SCHEMA objectpropertyex_ownerid_schema
 GO
 
-CREATE TABLE ownerid_schema.ownerid_table(a int) 
+CREATE TABLE objectpropertyex_ownerid_schema.objectpropertyex_ownerid_table(a int) 
 GO
 
 -- =============== BaseType ===============
 -- Setup
-CREATE TABLE basetype_table(a int)
+CREATE TABLE objectpropertyex_basetype_table(a int)
 GO
 
-CREATE VIEW basetype_view AS
+CREATE VIEW objectpropertyex_basetype_view AS
 SELECT 1
 GO
 
-CREATE FUNCTION basetype_function()
+CREATE FUNCTION objectpropertyex_basetype_function()
 RETURNS INTEGER
 AS
 BEGIN
@@ -28,7 +22,7 @@ RETURN 1;
 END
 GO
 
-CREATE PROC basetype_proc
+CREATE PROC objectpropertyex_basetype_proc
 AS
 SELECT 1
 GO
@@ -36,5 +30,5 @@ GO
 -- =============== Special Input Cases ===============
 
 -- Setup
-CREATE TABLE specialinput_table(a int)
+CREATE TABLE objectpropertyex_specialinput_table(a int)
 GO
--- test/JDBC/input/functions/objectpropertyex-vu-verify.sql
+++ test/JDBC/input/functions/objectpropertyex-vu-verify.sql
@@ -1,9 +1,6 @@
-USE db2
-GO
-
 -- Check for correct case
 SELECT CASE
-    WHEN OBJECTPROPERTY(OBJECT_ID('ownerid_schema.ownerid_table'), 'OwnerId')  = (SELECT principal_id 
+    WHEN OBJECTPROPERTY(OBJECT_ID('objectpropertyex_ownerid_schema.objectpropertyex_ownerid_table'), 'OwnerId')  = (SELECT principal_id 
             FROM sys.database_principals
             WHERE name = CURRENT_USER)
         Then 'SUCCESS'
@@ -20,16 +17,16 @@ GO
 
 -- Tests valid cases
 
-SELECT OBJECTPROPERTYEX(OBJECT_ID('basetype_table'), 'BaseType')
+SELECT OBJECTPROPERTYEX(OBJECT_ID('objectpropertyex_basetype_table'), 'BaseType')
 GO
 
-SELECT OBJECTPROPERTYEX(OBJECT_ID('basetype_view'), 'BaseType')
+SELECT OBJECTPROPERTYEX(OBJECT_ID('objectpropertyex_basetype_view'), 'BaseType')
 GO
 
-SELECT OBJECTPROPERTYEX(OBJECT_ID('basetype_function'), 'BaseType')
+SELECT OBJECTPROPERTYEX(OBJECT_ID('objectpropertyex_basetype_function'), 'BaseType')
 GO
 
-SELECT OBJECTPROPERTYEX(OBJECT_ID('basetype_proc'), 'BaseType')
+SELECT OBJECTPROPERTYEX(OBJECT_ID('objectpropertyex_basetype_proc'), 'BaseType')
 GO
 
 -- Tests invalid object
@@ -39,11 +36,11 @@ GO
 -- =============== Special Input Cases ===============
 
 -- Tests special input cases
-SELECT OBJECTPROPERTYEX(OBJECT_ID('specialinput_table'), 'BASETYPE')
+SELECT OBJECTPROPERTYEX(OBJECT_ID('objectpropertyex_specialinput_table'), 'BASETYPE')
 GO
 
-SELECT OBJECTPROPERTYEX(OBJECT_ID('specialinput_table'), 'basetype')
+SELECT OBJECTPROPERTYEX(OBJECT_ID('objectpropertyex_specialinput_table'), 'basetype')
 GO
 
-SELECT OBJECTPROPERTYEX(OBJECT_ID('specialinput_table'), 'BASETYPE       ')
+SELECT OBJECTPROPERTYEX(OBJECT_ID('objectpropertyex_specialinput_table'), 'BASETYPE       ')
 GO
