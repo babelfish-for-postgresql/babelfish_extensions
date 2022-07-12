1b639b779d2b6cd5412d8f41692085989a8acfcf
Sangil Song <sonsangi@amazon.com>
2022-07-12 01:10:24 +0000

    Add test case of MVU failure when comma is used in a column name
    Task: BABEL-3121
    Authored-by: Sangil Song <sonsangi@amazon.com>
    Signed-off-by: Sangil Song <sonsangi@amazon.com>

--- test/JDBC/upgrade/14_3/verification_cleanup/13_6/BABEL-3121-vu-verify.sql
+++ test/JDBC/upgrade/14_3/verification_cleanup/13_6/BABEL-3121-vu-verify.sql
@@ -37,6 +37,12 @@ go
 select attname, array_to_string(attoptions,',') attoptions from pg_class C, pg_attribute A where C.oid = A.attrelid and C.relname like 'babel_3121_t6' and A.attnum > 0 and attisdropped = 'f' order by attname;
 go
 
+select * from babel_3121_t7;
+go
+
+select attname, array_to_string(attoptions,',') attoptions from pg_class C, pg_attribute A where C.oid = A.attrelid and C.relname like 'babel_3121_t7' and A.attnum > 0 and attisdropped = 'f' order by attname;
+go
+
 USE master;
 go
 
