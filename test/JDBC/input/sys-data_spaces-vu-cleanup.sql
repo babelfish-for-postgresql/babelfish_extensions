c5f9bd33d0dccd361ef91e85544a8d034486b349
Avneet Toor <avneett@bitquilltech.com>
2022-07-12 23:50:31 +0000

    Column datatype corrections - sys.fulltext_indexes, sys.filetables, sys.filegroups, sys.data_spaces
    This CR corrects some column types for the views sys.fulltext_indexes, sys.filetables,
    sys.filegroups, sys.data_spaces. Specifically, the schema sys was added in front of some column types & char was replaced with sys.bpchar. These views are required for the Table Create To enumeration in SSMS.
    
    Task: BABELFISH-487, BABELFISH-462, BABELFISH-463
    
    Signed-off-by: Avneet Toor <avneett@bitquilltech.com>

---
+++ test/JDBC/input/sys-data_spaces-vu-cleanup.sql
@@ -0,0 +1,11 @@
+USE master
+GO
+
+DROP VIEW sys_data_spaces_vu_prepare_view
+GO
+
+DROP PROC sys_data_spaces_vu_prepare_proc
+GO
+
+DROP FUNCTION sys_data_spaces_vu_prepare_func
+GO
---
+++ test/JDBC/input/sys-data_spaces-vu-verify.sql
@@ -0,0 +1,14 @@
+USE master
+GO
+
+SELECT * FROM sys.data_spaces
+GO
+
+SELECT * FROM sys_data_spaces_vu_prepare_view
+GO
+
+EXEC sys_data_spaces_vu_prepare_proc
+GO
+
+SELECT dbo.sys_data_spaces_vu_prepare_func()
+GO
