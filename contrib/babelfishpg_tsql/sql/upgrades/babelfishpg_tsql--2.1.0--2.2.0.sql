3836d8bc77bc4503a094501cbe16c8e19ba4eb62
Favian (Ian) Samatha <60306813+faviansamatha@users.noreply.github.com>
2022-07-14 13:05:01 -0700

    Added entries to sys.configurations view to support table export in SSMS (#363)
    Previously, there were missing values in the sys.configurations which blocked the Table - Create To feature in SSMS. This commit fixes that issue by adding values to sys.babelfish_configurations, the underlying table for sys.configurations view.
    
    Task: BABELFISH-296
    Signed-off-by: Favian (Ian) Samatha ians@bitquilltech.com

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.1.0--2.2.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.1.0--2.2.0.sql
@@ -1155,6 +1155,87 @@ LANGUAGE plpgsql;
 
 CALL sys.babelfish_drop_deprecated_function('sys', 'objectpropertyex_deprecated_2_1_0');
 
+INSERT INTO sys.babelfish_configurations
+VALUES
+  (
+    1534,
+    'user options',
+    0,
+    0,
+    32767,
+    0,
+    'user options',
+    sys.bitin('1'),
+    sys.bitin('0'),
+    'user options',
+    'user options'
+  ),
+  (
+    115,
+    'nested triggers',
+    1,
+    0,
+    1,
+    1,
+    'Allow triggers to be invoked within triggers',
+    sys.bitin('1'),
+    sys.bitin('0'),
+    'Allow triggers to be invoked within triggers',
+    'Allow triggers to be invoked within triggers'
+  ),
+  (
+    124,
+    'default language',
+    0,
+    0,
+    9999,
+    0,
+    'default language',
+    sys.bitin('1'),
+    sys.bitin('0'),
+    'default language',
+    'default language'
+  ),
+  (
+    1126,               
+    'default full-text language',
+    1033,
+    0,
+    2147483647,
+    1033,
+    'default full-text language',
+    sys.bitin('1'),
+    sys.bitin('1'),
+    'default full-text language',
+    'default full-text language'
+  ),
+  (
+    1127,
+    'two digit year cutoff',
+    2049,
+    1753,
+    9999,
+    2049,
+    'two digit year cutoff',
+    sys.bitin('1'),
+    sys.bitin('1'),
+    'two digit year cutoff',
+    'two digit year cutoff'
+  ),
+  (
+    1555,
+    'transform noise words',
+    0,
+    0,
+    1,
+    0,
+    'Transform noise words for full-text query',
+    sys.bitin('1'),
+    sys.bitin('1'),
+    'Transform noise words for full-text query',
+    'Transform noise words for full-text query'
+  );
+
 CREATE OR REPLACE VIEW sys.spatial_index_tessellations 
 AS
 SELECT 
---
+++ test/JDBC/expected/sys-configurations-vu-cleanup.out
@@ -0,0 +1,26 @@
+DROP VIEW sys_configurations_view
+GO
+
+DROP PROC sys_configurations_proc
+GO
+
+DROP FUNCTION sys_configurations_func
+GO
+
+DROP VIEW sys_sysconfigures_view
+GO
+
+DROP PROC sys_sysconfigures_proc
+GO
+
+DROP FUNCTION sys_sysconfigures_func
+GO
+
+DROP VIEW sys_syscurconfigs_view
+GO
+
+DROP PROC sys_syscurconfigs_proc
+GO
+
+DROP FUNCTION sys_syscurconfigs_func
+GO
