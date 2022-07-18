69a2f3a75ab8918337f3c8991bf30e7dd91d157b
Favian (Ian) Samatha <ians@bitquilltech.com>
2022-07-18 19:30:04 +0000

    Add explicit casts
    Signed-off-by: Favian (Ian) Samatha <ians@bitquilltech.com>

--- test/JDBC/input/functions/sys-suser_sid-vu-prepare.sql
+++ test/JDBC/input/functions/sys-suser_sid-vu-prepare.sql
@@ -1,16 +1,16 @@
 CREATE VIEW sys_suser_sid_view AS
-SELECT suser_sid('-1')
+SELECT suser_sid(CAST('-1' AS sys.sysname))
 GO
 
 CREATE PROC sys_suser_sid_proc AS
-SELECT suser_sid('-1')
+SELECT suser_sid(CAST('-1' AS sys.sysname))
 GO
 
 CREATE FUNCTION sys_suser_sid_func()
 RETURNS SYS.VARBINARY(85)
 AS
 BEGIN
-    RETURN suser_sid('-1')
+    RETURN suser_sid(CAST('-1' AS sys.sysname))
 END
 GO
 
