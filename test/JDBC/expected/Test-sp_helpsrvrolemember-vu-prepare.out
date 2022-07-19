8e68220856855c71f35010895a5eceb2847b95db
Xiaohui Fanhe <hexiaohu@amazon.com>
2022-07-19 20:08:02 +0000

    Add dependent function and view to upgrade tests for sp_helpsrvrolemember
    Task: BABEL-3046
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

--- test/JDBC/expected/Test-sp_helpsrvrolemember-vu-prepare.out
+++ test/JDBC/expected/Test-sp_helpsrvrolemember-vu-prepare.out
@@ -10,5 +10,20 @@ BEGIN
 END
 GO
 
+CREATE FUNCTION dbo.test_sp_helpsrvrolemember_func() RETURNS INT
+AS
+BEGIN
+	DECLARE @tmp_sp_helpsrvrolemember TABLE(ServerRole sys.SYSNAME,
+											MemberName sys.SYSNAME,
+											MemberSID sys.VARBINARY(85));
+	INSERT INTO @tmp_sp_helpsrvrolemember (ServerRole, MemberName, MemberSID) EXEC sp_helpsrvrolemember @rolename;
+	RETURN (SELECT COUNT(*) FROM @tmp_sp_helpsrvrolemember);
+END
+GO
+
+CREATE VIEW test_sp_helpsrvrolemember_view AS
+SELECT dbo.test_sp_helpsrvrolemember_func() AS num
+GO
+
 CREATE LOGIN test_sp_helpsrvrolemember_login WITH PASSWORD='123'
 GO
--- test/JDBC/input/storedProcedures/Test-sp_helpsrvrolemember-vu-verify.sql
+++ test/JDBC/input/storedProcedures/Test-sp_helpsrvrolemember-vu-verify.sql
@@ -1,16 +1,34 @@
 EXEC test_sp_helpsrvrolemember_proc
 GO
 
+SELECT dbo.test_sp_helpsrvrolemember_func()
+GO
+
+SELECT * FROM test_sp_helpsrvrolemember_view()
+GO
+
 ALTER SERVER ROLE sysadmin ADD MEMBER test_sp_helpsrvrolemember_login
 GO
 
 EXEC test_sp_helpsrvrolemember_proc 'sysadmin'
 GO
 
+SELECT dbo.test_sp_helpsrvrolemember_func()
+GO
+
+SELECT * FROM test_sp_helpsrvrolemember_view()
+GO
+
 ALTER SERVER ROLE sysadmin DROP MEMBER test_sp_helpsrvrolemember_login
 GO
 
 EXEC test_sp_helpsrvrolemember_proc 'sysadmin'
+GO
+
+SELECT dbo.test_sp_helpsrvrolemember_func()
+GO
+
+SELECT * FROM test_sp_helpsrvrolemember_view()
 GO
 
 EXEC sp_helpsrvrolemember 'error'
