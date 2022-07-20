a2a1add0dfb2743c72da3806225c9721eaaa9c14
Xiaohui Fanhe <hexiaohu@amazon.com>
2022-07-20 14:03:08 +0000

    Add upgrade tests for currently supported Babelfish versions
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

--- test/JDBC/expected/Test-sp_helpsrvrolemember-vu-cleanup.out
+++ test/JDBC/expected/Test-sp_helpsrvrolemember-vu-cleanup.out
@@ -1,11 +1,5 @@
 DROP LOGIN test_sp_helpsrvrolemember_login
 GO
 
-DROP VIEW test_sp_helpsrvrolemember_view
-GO
-
-DROP FUNCTION test_sp_helpsrvrolemember_func
-GO
-
-DROP PROC test_sp_helpsrvrolemember_proc
+DROP TABLE test_sp_helpsrvrolemember_tbl
 GO
--- test/JDBC/input/storedProcedures/Test-sp_helpsrvrolemember-vu-verify.sql
+++ test/JDBC/input/storedProcedures/Test-sp_helpsrvrolemember-vu-verify.sql
@@ -1,34 +1,31 @@
-EXEC test_sp_helpsrvrolemember_proc
+INSERT INTO test_sp_helpsrvrolemember_tbl (ServerRole, MemberName, MemberSID) EXEC sp_helpsrvrolemember
 GO
 
-SELECT dbo.test_sp_helpsrvrolemember_func()
-GO
-
-SELECT * FROM test_sp_helpsrvrolemember_view
+SELECT * FROM test_sp_helpsrvrolemember_tbl
 GO
 
 ALTER SERVER ROLE sysadmin ADD MEMBER test_sp_helpsrvrolemember_login
 GO
 
-EXEC test_sp_helpsrvrolemember_proc 'sysadmin'
+INSERT INTO test_sp_helpsrvrolemember_tbl (ServerRole, MemberName, MemberSID) EXEC sp_helpsrvrolemember 'sysadmin'
 GO
 
-SELECT dbo.test_sp_helpsrvrolemember_func()
+TRUNCATE TABLE test_sp_helpsrvrolemember_tbl
 GO
 
-SELECT * FROM test_sp_helpsrvrolemember_view
+SELECT * FROM test_sp_helpsrvrolemember_tbl
 GO
 
 ALTER SERVER ROLE sysadmin DROP MEMBER test_sp_helpsrvrolemember_login
 GO
 
-EXEC test_sp_helpsrvrolemember_proc 'sysadmin'
+INSERT INTO test_sp_helpsrvrolemember_tbl (ServerRole, MemberName, MemberSID) EXEC sp_helpsrvrolemember 'sysadmin'
 GO
 
-SELECT dbo.test_sp_helpsrvrolemember_func()
+TRUNCATE TABLE test_sp_helpsrvrolemember_tbl
 GO
 
-SELECT * FROM test_sp_helpsrvrolemember_view
+SELECT * FROM test_sp_helpsrvrolemember_tbl
 GO
 
 EXEC sp_helpsrvrolemember 'error'
