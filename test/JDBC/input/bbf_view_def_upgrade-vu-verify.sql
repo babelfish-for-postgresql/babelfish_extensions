4f13152f2c3c5698cd6558529ba3f83e119ab2e4
Harsh Lunagariya <lunharsh@amazon.com>
2022-07-20 06:26:12 +0000

    Update test

--- test/JDBC/input/bbf_view_def_upgrade-vu-verify.sql
+++ test/JDBC/input/bbf_view_def_upgrade-vu-verify.sql
@@ -1,10 +1,10 @@
-SELECT vd.schema_name, vd.object_name, vd.definition FROM babelfish_view_def vd ORDER BY vd.schema_name, vd.object_name, vd.definition;
+SELECT vd.schema_name, vd.object_name, vd.definition FROM babelfish_view_def vd WHERE vd.object_name LIKE '%view_3135%' ORDER BY vd.schema_name, vd.object_name, vd.definition;
 GO
 
-SELECT vd.schema_name, vd.object_name, vd.definition FROM dep_view_3135 vd ORDER BY vd.schema_name, vd.object_name, vd.definition;
+SELECT vd.schema_name, vd.object_name, vd.definition FROM dep_view_3135 vd WHERE vd.object_name LIKE '%view_3135%' ORDER BY vd.schema_name, vd.object_name, vd.definition;
 GO
 
-SELECT * FROM dep_view_3135_2;
+SELECT * FROM dep_view_3135_2 WHERE "TABLE_NAME" LIKE '%view_3135%';
 GO
 
 DROP VIEW view_3135_upd;
