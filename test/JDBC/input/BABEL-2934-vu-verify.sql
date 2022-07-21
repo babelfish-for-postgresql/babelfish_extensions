ffc47f65a3db0c56c708eb305f00ae57af9619ee
Jason Teng <jasonten@amazon.com>
2022-07-21 19:27:20 +0000

    Test commit for upgrade tests
    Signed-off-by: Jason Teng <jasonten@amazon.com>

---
+++ test/JDBC/input/BABEL-2934-vu-verify.sql
@@ -0,0 +1,12 @@
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_1
+go
+drop table dbo.babel2934_1
+go
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_2
+go
+drop table dbo.babel2934_2
+go
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_3
+go
+drop table dbo.babel2934_3
+go
