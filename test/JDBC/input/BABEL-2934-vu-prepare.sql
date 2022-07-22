c8e9c147b6b170c4e685ed5d0ac4313e317b68f2
Jason Teng <jasonten@amazon.com>
2022-07-22 16:23:45 +0000

    Update tests with proper names and add expected output for verify test
    Signed-off-by: Jason Teng <jasonten@amazon.com>

--- test/JDBC/input/BABEL-2934-vu-prepare.sql
+++ test/JDBC/input/BABEL-2934-vu-prepare.sql
@@ -1,38 +1,38 @@
-CREATE TABLE dbo.babel2934_1 (c TIME(7) NULL, d TIME(7) NULL)
+CREATE TABLE BABEL-2934-vu_1 (c TIME(7) NULL, d TIME(7) NULL)
 GO
-INSERT dbo.babel2934_1 VALUES ('00:59:59.9999123' , NULL)
+INSERT BABEL-2934-vu_1 VALUES ('00:59:59.9999123' , NULL)
 GO
-INSERT dbo.babel2934_1 VALUES ('00:00:01.0000120' , NULL)
+INSERT BABEL-2934-vu_1 VALUES ('00:00:01.0000120' , NULL)
 go
 
 -- add 300 nanoseconds (the 7th digit shows as '3')
-update dbo.babel2934_1 set d = dateadd(ns, 300, c)
+update BABEL-2934-vu_1 set d = dateadd(ns, 300, c)
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_1
-go
-
-CREATE TABLE dbo.babel2934_2 (c DATETIME2(7) NULL, d DATETIME2(7) NULL)
-GO
-INSERT dbo.babel2934_2 VALUES ('00:59:59.9999123' , NULL)
-GO
-INSERT dbo.babel2934_2 VALUES ('00:00:01.0000120' , NULL)
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934-vu_1
 go
 
--- add 300 nanoseconds (the 7th digit shows as '3')
-update dbo.babel2934_2 set d = dateadd(ns, 300, c)
-go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_2
-go
-
-CREATE TABLE dbo.babel2934_3 (c DATETIMEOFFSET(7) NULL, d DATETIMEOFFSET(7) NULL)
+CREATE TABLE BABEL-2934-vu_2 (c DATETIME2(7) NULL, d DATETIME2(7) NULL)
 GO
-INSERT dbo.babel2934_3 VALUES ('00:59:59.9999123' , NULL)
+INSERT BABEL-2934-vu_2 VALUES ('00:59:59.9999123' , NULL)
 GO
-INSERT dbo.babel2934_3 VALUES ('00:00:01.0000120' , NULL)
+INSERT BABEL-2934-vu_2 VALUES ('00:00:01.0000120' , NULL)
 go
 
 -- add 300 nanoseconds (the 7th digit shows as '3')
-update dbo.babel2934_3 set d = dateadd(ns, 300, c)
+update BABEL-2934-vu_2 set d = dateadd(ns, 300, c)
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_3
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934-vu_2
+go
+
+CREATE TABLE BABEL-2934-vu_3 (c DATETIMEOFFSET(7) NULL, d DATETIMEOFFSET(7) NULL)
+GO
+INSERT BABEL-2934-vu_3 VALUES ('00:59:59.9999123' , NULL)
+GO
+INSERT BABEL-2934-vu_3 VALUES ('00:00:01.0000120' , NULL)
+go
+
+-- add 300 nanoseconds (the 7th digit shows as '3')
+update BABEL-2934-vu_3 set d = dateadd(ns, 300, c)
+go
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934-vu_3
 go
--- test/JDBC/input/BABEL-2934-vu-verify.sql
+++ test/JDBC/input/BABEL-2934-vu-verify.sql
@@ -1,12 +1,12 @@
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_1
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934-vu_1
 go
-drop table dbo.babel2934_1
+drop table BABEL-2934-vu_1
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_2
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934-vu_2
 go
-drop table dbo.babel2934_2
+drop table BABEL-2934-vu_2
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from dbo.babel2934_3
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934-vu_3
 go
-drop table dbo.babel2934_3
+drop table BABEL-2934-vu_3
 go
