b20d6a061e92ec68b20fe661ccdc25338339897d
Jason Teng <jasonten@amazon.com>
2022-07-22 16:50:15 +0000

    Fix object names
    Signed-off-by: Jason Teng <jasonten@amazon.com>

--- test/JDBC/input/BABEL-2934.sql
+++ test/JDBC/input/BABEL-2934.sql
@@ -1,49 +1,49 @@
-CREATE TABLE BABEL-2934 (c TIME(7) NULL, d TIME(7) NULL)
+CREATE TABLE BABEL_2934 (c TIME(7) NULL, d TIME(7) NULL)
 GO
-INSERT BABEL-2934 VALUES ('00:59:59.9999123' , NULL)
+INSERT BABEL_2934 VALUES ('00:59:59.9999123' , NULL)
 GO
-INSERT BABEL-2934 VALUES ('00:00:01.0000120' , NULL)
+INSERT BABEL_2934 VALUES ('00:00:01.0000120' , NULL)
 go
 
 -- add 300 nanoseconds (the 7th digit shows as '3')
-update BABEL-2934 set d = dateadd(ns, 300, c)
+update BABEL_2934 set d = dateadd(ns, 300, c)
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934
-go
-
-drop table BABEL-2934
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL_2934
 go
 
-CREATE TABLE BABEL-2934 (c DATETIME2(7) NULL, d DATETIME2(7) NULL)
+drop table BABEL_2934
+go
+
+CREATE TABLE BABEL_2934 (c DATETIME2(7) NULL, d DATETIME2(7) NULL)
 GO
-INSERT BABEL-2934 VALUES ('00:59:59.9999123' , NULL)
+INSERT BABEL_2934 VALUES ('00:59:59.9999123' , NULL)
 GO
-INSERT BABEL-2934 VALUES ('00:00:01.0000120' , NULL)
+INSERT BABEL_2934 VALUES ('00:00:01.0000120' , NULL)
 go
 
 -- add 300 nanoseconds (the 7th digit shows as '3')
-update BABEL-2934 set d = dateadd(ns, 300, c)
+update BABEL_2934 set d = dateadd(ns, 300, c)
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934
-go
-
-drop table BABEL-2934
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL_2934
 go
 
-CREATE TABLE BABEL-2934 (c DATETIMEOFFSET(7) NULL, d DATETIMEOFFSET(7) NULL)
+drop table BABEL_2934
+go
+
+CREATE TABLE BABEL_2934 (c DATETIMEOFFSET(7) NULL, d DATETIMEOFFSET(7) NULL)
 GO
-INSERT BABEL-2934 VALUES ('00:59:59.9999123' , NULL)
+INSERT BABEL_2934 VALUES ('00:59:59.9999123' , NULL)
 GO
-INSERT BABEL-2934 VALUES ('00:00:01.0000120' , NULL)
+INSERT BABEL_2934 VALUES ('00:00:01.0000120' , NULL)
 go
 
 -- add 300 nanoseconds (the 7th digit shows as '3')
-update BABEL-2934 set d = dateadd(ns, 300, c)
+update BABEL_2934 set d = dateadd(ns, 300, c)
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL-2934
+select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL_2934
 go
 
-drop table BABEL-2934
+drop table BABEL_2934
 go
 
 select cast('12:15:04.1234567' as TIME(7))
