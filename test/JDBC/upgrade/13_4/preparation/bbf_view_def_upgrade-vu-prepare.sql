7d20c300adae186eb90fbeb3134e5dc9f7651be3
Harsh Lunagariya <lunharsh@amazon.com>
2022-07-25 13:53:50 +0000

    Added comments in upgrade tests
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>

--- test/JDBC/upgrade/13_4/preparation/bbf_view_def_upgrade-vu-prepare.sql
+++ test/JDBC/upgrade/13_4/preparation/bbf_view_def_upgrade-vu-prepare.sql
@@ -4,6 +4,7 @@ GO
 CREATE VIEW dep_view_3135 AS SELECT * FROM sys.babelfish_view_def;
 GO
 
+-- Won't fail because it will refer PG's information_schema
 CREATE VIEW dep_view_3135_2 AS SELECT * FROM information_schema.views;
 GO
 
--- test/JDBC/upgrade/13_6/preparation/bbf_view_def_upgrade-vu-prepare.sql
+++ test/JDBC/upgrade/13_6/preparation/bbf_view_def_upgrade-vu-prepare.sql
@@ -4,6 +4,7 @@ GO
 CREATE VIEW dep_view_3135 AS SELECT * FROM sys.babelfish_view_def;
 GO
 
+-- Will fail because isc.views(TSQL version) is not implemented
 CREATE VIEW dep_view_3135_2 AS SELECT * FROM information_schema.views;
 GO
 
