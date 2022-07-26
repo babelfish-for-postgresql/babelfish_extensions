ff41b1a68cf426dad5e72cd611d89f590607f9dc
Jason Teng <jasonten@amazon.com>
2022-07-26 16:42:52 +0000

    remove select calls from prepare script
    Signed-off-by: Jason Teng <jasonten@amazon.com>

--- test/JDBC/input/BABEL-2934-vu-prepare.sql
+++ test/JDBC/input/BABEL-2934-vu-prepare.sql
@@ -8,8 +8,6 @@ go
 -- add 300 nanoseconds (the 7th digit shows as '3')
 update BABEL_2934_vu_1 set d = dateadd(ns, 300, c)
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL_2934_vu_1
-go
 
 CREATE TABLE BABEL_2934_vu_2 (c DATETIME2(7) NULL, d DATETIME2(7) NULL)
 GO
@@ -21,8 +19,6 @@ go
 -- add 300 nanoseconds (the 7th digit shows as '3')
 update BABEL_2934_vu_2 set d = dateadd(ns, 300, c)
 go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL_2934_vu_2
-go
 
 CREATE TABLE BABEL_2934_vu_3 (c DATETIMEOFFSET(7) NULL, d DATETIMEOFFSET(7) NULL)
 GO
@@ -33,8 +29,6 @@ go
 
 -- add 300 nanoseconds (the 7th digit shows as '3')
 update BABEL_2934_vu_3 set d = dateadd(ns, 300, c)
-go
-select c, d, convert(varchar(30), c, 109) as c2, convert(varchar(30), d, 109) as d2 from BABEL_2934_vu_3
 go
 
 CREATE VIEW BABEL_2934_vu_v1 as select cast('12:15:04.1234567' as TIME(7))
