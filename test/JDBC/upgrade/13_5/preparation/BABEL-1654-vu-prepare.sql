67afb96db531fec3b6c441fb14b4a3e66c815a40
Nirmit Shah <nirmisha@amazon.com>
2022-07-27 12:39:08 +0000

    Added missing expected files and modified sql_val expected file
    Signed-off-by: Nirmit Shah <nirmisha@amazon.com>

--- test/JDBC/upgrade/13_5/preparation/BABEL-1654-vu-prepare.sql
+++ test/JDBC/upgrade/13_5/preparation/BABEL-1654-vu-prepare.sql
@@ -4,19 +4,19 @@ GO
 select COLUMNS_UPDATED();
 GO
 
-CREATE TABLE bbl_1654_employeeData( ID INT IDENTITY (1,1) PRIMARY KEY,Emp_First_name VARCHAR (50),Emp_Last_name VARCHAR (50),Emp_Salary INT,
+CREATE TABLE babel_1654_vu_prepare_employeeData( ID INT IDENTITY (1,1) PRIMARY KEY,Emp_First_name VARCHAR (50),Emp_Last_name VARCHAR (50),Emp_Salary INT,
 a varchar (50), b varchar(50), c varchar(50), d varchar(50), e varchar(50), f varchar(50))
 GO
 
-create table bbl_1654_t ( ID INT IDENTITY (1,1) PRIMARY KEY , a varchar(50), b varchar(50))
+create table babel_1654_vu_prepare_t ( ID INT IDENTITY (1,1) PRIMARY KEY , a varchar(50), b varchar(50))
 GO
 
-CREATE TRIGGER bbl_1654_trig_t on bbl_1654_t after update as
+CREATE TRIGGER babel_1654_vu_prepare_trig_t on babel_1654_vu_prepare_t after update as
 	select COLUMNS_UPDATED();
 GO
 
-CREATE TRIGGER bbl_1654_updEmployeeDatas ON bbl_1654_employeeData  AFTER UPDATE,INSERT AS   
+CREATE TRIGGER babel_1654_vu_prepare_updEmployeeDatas ON babel_1654_vu_prepare_employeeData  AFTER UPDATE,INSERT AS   
 	select COLUMNS_UPDATED();
-	update bbl_1654_t set a = 'sss' , b = 'sss' where id = 1;
+	update babel_1654_vu_prepare_t set a = 'sss' , b = 'sss' where id = 1;
 	select COLUMNS_UPDATED();
 GO
