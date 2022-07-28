29a9e8a387d189d23292d767064d11d24a9c9550
Rishabh Tanwar <ritanwar@amazon.com>
2022-07-28 09:51:26 +0000

    Prefix object names with filename
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>

--- test/JDBC/input/Test-Default-Columns-vu-verify.sql
+++ test/JDBC/input/Test-Default-Columns-vu-verify.sql
@@ -1,74 +1,74 @@
-insert into default_columns_t1 DEFAULT VALUES
+insert into test_default_columns_vu_prepare_t1 DEFAULT VALUES
 GO
 
-SELECT * FROM default_columns_t1
+SELECT * FROM test_default_columns_vu_prepare_t1
 GO
 
-insert into default_columns_t2 DEFAULT VALUES
+insert into test_default_columns_vu_prepare_t2 DEFAULT VALUES
 GO
 
-SELECT * FROM default_columns_t2
+SELECT * FROM test_default_columns_vu_prepare_t2
 GO
 
-insert into default_columns_t3 DEFAULT VALUES
+insert into test_default_columns_vu_prepare_t3 DEFAULT VALUES
 GO
 
-SELECT * FROM default_columns_t3
+SELECT * FROM test_default_columns_vu_prepare_t3
 GO
 
-insert into default_columns_t4 DEFAULT VALUES
+insert into test_default_columns_vu_prepare_t4 DEFAULT VALUES
 GO
 
-SELECT * FROM default_columns_t4
+SELECT * FROM test_default_columns_vu_prepare_t4
 GO
 
-insert into default_columns_t5 DEFAULT VALUES
+insert into test_default_columns_vu_prepare_t5 DEFAULT VALUES
 GO
 
-SELECT * FROM default_columns_t5
+SELECT * FROM test_default_columns_vu_prepare_t5
 GO
 
-EXEC default_columns_p1
+EXEC test_default_columns_vu_prepare_p1
 GO
 
-SELECT * FROM default_columns_t1
+SELECT * FROM test_default_columns_vu_prepare_t1
 GO
 
-EXEC default_columns_p2
+EXEC test_default_columns_vu_prepare_p2
 GO
 
-SELECT * FROM default_columns_t2
+SELECT * FROM test_default_columns_vu_prepare_t2
 GO
 
-EXEC default_columns_p3
+EXEC test_default_columns_vu_prepare_p3
 GO
 
-SELECT * FROM default_columns_t3
+SELECT * FROM test_default_columns_vu_prepare_t3
 GO
 
-EXEC default_columns_p4
+EXEC test_default_columns_vu_prepare_p4
 GO
 
-SELECT * FROM default_columns_t4
+SELECT * FROM test_default_columns_vu_prepare_t4
 GO
 
-EXEC default_columns_p5
+EXEC test_default_columns_vu_prepare_p5
 GO
 
-SELECT * FROM default_columns_t5
+SELECT * FROM test_default_columns_vu_prepare_t5
 GO
 
-SELECT * from default_columns_func_1(1)
+SELECT * from test_default_columns_vu_prepare_func_1(1)
 GO
 
-SELECT * from default_columns_func_2(1)
+SELECT * from test_default_columns_vu_prepare_func_2(1)
 GO
 
-SELECT * from default_columns_func_3(1)
+SELECT * from test_default_columns_vu_prepare_func_3(1)
 GO
 
-SELECT * from default_columns_func_4(1)
+SELECT * from test_default_columns_vu_prepare_func_4(1)
 GO
 
-SELECT * from default_columns_func_5(1)
+SELECT * from test_default_columns_vu_prepare_func_5(1)
 GO
