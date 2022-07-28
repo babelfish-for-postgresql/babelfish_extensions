4cc0f75b5ab4b67f14b3d484fa75d1bee93d77d8
rossd-bitquill <dutkiert@amazon.com>
2022-07-28 23:39:59 +0000

    Apply same changes for sys.check_constraints
    sys.check_constraints view was found to have the same issue as
    sys.default_constraints, so the same fix has been applied.
    
    Additionally, updated the regex for both views to be more strict, so it
    doesn't strip trailing closing parentheses, and properly handles
    multiple instances of datatypes in constraints, and constraint datatypes
    not at the end of the string.
    
    Add test scripts for sys.check_constraints.
    
    Signed-off-by: rossd-bitquill <dutkiert@amazon.com>

--- test/JDBC/input/sys-default_constraints-vu-verify.sql
+++ test/JDBC/input/sys-default_constraints-vu-verify.sql
@@ -1,17 +1,19 @@
 SELECT definition FROM sys.default_constraints where name LIKE '%sys_default_definitions%' ORDER BY name;
 GO
 
-ALTER TABLE sys_default_definitions ADD CONSTRAINT default_column_a_varchar DEFAULT 'ab' FOR column_a
+SELECT COUNT(*) FROM sys.default_constraints;
+
+ALTER TABLE sys_default_definitions_vu_prepare ADD CONSTRAINT default_column_a_varchar DEFAULT 'ab' FOR column_a
 GO
 
 SELECT COUNT(*) FROM sys.all_columns WHERE object_id = object_id('sys.default_constraints');
 GO
 
-EXEC sys_default_definitions_proc;
+EXEC sys_default_definitions_vu_prepare_proc;
 GO
 
-SELECT * FROM sys_default_definitions_func();
+SELECT * FROM sys_default_definitions_vu_prepare_func();
 GO
 
-SELECT * FROM sys_default_definitions_view;
+SELECT * FROM sys_default_definitions_vu_prepare_view;
 GO
