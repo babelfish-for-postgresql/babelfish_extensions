220bd7ae07fea82f12ad31d37bc22e78762df60f
Sai Rohan Basa <108261379+basasairohan@users.noreply.github.com>
2022-07-29 19:29:24 +0530

    Fixed issues regarding upgrade tests (#465)
    Upgrade tests are broken for latest release due to incorrect directory names. Fix makes sure that we pick the write directory for latest release version.
    
    Signed-off-by: Surendra Vishnoi vishnosu@amazon.com

---
+++ test/JDBC/upgrade/latest/verification_cleanup/13_6/BABEL-2769-vu-verify.sql
@@ -0,0 +1,11 @@
+SELECT * FROM BABEL_2769_vu_prepare_Smalldatetime;
+go
+
+SELECT * FROM BABEL_2769_vu_prepare_Datetime;
+go
+
+SELECT * FROM BABEL_2769_vu_prepare_Datetime2;
+go
+
+SELECT * FROM BABEL_2769_vu_prepare_Datetimeoffset;
+go
