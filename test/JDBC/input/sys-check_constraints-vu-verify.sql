dca951d66ca6d3985f470743e9e4f4d71d4fcf18
rossd-bitquill <dutkiert@amazon.com>
2022-07-29 22:43:02 +0000

    Fix typos in test scripts
    Also, update the sort order to be more consistent
    
    Signed-off-by: rossd-bitquill <dutkiert@amazon.com>

--- test/JDBC/input/sys-check_constraints-vu-verify.sql
+++ test/JDBC/input/sys-check_constraints-vu-verify.sql
@@ -1,4 +1,4 @@
-SELECT definition FROM sys.check_constraints where name LIKE '%sys_check_constraints%' ORDER BY name;
+SELECT definition FROM sys.check_constraints where name LIKE '%sys_check_constraints%' ORDER BY definition;
 GO
 
 SELECT COUNT(*) FROM sys.all_columns WHERE object_id = object_id('sys.check_constraints');
