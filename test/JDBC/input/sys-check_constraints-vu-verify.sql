810ce923938f6eac527e28f94b2dfb6d1a9d9e89
Favian (Ian) Samatha <ians@bitquilltech.com>
2022-07-29 01:59:05 +0000

    Fixed JDBC tests
    Signed-off-by: Favian (Ian) Samatha <ians@bitquilltech.com>

--- test/JDBC/input/sys-check_constraints-vu-verify.sql
+++ test/JDBC/input/sys-check_constraints-vu-verify.sql
@@ -1,4 +1,4 @@
-SELECT definition FROM sys.check_constraints where name LIKE '%sys_check_definitions%' ORDER BY name;
+SELECT definition FROM sys.check_constraints where name LIKE '%sys_check_constraints%' ORDER BY name;
 GO
 
 SELECT COUNT(*) FROM sys.check_constraints;
@@ -6,11 +6,11 @@ SELECT COUNT(*) FROM sys.check_constraints;
 SELECT COUNT(*) FROM sys.all_columns WHERE object_id = object_id('sys.check_constraints');
 GO
 
-EXEC sys_check_definitions_vu_prepare_proc;
+EXEC sys_check_constraints_vu_prepare_proc;
 GO
 
-SELECT * FROM sys_check_definitions_vu_prepare_func();
+SELECT * FROM sys_check_constraints_vu_prepare_func();
 GO
 
-SELECT * FROM sys_check_definitions_vu_prepare_view;
+SELECT * FROM sys_check_constraints_vu_prepare_view;
 GO
