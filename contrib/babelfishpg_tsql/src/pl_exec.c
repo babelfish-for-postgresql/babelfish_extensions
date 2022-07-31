722cba2182aaade66b5af3ba12521357aaa005d5
Shalini Lohia <shalinilohia50@gmail.com>
2022-07-31 16:46:44 +0000

    Remove sys condition for functions

--- contrib/babelfishpg_tsql/src/pl_exec.c
+++ contrib/babelfishpg_tsql/src/pl_exec.c
@@ -10046,7 +10046,12 @@ bool reset_search_path(PLtsql_stmt_execsql *stmt, char *old_search_path)
 	const char	*schema;
 	top_es_entry = exec_state_call_stack->next;
 
-	if (stmt->func_call && stmt->schema_name != NULL && strncmp(stmt->schema_name, "sys", 3) != 0)
+	/*
+	 * When there is a function call:
+	 * search the specified schema for the object. If not found,
+	 * then search the dbo schema.
+	 */
+	if (stmt->func_call && stmt->schema_name != NULL)
 	{
 		physical_schema = get_physical_schema_name(cur_dbname, stmt->schema_name);
 		dbo_schema = get_dbo_schema_name(cur_dbname);
