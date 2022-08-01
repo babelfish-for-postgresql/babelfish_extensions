e716f9c00475c3da2b9bd1db31ed5078abedbad1
Sid Patllollu <sidred@amazon.com>
2022-08-01 22:54:24 +0000

    modified tests to work better out of order

--- test/JDBC/input/views/view_table_usage.sql
+++ test/JDBC/input/views/view_table_usage.sql
@@ -1,3 +1,5 @@
+drop database if exists db_view_table_usage;
+go
 create database db_view_table_usage;
 go
 
@@ -10,11 +12,13 @@ select * from information_schema.view_table_usage where table_name not like 'sys
 go
 
 
-
+drop table if exists view_table_usage_tb1;
+go
 create table view_table_usage_tb1(arg1 int, arg2 char, arg3 varchar);
 go
 
-
+drop view if exists view_table_usage_v1;
+go
 create view view_table_usage_v1 as select * from view_table_usage_tb1;
 go
 
