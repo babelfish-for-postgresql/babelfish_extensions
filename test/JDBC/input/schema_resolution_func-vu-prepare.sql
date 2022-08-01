c8977d7bd3e4ae06312433b4cbcf731989d75d50
Shalini Lohia <shalinilohia50@gmail.com>
2022-08-01 13:22:39 +0000

    Rename the objects name

--- test/JDBC/input/schema_resolution_func-vu-prepare.sql
+++ test/JDBC/input/schema_resolution_func-vu-prepare.sql
@@ -1,38 +1,38 @@
-create schema schema_resolution_func_s1;
-create schema schema_resolution_func_s2;
+create schema schema_resolution_func_vu_prepare_s1;
+create schema schema_resolution_func_vu_prepare_s2;
 go
 
-create table schema_resolution_func_t1(a int);
+create table schema_resolution_func_vu_prepare_t1(a int);
 go
 
-create table schema_resolution_func_s1.schema_resolution_func_t1(a int);
+create table schema_resolution_func_vu_prepare_s1.schema_resolution_func_vu_prepare_t1(a int);
 go
 
-insert into schema_resolution_func_s1.schema_resolution_func_t1 values (1);
+insert into schema_resolution_func_vu_prepare_s1.schema_resolution_func_vu_prepare_t1 values (1);
 go
 
-create table schema_resolution_func_s2.schema_resolution_func_t1(b int);
-insert into schema_resolution_func_s2.schema_resolution_func_t1 values (1);
-insert into schema_resolution_func_s2.schema_resolution_func_t1 values (1);
+create table schema_resolution_func_vu_prepare_s2.schema_resolution_func_vu_prepare_t1(b int);
+insert into schema_resolution_func_vu_prepare_s2.schema_resolution_func_vu_prepare_t1 values (1);
+insert into schema_resolution_func_vu_prepare_s2.schema_resolution_func_vu_prepare_t1 values (1);
 go
 
-create function dbo.schema_resolution_func_f1()
+create function dbo.schema_resolution_func_vu_prepare_f1()
 returns int
 as
 begin
-	return (select count(*) from schema_resolution_func_t1);
+	return (select count(*) from schema_resolution_func_vu_prepare_t1);
 end
 go
 
-create function schema_resolution_func_s1.schema_resolution_func_f1()
+create function schema_resolution_func_s1.schema_resolution_func_vu_prepare_f1()
 returns int
 as
 begin
-	return (select count(*) from schema_resolution_func_t1);
+	return (select count(*) from schema_resolution_func_vu_prepare_t1);
 end
 go
 
-create function schema_resolution_func_s1.schema_resolution_func_f2()
+create function schema_resolution_func_s1.schema_resolution_func_vu_prepare_f2()
 returns int
 as
 begin
@@ -40,29 +40,29 @@ begin
 end
 go
 
-create function schema_resolution_func_s1.schema_resolution_func_f3()
+create function schema_resolution_func_vu_prepare_s1.schema_resolution_func_vu_prepare_f3()
 returns int
 as
 begin
-	return (select count(*) from schema_resolution_func_s2.schema_resolution_func_t1);
+	return (select count(*) from schema_resolution_func_vu_prepare_s2.schema_resolution_func_vu_prepare_t1);
 end
 go
 
-create function schema_resolution_func_s1.schema_resolution_func_f4()
+create function schema_resolution_func_vu_prepare_s1.schema_resolution_func_vu_prepare_f4()
 returns table
 as
 return
-	select * from schema_resolution_func_t1;
+	select * from schema_resolution_func_vu_prepare_t1;
 go
 
-create view dbo.schema_resolution_func_v1
+create view dbo.schema_resolution_func_vu_prepare_v1
 as
-select schema_resolution_func_s1.schema_resolution_func_f3()
+select schema_resolution_func_vu_prepare_s1.schema_resolution_func_vu_prepare_f3()
 go
 
-create proc schema_resolution_func_s2.schema_resolution_func_p1
+create proc schema_resolution_func_vu_prepare_s2.schema_resolution_func_vu_prepare_p1
 as
-select schema_resolution_func_s1.schema_resolution_func_f1();
-select * from dbo.schema_resolution_func_v1;
+select schema_resolution_func_vu_prepare_s1.schema_resolution_func_vu_prepare_f1();
+select * from dbo.schema_resolution_func_vu_prepare_v1;
 go
 
