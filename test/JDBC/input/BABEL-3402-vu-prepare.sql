1e996f64c71d32a59c0d2f9af4ab3021e260e6ec
Jungkook Lee <jungkook@amazon.com>
2022-08-01 04:42:32 +0000

    Add more tests
    Signed-off-by: Jungkook Lee <jungkook@amazon.com>

--- test/JDBC/input/BABEL-3402-vu-prepare.sql
+++ test/JDBC/input/BABEL-3402-vu-prepare.sql
@@ -1,11 +1,33 @@
 use master
 go
 
-DROP VIEW IF EXISTS babel_3402_v1
-go
-
-CREATE VIEW babel_3402_v1 AS SELECT suser_sid(-10)
-go
-
 SELECT suser_sid(-10)
+go
+
+DROP VIEW IF EXISTS babel_3402_vu_prepare_v1
+go
+CREATE VIEW babel_3402_vu_prepare_v1 AS SELECT suser_sid(-10)
+go
+select * from babel_3402_vu_prepare_v1;
+go
+
+drop function if exists babel_3402_vu_prepare_f1(int);
+go
+CREATE FUNCTION babel_3402_vu_prepare_f1(@a int)
+RETURNS VARBINARY(85)
+AS
+BEGIN
+    return suser_sid(@a);
+END;
+GO
+select babel_3402_vu_prepare_f1(-10);
+go
+
+drop procedure if exists babel_3402_vu_prepare_p1;
+go
+CREATE PROCEDURE babel_3402_vu_prepare_p1 @a int
+AS
+SELECT suser_sid(@a)
+GO
+exec babel_3402_vu_prepare_p1 -10;
 go
--- test/JDBC/input/BABEL-3402-vu-verify.sql
+++ test/JDBC/input/BABEL-3402-vu-verify.sql
@@ -4,5 +4,18 @@ go
 SELECT suser_sid(-10)
 go
 
-DROP VIEW IF EXISTS babel_3402_v1
+select * from babel_3402_vu_prepare_v1;
+go
+
+select babel_3402_vu_prepare_f1(-10);
+go
+
+exec babel_3402_vu_prepare_p1 -10;
+go
+
+DROP VIEW IF EXISTS babel_3402_vu_prepare_v1
+go
+drop function if exists babel_3402_vu_prepare_f1(int);
+go
+drop procedure if exists babel_3402_vu_prepare_p1;
 go
