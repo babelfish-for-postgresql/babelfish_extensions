af342ac776dd5a6775ffb95053bb24d6b471b50d
Tim Chang <changti@amazon.com>
2022-08-02 16:26:55 +0000

    Add test for UPDATE trigger

--- test/JDBC/input/BABEL-3299.sql
+++ test/JDBC/input/BABEL-3299.sql
@@ -9,3 +9,20 @@ GO
 
 SELECT * FROM ::
 GO
+
+CREATE TABLE t3299 (a int);
+GO
+
+/* Should throw a syntax error */
+CREATE TRIGGER t1
+ON t3299
+AFTER UPDATE
+AS
+IF (::UPDATE(a))
+BEGIN
+RAISERROR(0, 0, 0)
+END;
+GO
+
+DROP TABLE t3299;
+GO
