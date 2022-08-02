9aa9d04c4fe258c50d2db7a8ea794487e0f0acfe
Sid Patllollu <sidred@amazon.com>
2022-08-02 18:11:15 +0000

    put all create statements in prepare test file

--- test/JDBC/input/views/view_table_usage-vu-verify.sql
+++ test/JDBC/input/views/view_table_usage-vu-verify.sql
@@ -4,30 +4,13 @@ go
 select * from information_schema.view_table_usage where table_name not like 'sys%';
 go
 
-
-
-drop table if exists view_table_usage_tb1;
-go
-create table view_table_usage_tb1(arg1 int, arg2 char, arg3 varchar);
-go
-
-
-drop view if exists view_table_usage_v1;
-go
-create view view_table_usage_v1 as select * from view_table_usage_tb1;
-go
-
-
-select * from information_schema.view_table_usage where table_name not like 'sys%';
-go
-
-
-
-
 drop view view_table_usage_v1
 go
 
 drop table view_table_usage_tb1
+go
+
+select * from information_schema.view_table_usage where table_name not like 'sys%';
 go
 
 use master;
