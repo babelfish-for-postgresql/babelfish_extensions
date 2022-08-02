9acac460f279bb7244f8aed3b15dbe114e34df53
Shalini Lohia <shalinilohia50@gmail.com>
2022-08-02 20:37:28 +0000

    run the query inside a database

--- test/JDBC/expected/BABEL-2701-vu-prepare.out
+++ test/JDBC/expected/BABEL-2701-vu-prepare.out
@@ -1,4 +1,6 @@
-use master;
+create database babel_2701_vu_prepare_d1;
+go
+use babel_2701_vu_prepare_d1;
 go
 
 create table babel_2701_vu_prepare_t1(a int)
--- test/JDBC/input/BABEL-2701-vu-verify.sql
+++ test/JDBC/input/BABEL-2701-vu-verify.sql
@@ -1,5 +1,8 @@
-use master;
+use database babel_2701_vu_prepare_d1;
 go
 
 select object_name(object_id) from sys.objects where name = 'babel_2701_vu_prepare_t1';
 GO
+
+drop database database babel_2701_vu_prepare_d1;
+go
