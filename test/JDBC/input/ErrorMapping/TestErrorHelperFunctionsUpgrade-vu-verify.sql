429db4551d6d38e6f4bc8c39fa10aba289bba11f
Dipesh Dhameliya <dddhamel@amazon.com>
2022-08-02 16:29:47 +0530

    ALTER VIEW does not work in Babelfish so updating the tests accordingly

--- test/JDBC/input/ErrorMapping/TestErrorHelperFunctionsUpgrade-vu-verify.sql
+++ test/JDBC/input/ErrorMapping/TestErrorHelperFunctionsUpgrade-vu-verify.sql
@@ -2,7 +2,10 @@
 SELECT * FROM TestErrorHelperFunctionsUpgrade_VU_PREPARE_VIEW;
 GO
 
-CREATE OR REPLACE VIEW TestErrorHelperFunctionsUpgrade_VU_PREPARE_VIEW AS
+DROP VIEW TestErrorHelperFunctionsUpgrade_VU_PREPARE_VIEW
+GO
+
+CREATE VIEW TestErrorHelperFunctionsUpgrade_VU_PREPARE_VIEW AS
 SELECT sql_error_code from sys.fn_mapped_system_error_list();
 GO
 
