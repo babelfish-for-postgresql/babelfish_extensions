2655c8962712f05e7eb4923b361c38058257bf58
Shalini Lohia <shalinilohia50@gmail.com>
2022-08-03 04:41:48 +0000

    add fix

--- test/JDBC/expected/BABEL-2701-vu-cleanup.out
+++ test/JDBC/expected/BABEL-2701-vu-cleanup.out
@@ -1,2 +1,10 @@
+use babel_2701_vu_prepare_d1;
+go
+
 drop table babel_2701_vu_prepare_t1
 GO
+
+use master;
+go
+drop database babel_2701_vu_prepare_d1;
+go
--- test/JDBC/input/BABEL-2701-vu-verify.sql
+++ test/JDBC/input/BABEL-2701-vu-verify.sql
@@ -3,6 +3,3 @@ go
 
 select object_name(object_id) from sys.objects where name = 'babel_2701_vu_prepare_t1';
 GO
-
-drop database database babel_2701_vu_prepare_d1;
-go
