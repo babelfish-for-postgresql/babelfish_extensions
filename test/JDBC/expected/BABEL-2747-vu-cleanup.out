3b1630fadc7498f11f9f09bc4be528a6122971aa
Favian (Ian) Samatha <ians@bitquilltech.com>
2022-08-03 21:43:38 +0000

    Added tests and modified existing tests to use new test framework
    Signed-off-by: Favian (Ian) Samatha <ians@bitquilltech.com>

---
+++ test/JDBC/expected/BABEL-2747-vu-cleanup.out
@@ -0,0 +1,11 @@
+DROP PROCEDURE babel_2747_vu_prepare
+GO
+
+DROP PROCEDURE babel_2747__vu_prepare_2
+GO
+
+DROP FUNCTION babel_2747__vu_prepare_3
+GO
+
+DROP TABLE babel_2747__vu_prepare_t1
+GO
---
+++ test/JDBC/input/BABEL-2747-vu-verify.sql
@@ -0,0 +1,5 @@
+SELECT type, type_desc from sys.procedures  where name like 'babel_2747__vu_prepare%' order by type;
+GO
+
+SELECT type, type_desc from sys.all_objects  where name like 'babel_2747__vu_prepare%' order by type;
+GO
