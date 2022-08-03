a90de4579a3d9a1d93ee77e94edeca59b9c38011
Shalini Lohia <shalinilohia50@gmail.com>
2022-08-03 06:40:46 +0000

    check if sys.objects has the table

--- test/JDBC/input/BABEL-2701-vu-verify.sql
+++ test/JDBC/input/BABEL-2701-vu-verify.sql
@@ -1,4 +1,4 @@
-select count(*) from pg_class where relname = 'babel_2701_vu_prepare_t1';
+select count(*) from sys.objects where name = 'babel_2701_vu_prepare_t1';
 go
 select object_name(object_id) from sys.objects where name = 'babel_2701_vu_prepare_t1';
 GO
