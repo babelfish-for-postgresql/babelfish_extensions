7aaae9a6e7be64c70a4e7e7e2d065ea24eb8776d
Dipesh Dhameliya <dddhamel@amazon.com>
2022-08-03 17:45:18 +0530

    Temporory tests for BABEL-2701

--- test/JDBC/input/BABEL-2701-vu-verify.sql
+++ test/JDBC/input/BABEL-2701-vu-verify.sql
@@ -1,2 +1,23 @@
+select * from sys.schemas;
+GO
+
+select name, has_schema_privilege(schema_id, 'USAGE') from sys.schemas
+GO
+
+select * from sys.tables where name = 'babel_2701_vu_prepare_t1';
+GO
+
+select * from pg_class where relname = 'babel_2701_vu_prepare_t1';
+GO
+
+select relname, has_table_privilege(oid, 'SELECT,INSERT,UPDATE,DELETE,TRUNCATE,TRIGGER') from pg_class where relname = 'babel_2701_vu_prepare_t1';
+GO
+
+insert into table babel_2701_vu_prepare_t1 values (1);
+GO
+
+select * from babel_2701_vu_prepare_t1;
+go
+
 select object_name(object_id) from sys.objects where name = 'babel_2701_vu_prepare_t1';
 GO
