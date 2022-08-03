143b022be0115c56295467881d5fa460d913a8cd
Shalini Lohia <shalinilohia50@gmail.com>
2022-08-03 10:14:06 +0000

    check schema

--- test/JDBC/input/BABEL-2701-vu-verify.sql
+++ test/JDBC/input/BABEL-2701-vu-verify.sql
@@ -1,4 +1,5 @@
-select count(*) from sys.tables where name = 'babel_2701_vu_prepare_t1';
+select schema_name(t.relnamespace), b.nspname from pg_class t inner join sys.schemas sch on t.relnamespace = sch.schema_id inner join sys.babelfish_namespace_ext b on sch.name=b.orig_name where t.relname = 'babel_2701_vu_prepare_t1';
 go
+
 select object_name(object_id) from sys.objects where name = 'babel_2701_vu_prepare_t1';
 GO
