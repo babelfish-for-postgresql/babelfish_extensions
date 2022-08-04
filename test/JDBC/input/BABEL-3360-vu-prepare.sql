b751b5c912d867147ac554732f6f1006a20dcddb
Zhenye Li <zhenye@amazon.com>
2022-08-04 17:22:56 +0000

    Update prepare files and output
    Get rid of select and exec statement in prepare input file, and update
    output files
    
    Signed-off-by: Zhenye Li <zhenye@amazon.com>

--- test/JDBC/input/BABEL-3360-vu-prepare.sql
+++ test/JDBC/input/BABEL-3360-vu-prepare.sql
@@ -1,42 +1,25 @@
 create view BABEL_3360_vu_prepare_v1 as (select dateadd(millisecond, 56, cast('2016-12-26 23:29:29' as datetime2)));
 go
-select * from BABEL_3360_vu_prepare_v1;
-go
 
 create view BABEL_3360_vu_prepare_v2 as (select dateadd(microsecond, 56, cast('2016-12-26 23:29:29' as datetime2)));
-go
-select * from BABEL_3360_vu_prepare_v2;
 go
 
 create view BABEL_3360_vu_prepare_v3 as (select dateadd(millisecond, 56, null));
 go
-select * from BABEL_3360_vu_prepare_v3;
-go
 
 create view BABEL_3360_vu_prepare_v4 as (select dateadd(microsecond, 56, null));
-go
-select * from BABEL_3360_vu_prepare_v4;
 go
 
 create procedure  BABEL_3360_vu_prepare_p1 as (select dateadd(millisecond, 56, cast('2016-12-26 23:29:29' as datetime2)));
 go
-exec BABEL_3360_vu_prepare_p1;
-go
 
 create procedure  BABEL_3360_vu_prepare_p2 as (select dateadd(microsecond, 56, cast('2016-12-26 23:29:29' as datetime2)));
-go
-go
-exec BABEL_3360_vu_prepare_p2;
 go
 
 create procedure  BABEL_3360_vu_prepare_p3 as (select dateadd(millisecond, 56, null));
 go
-exec BABEL_3360_vu_prepare_p3;
-go
 
 create procedure  BABEL_3360_vu_prepare_p4 as (select dateadd(microsecond, 56, null));
-go
-exec BABEL_3360_vu_prepare_p4;
 go
 
 create function BABEL_3360_vu_prepare_f1()
