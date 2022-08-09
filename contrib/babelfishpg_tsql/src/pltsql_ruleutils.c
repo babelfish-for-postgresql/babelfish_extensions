6ef70ad071e36b78b4a26a503793e45e28c86b0d
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2022-08-09 22:47:47 +0530

    ISC.routines throws errors while showing C-language function definitions (#515)
    * ISC.routines throws errors while showing C-language function definitions
    
    This is due to C-language function like xp_qv, xp_instance_regread contains
    probin as `babelfishpg_tsql`. Usually in information_schema it won't show
    system C-language function but for xp_* functions we need to handle it
    gracefully(by returning NULL instead).
    
    Task: BABEL-3444
    Signed-off-by: Shalini Lohia <lshalini@amazon.com>
    
    * Add a check before cleaning nnsp, it might be NULL in some cases
    
    * empty commit
    
    * Update a condition for null string
    
    * Remove xp_* procedures from the information_schema.routines
    
    * include sys_views before information_schema_tsql
    
    * Add upgrade script change

--- contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
+++ contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
@@ -435,11 +435,15 @@ tsql_get_functiondef(PG_FUNCTION_ARGS)
          * also at the end, we’re releasing proctup (that will free proc->proname).  
          */
 	pfree(nsp);
-	pfree(nnsp);
+	if (nnsp)
+		pfree(nnsp);
         
 	tmp = SysCacheGetAttr(PROCOID, proctup, Anum_pg_proc_probin, &isnull);
         number_args = proc->pronargs;
         if(isfunction) number_args++;
+	/* Return NULL for the definition if procedure language is not pltsql. */
+	if(strcmp(get_language_name(proc->prolang, false), "pltsql") != 0)
+		PG_RETURN_NULL();
        	probin_json_reader(tmp, &typmod_arr, number_args);
 	(void) print_function_arguments(&buf, proctup, false, true, &typmod_arr);
 	if(isfunction || proc->pronargs > 0)
@@ -790,8 +794,7 @@ print_function_arguments(StringInfo buf, HeapTuple proctup,
 	       	        (*typmod_arr_arg)[i] += adjustTypmod(argtype, (*typmod_arr_arg)[i]);
 		appendStringInfoString(buf, tsql_format_type_extended(argtype, (*typmod_arr_arg)[i], FORMAT_TYPE_TYPEMOD_GIVEN));
 
-
-		if(modename != "")
+		if (modename && strcmp(modename, "") != 0)
 		       	appendStringInfo(buf," %s", modename);
 
 		if (print_defaults && isinput && inputargno > nlackdefaults)
