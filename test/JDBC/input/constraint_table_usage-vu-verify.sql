4bc3c6efda5cae6216db0e8b0ef83372a1c8d530
Ozzy Nolen <oscarno@amazon.com>
2022-08-12 06:29:52 +0000

    Updated Naming in Test Files
    This commit updates the view, function, and procedure created in the
    verify file to follow the naming convention:
    "constraint_table_usage_vu_verify_%".
    
    TASK:
    
    Signed-off-by: Ozzy Nolen <oscarno@amazon.com>

--- test/JDBC/input/constraint_table_usage-vu-verify.sql
+++ test/JDBC/input/constraint_table_usage-vu-verify.sql
@@ -4,25 +4,25 @@ go
 select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%';
 go
 
-create view constraint_table_usage_vu_prepare_view as select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%';
+create view constraint_table_usage_vu_verify_view as select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%';
 go
 
-create procedure constraint_table_usage_vu_prepare_proc as select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%';
+create procedure constraint_table_usage_vu_verify_proc as select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%';
 go
 
-create function constraint_table_usage_vu_prepare_func() returns table as return(select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%');
+create function constraint_table_usage_vu_verify_func() returns table as return(select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%');
 go
 
 select * from information_schema.constraint_table_usage where table_name like 'constraint_table_usage_vu_prepare_tb%';
 go
 
-drop function constraint_table_usage_vu_prepare_func;
+drop function constraint_table_usage_vu_verify_func;
 go
 
-drop procedure constraint_table_usage_vu_prepare_proc;
+drop procedure constraint_table_usage_vu_verify_proc;
 go
 
-drop view constraint_table_usage_vu_prepare_view;
+drop view constraint_table_usage_vu_verify_view;
 go
 
 drop table constraint_table_usage_vu_prepare_tb4;
