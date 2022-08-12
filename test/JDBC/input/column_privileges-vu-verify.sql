6aceb3f56de4c3717ffb0be76c55254ff6979360
Ozzy Nolen <oscarno@amazon.com>
2022-08-12 06:19:51 +0000

    Updated Naming in Testing Files
    This commit updates the view, function, and procedure created in the
    verify file to follow the naming practice:
    "column_privileges_vu_verify_%".
    
    TASK:
    
    Signed-off-by: Ozzy Nolen <oscarno@amazon.com>

--- test/JDBC/input/column_privileges-vu-verify.sql
+++ test/JDBC/input/column_privileges-vu-verify.sql
@@ -22,13 +22,13 @@ go
 select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type;
 go
 
-create view column_privileges_vu_prepare_view as select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type;
+create view column_privileges_vu_verify_view as select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type;
 go
 
-create procedure column_privileges_vu_prepare_proc as select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type;
+create procedure column_privileges_vu_verify_proc as select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type;
 go
 
-create function column_privileges_vu_prepare_func() returns table as return(select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type);
+create function column_privileges_vu_verify_func() returns table as return(select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type);
 go
 
 select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' and is_grantable='NO' order by table_name,column_name,privilege_type;
@@ -49,13 +49,13 @@ go
 select * from information_schema.column_privileges where table_name like 'column_privileges_vu_prepare_tb%' order by table_name,column_name,privilege_type;
 go
 
-drop function column_privileges_vu_prepare_func;
+drop function column_privileges_vu_verify_func;
 go
 
-drop procedure column_privileges_vu_prepare_proc;
+drop procedure column_privileges_vu_verify_proc;
 go
 
-drop view column_privileges_vu_prepare_view;
+drop view column_privileges_vu_verify_view;
 go
 
 drop table column_privileges_vu_prepare_tb1;
