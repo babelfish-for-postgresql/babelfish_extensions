490ef09c96d2b6c0b2778567a8575345791810dc
Ozzy Nolen <oscarno@amazon.com>
2022-08-14 05:48:38 +0000

    Updated vu-verify File for Upgrade Tests
    This commit updates the vu-verify file by adding an "order by" clause
    when calling the view. This will ensure that the output is consistent
    with what is expected.
    
    TASK:
    
    Signed-off-by: Ozzy Nolen <oscarno@amazon.com>

--- test/JDBC/input/key_column_usage-vu-verify.sql
+++ test/JDBC/input/key_column_usage-vu-verify.sql
@@ -1,16 +1,16 @@
-select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%';
+select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%' order by table_name,constraint_name,table_schema;
 go
 
-create view key_column_usage_vu_verify_view as select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%';
+create view key_column_usage_vu_verify_view as select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%' order by table_name,constraint_name,table_schema;
 go
 
-create procedure key_column_usage_vu_verify_proc as select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%';
+create procedure key_column_usage_vu_verify_proc as select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%' order by table_name,constraint_name,table_schema;
 go
 
-create function key_column_usage_vu_verify_func() returns table as return(select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%');
+create function key_column_usage_vu_verify_func() returns table as return(select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%' order by table_name,constraint_name,table_schema);
 go
 
-select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%';
+select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%' order by table_name,constraint_name,table_schema;
 go
 
 drop function key_column_usage_vu_verify_func;
@@ -31,7 +31,7 @@ go
 drop table key_column_usage_vu_prepare_tb3;
 go
 
-select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%';
+select * from information_schema.key_column_usage where table_name like 'key_column_usage_vu_prepare_tb%' order by table_name,constraint_name,table_schema;
 go
 
 drop table key_column_usage_vu_prepare_tb2;
