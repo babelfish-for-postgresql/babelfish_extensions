37709fd934422a0544106adefd5f9d29090030ea
Sid Patllollu <sidred@amazon.com>
2022-08-16 18:20:05 +0000

    Changed tests to not use custom dbs and include functions and procedures
    Task: JIRA pending
    Signed-off-by: Sid Patllollu <sidred@amazon.com>

--- test/JDBC/input/views/view_table_usage-vu-prepare.sql
+++ test/JDBC/input/views/view_table_usage-vu-prepare.sql
@@ -1,17 +1,3 @@
-drop database if exists db_view_table_usage;
-go
-create database db_view_table_usage;
-go
-
-use db_view_table_usage;
-go
-
-drop table if exists view_table_usage_tb1;
-go
-create table view_table_usage_tb1(arg1 int, arg2 char, arg3 varchar);
-go
-
-drop view if exists view_table_usage_v1;
-go
-create view view_table_usage_v1 as select * from view_table_usage_tb1;
+create table view_table_usage_tb1(view_table_usage_c1 int, 
+view_table_usage_c2 char, view_table_usage_c3 varchar);
 go
--- test/JDBC/input/views/view_table_usage-vu-verify.sql
+++ test/JDBC/input/views/view_table_usage-vu-verify.sql
@@ -1,20 +1,27 @@
-use db_view_table_usage;
+select * from information_schema.view_table_usage where view_name like 'view_table_usage_v%' order by view_name;
 go
 
-select * from information_schema.view_table_usage where table_name not like 'sys%';
+create view view_table_usage_v1 as select * from view_table_usage_tb1;
 go
+
+select * from information_schema.view_table_usage where view_name like 'view_table_usage_v%' order by view_name;
+go
+
+create procedure view_table_usag_proc as select * from information_schema.view_table_usage where view_name like 'view_table_usage_v%' order by view_name;
+go
+
+create function view_table_usag_func() returns table as return(select * from information_schema.view_table_usage where view_name like 'view_table_usage_v%' order by view_name);
+go
+
+select * from information_schema.view_table_usage where view_name like 'view_table_usage_v%' order by view_name;
+go
+
+drop function view_table_usag_func
+
+drop procedure view_table_usag_proc
 
 drop view view_table_usage_v1
 go
 
 drop table view_table_usage_tb1
-go
-
-select * from information_schema.view_table_usage where table_name not like 'sys%';
-go
-
-use master;
-go
-
-drop database db_view_table_usage;
 go
