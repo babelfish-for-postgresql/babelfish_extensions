b6d5a1dc142059982232f2adfead957d4dca683c
rossd-bitquill <dutkiert@amazon.com>
2022-08-16 22:55:53 +0000

    Update tests to fix flaky tests
    Tests were failing due to SELECT COUNT(*) FROM sys.default_constraints
    (or check_constraints) because other upgrade test objects may also have
    added reccords to those views.
    
    Signed-off-by: rossd-bitquill <dutkiert@amazon.com>

--- test/JDBC/upgrade/latest/verification_cleanup/14_3/sys-default_constraints-vu-verify.sql
+++ test/JDBC/upgrade/latest/verification_cleanup/14_3/sys-default_constraints-vu-verify.sql
@@ -1,8 +1,6 @@
 SELECT definition FROM sys.default_constraints where name LIKE '%sys_default_definitions%' ORDER BY name;
 GO
 
-SELECT COUNT(*) FROM sys.default_constraints;
-
 ALTER TABLE sys_default_definitions_vu_prepare ADD CONSTRAINT default_column_a_varchar DEFAULT 'ab' FOR column_a
 GO
 
