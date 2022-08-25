15196562956f6716f0a95a96c311dea76e3b645b
Ozzy Nolen <oscarno@amazon.com>
2022-08-25 14:07:30 +0000

    Added Support for TSQL's key_column_usage
    Currently, Babelfish does not support
    information_schema_tsql.key_column_usage. This commit updates the state
    by implementing the view.
    
    TASK:
    
    Signed-off-by: Ozzy Nolen <oscarno@amazon.com>

---
+++ test/JDBC/input/key_column_usage-database-test.sql
@@ -0,0 +1,50 @@
+use master;
+go
+
+create table key_column_usage_db_test_tb1(redID INT PRIMARY KEY, greenID INT NOT NULL UNIQUE, blueID INT NOT NULL, UNIQUE(greenID,blueID));
+go
+
+create table key_column_usage_db_test_tb2( greenID char(10) PRIMARY KEY, redID INT NOT NULL, CONSTRAINT FK_RED FOREIGN KEY (redID) REFERENCES key_column_usage_db_test_tb1 (redID));
+go
+
+create database key_column_usage_db_test_db;
+go
+
+use key_column_usage_db_test_db;
+go
+
+create table key_column_usage_db_test_tb3(blueID INT PRIMARY KEY, greenID char(10));
+go
+
+create table key_column_usage_db_test_tb4(purpleID INT PRIMARY KEY, blueID INT NOT NULL, CONSTRAINT FK_GREEN FOREIGN KEY (blueID) REFERENCES key_column_usage_db_test_tb3(blueID));
+go
+
+use master;
+go
+
+select * from information_schema.key_column_usage where table_name like 'key_column_usage_db_test_tb%' order by table_name,constraint_name,ordinal_position;
+go
+
+use key_column_usage_db_test_db;
+go
+
+select * from information_schema.key_column_usage where table_name like 'key_column_usage_db_test_tb%' order by table_name,constraint_name,ordinal_position;
+go
+
+drop table key_column_usage_db_test_tb4;
+go
+
+drop table key_column_usage_db_test_tb3;
+go
+
+use master;
+go
+
+drop table key_column_usage_db_test_tb2;
+go
+
+drop table key_column_usage_db_test_tb1;
+go
+
+drop database key_column_usage_db_test_db;
+go
