5c32bfbb3b504da0e9127f6670a6ed2ec3f4682c
Walt Boettge <61607770+wboettge@users.noreply.github.com>
2022-09-12 19:27:07 -0700

    Fix sp_helpsrvrolemember for predefined roles  (#591)
    * Fix sp_helpsrvrolemember for predefined roles
    
    sp_helpsrvrolemember returns information about the members of a fixed
    server role. SQL-server includes predefined server-level roles, for
    example serveradmin, that are not present in Babelfish. These roles
    are:
        serveradmin
        setupadmin
        securityadmin
        processadmin
        dbcreator
        diskadmin
        bulkadmin
    
    Currently, running sp_helpsrvrolemember 'securityadmin' raises
    an error that the given role does not exist. This fix will change
    the behavior so that it will instead return an empty table, matching
    the behavior of SQL Server.
    
    Task: BABEL-3462
    
    Signed-off-by: Walt Boettge <wboettge@amazon.com>
    
    * Update upgrade script with fixed helpsrvrolemember
    
    2.2.0--2.3.0 upgrade script includes fixed helpsrvrolemember stored procedure.
    The procedure now returns an empty table when given certain built-in roles as
    arguments instead of raising an error.
    
    Task: BABEL-3462
    
    Signed-off-by: Walt Boettge <wboettge@amazon.com>
    
    Signed-off-by: Walt Boettge <wboettge@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.2.0--2.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.2.0--2.3.0.sql
@@ -31,7 +31,54 @@ LANGUAGE plpgsql;
 
 
 -- please add your SQL here
-
+CREATE OR REPLACE PROCEDURE sys.sp_helpsrvrolemember("@srvrolename" sys.SYSNAME = NULL) AS
+$$
+BEGIN
+	-- If server role is not specified, return info for all server roles
+	IF @srvrolename IS NULL
+	BEGIN
+		SELECT CAST(Ext1.rolname AS sys.SYSNAME) AS 'ServerRole',
+			   CAST(Ext2.rolname AS sys.SYSNAME) AS 'MemberName',
+			   CAST(CAST(Base2.oid AS INT) AS sys.VARBINARY(85)) AS 'MemberSID'
+		FROM pg_catalog.pg_auth_members AS Authmbr
+		INNER JOIN pg_catalog.pg_roles AS Base1 ON Base1.oid = Authmbr.roleid
+		INNER JOIN pg_catalog.pg_roles AS Base2 ON Base2.oid = Authmbr.member
+		INNER JOIN sys.babelfish_authid_login_ext AS Ext1 ON Base1.rolname = Ext1.rolname
+		INNER JOIN sys.babelfish_authid_login_ext AS Ext2 ON Base2.rolname = Ext2.rolname
+		WHERE Ext1.type = 'R'
+		ORDER BY ServerRole, MemberName;
+	END
+	-- If a valid server role is specified, return its member info
+	-- If the role is a SQL server predefined role (i.e. serveradmin), 
+	-- do not raise an error even if it does not exist
+	ELSE IF EXISTS (SELECT 1
+				FROM sys.babelfish_authid_login_ext
+				WHERE (rolname = @srvrolename
+				OR lower(rolname) = lower(@srvrolename))
+				AND type = 'R') 
+			OR lower(@srvrolename) IN (
+				'serveradmin', 'setupadmin', 'securityadmin', 'processadmin',
+				'dbcreator', 'diskadmin', 'bulkadmin')
+		BEGIN
+		SELECT CAST(Ext1.rolname AS sys.SYSNAME) AS 'ServerRole',
+			   CAST(Ext2.rolname AS sys.SYSNAME) AS 'MemberName',
+			   CAST(CAST(Base2.oid AS INT) AS sys.VARBINARY(85)) AS 'MemberSID'
+		FROM pg_catalog.pg_auth_members AS Authmbr
+		INNER JOIN pg_catalog.pg_roles AS Base1 ON Base1.oid = Authmbr.roleid
+		INNER JOIN pg_catalog.pg_roles AS Base2 ON Base2.oid = Authmbr.member
+		INNER JOIN sys.babelfish_authid_login_ext AS Ext1 ON Base1.rolname = Ext1.rolname
+		INNER JOIN sys.babelfish_authid_login_ext AS Ext2 ON Base2.rolname = Ext2.rolname
+		WHERE Ext1.type = 'R'
+		AND (Ext1.rolname = @srvrolename OR lower(Ext1.rolname) = lower(@srvrolename))
+		ORDER BY ServerRole, MemberName;
+	END
+	-- If the specified server role is not valid
+	ELSE
+		RAISERROR('%s is not a known fixed role.', 16, 1, @srvrolename);
+END;
+$$
+LANGUAGE 'pltsql';
+GRANT EXECUTE ON PROCEDURE sys.sp_helpsrvrolemember TO PUBLIC;
 
 -- Drops the temporary procedure used by the upgrade script.
 -- Please have this be one of the last statements executed in this upgrade script.
