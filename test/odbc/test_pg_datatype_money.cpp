3a99c0d97673ad721885cc8cef0d7b1cdf94b73f
Colin  Yuen <Colin_Yuen@sfu.ca>
2022-09-15 11:26:46 -0700

    Added arithmetic tests

--- test/odbc/test_pg_datatype_money.cpp
+++ test/odbc/test_pg_datatype_money.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 #include <iostream>
 using std::pair;
-
 const string TABLE_NAME = "master_dbo.money_table_odbc_test";
 const string COL1_NAME = "pk";
 const string COL2_NAME = "data";
@@ -374,12 +373,201 @@ TEST_F(PSQL_DataTypes_Money, Update_Fail) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-// Math
-// + - 
-// / *
+TEST_F(PSQL_DataTypes_Money, Arithmetic_Operators) {
+  const int BYTES_EXPECTED = 8;
 
-// Math 2
-// Min, Max, Mode, Avg, Sum
+  double pk;
+  double data;
+  SQLLEN pk_len;
+  SQLLEN data_len;
+  SQLLEN affected_rows;
+
+  RETCODE rcode;
+  OdbcHandler odbcHandler;
+
+  vector <string> inserted_pk = {
+    "-987654321.1234",
+    "-123456789.4321",
+    "0"
+  };
+
+  vector <string> inserted_data = {
+		"2.01",
+		"-100000000.01",
+    "922337203685477.5807"
+  };
+
+  vector <string> operations_query = {
+    COL1_NAME + "+" + COL2_NAME,
+    COL1_NAME + "-" + COL2_NAME,
+    COL1_NAME + "/" + COL2_NAME,
+    COL1_NAME + "*" + COL2_NAME
+  };
+
+  vector<vector<double>>expected_results = {};
+
+  // initialization of expected_results
+  for (int i = 0; i < inserted_data.size(); i++) {
+    expected_results.push_back({});
+
+    expected_results[i].push_back(StringToDouble(inserted_pk[i]) + StringToDouble(inserted_data[i]));
+    expected_results[i].push_back(StringToDouble(inserted_pk[i]) - StringToDouble(inserted_data[i]));
+    expected_results[i].push_back(StringToDouble(inserted_pk[i]) / StringToDouble(inserted_data[i]));
+    expected_results[i].push_back(StringToDouble(inserted_pk[i]) * StringToDouble(inserted_data[i]));
+  }
+
+  double col_results[operations_query.size()];
+  SQLLEN col_len[operations_query.size()];
+  vector<tuple<int, int, SQLPOINTER, int, SQLLEN* >> bind_columns = {};
+
+  // initialization for bind_columns
+  for (int i = 0; i < operations_query.size(); i++) {
+    tuple<int, int, SQLPOINTER, int, SQLLEN*> tuple_to_insert(i + 1, SQL_C_DOUBLE, (SQLPOINTER) &col_results[i], 0, &col_len[i]);
+    bind_columns.push_back(tuple_to_insert);
+  }
+
+  string insert_string{}; 
+  string comma{};
+  
+  // insert_string initialization
+  for (int i = 0; i < inserted_data.size(); i++) {
+    insert_string += comma + "(" + inserted_pk[i] + "," + inserted_data[i] + ")";
+    comma = ",";
+  }
+
+  // Create table
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS));
+  odbcHandler.CloseStmt();
+
+  // Insert valid values into the table and assert affected rows
+  odbcHandler.ExecQuery(InsertStatement(TABLE_NAME, insert_string));
+ 
+  rcode = SQLRowCount(odbcHandler.GetStatementHandle(), &affected_rows);
+  ASSERT_EQ(rcode, SQL_SUCCESS);
+  ASSERT_EQ(affected_rows, inserted_data.size());
+
+  // Make sure inserted values are correct and operations
+  ASSERT_NO_FATAL_FAILURE(odbcHandler.BindColumns(bind_columns));
+
+  odbcHandler.CloseStmt();
+  odbcHandler.ExecQuery(SelectStatement(TABLE_NAME, operations_query, vector<string> {}) + " ORDER BY " + COL1_NAME);
+  ASSERT_NO_FATAL_FAILURE(odbcHandler.BindColumns(bind_columns));
+
+  for (int i = 0; i < inserted_data.size(); i++) {
+    rcode = SQLFetch(odbcHandler.GetStatementHandle());
+    ASSERT_EQ(rcode, SQL_SUCCESS);
+
+    for (int j = 0; j < operations_query.size(); j++) {
+      ASSERT_EQ(col_len[j], BYTES_EXPECTED);
+      ASSERT_EQ(col_results[j], expected_results[i][j]);
+    }
+  }
+
+  // Assert that there is no more data
+  rcode = SQLFetch(odbcHandler.GetStatementHandle());
+  ASSERT_EQ(rcode, SQL_NO_DATA);
+
+  odbcHandler.CloseStmt();
+  odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
+}
+
+TEST_F(PSQL_DataTypes_Money, Arithmetic_Functions) {
+  const int BYTES_EXPECTED = 8;
+
+  double pk;
+  double data;
+  SQLLEN pk_len;
+  SQLLEN data_len;
+  SQLLEN affected_rows;
+
+  RETCODE rcode;
+  OdbcHandler odbcHandler;
+
+  vector <string> inserted_pk = {
+    "-987654321.1234",
+    "-123456789.4321",
+    "0"
+  };
+
+  vector <string> inserted_data = {
+		"2.01",
+		"-100000000.01",
+    "922337203685477.5807"
+  };
+
+  vector <string> operations_query = {
+    "MIN(" + COL1_NAME + ")",
+    "MAX(" + COL1_NAME + ")",
+    "SUM(" + COL1_NAME + ")",
+    "AVG(" + COL1_NAME + ")"
+  };
+
+  // initialization of expected_results
+  vector<double>expected_results = {};
+  double min_expected, max_expected, sum_expected, avg_expected = 0;
+  for (int i = 0; i < inserted_pk.size(); i++) {
+    double curr = StringToDouble(inserted_pk[i]);
+    min_expected = std::min(min_expected, curr);
+    max_expected = std::max(max_expected, curr);
+    sum_expected += curr;
+  }
+  avg_expected = sum_expected / inserted_pk.size();
+  expected_results.push_back(min_expected);
+  expected_results.push_back(max_expected);
+  expected_results.push_back(sum_expected);
+  expected_results.push_back(avg_expected);
+
+  double col_results[operations_query.size()];
+  SQLLEN col_len[operations_query.size()];
+  vector<tuple<int, int, SQLPOINTER, int, SQLLEN* >> bind_columns = {};
+
+  // initialization for bind_columns
+  for (int i = 0; i < operations_query.size(); i++) {
+    tuple<int, int, SQLPOINTER, int, SQLLEN*> tuple_to_insert(i + 1, SQL_C_DOUBLE, (SQLPOINTER) &col_results[i], 0, &col_len[i]);
+    bind_columns.push_back(tuple_to_insert);
+  }
+
+  string insert_string{}; 
+  string comma{};
+  
+  // insert_string initialization
+  for (int i = 0; i < inserted_data.size(); i++) {
+    insert_string += comma + "(" + inserted_pk[i] + "," + inserted_data[i] + ")";
+    comma = ",";
+  }
+
+  // Create table
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS));
+  odbcHandler.CloseStmt();
+
+  // Insert valid values into the table and assert affected rows
+  odbcHandler.ExecQuery(InsertStatement(TABLE_NAME, insert_string));
+ 
+  rcode = SQLRowCount(odbcHandler.GetStatementHandle(), &affected_rows);
+  ASSERT_EQ(rcode, SQL_SUCCESS);
+  ASSERT_EQ(affected_rows, inserted_data.size());
+
+  // Make sure inserted values are correct and operations
+  ASSERT_NO_FATAL_FAILURE(odbcHandler.BindColumns(bind_columns));
+
+  odbcHandler.CloseStmt();
+  odbcHandler.ExecQuery(SelectStatement(TABLE_NAME, operations_query, vector<string> {}));
+  ASSERT_NO_FATAL_FAILURE(odbcHandler.BindColumns(bind_columns));
+
+  rcode = SQLFetch(odbcHandler.GetStatementHandle());
+  ASSERT_EQ(rcode, SQL_SUCCESS);
+  for (int i = 0; i < operations_query.size(); i++) {    
+    ASSERT_EQ(col_len[i], BYTES_EXPECTED);
+    ASSERT_EQ(col_results[i], expected_results[i]);
+  }
+
+  // Assert that there is no more data
+  rcode = SQLFetch(odbcHandler.GetStatementHandle());
+  ASSERT_EQ(rcode, SQL_NO_DATA);
+
+  odbcHandler.CloseStmt();
+  odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
+}
 
 TEST_F(PSQL_DataTypes_Money, View_Creation) {
   const string VIEW_QUERY = "SELECT * FROM " + TABLE_NAME;
