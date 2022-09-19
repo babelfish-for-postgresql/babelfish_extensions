9292bca4dbfc3e23a4a3f62d05dceb66122551a0
Jason Teng <akariass@gmail.com>
2022-09-19 15:05:59 -0400

    Fix issue with using strict/lax jsonpath with arrays (#613)
    Signed-off-by: Jason Teng <jasonten@amazon.com>
    
    Signed-off-by: Jason Teng <jasonten@amazon.com>
    Co-authored-by: Jason Teng <jasonten@amazon.com>

--- test/JDBC/input/BABEL-OPENJSON.sql
+++ test/JDBC/input/BABEL-OPENJSON.sql
@@ -128,3 +128,14 @@ WITH  (
         [Cats]      nvarchar(max)   '$' AS JSON   
     );
 GO
+
+DECLARE @json NVARCHAR(MAX);
+SET @json = N'[
+  {"id": 2, "info": {"name": "John", "surname": "Smith"}, "age": 25},
+  {"id": 5, "info": {"name": "Jane", "surname": "Smith"}, "dob": "2005-11-04T12:00:00"}
+]';
+
+SELECT *
+FROM OPENJSON(@json)
+  WITH ( id INT 'strict $.id', firstName NVARCHAR(50) '$.info.name', lastName NVARCHAR(50) '$.info.surname', age INT, dateOfBirth DATETIME2 '$.dob' );
+go
