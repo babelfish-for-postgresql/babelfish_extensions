358333d5c9177cc453a56bed57e0e64eb5101cc1
Jason Teng <jasonten@amazon.com>
2022-09-22 15:41:06 +0000

    Fix various errors with DATETIME operations
    Currently, DATETIME still only has a partial implementation (see BABEL-323).
    Therefore, some operations related to adding together combinations of DATETIME
    values with other values does not always produce the correct results. For now,
    add a new sys.+ operator for (datetime + datetime) and fix up some of the type
    coersion definitions to explicitly cast to an appropriate data type.
    
    Task: BABEL-2812
    
    Signed-off-by: Jason Teng <jasonten@amazon.com>

---
+++ test/JDBC/input/BABEL-2812.sql
@@ -0,0 +1,14 @@
+declare @refreshTime varchar(20) = '17:30:00'
+declare @yyyymmdd    varchar(20) = '20211212'
+select CONVERT(datetime,@refreshTime,14) + @yyyymmdd as NextTime
+go
+
+declare @refreshTime datetime = '17:30:00'
+declare @yyyymmdd    datetime = '20211212'
+select  @refreshTime + @yyyymmdd as NextTime
+go
+
+declare @refreshTime datetime = '17:30:00'
+declare @yyyymmdd    datetime = '20211212'
+SELECT DATEADD(day ,DATEDIFF(day, 0, @yyyymmdd) ,@refreshTime) as NextTime;
+go
