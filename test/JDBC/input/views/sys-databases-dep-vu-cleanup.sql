ed7ec8e51ec4100be285c4749c677b84294ca6d6
Favian (Ian) Samatha <ians@bitquilltech.com>
2022-09-27 16:55:19 +0000

    Removed some duplicated running tests and fixed the object names of dep_vu tests
    Signed-off-by: Favian (Ian) Samatha <ians@bitquilltech.com>

--- test/JDBC/input/views/sys-databases-dep-vu-cleanup.sql
+++ test/JDBC/input/views/sys-databases-dep-vu-cleanup.sql
@@ -1,11 +1,11 @@
-DROP VIEW sys_databases_view_vu_prepare
+DROP VIEW sys_databases_view_dep_vu_prepare
 GO
 
-DROP PROC sys_databases_proc_vu_prepare
+DROP PROC sys_databases_proc_dep_vu_prepare
 GO
 
-DROP FUNCTION sys_databases_func_vu_prepare
+DROP FUNCTION sys_databases_func_dep_vu_prepare
 GO
 
-DROP DATABASE db_sys_databases_vu_prepare
+DROP DATABASE db_sys_databases_dep_vu_prepare
 GO
--- test/JDBC/input/views/sys-databases-dep-vu-prepare.sql
+++ test/JDBC/input/views/sys-databases-dep-vu-prepare.sql
@@ -1,18 +1,18 @@
-CREATE DATABASE db_sys_databases_vu_prepare
+CREATE DATABASE db_sys_databases_dep_vu_prepare
 GO
 
-CREATE VIEW sys_databases_view_vu_prepare AS
-SELECT name, compatibility_level, collation_name FROM sys.databases WHERE name = 'db_sys_databases_vu_prepare'
+CREATE VIEW sys_databases_view_dep_vu_prepare AS
+SELECT name, compatibility_level, collation_name FROM sys.databases WHERE name = 'db_sys_databases_dep_vu_prepare'
 GO
 
-CREATE PROC sys_databases_proc_vu_prepare AS
-SELECT name, compatibility_level, collation_name FROM sys.databases WHERE name = 'db_sys_databases_vu_prepare'
+CREATE PROC sys_databases_proc_dep_vu_prepare AS
+SELECT name, compatibility_level, collation_name FROM sys.databases WHERE name = 'db_sys_databases_dep_vu_prepare'
 GO
 
-CREATE FUNCTION sys_databases_func_vu_prepare()
+CREATE FUNCTION sys_databases_func_dep_vu_prepare()
 RETURNS TINYINT
 AS
 BEGIN
-    RETURN (SELECT compatibility_level FROM sys.databases WHERE name = 'db_sys_databases_vu_prepare')
+    RETURN (SELECT compatibility_level FROM sys.databases WHERE name = 'db_sys_databases_dep_vu_prepare')
 END
 GO
