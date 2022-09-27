f15e588dfe224ba3d9037efd7a581a9ff5ce9418
Favian (Ian) Samatha <ians@bitquilltech.com>
2022-09-27 01:15:59 +0000

    Fixed upgrade error
    Signed-off-by: Favian (Ian) Samatha <ians@bitquilltech.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.2.0--2.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.2.0--2.3.0.sql
@@ -518,7 +518,7 @@ SELECT pg_catalog.pg_extension_config_dump('sys.babelfish_function_ext', '');
 
 -- Drops the temporary procedure used by the upgrade script.
 -- Please have this be one of the last statements executed in this upgrade script.
-DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar);
+DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar, varchar);
 
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
