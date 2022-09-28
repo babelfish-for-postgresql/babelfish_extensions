0a2eccb7181b2be1c7878929d1237051480a02fd
Zhenye Li <zhenye@amazon.com>
2022-09-28 20:57:46 +0000

    Update syscolumns verify file
    Signed-off-by: Zhenye Li <zhenye@amazon.com>

--- test/JDBC/input/views/sys-syscolumns-vu-verify.sql
+++ test/JDBC/input/views/sys-syscolumns-vu-verify.sql
@@ -1,6 +1,9 @@
 use sys_syscolumns_vu_prepare_db1;
 go
 
+SELECT * FROM sys.syscolumns
+go
+
 select name, sys_syscolumns_vu_prepare_OidToObject(id), sys_syscolumns_vu_prepare_OidToDataType(xtype), typestat, length from sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by sys_syscolumns_vu_prepare_OidToObject(id) asc, name
 GO
 
@@ -34,7 +37,4 @@ go
 
 -- should not be visible here
 SELECT COUNT(*) FROM sys.syscolumns where name = '@thirdparam'
-go
-
-SELECT * FROM sys.syscolumns
 go
