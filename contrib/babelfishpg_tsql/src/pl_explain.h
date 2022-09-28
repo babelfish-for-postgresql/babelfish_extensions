908bd1ba783c295d614e0ec0c5959119d2d4010c
jusjosj <96209028+jusjosj@users.noreply.github.com>
2022-09-28 16:05:55 -0700

    Expand explain for PRINT, THROW, USE and RAISERROR (#561)
    This commit extends the functionality of babelfish_showplan_all to include print, throw, raiserror, and use
    This is largely done in a manner consistent with the other explain functions existing throughout the codebase.
    
    Task: BABEL-2902
    Signed-off-by: Justin Jossick <jusjosj@amazon.com>

--- contrib/babelfishpg_tsql/src/pl_explain.h
+++ contrib/babelfishpg_tsql/src/pl_explain.h
@@ -18,5 +18,14 @@ extern bool is_explain_analyze_mode(void);
 extern void increment_explain_indent(void);
 extern void decrement_explain_indent(void);
 extern void append_explain_info(QueryDesc *queryDesc, const char *queryString);
+extern void set_explain_database(const char *db_name);
+extern const char *get_explain_database(void);
+
+/*
+ * expr will be a PLtsql_expr * declared as void to avoid including the pltsql.h file
+ * in this file
+*/
+extern const char *strip_select_from_expr(void * expr);
+
 
 #endif  /* PL_EXPLAIN_H */
