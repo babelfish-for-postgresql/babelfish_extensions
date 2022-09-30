07b08d0da8e618066e38028a34335b61cb0050f4
Walt Boettge <wboettge@amazon.com>
2022-09-30 15:30:18 -0500

    Login who owns database should always be dbo also without sysadmin role
    In SQL Server, the login that creates a database is automatically the
    database owner. This means that the login can always access the
    databases they create.
    
    However, previously in babelfish, if a login is stripped of the sysadmin
    role after creating a database, and the login does not have a
    user associated with the database, then the login can no longer access
    the database. Now, when a login attempts to access and there
    is not an associated user, the login will be considered the dbo and
    allowed access.
    
    Task: BABEL-3548
    
    Signed-off-by: Walt Boettge <wboettge@amazon.com>

--- contrib/babelfishpg_tsql/src/dbcmds.h
+++ contrib/babelfishpg_tsql/src/dbcmds.h
@@ -4,5 +4,6 @@
 #include "nodes/parsenodes.h"
 extern Oid create_bbf_db(ParseState *pstate, const CreatedbStmt *stmt);
 extern void drop_bbf_db(const char *dbname, bool missing_ok, bool force_drop);
+extern const char *get_owner_of_db(const char *dbname);
 
 #endif
