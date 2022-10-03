32d73d3f5fcf489d59d68f3aec70f61928aec378
Zhenye Li <zhenye@amazon.com>
2022-10-03 17:43:48 +0000

    Revert "Get full record"
    This reverts commit 5b4d7a374dc8ce143b4cc86db68dd317ba3a16a8.

--- test/JDBC/input/views/sys-syscolumns-vu-verify.sql
+++ test/JDBC/input/views/sys-syscolumns-vu-verify.sql
@@ -1,9 +1,6 @@
 use sys_syscolumns_vu_prepare_db1;
 go
 
-SELECT * FROM sys.syscolumns
-go
-
 select name, sys_syscolumns_vu_prepare_OidToObject(id), sys_syscolumns_vu_prepare_OidToDataType(xtype), typestat, length from sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by sys_syscolumns_vu_prepare_OidToObject(id) asc, name
 GO
 
