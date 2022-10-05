0f18c5066e3de409678f3dfe58be75736dc97ca5
Shalini Lohia <shalinilohia50@gmail.com>
2022-10-05 11:34:13 +0000

    Remove extra tests

--- test/JDBC/input/storedProcedures/Test-sp_helpuser-vu-verify.sql
+++ test/JDBC/input/storedProcedures/Test-sp_helpuser-vu-verify.sql
@@ -1,16 +1,4 @@
 -- verify
-sp_helpuser
-go
-
-select * from sys.babelfish_authid_user_ext;
-go
-
-use Test_sp_helpuser_vu_prepare_db;
-go
-
-sp_helpuser
-go
-
 EXEC Test_sp_helpuser_vu_prepare_check_helpuser 'dbo';
 GO
 
