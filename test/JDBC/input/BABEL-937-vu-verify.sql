c0009ba1eabba963a103effc1aeb55d9071eed57
anujsarda3 <anujsarda3@gmail.com>
2022-10-06 15:03:07 +0000

    Updated JSON_MODIFY implementation and test cases
    - Provided SQLSTATE to the error messages
    - Created view to run multiple select statements
    - Removed the file BABEL-937.sql and made changes in BABEL-937-vu files
    - Followed the community block-comment formatting standard
    
    TASK: BABEL-937
    Signed-off-by: Anuj Sarda <sardanuj@amazon.com>

--- test/JDBC/input/BABEL-937-vu-verify.sql
+++ test/JDBC/input/BABEL-937-vu-verify.sql
@@ -1,2 +1,42 @@
-select * from babel_937_vu_v1
+select * from babel_937_not_append_kp
+go
+
+select * from babel_937_not_append_knp_1
+go
+select * from babel_937_not_append_knp_2
+go
+select * from babel_937_not_append_knp_3
+go
+
+select * from babel_937_append_kp_notarr_1
+go
+select * from babel_937_append_kp_notarr_2
+go
+select * from babel_937_append_kp_notarr_3
+go
+
+select * from babel_937_append_kp_arr
+go
+
+select * from babel_937_append_knp_notarr_1
+go
+select * from babel_937_append_knp_notarr_2
+go
+select * from babel_937_append_knp_notarr_3
+go
+
+select * from babel_937_case_sensitive_1
+go
+select * from babel_937_case_sensitive_2
+go
+select * from babel_937_case_sensitive_3
+go
+
+select * from babel_937_spaces
+go
+
+select * from babel_937_multi_function
+go
+
+select * from babel_937_test_array
 go
