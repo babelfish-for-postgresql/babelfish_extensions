f8dabce46e1fc2172f23e921464ae19cae805315
Ray Kim <raydhkim@amazon.com>
2022-10-12 20:37:51 +0000

    Fix issue where sp_helpdb incorrectly rejects dbname parameter
    Previously, when sp_helpdb is executed with an optional database name as a parameter, Babelfish rejected the execution with an error saying that “The database ‘@dbname’ does not exist”, whereas sp_helpdb without any parameter works correctly.
    
    Now, execution of sp_helpdb with a database name input works correctly, reporting information about the specified database, without throwing a wrong error message.
    
    Task: BABEL-3539
    
    Signed-off-by: Ray Kim <raydhkim@amazon.com>

--- test/JDBC/input/BABEL-sp_helpdb-vu-verify.sql
+++ test/JDBC/input/BABEL-sp_helpdb-vu-verify.sql
@@ -4,10 +4,6 @@ GO
 -- Executing sp_helpdb with already existing dbname as an input
 SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('master');
 GO
-SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('tempdb');
-GO
-SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('msdb');
-GO
 SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('babel_sp_helpdb_db');
 GO
 
