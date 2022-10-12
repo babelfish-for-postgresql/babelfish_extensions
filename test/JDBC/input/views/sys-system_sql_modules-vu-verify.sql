cdce6c256a6c853c1e6134266621e9f6cab01bea
Favian (Ian) Samatha <60306813+faviansamatha@users.noreply.github.com>
2022-10-11 17:27:10 -0700

    Fix flaky test caused by sys-sql_modules tests and sys-events tests (#687)
    Previously, the GH actions had several flaky tests in the upgrade workflow due to multiple objects sharing the same object ids. This is because in objects in pg_proc and pg_class may have the ame oids after MVU. This commit fixes the following tests:
    
    - sys.sql_modules
    - sys.all_sql_modules
    - sys.system_sql_modules
    - sys.events
    
    For sys.events, a stricter query was used. For the sql modules view, tests were disabled for the upgrade process, and regular JDBC non-upgrade tests were added. This is because sql_modules only has 2 fields that can uniquely identify an object: object_id, and the definition. However, not all the definition is supported for all objects which could make these queries still flaky. So objects without a supported definition will not be tested in the upgrade path.
    
    Signed-off-by: Favian (Ian) Samatha ians@bitquilltech.com

--- test/JDBC/input/views/sys-system_sql_modules-vu-verify.sql
+++ test/JDBC/input/views/sys-system_sql_modules-vu-verify.sql
@@ -1,46 +1,42 @@
--- Test for system function
-SELECT
-    uses_ansi_nulls,
-    uses_quoted_identifier,
-    is_schema_bound,
-    uses_database_collation,
-    is_recompiled,
-    null_on_null_input,
-    execute_as_principal_id,
-    uses_native_compilation
-FROM sys.system_sql_modules
-WHERE object_id = OBJECT_ID('sys.fn_listextendedproperty')
-GO
+-- NOTE: Some tests will be marked as "DISABLED DUE TO NON-UNIQUE OBJECT_ID"
+-- This is because during MVU, items in pg_class an pg_proc could have the same OBJECT_ID
+-- Some tests have no way of identification except for OBJECT_ID since definition will be missing
+-- These will be tested in the regular JDBC test
 
+-- DISABLED DUE TO NON-UNIQUE OBJECT_ID
+-- Test for system function
+-- SELECT
+--     definition,
+--     uses_ansi_nulls,
+--     uses_quoted_identifier,
+--     is_schema_bound,
+--     uses_database_collation,
+--     is_recompiled,
+--     null_on_null_input,
+--     execute_as_principal_id,
+--     uses_native_compilation
+-- FROM sys.system_sql_modules
+-- WHERE object_id = OBJECT_ID('sys.fn_listextendedproperty')
+-- GO
+
+-- DISABLED DUE TO NON-UNIQUE OBJECT_ID
 -- Test for system views
-SELECT
-    uses_ansi_nulls,
-    uses_quoted_identifier,
-    is_schema_bound,
-    uses_database_collation,
-    is_recompiled,
-    null_on_null_input,
-    execute_as_principal_id,
-    uses_native_compilation
-FROM sys.system_sql_modules
-WHERE object_id = OBJECT_ID('sys.tables')
-GO
+-- SELECT
+--     definition,
+--     uses_ansi_nulls,
+--     uses_quoted_identifier,
+--     is_schema_bound,
+--     uses_database_collation,
+--     is_recompiled,
+--     null_on_null_input,
+--     execute_as_principal_id,
+--     uses_native_compilation
+-- FROM sys.system_sql_modules
+-- WHERE object_id = OBJECT_ID('sys.tables')
+-- GO
+
 
 -- Test for system proc
-SELECT
-    uses_ansi_nulls,
-    uses_quoted_identifier,
-    is_schema_bound,
-    uses_database_collation,
-    is_recompiled,
-    null_on_null_input,
-    execute_as_principal_id,
-    uses_native_compilation
-FROM sys.system_sql_modules
-WHERE object_id = OBJECT_ID('sys.sp_tables')
-GO
-
--- Test for system function written in c 
 SELECT
     definition,
     uses_ansi_nulls,
@@ -52,5 +48,21 @@ SELECT
     execute_as_principal_id,
     uses_native_compilation
 FROM sys.system_sql_modules
-WHERE object_id = OBJECT_ID('sys.user_name')
+WHERE definition LIKE 'CREATE PROCEDURE sp_tables%'
 GO
+
+-- DISABLED DUE TO NON-UNIQUE OBJECT_ID
+-- Test for system function written in c 
+-- SELECT
+--     definition,
+--     uses_ansi_nulls,
+--     uses_quoted_identifier,
+--     is_schema_bound,
+--     uses_database_collation,
+--     is_recompiled,
+--     null_on_null_input,
+--     execute_as_principal_id,
+--     uses_native_compilation
+-- FROM sys.system_sql_modules
+-- WHERE object_id = OBJECT_ID('sys.user_name')
+-- GO
