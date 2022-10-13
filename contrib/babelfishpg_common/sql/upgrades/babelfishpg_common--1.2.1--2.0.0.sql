7766f18b00e7d7ad49122b53e5aa1eb81a6daaa1
Jungkook Lee <jungkook@amazon.com>
2022-10-13 16:24:24 +0000

    Debug logs
    Signed-off-by: Jungkook Lee <jungkook@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--1.2.1--2.0.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--1.2.1--2.0.0.sql
@@ -1,4 +1,3 @@
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '2.0.0'" to load this file. \quit
 
-checkpoint;
