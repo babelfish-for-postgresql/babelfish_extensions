5e04db5ab7e302d9787bf83f0bab5c9b0f67b926
Avantika Dasgupta <davantik@amazon.com>
2022-10-13 20:05:02 +0000

    Figuring out the upgrade test suite to show previous behavior

--- test/JDBC/input/BABEL-CHECK-CONSTRAINT-before-14_6-vu-prepare.sql
+++ test/JDBC/input/BABEL-CHECK-CONSTRAINT-before-14_6-vu-prepare.sql
@@ -4,3 +4,6 @@ CREATE TABLE [dbo].[alter_table_check_constraint](
 GO
 INSERT [dbo].[alter_table_check_constraint] VALUES ('abc')
 GO
+
+ALTER TABLE [dbo].[alter_table_check_constraint] ADD CONSTRAINT [check_col1] CHECK ([col1] like N'%TP')
+GO
