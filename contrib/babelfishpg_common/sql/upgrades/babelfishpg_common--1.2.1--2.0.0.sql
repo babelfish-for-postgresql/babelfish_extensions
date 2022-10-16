f992df2ef0814e5fbd1c7c42c3005fbcb4223550
Jungkook Lee <jungkook@amazon.com>
2022-10-16 07:53:06 +0000

    Add BabelfishSetNextOid function
    Signed-off-by: Jungkook Lee <jungkook@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--1.2.1--2.0.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--1.2.1--2.0.0.sql
@@ -1,3 +1,19 @@
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '2.0.0'" to load this file. \quit
 
+CREATE OR REPLACE FUNCTION sys.babelfish_set_next_oid(OID)
+RETURNS void
+AS 'babelfishpg_common', 'BabelfishSetNextOid'
+LANGUAGE C;
+
+DO $$
+DECLARE
+    maxOidFromAllObjects INT;
+    oidFromCheckpoint INT;
+BEGIN
+        SELECT next_oid FROM pg_control_checkpoint() INTO oidFromCheckpoint;
+        SELECT max(object_id) FROM sys.all_objects INTO maxOidFromAllObjects;
+        perform sys.babelfish_set_next_oid(GREATEST(oidFromCheckpoint, maxOidFromAllObjects));
+END$$;
+
+DROP FUNCTION sys.babelfish_set_next_oid(OID);
