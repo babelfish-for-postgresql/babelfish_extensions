13957b9a2d4bcce1437ed2ace81defb2d3c80f47
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2022-10-21 22:15:07 +0530

    Incorrect result when selecting from tempdb.sys.objects
    Task: BABEL-3211
    Signed-off-by: Shalini Lohia <lshalini@amazon.com>

--- test/JDBC/input/views/sys-objects-vu-verify.sql
+++ test/JDBC/input/views/sys-objects-vu-verify.sql
@@ -13,7 +13,24 @@ GO
 select parent_object_id from sys.objects where type = 'P' and name = 'sys_objects_vu_prepare_proc1';
 GO
 
+-- Test cross db reference to sys schemas
+select name from sys_objects_vu_prepare_db1.sys.objects order by name;
+GO
+
+use tempdb
+GO
+
+select name, type, type_desc from tempdb.sys.objects order by name;
+GO
+
 USE master
+GO
+
+-- Verify cross db reference, it should show the same rows as displayed by the cross db query above.
+select name from sys_objects_vu_prepare_db1.sys.objects order by name;
+GO
+
+select name, type, type_desc from tempdb.sys.objects order by name;
 GO
 
 select count(*) from sys.objects where type = 'P' and name = 'sys_objects_vu_prepare_proc1';
