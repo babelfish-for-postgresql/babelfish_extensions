ac228b5a1856dcb9cc54d4fd43f58adff8145510
Kushaal Shroff <kushaal@amazon.com>
2022-10-21 07:04:51 +0000

    Update comments

--- contrib/babelfishpg_tsql/src/pltsql_bulkcopy.c
+++ contrib/babelfishpg_tsql/src/pltsql_bulkcopy.c
@@ -2,8 +2,8 @@
  *
  * pltsql_bulkcopy.c		- Bulk Copy for PL/tsql
  *
- * Portions Copyright (c) 2020, AWS
- * Portions Copyright (c) 1996-2018, PostgreSQL Global Development Group
+ * Portions Copyright (c) 2022, AWS
+ * Portions Copyright (c) 1996-2022, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
