674f996e61d538dd4a8b4e45ec079392af5b2138
Shalini Lohia <shalinilohia50@gmail.com>
2022-10-21 11:27:35 +0000

    Add tests to verify the cross db references to sys and information_schema schemas

--- test/JDBC/input/views/sys-objects-vu-verify.sql
+++ test/JDBC/input/views/sys-objects-vu-verify.sql
@@ -13,7 +13,21 @@ GO
 select parent_object_id from sys.objects where type = 'P' and name = 'sys_objects_vu_prepare_proc1';
 GO
 
+-- Test cross db reference to sys schemas
+select name from master.sys.objects order by name;
+GO
+
+select name from sys_objects_vu_prepare_db1.sys.objects order by name;
+GO
+
 USE master
+GO
+
+-- Verify cross db reference, it should show the same rows as displayed by the cross db query above.
+select name from master.sys.objects order by name;
+GO
+
+select name from sys_objects_vu_prepare_db1.sys.objects order by name;
 GO
 
 select count(*) from sys.objects where type = 'P' and name = 'sys_objects_vu_prepare_proc1';
