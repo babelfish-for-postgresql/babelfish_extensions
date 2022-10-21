ad85aa722ebc61d3b047c3f0e37a58a1553efbaf
Justin Jossick <jusjosj@amazon.com>
2022-10-21 21:51:37 +0000

    [BABEl-3347] Allow nulls as first argument to substring
    This changes the way in which null is handled by the substring function
    for the first input.  Initially we raised an error, however this was a
    major difference compared to TSQL which only raises an error on the null
    literal.  With this commit, we will return null instead of raising an
    error on null inputs

---
+++ test/JDBC/input/BABEL-3347-vu-verify.sql
@@ -0,0 +1,28 @@
+-- table variable
+SELECT * FROM babel_3347_vu1
+GO
+
+-- null constant
+SELECT * FROM babel_3347_vu2
+GO
+
+-- function that returns null
+SELECT * FROM babel_3347_vu3
+GO
+
+-- table for substring start index
+SELECT * FROM babel_3347_vu4
+GO
+
+-- table for substring end index
+SELECT * FROM babel_3347_vu5
+GO
+
+-- cast BABEL-3599
+SELECT * FROM babel_3347_vu6
+GO
+
+-- varibles BABEL-3599
+DECLARE @babel_3347_vu7_var NVARCHAR(MAX) = NULL
+SELECT SUBSTRING(@babel_3347_vu7_var, 2,3)
+GO
