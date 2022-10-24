ed1451945b4b3b75172cff3381abbc35c656955c
jungkook <2jungkook@users.noreply.github.com>
2022-10-24 16:14:33 -0700

    Avoid duplicate OBJECT_IDs after MVU (#693)
    PG allows duplicated OIDs in different catalog tables. However, in Babelfish, we use PG's OID as OBJECT_ID in all system catalog views and it should be unique within a logical database (https://github.com/MicrosoftDocs/sql-docs/blob/live/docs/relational-databases/system-catalog-views/sys-all-objects-transact-sql.md). Duplicate OIDs can break SQL server tools, e.g., SSMS, as well as customer applications.
    
    In PG/Babelfish, duplicate OIDs can occur in the following two cases:
    - After MVU (Major Version Upgrade)
    - After hitting OID wraparound
    
    This commit is to prevent duplicate OIDs during MVU and consists of two parts:
    
    First of all, from the old cluster, it finds the max OID in the 5 catalog tables: pg_extension, pg_authid, pg_enum, pg_class and pg_type. And then pass the max OID to the new cluster as a form of GUC, babelfishpg_tsql.dump_restore_min_oid. In the new cluster, set the nextOid greater than the max OID from the old cluster using GetNewObjectId_hook.
    
    Secondly, in pg_upgrade.c, it skips the step of transferring the next OID from the old cluster to the new cluster.
    
    Also note that the type of babelfishpg_tsql.dump_restore_min_oid is defined by DefineCustomStringVariable() to support UINT_MAX.
    
    Also, this commit contains minor improvements on the pg-upgrade Github action and dev-tools.sh:
    - pg-upgrade Github action: add retain option to see pg_upgrade log even if pg_upgrade succeeds and jdbc tests fails.
    - dev-tools.sh: developers can choose the migration mode, e.g., ./dev-tools.sh test input multi-db.
    
    Engine PR: babelfish-for-postgresql/postgresql_modified_for_babelfish#54
    
    Task: BABEL-3613
    Signed-off-by: Jungkook Lee jungkook@amazon.com

---
+++ test/JDBC/input/BABEL-3613-vu-verify.sql
@@ -0,0 +1,31 @@
+USE MASTER
+GO
+
+select current_setting('babelfishpg_tsql.dump_restore_min_oid');
+go
+
+-- wrong value
+select set_config('babelfishpg_tsql.dump_restore_min_oid', 'wrong value', false);
+go
+
+-- invalid OID
+select set_config('babelfishpg_tsql.dump_restore_min_oid', 0, false);
+go
+
+-- uint32 max should be allowed
+select set_config('babelfishpg_tsql.dump_restore_min_oid', 4294967295, false);
+go
+
+-- out of range
+select set_config('babelfishpg_tsql.dump_restore_min_oid', 4294967296, false);
+go
+
+-- There should not have any duplicated object_ids
+select object_id, name, schema_id, type_desc from sys.all_objects
+where 
+      object_id in (select object_id from sys.all_objects group by object_id having count(object_id) > 1)
+
+      -- should be removed after fixing duplicated object_ids for Table Type and Table Valued Functions
+      and type not in ('U', 'TT')
+order by object_id, name;
+GO
