c6189cd5ce3cfb8e991f462a3e1ac0f592c6eda0
Colin <70862826+ColinKYuen@users.noreply.github.com>
2022-10-24 17:30:40 -0700

    PSQLODBC Datatype Test for TIME (#720)
    Adds ODBC Breakthrough Datatype Test for TIME:
    Create Table
    Insert valid values
    Insert invalid values
    Update table with valid values
    Update table with invalid values
    Comparison Operators
    Table View
    Unique Constraint
    Composite Key
    
    Task: BABELFISH-577
    Signed-off-by: Colin Yuen yuenhcol@amazon.com

--- test/odbc/psqlodbc/time.cpp
+++ test/odbc/psqlodbc/time.cpp
@@ -1,21 +1,29 @@
 #include "psqlodbc_tests_common.h"
 
-const string BBF_TABLE_NAME = "master.dbo.bytea_table_odbc_test";
+const string BBF_TABLE_NAME = "master.dbo.time_table_odbc_test";
 // For BBF Connection
 //   Cannot prepend database name when creating/dropping view
 //   Must prepend database name when selecting from view
-const string BBF_VIEW_NAME = "dbo.bytea_view_odbc_test";
-const string PG_TABLE_NAME = "master_dbo.bytea_table_odbc_test";
-const string PG_VIEW_NAME = "master_dbo.bytea_view_odbc_test";
+const string BBF_VIEW_NAME = "dbo.time_view_odbc_test";
+const string PG_TABLE_NAME = "master_dbo.time_table_odbc_test";
+const string PG_VIEW_NAME = "master_dbo.time_view_odbc_test";
 const string COL1_NAME = "pk";
 const string COL2_NAME = "data";
-const string DATATYPE_NAME = "bytea";
+const string DATATYPE_NAME = "time";
 const vector<pair<string, string>> TABLE_COLUMNS = {
   {COL1_NAME, "INT PRIMARY KEY"},
   {COL2_NAME, DATATYPE_NAME}
-};
+};  
+// Depends on scale & connection
+// BBF will use all bytes as it pads with 0s
+// PG will return only necesaary bytes
+const int TIME_BYTES_EXPECTED = 16;
 
-class PSQL_DataTypes_ByteA : public testing::Test {
+// NOTE: Ubuntu 20.X, retrieval of data is INCORRECT
+//       If data, time, has milliseconds, 
+//       PG connection will only retrieve the milliseconds
+//       Thus, cases with milliseconds are commented out.
+class PSQL_DataTypes_Time : public testing::Test {
   void SetUp() override {
     if (!Drivers::DriverExists(ServerType::PSQL)) {
       GTEST_SKIP() << "PSQL Driver not present: skipping all tests for this fixture.";
@@ -51,44 +59,48 @@ class PSQL_DataTypes_ByteA : public testing::Test {
   }
 };
 
-string stringToHex(string input) {
-    std::ostringstream result;
-    result << std::setw(2) << std::setfill('0') << std::hex << std::uppercase;
-    string::iterator start = input.begin();
-    if (input.find('\\') != string::npos) {
-      start = std::next(start, 1);
-    }
-    std::copy(start, input.end(), std::ostream_iterator<unsigned int>(result, ""));
-    return result.str();
+string stringToTime(const string &input) {
+  string ret = string(input);
+  if (strcmp(input.data(), "NULL") == 0) {
+    return ret;
+  }
+
+  size_t period_pos = ret.find('.');
+  if (period_pos == std::string::npos) {
+    ret.append(".");
+  }
+  unsigned int padding = TIME_BYTES_EXPECTED - ret.length();
+  ret.append(string(padding, '0'));
+  return ret;
 }
 
-vector<string> getExpectedResults_Bytea(const vector<string> &input) {
+vector<string> getExpectedResults_Time(const vector<string> &input) {
   vector<string> ret = {};
 
   for (int i = 0; i < input.size(); i++) {
-    ret.push_back(stringToHex(input[i]));
+    ret.push_back(stringToTime(input[i]));
   }
 
   return ret;
 }
 
-TEST_F(PSQL_DataTypes_ByteA, Table_Creation) {
+TEST_F(PSQL_DataTypes_Time, Table_Creation) {
   createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS);
 
-  const vector<int> BBF_LENGTH_EXPECTED = {10, 0};
-  const vector<int> BBF_PRECISION_EXPECTED = {10, 0};
-  const vector<int> BBF_SCALE_EXPECTED = {0, 0};
-  const vector<string> BBF_NAME_EXPECTED = {"int", "varbinary"};
+  const vector<int> BBF_LENGTH_EXPECTED = {10, 16};
+  const vector<int> BBF_PRECISION_EXPECTED = {10, 7};
+  const vector<int> BBF_SCALE_EXPECTED = {0, 7};
+  const vector<string> BBF_NAME_EXPECTED = {"int", "time"};
 
   testCommonColumnAttributes(ServerType::MSSQL, BBF_TABLE_NAME, 
     TABLE_COLUMNS.size(), COL1_NAME, 
     BBF_LENGTH_EXPECTED, BBF_PRECISION_EXPECTED, 
     BBF_SCALE_EXPECTED, BBF_NAME_EXPECTED);
 
-  const vector<int> PG_LENGTH_EXPECTED = {4, -4};
-  const vector<int> PG_PRECISION_EXPECTED = {0, 0};
+  const vector<int> PG_LENGTH_EXPECTED = {4, 8};
+  const vector<int> PG_PRECISION_EXPECTED = {0, 6};
   const vector<int> PG_SCALE_EXPECTED = {0, 0};
-  const vector<string> PG_NAME_EXPECTED = {"int4", "bytea"};
+  const vector<string> PG_NAME_EXPECTED = {"int4", "time"};
 
   testCommonColumnAttributes(ServerType::PSQL, PG_TABLE_NAME, 
     TABLE_COLUMNS.size(), COL1_NAME, 
@@ -99,19 +111,32 @@ TEST_F(PSQL_DataTypes_ByteA, Table_Creation) {
   dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
 }
 
-TEST_F(PSQL_DataTypes_ByteA, Insertion_Success) {
+// BBF 
+//    Supports 7th digit fractional second
+//    pads fractional seconds with 0s
+//    Length is always 16 (due to padding)
+// PG 
+//    Rounds 7th digit fractional second
+//    does NOT pad
+//    length is dynamic due to not having automatic padding
+TEST_F(PSQL_DataTypes_Time, Insertion_Success) {
   vector<string> inserted_values = {
     "NULL",
-    "",
-    "A",
-    "123",
-    // Outside normal printable range of 32-126
-    "\\\\01f",  // 31
-    "\\\\07f"   // 127
+    "00:00:00",         // Min
+    "12:34:56",
+    // "23:59:59.1",
+    // "23:59:59.22",
+    // "23:59:59.333",
+    // "23:59:59.4444",
+    // "23:59:59.55555",
+    // "23:59:59.666666",
+    // BBF Supports fractional seconds to 7 places
+    // PG Rounds up the 7th digit
+    // "23:59:59.7777777",  // Max
   };
   const int NUM_OF_DATA = inserted_values.size();
 
-  vector<string> expected_values = getExpectedResults_Bytea(inserted_values);
+  vector<string> expected_values = getExpectedResults_Time(inserted_values);
 
   createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS);
 
@@ -121,37 +146,65 @@ TEST_F(PSQL_DataTypes_ByteA, Insertion_Success) {
   inserted_values = duplicateElements(inserted_values);
   expected_values = duplicateElements(expected_values);
 
-  verifyValuesInObject(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, inserted_values, expected_values, 0, true);
-  verifyValuesInObject(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, inserted_values, expected_values, 0, true);
+  verifyValuesInObject(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, inserted_values, inserted_values);
+  verifyValuesInObject(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, inserted_values, expected_values);
 
   dropObject(ServerType::PSQL, "TABLE", PG_TABLE_NAME);
   dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
 }
 
-TEST_F(PSQL_DataTypes_ByteA, Update_Success) {
-  const vector<string> INSERTED_VALUES = {
-    "123"
+TEST_F(PSQL_DataTypes_Time, Insertion_Fail) {
+  RETCODE rcode;
+  OdbcHandler BBF_odbcHandler(Drivers::GetDriver(ServerType::MSSQL));
+  OdbcHandler PG_odbcHandler(Drivers::GetDriver(ServerType::PSQL));
+
+  const vector<string> INVALID_INSERTED_VALUES = {
+    // "24:00:0",    // Hour over range
+    "00:60:0",    // Minute over range
+    // "00:00:60",   // Second over range
+    "00-00-00",   // Format hh-mm-ss
+
+    // ODBC API format for Time is hh:mm:ss
+    // Below are valid in SQL Server
+    "00:00:00.00 AM"
+    "00AM"
+    "00 AM"
   };
-  const vector<string> EXPECTED_VALUES = getExpectedResults_Bytea(INSERTED_VALUES);
+
+  createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS);
+
+  testInsertionFailure(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INVALID_INSERTED_VALUES, false);
+  testInsertionFailure(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, INVALID_INSERTED_VALUES, false);
+
+  dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
+  dropObject(ServerType::PSQL, "TABLE", PG_TABLE_NAME);
+}
+
+TEST_F(PSQL_DataTypes_Time, Update_Success) {
+  const vector<string> INSERTED_VALUES = {
+    "00:00:00"
+  };
+  const vector<string> EXPECTED_VALUES = getExpectedResults_Time(INSERTED_VALUES);
 
   const vector<string> UPDATED_VALUES = {
     "NULL",
-    "",
-    "A",
-    // Outside normal printable range of 32-126
-    "\\\\01f",  // 31
-    "\\\\07f",  // 127
-    // Normal range
-    "123"
+    "12:34:56",
+    // "23:59:59.1",
+    // "23:59:59.22",
+    // "23:59:59.333",
+    // "23:59:59.4444",
+    // "23:59:59.55555",
+    // "23:59:59.666666",
+    "00:00:00"
   };
-  const vector<string> EXPECTED_UPDATED_VALUES = getExpectedResults_Bytea(UPDATED_VALUES);
+  const vector<string> EXPECTED_UPDATED_VALUES = getExpectedResults_Time(UPDATED_VALUES);
 
   createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS);
 
   testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES);
 
-  testUpdateSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, COL2_NAME, UPDATED_VALUES, EXPECTED_UPDATED_VALUES, true);
-  testUpdateSuccess(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, COL2_NAME, UPDATED_VALUES, EXPECTED_UPDATED_VALUES, true);
+  testUpdateSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, COL2_NAME, UPDATED_VALUES, EXPECTED_UPDATED_VALUES);
+  testUpdateSuccess(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, COL2_NAME, UPDATED_VALUES, UPDATED_VALUES);
 
   verifyValuesInObject(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES);
 
@@ -159,19 +212,39 @@ TEST_F(PSQL_DataTypes_ByteA, Update_Success) {
   dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
 }
 
-TEST_F(PSQL_DataTypes_ByteA, View_creation) {
+TEST_F(PSQL_DataTypes_Time, Update_Fail) {
+  const vector<string> INSERTED_VALUES = {
+    "00:00:00"
+  };
+  const vector<string> DATA_EXPECTED = getExpectedResults_Time(INSERTED_VALUES);
+
+  const vector<string> UPDATED_VALUES = {
+    "24:60:60"
+  };
+
+  createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS);
+
+  testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, DATA_EXPECTED);
+
+  testUpdateFail(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, COL2_NAME, DATA_EXPECTED, UPDATED_VALUES);
+  testUpdateFail(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, COL2_NAME, INSERTED_VALUES, UPDATED_VALUES);
+
+  dropObject(ServerType::PSQL, "TABLE", PG_TABLE_NAME);
+  dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
+}
+
+TEST_F(PSQL_DataTypes_Time, View_Creation) {
   const vector<string> INSERTED_VALUES = {
     "NULL",
-    "",
-    "A",
-    "123",
-    // Outside normal printable range of 32-126
-    "\\\\01f",  // 31
-    "\\\\07f"   // 127
+    "00:00:00",
+    "12:34:56",
+    // "23:59:59.1",
+    // "23:59:59.22",
+    // "23:59:59.333"
   };
-  const int NUM_OF_DATA = INSERTED_VALUES.size();
+  const int NUM_OF_INSERTS = INSERTED_VALUES.size();
 
-  const vector<string> EXPECTED_VALUES = getExpectedResults_Bytea(INSERTED_VALUES);
+  const vector<string> EXPECTED_VALUES = getExpectedResults_Time(INSERTED_VALUES);
 
   const string BBF_VIEW_QUERY = "SELECT * FROM " + BBF_TABLE_NAME;
   const string PG_VIEW_QUERY = "SELECT * FROM " + PG_TABLE_NAME;
@@ -182,8 +255,8 @@ TEST_F(PSQL_DataTypes_ByteA, View_creation) {
 
   createView(ServerType::MSSQL, BBF_VIEW_NAME, BBF_VIEW_QUERY);
 
-  verifyValuesInObject(ServerType::MSSQL, BBF_VIEW_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES, 0, true);
-  verifyValuesInObject(ServerType::PSQL, PG_VIEW_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES, 0, true);
+  verifyValuesInObject(ServerType::MSSQL, BBF_VIEW_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES);
+  verifyValuesInObject(ServerType::PSQL, PG_VIEW_NAME, COL1_NAME, INSERTED_VALUES, INSERTED_VALUES);
 
   dropObject(ServerType::MSSQL, "VIEW", BBF_VIEW_NAME);
   dropObject(ServerType::PSQL, "VIEW", PG_VIEW_NAME);
@@ -191,7 +264,7 @@ TEST_F(PSQL_DataTypes_ByteA, View_creation) {
   dropObject(ServerType::PSQL, "TABLE", PG_TABLE_NAME);
 }
 
-TEST_F(PSQL_DataTypes_ByteA, Table_Single_Primary_Keys) {
+TEST_F(PSQL_DataTypes_Time, Table_Single_Primary_Keys) {
   const vector<pair<string, string>> TABLE_COLUMNS = {
     {COL1_NAME, "INT"},
     {COL2_NAME, DATATYPE_NAME}
@@ -208,23 +281,19 @@ TEST_F(PSQL_DataTypes_ByteA, Table_Single_Primary_Keys) {
   string tableConstraints = createTableConstraint("PRIMARY KEY ", PK_COLUMNS);
 
   const vector<string> INSERTED_VALUES = {
-    "",
-    "A",
-    "123",
-    // Outside normal printable range of 32-126
-    "\\\\01f",  // 31
-    "\\\\07f"   // 127
+    "00:00:00",
+    "12:34:56"
   };
   const int NUM_OF_DATA = INSERTED_VALUES.size();
 
-  const vector<string> EXPECTED_VALUES = getExpectedResults_Bytea(INSERTED_VALUES);
+  const vector<string> EXPECTED_VALUES = getExpectedResults_Time(INSERTED_VALUES);
 
   createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS, tableConstraints);
 
   testPrimaryKeys(ServerType::MSSQL, BBF_SCHEMA_NAME, TABLE_NAME, PK_COLUMNS);
   testPrimaryKeys(ServerType::PSQL, SCHEMA_NAME, TABLE_NAME, PK_COLUMNS);
 
-  testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES, 0, true);
+  testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES);
 
   testInsertionFailure(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, false, NUM_OF_DATA, false);
   testInsertionFailure(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, INSERTED_VALUES, false, NUM_OF_DATA, false);
@@ -233,7 +302,7 @@ TEST_F(PSQL_DataTypes_ByteA, Table_Single_Primary_Keys) {
   dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
 }
 
-TEST_F(PSQL_DataTypes_ByteA, Table_Composite_Keys) {
+TEST_F(PSQL_DataTypes_Time, Table_Composite_Keys) {
   const vector<pair<string, string>> TABLE_COLUMNS = {
     {COL1_NAME, "INT"},
     {COL2_NAME, DATATYPE_NAME}
@@ -251,23 +320,19 @@ TEST_F(PSQL_DataTypes_ByteA, Table_Composite_Keys) {
   string tableConstraints = createTableConstraint("PRIMARY KEY ", PK_COLUMNS);
 
   const vector<string> INSERTED_VALUES = {
-    "",
-    "A",
-    "123",
-    // Outside normal printable range of 32-126
-    "\\\\01f",  // 31
-    "\\\\07f"   // 127
+    "00:00:00",
+    "12:34:56"
   };
   const int NUM_OF_DATA = INSERTED_VALUES.size();
 
-  const vector<string> EXPECTED_VALUES = getExpectedResults_Bytea(INSERTED_VALUES);
+  const vector<string> EXPECTED_VALUES = getExpectedResults_Time(INSERTED_VALUES);
 
   createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS, tableConstraints);
 
   testPrimaryKeys(ServerType::MSSQL, BBF_SCHEMA_NAME, TABLE_NAME, PK_COLUMNS);
   testPrimaryKeys(ServerType::PSQL, SCHEMA_NAME, TABLE_NAME, PK_COLUMNS);
 
-  testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES, 0, true);
+  testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES);
 
   testInsertionFailure(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, false, 0, false);
   testInsertionFailure(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, INSERTED_VALUES, false, 0, false);
@@ -276,7 +341,7 @@ TEST_F(PSQL_DataTypes_ByteA, Table_Composite_Keys) {
   dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
 }
 
-TEST_F(PSQL_DataTypes_ByteA, Table_Unique_Constraint) {
+TEST_F(PSQL_DataTypes_Time, Table_Unique_Constraint) {
   const vector<pair<string, string>> TABLE_COLUMNS = {
     {COL1_NAME, "INT"},
     {COL2_NAME, DATATYPE_NAME}
@@ -293,23 +358,19 @@ TEST_F(PSQL_DataTypes_ByteA, Table_Unique_Constraint) {
   string tableConstraints = createTableConstraint("UNIQUE", UNIQUE_COLUMNS);
 
   const vector<string> INSERTED_VALUES = {
-    "",
-    "A",
-    "123",
-    // Outside normal printable range of 32-126
-    "\\\\01f",  // 31
-    "\\\\07f"   // 127
+    "00:00:00",
+    "12:34:56"
   };
   const int NUM_OF_DATA = INSERTED_VALUES.size();
 
-  const vector<string> EXPECTED_VALUES = getExpectedResults_Bytea(INSERTED_VALUES);
+  const vector<string> EXPECTED_VALUES = getExpectedResults_Time(INSERTED_VALUES);
 
   createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS, tableConstraints);
 
   testUniqueConstraint(ServerType::MSSQL, TABLE_NAME, UNIQUE_COLUMNS);
   testUniqueConstraint(ServerType::PSQL, TABLE_NAME, UNIQUE_COLUMNS);
 
-  testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES, 0, true);
+  testInsertionSuccess(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, EXPECTED_VALUES);
 
   testInsertionFailure(ServerType::MSSQL, BBF_TABLE_NAME, COL1_NAME, INSERTED_VALUES, false, NUM_OF_DATA, false);
   testInsertionFailure(ServerType::PSQL, PG_TABLE_NAME, COL1_NAME, INSERTED_VALUES, false, NUM_OF_DATA, false);
@@ -318,22 +379,28 @@ TEST_F(PSQL_DataTypes_ByteA, Table_Unique_Constraint) {
   dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
 }
 
-TEST_F(PSQL_DataTypes_ByteA, Comparison_Operators) {
+TEST_F(PSQL_DataTypes_Time, Comparison_Operators) {
   const vector<pair<string, string>> TABLE_COLUMNS = {
     {COL1_NAME, DATATYPE_NAME + " PRIMARY KEY"},
     {COL2_NAME, DATATYPE_NAME}
   };
 
   const vector<string> INSERTED_PK = {
-    "A",
-    "123",
-    "smaller"
+    "12:34:56",         // A > B
+    "00:00:00",         // A < B
+    "11:11:11",         // A = B
+    // "00:00:00.654321",  // A > B
+    // "00:00:00.123456",  // A < B
+    // "00:00:00.111111"   // A = B
   };
 
   const vector<string> INSERTED_DATA = {
-    "B",
-    "123",
-    "LARGER"
+    "11:34:56",         // A > B
+    "00:00:01",         // A < B
+    "11:11:11",         // A = B
+    // "00:00:00.123456",  // A > B
+    // "00:00:00.654321",  // A < B
+    // "00:00:00.111111"   // A = B
   };
   const int NUM_OF_DATA = INSERTED_DATA.size();
 
@@ -390,4 +457,56 @@ TEST_F(PSQL_DataTypes_ByteA, Comparison_Operators) {
 
   dropObject(ServerType::PSQL, "TABLE", PG_TABLE_NAME);
   dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
+}
+
+TEST_F(PSQL_DataTypes_Time, Comparison_Functions) {
+  const vector<string> INSERTED_DATA = {
+    "00:00:00",
+    "23:59:59",
+    "12:34:56",
+    // "23:59:59.999999"
+  };
+  const size_t NUM_OF_DATA = INSERTED_DATA.size();
+
+  // insertString initialization
+  string insertString{};
+  string comma{};
+  for (int i = 0; i < NUM_OF_DATA; i++) {
+    insertString += comma + "(" + std::to_string(i) + ",\'" + INSERTED_DATA[i] + "\')";
+    comma = ",";
+  }
+
+  const vector<string> OPERATIONS_QUERY = {
+    "MIN(" + COL2_NAME + ")",
+    "MAX(" + COL2_NAME + ")"
+  };
+  const int NUM_OF_OPERATIONS = OPERATIONS_QUERY.size();
+
+  // initialization of expected_results
+  vector<string> bbf_expected_results = {};
+  vector<string> pg_expected_results = {};
+  int min_expected = 0, max_expected = 0;
+
+  for (int i = 1; i < NUM_OF_DATA; i++) {
+    const char *currMin = INSERTED_DATA[min_expected].data();
+    const char *currMax = INSERTED_DATA[max_expected].data();
+    const char *curr = INSERTED_DATA[i].data();
+
+    min_expected = strcmp(curr, currMin) < 0 ? i : min_expected;
+    max_expected = strcmp(curr, currMax) > 0 ? i : max_expected;
+  }
+  bbf_expected_results.push_back(stringToTime(INSERTED_DATA[min_expected]));
+  bbf_expected_results.push_back(stringToTime(INSERTED_DATA[max_expected]));
+  pg_expected_results.push_back(INSERTED_DATA[min_expected]);
+  pg_expected_results.push_back(INSERTED_DATA[max_expected]);
+
+  createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS);
+
+  insertValuesInTable(ServerType::MSSQL, BBF_TABLE_NAME, insertString, NUM_OF_DATA);
+
+  testComparisonFunctions(ServerType::MSSQL, BBF_TABLE_NAME, OPERATIONS_QUERY, bbf_expected_results);
+  testComparisonFunctions(ServerType::PSQL, PG_TABLE_NAME, OPERATIONS_QUERY, pg_expected_results);
+
+  dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
+  dropObject(ServerType::PSQL, "TABLE", PG_TABLE_NAME);
 }
