a2e86f19334ce0183b2095f97de9838a62561952
pratikzode <pratik.zode@gmail.com>
2022-10-25 20:22:39 +0000

    Indentation Aligned
    Task:BABEL-2967
    Signed-off-by: pratikzode <pratik.zode@gmail.com>

--- test/JDBC/input/BABEL-2967.sql
+++ test/JDBC/input/BABEL-2967.sql
@@ -1,11 +1,10 @@
-select current_setting( 'babelfishpg_tsql.enable_metadata_inconsistency_check')
+SELECT current_setting( 'babelfishpg_tsql.enable_metadata_inconsistency_check')
 GO
-
 
 SELECT SET_CONFIG('babelfishpg_tsql.enable_metadata_inconsistency_check','off',true)
 GO
 
- select current_setting( 'babelfishpg_tsql.enable_metadata_inconsistency_check')
+SELECT current_setting( 'babelfishpg_tsql.enable_metadata_inconsistency_check')
 GO
 
 SELECT SET_CONFIG('babelfishpg_tsql.enable_metadata_inconsistency_check','false',true)
