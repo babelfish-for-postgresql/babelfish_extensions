f20974526592bb58e42b4b826e223b2d972356e6
Sai Rohan Basa <108261379+basasairohan@users.noreply.github.com>
2022-10-27 14:37:46 +0530

    Added missing condition in sys.all_objects view definition (#736)
    * Added missing condition in sys.all_objects view definition
    
    * Added tests in BABEL-3613 upgrade script
    
    * Modified expected output files
    
    * Removed duplicate definition of sys.all_objects in upgrade script.

--- test/JDBC/input/BABEL-3613-vu-verify.sql
+++ test/JDBC/input/BABEL-3613-vu-verify.sql
@@ -24,8 +24,5 @@ go
 select object_id, name, schema_id, type_desc from sys.all_objects
 where 
       object_id in (select object_id from sys.all_objects group by object_id having count(object_id) > 1)
-
-      -- should be removed after fixing duplicated object_ids for Table Type and Table Valued Functions
-      and type not in ('U', 'TT')
 order by object_id, name;
 GO
