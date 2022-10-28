1b2aee6798624adb02494486afc135b5dcd5d26f
Toby Cao <tobyc@bitquilltech.com>
2022-10-28 14:46:20 -0700

    Comparison operator test case added, add string_operator function
    Signed-off-by: Toby Cao <tobyc@bitquilltech.com>

--- test/odbc/psqlodbc/test_pg_ntext.cpp
+++ test/odbc/psqlodbc/test_pg_ntext.cpp
@@ -1,7 +1,5 @@
 #include "psqlodbc_tests_common.h"
 
-using std::pair;
-
 const string TABLE_NAME = "master_dbo.ntext_table_odbc_test";
 const string COL1_NAME = "pk";
 const string COL2_NAME = "data";
@@ -285,13 +283,15 @@ TEST_F(PSQL_DataTypes_Ntext, Comparison_Operators) {
   };
 
   const vector<string> INSERTED_PK = {
-    "one Two",
-    "three Four"
+    "One",
+    "BBBB",
+    "CCCC"
   };
 
   const vector<string> INSERTED_DATA = {
-    "Five Six",
-    "Seven Eight"
+    "One",
+    "AAAA",
+    "DDDD"
   };
   const int NUM_OF_DATA = INSERTED_DATA.size();
 
@@ -334,49 +334,54 @@ TEST_F(PSQL_DataTypes_Ntext, Comparison_Operators) {
   dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
 }
 
-// TEST_F(PSQL_DataTypes_Ntext, DISABLED_String_Operators) {
+TEST_F(PSQL_DataTypes_Ntext, String_Operators) {
 
-//   const vector<string> INSERTED_DATA = {
-//     "  One Two!"
-//   };
+  const vector<string> INSERTED_DATA = {
+    "  One Two!"
+  };
 
-//   const vector<string> INSERTED_PK = {
-//     "1"
-//   };
+  const vector<string> INSERTED_PK = {
+    "1"
+  };
 
-//   const int NUM_OF_DATA = INSERTED_DATA.size();
+  const int NUM_OF_DATA = INSERTED_DATA.size();
   
-//   // insertString initialization
-//   string insertString{};
-//   string comma{};
-//   for (int i = 0; i < NUM_OF_DATA; i++) {
-//     insertString += comma + "(" + INSERTED_PK[i] + ",\'" + INSERTED_DATA[i] + "\')";
-//     comma = ",";
-//   }
+  // insertString initialization
+  string insertString{};
+  string comma{};
+  for (int i = 0; i < NUM_OF_DATA; i++) {
+    insertString += comma + "(" + INSERTED_PK[i] + ",\'" + INSERTED_DATA[i] + "\')";
+    comma = ",";
+  }
 
-//   const vector<string> OPERATIONS_QUERY = {
-//     "lower(" + COL2_NAME + ")",
-//     "upper(" + COL2_NAME + ")",
-//     COL1_NAME +"||" + COL2_NAME,
-//     "Trim(" + COL2_NAME + ")",
-//     "Trim(TRAILING '!' from " + COL2_NAME + ")"
+  const vector<string> OPERATIONS_QUERY = {
+    "lower(" + COL2_NAME + ")",
+    "upper(" + COL2_NAME + ")",
+    COL1_NAME +"||" + COL2_NAME,
+    "Trim(" + COL2_NAME + ")",
+    "Trim(TRAILING '!' from " + COL2_NAME + ")",
+    "Trim(TRAILING ' ' from " + COL2_NAME + ")"
+  };
+  const int NUM_OF_OPERATIONS = OPERATIONS_QUERY.size();
 
-//   };
-//   const int NUM_OF_OPERATIONS = OPERATIONS_QUERY.size();
-
-//   // initialization of EXPECTED_RESULTS
-//   vector<vector<string>> EXPECTED_RESULTS = {{}};
+  // initialization of EXPECTED_RESULTS
+  vector<vector<string>> EXPECTED_RESULTS = {};
   
-//   string current = INSERTED_DATA[0];
-//   transform(current.begin(), current.end(), current.begin(), ::tolower);
-//   EXPECTED_RESULTS[0].push_back(current);
-//   EXPECTED_RESULTS[0].push_back("  ONE TWO!");
-//   EXPECTED_RESULTS[0].push_back(INSERTED_PK[0] + INSERTED_DATA[0]);
-//   EXPECTED_RESULTS[0].push_back("One Two!");
-//   EXPECTED_RESULTS[0].push_back("  One Two");
+  for(int i = 0; i < NUM_OF_OPERATIONS; i++){
+    EXPECTED_RESULTS.push_back({});
+  }
+
+  string current = INSERTED_DATA[0];
+  transform(current.begin(), current.end(), current.begin(), ::tolower);
+  EXPECTED_RESULTS[0].push_back(current);
+  EXPECTED_RESULTS[1].push_back("  ONE TWO!");
+  EXPECTED_RESULTS[2].push_back(INSERTED_PK[0] + INSERTED_DATA[0]);
+  EXPECTED_RESULTS[3].push_back("One Two!");
+  EXPECTED_RESULTS[4].push_back("  One Two");
+  EXPECTED_RESULTS[5].push_back("  One Two!");
   
-//   createTable(ServerType::PSQL, TABLE_NAME, TABLE_COLUMNS);
-//   insertValuesInTable(ServerType::PSQL, TABLE_NAME, insertString, NUM_OF_DATA);
-//   testStringFunctions(ServerType::PSQL, TABLE_NAME, OPERATIONS_QUERY, EXPECTED_RESULTS, INSERTED_PK, COL1_NAME);
-//   dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
-// }
+  createTable(ServerType::PSQL, TABLE_NAME, TABLE_COLUMNS);
+  insertValuesInTable(ServerType::PSQL, TABLE_NAME, insertString, NUM_OF_DATA);
+  testStringFunctions(ServerType::PSQL, TABLE_NAME, OPERATIONS_QUERY, EXPECTED_RESULTS, NUM_OF_DATA, COL1_NAME);
+  dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
+}
