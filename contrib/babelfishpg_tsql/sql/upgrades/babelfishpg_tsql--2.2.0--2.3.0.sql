ec621b5970d3d789ed5cd02726e28dca341a1b41
Jason Teng <jasonten@amazon.com>
2022-11-01 20:10:53 +0000

    Fix upgrade tests
    Signed-off-by: Jason Teng <jasonten@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.2.0--2.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.2.0--2.3.0.sql
@@ -3942,11 +3942,11 @@ VOLATILE;
 -- helper function sys.babelfish_conv_helper_to_datetime(anyelement, bool, numeric) is no longer needed,
 -- as all arguments should be casted to text anyways. Drop the function.
 ALTER FUNCTION sys.babelfish_conv_helper_to_datetime(anyelement, bool, numeric) RENAME TO babelfish_conv_helper_to_datetime_deprecated_in_2_3_0(anyelement, bool, numeric);
-CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'babelfish_conv_helper_to_datetime_deprecated_in_2_3_0(anyelement, bool, numeric)');
+CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'babelfish_conv_helper_to_datetime_deprecated_in_2_3_0');
 
 -- helper function sys.babelfish_try_conv_to_datetime(anyelement) needs to change return type to sys.DATETIME
 ALTER FUNCTION sys.babelfish_try_conv_to_datetime(anyelement) RENAME TO babelfish_try_conv_to_datetime_deprecated_in_2_3_0(anyelement);
-CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'babelfish_try_conv_to_datetime_deprecated_in_2_3_0(anyelement)');
+CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'babelfish_try_conv_to_datetime_deprecated_in_2_3_0');
 
 CREATE OR REPLACE FUNCTION sys.babelfish_try_conv_to_datetime(IN arg anyelement)
 RETURNS sys.DATETIME
