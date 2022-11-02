aea998a92fbabd82ba8dac8a7a244b6749cfd59b
Shalini Lohia <shalinilohia50@gmail.com>
2022-11-02 03:39:27 +0000

    fix the SID

--- contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
+++ contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
@@ -2342,13 +2342,13 @@ BEGIN
 					ELSE Ext2.orig_username END 
 					AS SYS.SYSNAME) AS 'RoleName',
 			   CAST(CASE WHEN Ext1.orig_username = 'dbo' THEN Base4.rolname
-					WHEN Ext1.orig_username = 'guest' THEN CAST(0 AS INT)
 					ELSE Base3.rolname END
 					AS SYS.SYSNAME) AS 'LoginName',
 			   CAST(LogExt.default_database_name AS SYS.SYSNAME) AS 'DefDBName',
 			   CAST(Ext1.default_schema_name AS SYS.SYSNAME) AS 'DefSchemaName',
 			   CAST(Base1.oid AS INT) AS 'UserID',
 			   CAST(CASE WHEN Ext1.orig_username = 'dbo' THEN CAST(Base4.oid AS INT)
+					WHEN Ext1.orig_username = 'guest' THEN CAST(0 AS INT)
 					ELSE CAST(Base3.oid AS INT) END
 					AS SYS.VARBINARY(85)) AS 'SID'
 		FROM sys.babelfish_authid_user_ext AS Ext1
