97eebba0d05ad84abd7b236df51435e1e48cddda
Toby Cao <tobyc@bitquilltech.com>
2022-11-03 13:47:54 -0700

    View creation and UpdateSuccess added
    Signed-off-by: Toby Cao <tobyc@bitquilltech.com>

--- test/odbc/psqlodbc/test/image.cpp
+++ test/odbc/psqlodbc/test/image.cpp
@@ -9,8 +9,10 @@ const string DATATYPE_NAME = "sys.image";
 const string VIEW_NAME = "master_dbo.image_view_odbc_test";
 
 // Relative to where `./build/main` is ran
-const std::string FILE_PATH = "psqlodbc/Images/Black.png";
-static string IMAGE_HEX_STR;
+const std::string INSERTED_FILE_PATH = "psqlodbc/Images/Black.png";
+const std::string UPDATED_FILE_PATH = "psqlodbc/Images/circle.png";
+static string INSERTED_IMAGE_HEX_STR;
+static string UPDATED_IMAGE_HEX_STR;
 
 const vector<pair<string, string>> TABLE_COLUMNS = {
   {COL1_NAME, " int PRIMARY KEY"},
@@ -21,7 +23,7 @@ class PSQL_DataTypes_Image : public testing::Test {
   protected:
     static void SetUpTestSuite() {
       // Load Image
-      std::ifstream infile(FILE_PATH, std::ios_base::binary);
+      std::ifstream infile(INSERTED_FILE_PATH, std::ios_base::binary);
 
       std::vector<char> buffer(
          (std::istreambuf_iterator<char>(infile)),
@@ -30,10 +32,24 @@ class PSQL_DataTypes_Image : public testing::Test {
       char stream_buffer[BUFFER_SIZE] = "";
       for (char c : buffer) {
         sprintf(stream_buffer, "%02x", c);
-        IMAGE_HEX_STR += stream_buffer;
+        INSERTED_IMAGE_HEX_STR += stream_buffer;
       }
 
       infile.close();
+
+      std::ifstream update_infile(UPDATED_FILE_PATH, std::ios_base::binary);
+
+      std::vector<char> update_buffer(
+         (std::istreambuf_iterator<char>(update_infile)),
+         (std::istreambuf_iterator<char>()));
+
+      char update_stream_buffer[BUFFER_SIZE] = "";
+      for (char c : update_buffer) {
+        sprintf(update_stream_buffer, "%02x", c);
+        UPDATED_IMAGE_HEX_STR += update_stream_buffer;
+      }
+
+      update_infile.close();
     }
 
   void SetUp() override {
@@ -70,14 +86,53 @@ TEST_F(PSQL_DataTypes_Image, Table_Creation) {
 }
 
 TEST_F(PSQL_DataTypes_Image, Insertion_Success) {
-  const vector<string> EXPECTED_VALUES = {
-    "0x" + IMAGE_HEX_STR
+  const vector<string> INSERTED_VALUES = {
+    "0x" + INSERTED_IMAGE_HEX_STR
   };
-  const int NUM_OF_INSERTS = EXPECTED_VALUES.size();
-  const string INSERT_STRING = "(0, DECODE('" + IMAGE_HEX_STR + "', 'hex'))";
+
+  const string INSERT_STRING = "(0, DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex'))";
 
   createTable(ServerType::PSQL, TABLE_NAME, TABLE_COLUMNS);
   insertValuesInTable(ServerType::PSQL, TABLE_NAME, INSERT_STRING, 1);
-  verifyValuesInObject(ServerType::PSQL, TABLE_NAME, COL1_NAME, EXPECTED_VALUES, EXPECTED_VALUES);
+  verifyValuesInObject(ServerType::PSQL, TABLE_NAME, COL1_NAME, INSERTED_VALUES, INSERTED_VALUES);
+  dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
+}
+
+TEST_F(PSQL_DataTypes_Image, Update_Success) {
+  const vector<string> INSERTED_VALUES = {
+    "0x" + INSERTED_IMAGE_HEX_STR
+  };
+  const vector<string> UPDATED_VALUES = {
+    "DECODE('" + UPDATED_IMAGE_HEX_STR + "', 'hex')"
+  };
+  const vector<string> EXPECTED_UPDATED_VALUES = {
+    "0x" + UPDATED_IMAGE_HEX_STR 
+  };
+  createTable(ServerType::PSQL, TABLE_NAME, TABLE_COLUMNS);
+  
+  const string INSERTED_STRING = "(0, DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex'))";
+
+  insertValuesInTable(ServerType::PSQL, TABLE_NAME, INSERTED_STRING, 1);
+  verifyValuesInObject(ServerType::PSQL, TABLE_NAME, COL1_NAME, INSERTED_VALUES, INSERTED_VALUES);
+  testUpdateSuccess(ServerType::PSQL, TABLE_NAME, COL1_NAME, COL2_NAME, UPDATED_VALUES, EXPECTED_UPDATED_VALUES, false, true);
+  dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
+}
+
+TEST_F(PSQL_DataTypes_Image, View_creation) {
+ const vector<string> INSERTED_VALUES = {
+    "0x" + INSERTED_IMAGE_HEX_STR
+  };
+
+  const string INSERT_STRING = "(0, DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex'))";
+  const string VIEW_QUERY = "SELECT * FROM " + TABLE_NAME;
+
+  createTable(ServerType::PSQL, TABLE_NAME, TABLE_COLUMNS);
+  insertValuesInTable(ServerType::PSQL, TABLE_NAME, INSERT_STRING, 1);
+  verifyValuesInObject(ServerType::PSQL, TABLE_NAME, COL1_NAME, INSERTED_VALUES, INSERTED_VALUES);
+
+  createView(ServerType::PSQL, VIEW_NAME, VIEW_QUERY);
+  verifyValuesInObject(ServerType::PSQL, VIEW_NAME, COL1_NAME, INSERTED_VALUES, INSERTED_VALUES);
+
+  dropObject(ServerType::PSQL, "VIEW", VIEW_NAME);
   dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
 }
