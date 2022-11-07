4be5f8ffdaa4adeddba174b34521187646b85b06
Sumit Jaiswal <54075285+sumitj824@users.noreply.github.com>
2022-11-07 15:16:22 +0530

    Fix pg_proc cache reference leaks (#802)
    Earlier we were getting cache reference leak warning for pg_proc cache. This commit fix all the pg_proc cache reference leaks.
    
    Task: [BABEL-3664]
    Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

--- contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
+++ contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
@@ -500,8 +500,11 @@ tsql_get_returnTypmodValue(PG_FUNCTION_ARGS){
 
         isfunction = (proc->prokind != PROKIND_PROCEDURE);
 
-        if (!isfunction)  
-                PG_RETURN_INT32(-1) ;
+        if (!isfunction)
+        {
+            ReleaseSysCache(proctup);
+            PG_RETURN_INT32(-1) ;
+        }
 
         tmp = SysCacheGetAttr(PROCOID, proctup, Anum_pg_proc_probin, &isnull);
         number_args = proc->pronargs;
@@ -511,7 +514,8 @@ tsql_get_returnTypmodValue(PG_FUNCTION_ARGS){
 
         if (typmod_arr[number_args-1] != -1)
                typmod_arr[number_args-1] += adjustTypmod(proc->prorettype, typmod_arr[number_args-1]);
-    
+        
+        ReleaseSysCache(proctup);
         PG_RETURN_INT32(typmod_arr[number_args-1]);
 
 }
