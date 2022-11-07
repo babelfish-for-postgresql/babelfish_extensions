4b43685dafeec8e2f58202054e2053fbbb45ee73
Toby Cao <tobyc@bitquilltech.com>
2022-11-07 15:34:43 -0800

    Image Operator test added but disabled due to PG issue, also add varbianry to insertion and update_value
    Signed-off-by: Toby Cao <tobyc@bitquilltech.com>

--- test/odbc/psqlodbc/test/image.cpp
+++ test/odbc/psqlodbc/test/image.cpp
@@ -2,6 +2,12 @@
 #include "../psqlodbc_tests_common.h"
 #include <fstream>
 
+const string BBF_TABLE_NAME = "master.dbo.char_table_odbc_test";
+// For BBF Connection
+//   Cannot prepend database name when creating/dropping view
+//   Must prepend database name when selecting from view
+const string BBF_VIEW_NAME = "dbo.char_view_odbc_test";
+
 const string TABLE_NAME = "master_dbo.image_table_odbc_test";
 const string COL1_NAME = "pk";
 const string COL2_NAME = "data";
@@ -59,6 +65,13 @@ class PSQL_DataTypes_Image : public testing::Test {
 
     OdbcHandler test_setup(Drivers::GetDriver(ServerType::PSQL));
     test_setup.ConnectAndExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
+
+    if (!Drivers::DriverExists(ServerType::MSSQL)) {
+      GTEST_SKIP() << "MSSQL Driver not present: skipping all tests for this fixture.";
+    }
+
+    OdbcHandler bbf_test_setup(Drivers::GetDriver(ServerType::MSSQL));
+    bbf_test_setup.ConnectAndExecQuery(DropObjectStatement("TABLE", BBF_TABLE_NAME));
   }
 
   void TearDown() override {
@@ -70,6 +83,14 @@ class PSQL_DataTypes_Image : public testing::Test {
     test_teardown.ConnectAndExecQuery(DropObjectStatement("VIEW", VIEW_NAME));
     test_teardown.CloseStmt();
     test_teardown.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
+
+    if (!Drivers::DriverExists(ServerType::MSSQL)) {
+      GTEST_SKIP() << "MSSQL Driver not present: skipping all tests for this fixture.";
+    }
+    OdbcHandler bbf_test_setup(Drivers::GetDriver(ServerType::MSSQL));
+    bbf_test_setup.ConnectAndExecQuery(DropObjectStatement("VIEW", BBF_VIEW_NAME));
+    bbf_test_setup.CloseStmt();
+    bbf_test_setup.ExecQuery(DropObjectStatement("TABLE", BBF_TABLE_NAME));
   }
 };
 
@@ -86,40 +107,55 @@ TEST_F(PSQL_DataTypes_Image, Table_Creation) {
 }
 
 TEST_F(PSQL_DataTypes_Image, Insertion_Success) {
-  const vector<string> INSERTED_VALUES = {
-    "0x" + INSERTED_IMAGE_HEX_STR
+  vector<string> INSERTED_VALUES = {
+    "NULL",
+    "00",     // Min
+    "0",      // Min, different format
+    "46",     // Rand
+    "49",     // Rand
+    "02",     // Rand, different format
+    "268435455",  // 7 Bytes
+    "4294967295", // 8 Bytes - 1
+    "4294967295", // 8 Bytes
+    "4294967296" // 8 Bytes + 1
   };
+  vector<string> EXPECTED_VALUES = getExpectedResults_VarBinary(INSERTED_VALUES);
 
-  const int NUM_OF_DATA = INSERTED_VALUES.size();
-
-  const string INSERT_STRING = "(0, DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex'))";
+  INSERTED_VALUES.push_back(" DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex')");
+  EXPECTED_VALUES.push_back("0x" + INSERTED_IMAGE_HEX_STR);
 
   createTable(ServerType::PSQL, TABLE_NAME, TABLE_COLUMNS);
-  insertValuesInTable(ServerType::PSQL, TABLE_NAME, INSERT_STRING, NUM_OF_DATA);
-  verifyValuesInObject(ServerType::PSQL, TABLE_NAME, COL1_NAME, INSERTED_VALUES, INSERTED_VALUES);
+  testInsertionSuccess(ServerType::PSQL, TABLE_NAME, COL1_NAME, 
+                      INSERTED_VALUES, EXPECTED_VALUES, 0, false, true);
   dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
 }
 
 TEST_F(PSQL_DataTypes_Image, Update_Success) {
   const vector<string> INSERTED_VALUES = {
-    "0x" + INSERTED_IMAGE_HEX_STR
+    "NULL"
   };
-  const vector<string> UPDATED_VALUES = {
-    "DECODE('" + UPDATED_IMAGE_HEX_STR + "', 'hex')"
-  };
-  const vector<string> EXPECTED_UPDATED_VALUES = {
-    "0x" + UPDATED_IMAGE_HEX_STR 
-  };
+  const vector<string> EXPECTED_VALUES = getExpectedResults_VarBinary(INSERTED_VALUES);
 
-  const int NUM_OF_DATA = INSERTED_VALUES.size();
+  vector<string> UPDATED_VALUES = {
+    "00",     // Min
+    "0",      // Min, different format
+    "46",     // Rand
+    "49",     // Rand
+    "02",     // Rand, different format
+    "268435455",  // 7 Bytes
+    "4294967295", // 8 Bytes - 1
+    "4294967295", // 8 Bytes
+    "4294967296" // 8 Bytes + 1
+  };
+  vector<string> EXPECTED_UPDATED_VALUES = getExpectedResults_VarBinary(UPDATED_VALUES);
+  UPDATED_VALUES.push_back(" DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex')");
+  EXPECTED_UPDATED_VALUES.push_back("0x" + INSERTED_IMAGE_HEX_STR);
 
   createTable(ServerType::PSQL, TABLE_NAME, TABLE_COLUMNS);
-  
-  const string INSERTED_STRING = "(0, DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex'))";
-
-  insertValuesInTable(ServerType::PSQL, TABLE_NAME, INSERTED_STRING, NUM_OF_DATA);
-  verifyValuesInObject(ServerType::PSQL, TABLE_NAME, COL1_NAME, INSERTED_VALUES, INSERTED_VALUES);
-  testUpdateSuccess(ServerType::PSQL, TABLE_NAME, COL1_NAME, COL2_NAME, UPDATED_VALUES, EXPECTED_UPDATED_VALUES, false, true);
+  testInsertionSuccess(ServerType::PSQL, TABLE_NAME, COL1_NAME, 
+                      INSERTED_VALUES, EXPECTED_VALUES, 0, false, true);
+  testUpdateSuccess(ServerType::PSQL, TABLE_NAME, COL1_NAME, COL2_NAME, 
+                    UPDATED_VALUES, EXPECTED_UPDATED_VALUES, false, true);
   dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
 }
 
@@ -142,4 +178,39 @@ TEST_F(PSQL_DataTypes_Image, View_creation) {
 
   dropObject(ServerType::PSQL, "VIEW", VIEW_NAME);
   dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
+}
+
+
+// BBF supports DATALENGTH() operator for sys.image but PG does not support sys.DATALENGTH()
+// since sys.DATALENGTH() has explicitly support 3 sys schema datatypes char,text and sql_variant
+// and besides those three, all other datatypes under ANYELEMENT could be supported. But PG somehow 
+// have conflicts recognize ANYELEMENT and sys.image, not unique datatype can't be supported
+TEST_F(PSQL_DataTypes_Image, DISABLED_String_Functions) {
+  const vector<string> INSERTED_VALUES = {
+    "0x" + INSERTED_IMAGE_HEX_STR
+  };
+
+  const int NUM_OF_DATA = INSERTED_VALUES.size();
+
+  const string INSERT_STRING = "(0, DECODE('" + INSERTED_IMAGE_HEX_STR + "', 'hex'))";
+
+  const vector<string> OPERATIONS_QUERY = {
+    "sys.DATALENGTH(" + COL2_NAME + ")"
+  };
+
+  // initialization of EXPECTED_RESULTS
+  vector<vector<string>> EXPECTED_RESULTS = {
+    {"122"}
+  };
+
+  createTable(ServerType::MSSQL, BBF_TABLE_NAME, TABLE_COLUMNS);
+  insertValuesInTable(ServerType::MSSQL, BBF_TABLE_NAME, INSERT_STRING, NUM_OF_DATA);
+
+  testStringFunctions(ServerType::MSSQL, BBF_TABLE_NAME, OPERATIONS_QUERY, 
+                      EXPECTED_RESULTS, NUM_OF_DATA, COL1_NAME);
+  testStringFunctions(ServerType::PSQL, TABLE_NAME, OPERATIONS_QUERY, 
+                      EXPECTED_RESULTS, NUM_OF_DATA, COL1_NAME);
+
+  dropObject(ServerType::PSQL, "TABLE", TABLE_NAME);
+  dropObject(ServerType::MSSQL, "TABLE", BBF_TABLE_NAME);
 }
