fc4b0d4380c396dff69d38c67c188f53a5dc31f3
pratikzode <pratik.zode@gmail.com>
2022-11-10 19:39:25 +0000

    DEGREES, RADIANS and POWER return float always Have added the test cases
    Task: BABEL-736
    Signed-off-by: pratikzode <pratik.zode@gmail.com>

---
+++ test/JDBC/input/BABEL-736-vu-prepare.sql
@@ -0,0 +1,42 @@
+CREATE TABLE BABEL_736_vu_prepare_t_1(
+		a int,
+		b float,
+		c int NOT NULL,
+		e float NOT NULL UNIQUE 
+);
+GO
+
+
+INSERT INTO BABEL_736_vu_prepare_t_1 VALUES (NULL, 10.1234, NULL, 20);
+GO
+INSERT INTO BABEL_736_vu_prepare_t_1 VALUES (10.1234, 101.123, 2,80);
+GO
+INSERT INTO BABEL_736_vu_prepare_t_1 VALUES (1, 20, 20,80);
+GO
+INSERT INTO BABEL_736_vu_prepare_t_1 VALUES (10.1234, 101, 780, 80990);
+GO
+
+
+
+
+-- CREATE TABLE BABEL_736_vu_prepare_t_2(
+-- 		a [BIGINT],
+-- 		b [BIGINT]
+-- )
+-- GO
+
+-- CREATE TABLE BABEL_736_vu_prepare_t_3(
+-- 		a [BIGINT],
+-- 		b [BIGINT],
+--         c [BIGINT]
+-- )
+-- GO
+
+-- INSERT INTO BABEL_736_vu_prepare_t_2 VALUES (32738,radians(1327380));
+-- GO
+
+-- INSERT INTO BABEL_736_vu_prepare_t_3 VALUES (CAST (24124 AS BIGINT),2,POWER(CAST (24124 AS BIGINT),2));
+-- GO
+
+
+
---
+++ test/JDBC/input/BABEL-736-vu-verify.sql
@@ -0,0 +1,2 @@
+SELECT * from BABEL_736_vu_prepare_t_1 ORDER BY 1
+GO
