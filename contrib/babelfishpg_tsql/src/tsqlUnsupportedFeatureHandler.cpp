88c874fe1a4f01648c8f3b2044d740b5e74b358d
Sai Rohan Basa <bsrohan@amazon.com>
2022-11-11 04:39:30 +0000

    Updated with a better error message for four part object names

--- contrib/babelfishpg_tsql/src/tsqlUnsupportedFeatureHandler.cpp
+++ contrib/babelfishpg_tsql/src/tsqlUnsupportedFeatureHandler.cpp
@@ -1346,7 +1346,7 @@ antlrcpp::Any TsqlUnsupportedFeatureHandlerImpl::visitFunc_proc_name_database_sc
 antlrcpp::Any TsqlUnsupportedFeatureHandlerImpl::visitFunc_proc_name_server_database_schema(TSqlParser::Func_proc_name_server_database_schemaContext *ctx)
 {
 	if (ctx->DOT().size() >= 3 && ctx->server) /* server.db.schema.funcname */
-		handle(INSTR_UNSUPPORTED_TSQL_SERVERNAME_IN_NAME, "servername", getLineAndPos(ctx));
+		handle(INSTR_UNSUPPORTED_TSQL_SERVERNAME_IN_NAME, "Remote object reference with 4-part object name", getLineAndPos(ctx));
 
 	if (ctx->DOT().empty())
 	{
---
+++ test/JDBC/input/babel-2646.sql
@@ -0,0 +1,3 @@
+-- servername (not supported)
+insert into yourserver.master.dbo.t1 values (1);
+go
