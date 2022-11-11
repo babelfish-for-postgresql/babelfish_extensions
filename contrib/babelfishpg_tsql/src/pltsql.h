41efb4383c5ea6e1ee60cf60f886af40153c1d4b
Avantika Dasgupta <davantik@amazon.com>
2022-11-11 15:57:35 -0800

    Support nondeterministic collations for LIKE in CHECK constraint clause (#684)
    Utility statements such as ALTER TABLE are not planned. Due to collation like to ilike transformation logic being called in a planner hook, CHECK constraint in utlity commands do not support LIKE for nondeterministic collations.
    
    To solve this problem, we add a hook to invoke the like to ilike transformation function after the raw parsetree is transformed into an executable expression.
    
    Task: BABEL-3319
    Signed-off-by: Avantika Dasgupta davantik@amazon.com
    
    Engine PR: babelfish-for-postgresql/postgresql_modified_for_babelfish#51

--- contrib/babelfishpg_tsql/src/pltsql.h
+++ contrib/babelfishpg_tsql/src/pltsql.h
@@ -1770,6 +1770,7 @@ extern int pltsql_lock_timeout;
 extern Portal pltsql_snapshot_portal;
 extern int pltsql_non_tsql_proc_entry_count;
 extern int pltsql_sys_func_entry_count;
+extern bool current_query_is_create_tbl_check_constraint;
 
 extern char *bulk_load_table_name;
 
