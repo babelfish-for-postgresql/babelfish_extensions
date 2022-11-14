0c2f5374a282eb45f974b3446b037b2024636ccd
Satarupa Biswas <satarupb@amazon.com>
2022-11-14 15:56:18 +0000

    Adding testcases for case-insensitive collation data
        The T-SQL version of information_schema.tables needs to display TABLE_NAME as lowercase for matching with sys.objects
        sys.sysname and other string types in Babelfish need to have the correct collation
        Resolved case-sensitivity issue with UDD column
        Added testcases when a view contains collatable constants
        sys.databases.name should have case-insensitive collation
        sys view columns need to have the correct output type and collation
    
    Task: BABEL-3217, BABEL-2981, BABEL-526, BABEL-3323, BABEL-3165, BABEL-3094
    Signed-off-by: Satarupa Biswas <satarupb@amazon.com>

--- test/JDBC/input/datatypes/case_insensitive_collation-before-13-5-vu-verify.sql
+++ test/JDBC/input/datatypes/case_insensitive_collation-before-13-5-vu-verify.sql
@@ -16,9 +16,6 @@ go
 SELECT COUNT(*) FROM sys.columns WHERE name = 'cAmElCoLuMn';
 go
 
-select COUNT(*) FROM sys.views WHERE name = 'CAMELview';
-go
-
 select name from sys.databases where name = 'test';
 go
 
@@ -30,10 +27,17 @@ go
 
 SELECT t.TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES AS t
-JOIN sys.objects o ON t.TABLE_NAME = o.name where o.name = 'mytable' AND o.object_id != 0;
+JOIN sys.objects o ON t.TABLE_NAME = o.name where o.name = 'mytable';
 go
 
 -- [BABEL-2981]
 -- sys.sysname and other string types in Babelfish need to have the correct collation
-select typ.typname, coll.collname from pg_type typ join pg_collation coll on typ.typcollation = coll.oid where typnamespace = (select oid from pg_namespace where nspname = 'sys');
+select typ.typname, coll.collname from pg_type typ join pg_collation coll on typ.typcollation = coll.oid
+ where typnamespace = (select oid from pg_namespace where nspname = 'sys') order by typ.typname;
+go
+
+create table t_rcv16 (a [dbo].[myTyp6]);
+go
+
+create table t_rcv17(a dbo.myTyp6);
 go
