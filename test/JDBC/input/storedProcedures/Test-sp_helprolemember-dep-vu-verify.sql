8c45f65350b24558110bf8d8d701eec6cfc26bc7
vasavi suthapalli <svasusri@amazon.com>
2022-11-15 18:46:59 +0000

    Add case insensitivity tests for sp_helprole, sp_helprolemember procedures

---
+++ test/JDBC/input/storedProcedures/Test-sp_helprolemember-dep-vu-verify.sql
@@ -0,0 +1,14 @@
+CREATE ROLE sp_helprolemember_role1;
+GO
+
+CREATE ROLE sp_helprolemember_role2;
+GO
+
+ALTER ROLE sp_helprolemember_role1 ADD MEMBER sp_helprolemember_role2;
+GO
+
+EXEC test_sp_helprolemember_proc "sp_helprolemember_role1";
+GO
+
+EXEC test_sp_helprolemember_proc "SP_helprolemember_RoLe1";
+GO
