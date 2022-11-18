018f34bd0c4ed2d067fb211de151f0123f32c526
Sai Rohan Basa <bsrohan@amazon.com>
2022-11-18 07:55:11 +0000

    Added new upgrade script in common extension

---
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--2.3.0--2.4.0.sql
@@ -0,0 +1,8 @@
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '2.4.0'" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
