9fd5b2f24ed6e768c12852b20663996dd714ba40
Harsh Lunagariya <lunharsh@amazon.com>
2022-11-18 02:31:43 +0000

    Add separate test file for 13_x upgrade tests
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>

---
+++ test/JDBC/input/ownership/sys_server_principals_dep_for_13_x-vu-verify.sql
@@ -0,0 +1,14 @@
+SELECT * FROM sys_server_principals_dep_for_13_x_vu_prepare_view
+GO
+
+EXEC sys_server_principals_dep_for_13_x_vu_prepare_proc
+GO
+
+SELECT sys_server_principals_dep_for_13_x_vu_prepare_func()
+GO
+
+SELECT name FROM sys.server_principals
+WHERE name LIKE 'sys_server_principals_dep_for_13_x_vu_prepare_login%'
+ORDER BY name
+GO
+
