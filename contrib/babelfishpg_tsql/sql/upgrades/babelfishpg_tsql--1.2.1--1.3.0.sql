affbb5458a6dcfe82ae7fe004c262670585584d6
HarshLunagariya <40052763+HarshLunagariya@users.noreply.github.com>
2022-11-18 06:31:52 +0530

    Add babelfishpg_tsql upgrade path from 1.2.1 to 1.3.0 and from 1.3.0 to 2.0.0 (#863)
    This commit adds babelfishpg_tsql upgrade path from 1.2.1 to 1.3.0 and from 1.3.0 to 2.0.0.
    
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>
    Signed-off-by: Ray Kim <raydhkim@amazon.com>
    Co-authored-by: Ray Kim <raydhkim@amazon.com>

---
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--1.2.1--1.3.0.sql
@@ -0,0 +1,7 @@
+ -- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_tsql"" UPDATE TO '1.3.0'" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
