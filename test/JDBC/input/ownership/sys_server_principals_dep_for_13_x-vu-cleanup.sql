ccaead1c93aeb369948ecdf01143d1826026a17a
Ray" DongHo Kim <112982631+raydhkim@users.noreply.github.com>
2022-11-18 03:51:10 -0800

    Replace publicly unreadable pg_catalog view instances to publicly readable one in SQL scripts (#850)
    Some of the pg_catalogs that are not publicly reachable caused several failures in the pipeline, so those pg_catalogs are replaced with publicly reachable views to resolve the failures.
    
    Signed-off-by: Ray Kim <raydhkim@amazon.com>

---
+++ test/JDBC/input/ownership/sys_server_principals_dep_for_13_x-vu-cleanup.sql
@@ -0,0 +1,15 @@
+DROP VIEW sys_server_principals_dep_for_13_x_vu_prepare_view
+GO
+
+DROP PROC sys_server_principals_dep_for_13_x_vu_prepare_proc
+GO
+
+DROP FUNCTION sys_server_principals_dep_for_13_x_vu_prepare_func
+GO
+
+DROP LOGIN sys_server_principals_dep_for_13_x_vu_prepare_login1
+GO
+
+DROP LOGIN sys_server_principals_dep_for_13_x_vu_prepare_login2
+GO
+
