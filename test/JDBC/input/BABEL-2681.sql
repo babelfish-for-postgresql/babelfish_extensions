8e217227d828506456d2f0f4d1a0da3c1190ef82
Yuhao Wei <71300970+LareinaWei@users.noreply.github.com>
2022-12-08 15:39:40 -0800

    Add Babelfish version number to @@Version (#919)
    Added the BABELFISH_VERSION_STR to the fourth line of @@VERSION.
    
    Task: BABEL-3767
    Signed-off-by: Yuhao Wei <yuhaowei@amazon.com>

--- test/JDBC/input/BABEL-2681.sql
+++ test/JDBC/input/BABEL-2681.sql
@@ -6,4 +6,6 @@ select cast(SERVERPROPERTY('ProductMajorVersion') as varchar) as ProductMajorVer
 -- only print till the server version i.e. first newline character
 select substring(@@version, 1, CHARINDEX(CHAR(10), @@version) - 1);
 select @@microsoftversion;
+-- check there is Bablefish version info
+select count(*) from (select @@version) a where version like '% (Babelfish %.%.%)';
 go
