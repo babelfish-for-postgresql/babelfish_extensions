b698ff91387d1f7377e79e72c31438f9ac24268f
Kushaal Shroff <kushaal@amazon.com>
2022-12-14 11:34:46 +0000

    Fix Bugs for SP_*

--- contrib/babelfishpg_tsql/src/pl_exec-2.c
+++ contrib/babelfishpg_tsql/src/pl_exec-2.c
@@ -2264,10 +2264,26 @@ read_param_def(InlineCodeBlockArgs *args, const char *paramdefstr)
 	foreach(lc, params)
 	{
 		FunctionParameter   *p;
+		List* type_names;
 
 		p = (FunctionParameter *) lfirst(lc);
 		args->argnames[i] = p->name;
 		args->argmodes[i] = p->mode;
+
+		type_names = p->argType->names;
+
+		/*
+		 * Handle User defined types with schema qualifiers. Convert logical Schema Name to
+		 * Physical Schema Name. Note: The list length can not be more than 2 since db name
+		 * can not be a qualifier for a UDT and error will be thrown in the pareser itself.
+		 */
+		if (list_length(type_names) == 2)
+		{
+			strVal(linitial(type_names)) = get_physical_schema_name(
+							get_cur_db_name(), strVal(linitial(type_names)));
+			p->argType->names = type_names;
+		}
+
 		typenameTypeIdAndMod(NULL, p->argType, &(args->argtypes[i]), &(args->argtypmods[i]));
 		i++;
 	}
@@ -2385,7 +2401,8 @@ read_param_val(PLtsql_execstate *estate, List *params, InlineCodeBlockArgs *args
 		{
 			for (j = i; j < args->numargs; j++)
 			{
-				if (strcmp(p->name, args->argnames[j]) == 0)
+				/* Case insensitive param names can be used. */
+				if (pg_strcasecmp(p->name, args->argnames[j]) == 0)
 				{
 					/* Check if the param's declared mode matches called mode */
 					if (!check_spexecutesql_param(&(args->argmodes[j]), p))
