fc345ac7cea12e847856d1f3e14368dc368dc3de
Satarupa Biswas <satarupabiswas89@gmail.com>
2022-12-17 14:29:33 +0530

    PG15: Fix tests related to Numeric datatype in REL3_X_DEV branch; Create table or proc should throw error for declaring non-permissible datalength in variable length datatypes (#923)
    Description
    PG15 has introduced a community change for numeric datatype as below:
    Formerly, when specifying NUMERIC(precision, scale), the scale had to be in the range [0, precision], which was per SQL spec. This commit extends the range of allowed scales to [-1000, 1000], independent of
    the precision (whose valid range remains [1, 1000]). A negative scale implies rounding before the decimal point. For
    example, a column might be declared with a scale of -3 to round values to the nearest thousand. Note that the display scale remains non-negative, so in this case the display scale will be zero, and all digits before the decimal point will be displayed.
    A scale greater than the precision supports fractional values with zeros immediately after the decimal point.
    
    From SQL spec aspect which means checking explicitly whether 0 <= scale <= precision and 1 <= precision <= 38, which was earlier done by underlying PG code for any numeric/decimal input data.
    
    Along with the above change, it is found that all variable length datatypes like numeric, decimal, time, datetime2, datetimeoffset need explicit checking on their permissible datalength at the time of table or stored procedure creation and generate error if required.
    Before fix, Babelfish was not complaining for the above behaviour.
    
    Task: BABEL-3720, BABEL-3793
    Signed-off-by: Satarupa Biswas (satarupb@amazon.com)

--- contrib/babelfishpg_tsql/src/datatypes.h
+++ contrib/babelfishpg_tsql/src/datatypes.h
@@ -22,3 +22,5 @@ extern bool is_tsql_varbinary_datatype(Oid oid);
 extern bool is_tsql_datetime2_datatype(Oid oid);
 extern bool is_tsql_smalldatetime_datatype(Oid oid);
 extern bool is_tsql_datetimeoffset_datatype(Oid oid);
+extern bool is_tsql_timestamp_datatype(Oid oid);
+extern bool is_tsql_decimal_datatype(Oid oid);
