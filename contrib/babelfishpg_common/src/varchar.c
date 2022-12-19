356006aea5aa7de1fef5fb5bd486bf8175b3d23f
Rishabh Tanwar <33982749+rishabhtanwar29@users.noreply.github.com>
2022-12-19 09:02:32 +0530

    Convert babelfishpg_tsql to not use direct links to babelfishpg_common (#910)
    Historically, we have referenced functions from `babelfishpg_common` directly
    into `babelfishpg_tsql` extension using `extern`keyword. Due to this `babelfishpg_tsql`
    library couldn't be loaded independently (without explicitly loading `babelfishpg_common`
    library first) due to unresolved/undefined symbols. To overcome this commit 06fdc75bfbb9ef8be16041ea052526ff761bea87
    linked `babelfishpg_common` to `babelfishpg_tsql` with a direct dependency.
    
    The direct linking fix mentioned above has it's own drawbacks and causes MVU failures
    due to incorrect library linking between source and target data directories during MVU.
    Instead, let's remove these `extern` references (unresolved symbols) and use function
    pointers that are manually filled in by the module's _PG_init function and all are clubbed
    together into one `rendezvous` variable called `common_utility_plugin`.
    Additionally, change all occurrences of PG directory to `psql` so that directory names
    do not contain `postgres` or `pgsql` keywords.
    
    Task: BABEL-3758
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>

--- contrib/babelfishpg_common/src/varchar.c
+++ contrib/babelfishpg_common/src/varchar.c
@@ -41,14 +41,13 @@
 #include "utils/timestamp.h"
 #include "utils/numeric.h"
 #include "typecode.h"
+#include "varchar.h"
 
 int  TsqlUTF8LengthInUTF16(const void *vin, int len);
 void TsqlCheckUTF16Length_varchar(const char *s_data, int32 len, int32 maxlen, bool isExplicit);
 void TsqlCheckUTF16Length_bpchar(const char *s, int32 len, int32 maxlen, int charlen, bool isExplicit);
 void TsqlCheckUTF16Length_bpchar_input(const char *s, int32 len, int32 maxlen, int charlen);
 void TsqlCheckUTF16Length_varchar_input(const char *s, int32 len, int32 maxlen);
-void *tsql_varchar_input(const char *s, size_t len, int32 atttypmod);
-void *tsql_bpchar_input(const char *s, size_t len, int32 atttypmod);
 static inline int varcharTruelen(VarChar *arg);
 
 #define DEFAULT_LCID 1033
