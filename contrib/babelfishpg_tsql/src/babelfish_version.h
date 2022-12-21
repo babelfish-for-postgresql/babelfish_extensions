9c00b539c894def3f975efa2dd626683e18f2e97
Kuntal Ghosh <kuntalgh@amazon.com>
2022-12-21 16:11:07 +0000

    Bump Babelfish version from 1.2.3" to 1.2.4"

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "1.2.3"
+#define BABELFISH_VERSION_STR "1.2.4"
 #define BABELFISH_INTERNAL_VERSION_STR "Babelfish 13.6.4.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
