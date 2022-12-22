d044fc9b0cb49e1bae2db2790407db685afaaf05
Sumit Jaiswal <sumiji@amazon.com>
2022-12-22 10:53:48 +0000

    Fix error

--- contrib/babelfishpg_tsql/runtime/functions.c
+++ contrib/babelfishpg_tsql/runtime/functions.c
@@ -940,6 +940,7 @@ static char* remove_delimiter_pair(char *str)
 		{	
 			char *res = (char *) palloc((len - 1) * sizeof(char));
 			strncpy(res, &str[1], len - 2);
+			res[len - 2] = '\n';
 			return res;
 		} 
 		else
