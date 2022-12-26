01375fd489bcf11a72b9036e8a4e526f3c414b93
Sai Rohan Basa <bsrohan@amazon.com>
2022-12-26 06:25:00 +0000

    updated upgrade script

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.3.0--3.0.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.3.0--3.0.0.sql
@@ -161,6 +161,7 @@ CALL sys.create_linked_server_procs_in_master_dbo();
 ALTER PROCEDURE master_dbo.sp_addlinkedserver OWNER TO sysadmin;
 ALTER PROCEDURE master_dbo.sp_addlinkedsrvlogin OWNER TO sysadmin;
 ALTER PROCEDURE master_dbo.sp_dropserver OWNER TO sysadmin;
+ALTER PROCEDURE master_dbo.sp_droplinkedsrvlogin OWNER TO sysadmin;
 DROP PROCEDURE sys.create_linked_server_procs_in_master_dbo;
 
 CREATE OR REPLACE VIEW sys.servers
