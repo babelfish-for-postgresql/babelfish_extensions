c4a45c6276d3abccf65f63b1cc9f06e80862f5da
Ashish Prasad <pashisht@amazon.com>
2022-12-27 17:03:00 +0000

    Fixing test cases.
    Signed-off-by: Ashish Prasad <pashisht@amazon.com>

--- test/JDBC/input/views/sys-computed_columns-vu-verify.sql
+++ test/JDBC/input/views/sys-computed_columns-vu-verify.sql
@@ -4,11 +4,11 @@ GO
 SELECT COUNT(*) FROM sys.all_columns WHERE object_id = object_id('sys.computed_columns');
 GO
 
-Select definition FROM sys.computed_columns WHERE name="scc_first_number1";
+Select definition FROM sys.computed_columns WHERE name='scc_first_number1';
 GO
 
-Select definition FROM sys.computed_columns WHERE name="scc_second_number1";
+Select definition FROM sys.computed_columns WHERE name='scc_second_number1';
 GO
 
-Select definition FROM sys.computed_columns WHERE name="scc_multiplied1";
+Select definition FROM sys.computed_columns WHERE name='scc_multiplied1';
 GO
