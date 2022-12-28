7c57eac32c6f3a1b50e0f4ee22a1a33d9bde3ab5
lshalini <lshalini@amazon.com>
2022-12-28 11:07:55 +0000

    Add one more test to show tsql definition

--- test/JDBC/input/views/sys-check_constraints.sql
+++ test/JDBC/input/views/sys-check_constraints.sql
@@ -2,11 +2,12 @@ DROP TABLE IF EXISTS sys_check_constraints
 GO
 
 CREATE TABLE sys_check_constraints (
-	sck_date_col DATETIME CHECK (sck_date_col IS NOT NULL)
+	sck_date_col DATETIME CHECK (sck_date_col IS NOT NULL),
+	sck_datetime_2 datetime2 check(sck_datetime_2 < cast('2020-10-20 09:00:00' as datetime2))
 )
 GO
 
-SELECT name FROM sys.check_constraints WHERE NAME IN ('sys_check_constraints_sck_date_col_check')
+SELECT name, definition FROM sys.check_constraints WHERE NAME IN ('sys_check_constraints_sck_date_col_check', 'sys_check_constraints_sck_datetime_2_check') ORDER BY NAME
 GO
 
 DROP TABLE IF EXISTS sys_check_constraints
