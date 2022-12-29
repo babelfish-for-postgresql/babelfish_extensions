d5bbc63d50993042020c508632fc7dd5b341b9a7
lshalini <lshalini@amazon.com>
2022-12-29 15:18:34 +0000

    Fix tests

--- test/JDBC/input/views/sys-check_constraints-vu-prepare.sql
+++ test/JDBC/input/views/sys-check_constraints-vu-prepare.sql
@@ -7,7 +7,7 @@ CREATE TABLE sys_check_constraints_vu_prepare_t2
 (
 column_a bit default 0,
 column_b varchar(5) CHECK (column_b <> 'wrong'),
-column_c datetime2 check(column_c < cast('2020-10-20 09:00:00' as datetime2)),
+column_c datetime2 check(column_c < cast('2020-10-20 09:00:00' as datetime2(6))),
 CHECK (column_a = 0)
 )
 GO
