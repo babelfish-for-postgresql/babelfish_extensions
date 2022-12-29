d5e1ec989b5332f3286e93e3ee7e4d1a90c0cd97
Ashish Prasad <pashisht@amazon.com>
2022-12-29 08:52:20 +0000

    Fix Upgrade Test

--- test/JDBC/input/views/sys-computed_columns-vu-cleanup.sql
+++ test/JDBC/input/views/sys-computed_columns-vu-cleanup.sql
@@ -1,5 +1,2 @@
 DROP TABLE IF EXISTS sys_computed_columns_vu_prepare_t1
 GO
-
-DROP TABLE IF EXISTS sys_computed_columns_vu_prepare_t2
-GO
--- test/JDBC/input/views/sys-computed_columns-vu-prepare.sql
+++ test/JDBC/input/views/sys-computed_columns-vu-prepare.sql
@@ -10,10 +10,3 @@ CREATE TABLE sys_computed_columns_vu_prepare_t1 (
   scc_multiplied AS scc_first_number * scc_second_number
 )
 GO
-
-CREATE TABLE sys_computed_columns_vu_prepare_t2 (
-  scc_first_number1 smallint,
-  scc_second_number1 money,
-  scc_multiplied1 AS (scc_first_number1 * scc_second_number1)
-)
-GO
