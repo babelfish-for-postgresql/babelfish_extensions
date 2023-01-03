fd0b81f8881d25032346dee2692ff51a9ca201fd
lshalini <lshalini@amazon.com>
2023-01-03 16:10:50 +0000

    Fix tests

--- test/JDBC/input/ISC-Check-Constraints-vu-prepare.sql
+++ test/JDBC/input/ISC-Check-Constraints-vu-prepare.sql
@@ -30,7 +30,7 @@ go
 
 create table test_tsql_collate(
 	c_varchar varchar check(c_varchar <> cast('sflkjasdlkfjf' as varchar(12)) COLLATE latin1_general_ci_as),
-	c_char char check(c_char <> cast('sflkjasdlkfjf' as char(7)) COLLATE chinese_prc_cs_as),
+	c_char char check(c_char <> cast('sflkjasdlkfjf' as char(7)) COLLATE japanese_ci_as),
 	c_char1 char check(c_char <> cast('abcd' as char(7)) COLLATE "default"),
 	c_nchar nchar check(cast(c_nchar as nchar(7)) <> cast('sflkjasdlkfjf' as nchar(7)) COLLATE bbf_unicode_cp1_ci_as),
 );
