93c366decb96cb7f7fc7bb18c3560cb4d3e96942
Sharu Goel <goelshar@amazon.com>
2023-01-03 06:18:03 +0000

    Add test to a different file

--- test/JDBC/input/BABEL-1797.sql
+++ test/JDBC/input/BABEL-1797.sql
@@ -11,7 +11,3 @@ go
 -- cleanup
 drop table [dbo].[t23];
 go
-
--- Should throw an error
-exec sys.sp_describe_undeclared_parameters N'insert into pg_shadow (a,b,c,d,e,f) values (@a,@b,@c,@d,@e,@f)'
-go
