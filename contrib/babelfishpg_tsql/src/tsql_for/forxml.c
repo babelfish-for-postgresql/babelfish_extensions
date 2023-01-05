6522e8fe54f1e1d1dad440f520edf018446dc5e3
Jason Teng <jasonten@amazon.com>
2023-01-05 19:01:36 +0000

    fix compiler error
    Signed-off-by: Jason Teng <jasonten@amazon.com>

--- contrib/babelfishpg_tsql/src/tsql_for/forxml.c
+++ contrib/babelfishpg_tsql/src/tsql_for/forxml.c
@@ -332,6 +332,7 @@ PG_FUNCTION_INFO_V1(tsql_query_to_xml);
 Datum
 tsql_query_to_xml(PG_FUNCTION_ARGS)
 {
+	StringInfo s = makeStringInfo();
 	/*
 	 * This function has been deprecated as of v2.4.
 	 * However, we cannot remove this function entirely because
@@ -344,7 +345,6 @@ tsql_query_to_xml(PG_FUNCTION_ARGS)
 	ereport(WARNING,
 			(errcode(ERRCODE_WARNING_DEPRECATED_FEATURE),
 			 errmsg("This function has been deprecated.")));
-	StringInfo s = makeStringInfo();
 	PG_RETURN_XML_P((xmltype *) cstring_to_text_with_len(s->data, s->len));
 }
 
@@ -352,6 +352,7 @@ PG_FUNCTION_INFO_V1(tsql_query_to_xml_text);
 Datum
 tsql_query_to_xml_text(PG_FUNCTION_ARGS)
 {
+	StringInfo s = makeStringInfo();
 	/*
 	 * This function has been deprecated as of v2.4.
 	 * However, we cannot remove this function entirely because
@@ -364,6 +365,5 @@ tsql_query_to_xml_text(PG_FUNCTION_ARGS)
 	ereport(WARNING,
 			(errcode(ERRCODE_WARNING_DEPRECATED_FEATURE),
 			 errmsg("This function has been deprecated.")));
-	StringInfo s = makeStringInfo();
 	PG_RETURN_TEXT_P(cstring_to_text_with_len(s->data, s->len));
 }
