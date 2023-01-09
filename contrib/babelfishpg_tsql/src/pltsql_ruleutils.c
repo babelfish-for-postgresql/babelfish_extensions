559fcc7326a0074e10bbff49eb26488c08a36f96
Ashish Prasad <56514722+hash-16@users.noreply.github.com>
2023-01-09 09:57:20 +0530

    Support SSMS scripting for Computed Columns (#968)
    * Support SSMS scripting for Computed Columns
    
    Initially the value of definition column in sys.computed_columns is derived from a PG function pg_get_expr.Have added
    a tsql function to get the computed column definition value.
    
    BABEL-3806

--- contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
+++ contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
@@ -342,6 +342,8 @@ static Plan *find_recursive_union(deparse_namespace *dpns,
 static text *string_to_text(char *str);
 static char *tsql_get_constraintdef_worker(Oid constraintId, bool fullCommand,
 										 int prettyFlags, bool missing_ok);
+static text *tsql_get_expr_worker(text *expr, Oid relid, const char *relname,
+                                         int prettyFlags);
 static char *tsql_printTypmod(const char *typname, int32 typmod, Oid typmodout);
 static char *tsql_format_type_extended(Oid type_oid, int32 typemod, bits16 flags);
 int tsql_print_function_arguments(StringInfo buf, HeapTuple proctup,
@@ -375,6 +377,77 @@ tsql_get_constraintdef(PG_FUNCTION_ARGS)
 		PG_RETURN_NULL();
 
 	PG_RETURN_TEXT_P(string_to_text(res));
+}
+
+PG_FUNCTION_INFO_V1(tsql_get_expr);
+
+/* ----------
+ * tsql_get_expr          - Decompile an expression tree
+ *
+ * Input: an expression tree in nodeToString form, and a relation OID
+ *
+ * Output: reverse-listed expression
+ *
+ * Currently, the expression can only refer to a single relation, namely
+ * the one specified by the second parameter.  This is sufficient for
+ * partial indexes, column default expressions, etc.  We also support
+ * Var-free expressions, for which the OID can be InvalidOid.
+ * ----------
+ */
+
+Datum
+tsql_get_expr(PG_FUNCTION_ARGS)
+{
+    text       *expr = PG_GETARG_TEXT_PP(0);
+    Oid         relid = PG_GETARG_OID(1);
+    int         prettyFlags;
+    char       *relname;
+
+    prettyFlags = PRETTYFLAG_INDENT;
+
+    if (OidIsValid(relid))
+    {
+        /* Get the name for the relation */
+        relname = get_rel_name(relid);
+    }
+    else
+    {
+        relname = NULL;
+    }
+    /*
+     * If the relname is NULL, don't throw an error, just return
+     * NULL.  This is a bit questionable, but it's what we've done
+     * historically, and it can help avoid unwanted failures when
+     * examining catalog entries for just-deleted relations.
+     */
+    if (relname == NULL)
+	PG_RETURN_NULL();
+
+    PG_RETURN_TEXT_P(tsql_get_expr_worker(expr, relid, relname, prettyFlags));
+}
+
+static text *
+tsql_get_expr_worker(text *expr, Oid relid, const char *relname, int prettyFlags)
+{
+    Node       *node;
+    List       *context;
+    char       *exprstr;
+
+    /* Convert input TEXT object to C string */
+    exprstr = text_to_cstring(expr);
+
+    /* Convert expression to node tree */
+    node = (Node *) stringToNode(exprstr);
+
+    pfree(exprstr);
+
+    /* Prepare deparse context if needed */
+    if (OidIsValid(relid))
+        context = deparse_context_for(relname, relid);
+    else
+        context = NIL;
+							 /* Deparse */
+    return string_to_text(deparse_expression_pretty(node, context, false, false,prettyFlags, 0));
 }
 
 /*
