4a4a2117fce9ea1ce6c34bee690348a4d0b7b8a5
Sai Rohan Basa <bsrohan@amazon.com>
2023-01-10 09:52:18 +0000

    updated sp_droplinkedsrvlogin implementation

--- contrib/babelfishpg_tsql/src/procedures.c
+++ contrib/babelfishpg_tsql/src/procedures.c
@@ -2294,7 +2294,7 @@ sp_droplinkedsrvlogin_internal(PG_FUNCTION_ARGS)
 	 * DROP USER MAPPING FOR @LOCALLOGIN SERVER @SERVERNAME
 	 *
 	 */
-	appendStringInfo(&query, "DROP USER MAPPING FOR PUBLIC SERVER %s", locallogin, servername);
+	appendStringInfo(&query, "DROP USER MAPPING FOR PUBLIC SERVER %s", servername);
 
 	exec_utility_cmd_helper(query.data);
 
