a8564a52a4d11e399a88b0171a2ab52a43655bab
Kushaal Shroff <kushaal@amazon.com>
2023-01-11 07:22:02 +0000

    Rewrite sys-types

--- contrib/babelfishpg_tsql/sql/sys_views.sql
+++ contrib/babelfishpg_tsql/sql/sys_views.sql
@@ -1,16 +1,19 @@
 /* Tsql system catalog views */
 
--- The sys.table_types_internal view mimics the logic used in sys.is_table_type function
+/*
+ * We do not need to join with pg_class since we can check the dependent class-id in
+ * pg_depend directly.
+ * Note: this view, although written efficiently might cause perfomance degradation when
+ * joined with other system objects. One should try to use them as part of a materialized CTE.
+ */
 create or replace view sys.table_types_internal as
 SELECT pt.typrelid
     FROM pg_catalog.pg_type pt
-    INNER JOIN pg_catalog.pg_depend dep
-    ON pt.typrelid = dep.objid
-    INNER JOIN pg_catalog.pg_class pc ON pc.oid = dep.objid
+    INNER JOIN pg_catalog.pg_depend dep ON pt.typrelid = dep.objid
+    INNER join sys.schemas sch on pt.typnamespace = sch.schema_id
     WHERE 
-    pt.typnamespace in (select schema_id from sys.schemas) 
-    and (pt.typtype = 'c' AND dep.deptype = 'i'  AND pc.relkind = 'r')
-;
+    (pt.typtype = 'c' AND dep.deptype = 'i' AND pt.oid = dep.refobjid AND dep.deptype = 'i'
+    AND dep.classid = 'pg_catalog.pg_class'::regclass AND dep.refclassid = 'pg_catalog.pg_type'::regclass);
 
 create or replace view sys.tables as
 select
@@ -1040,21 +1043,20 @@ left join pg_catalog.pg_locks         blocking_locks
 GRANT SELECT ON sys.sysprocesses TO PUBLIC;
 
 create or replace view sys.types As
-with type_code_list as
+with tt_internal as MATERIALIZED
 (
-    select distinct  pg_typname as pg_type_name, tsql_typname as tsql_type_name
-    from sys.babelfish_typecode_list()
+  Select * from sys.table_types_internal
 )
 -- For System types
 select 
-  ti.tsql_type_name as name
+  tsql_type_name as name
   , t.oid as system_type_id
   , t.oid as user_type_id
   , s.oid as schema_id
   , cast(NULL as INT) as principal_id
-  , sys.tsql_type_max_length_helper(ti.tsql_type_name, t.typlen, t.typtypmod, true) as max_length
-  , cast(sys.tsql_type_precision_helper(ti.tsql_type_name, t.typtypmod) as int) as precision
-  , cast(sys.tsql_type_scale_helper(ti.tsql_type_name, t.typtypmod, false) as int) as scale
+  , sys.tsql_type_max_length_helper(tsql_type_name, t.typlen, t.typtypmod, true) as max_length
+  , cast(sys.tsql_type_precision_helper(tsql_type_name, t.typtypmod) as int) as precision
+  , cast(sys.tsql_type_scale_helper(tsql_type_name, t.typtypmod, false) as int) as scale
   , CASE c.collname
     WHEN 'default' THEN default_collation_name
     ELSE  c.collname
@@ -1067,11 +1069,11 @@ select
   , 0 as is_table_type
 from pg_type t
 inner join pg_namespace s on s.oid = t.typnamespace
-inner join type_code_list ti on t.typname = ti.pg_type_name
 left join pg_collation c on c.oid = t.typcollation
+,sys.translate_pg_type_to_tsql(t.oid) AS tsql_type_name
 ,cast(current_setting('babelfishpg_tsql.server_collation_name') as name) as default_collation_name
 where
-ti.tsql_type_name IS NOT NULL  
+tsql_type_name IS NOT NULL
 and pg_type_is_visible(t.oid)
 and (s.nspname = 'pg_catalog' OR s.nspname = 'sys')
 union all 
@@ -1100,15 +1102,12 @@ select cast(t.typname as text) as name
   , case when tt.typrelid is not null then 1 else 0 end as is_table_type
 from pg_type t
 join sys.schemas sch on t.typnamespace = sch.schema_id
-left join type_code_list ti on t.typname = ti.pg_type_name
 left join pg_collation c on c.oid = t.typcollation
-left join sys.table_types_internal tt on t.typrelid = tt.typrelid
+left join tt_internal tt on t.typrelid = tt.typrelid
 , sys.translate_pg_type_to_tsql(t.typbasetype) AS tsql_base_type_name
 , cast(current_setting('babelfishpg_tsql.server_collation_name') as name) as default_collation_name
 -- we want to show details of user defined datatypes created under babelfish database
 where 
- ti.tsql_type_name IS NULL
-and
   (
     -- show all user defined datatypes created under babelfish database except table types
     t.typtype = 'd'
