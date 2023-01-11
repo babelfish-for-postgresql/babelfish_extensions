007b9b8fd0ebcfb6aee7a696ad7fdc1b0bf8524a
Nirmit Shah <nirmisha@amazon.com>
2023-01-11 11:33:08 +0000

    Fixed issue with upgrade script
    Signed-off-by: Nirmit Shah <nirmisha@amazon.com>

---
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--3.0.0--3.1.0.sql
@@ -0,0 +1,54 @@
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '3.1.0'" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+CREATE OR REPLACE FUNCTION sys.bigint_avg(INTERNAL)
+RETURNS BIGINT
+AS 'babelfishpg_common', 'bigint_avg'
+LANGUAGE C IMMUTABLE PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION sys.int4int2_avg(pg_catalog._int8)
+RETURNS INT
+AS 'babelfishpg_common', 'int4int2_avg'
+LANGUAGE C IMMUTABLE PARALLEL SAFE;
+
+CREATE OR REPLACE AGGREGATE sys.avg(TINYINT)(
+SFUNC = int2_avg_accum,
+FINALFUNC = int4int2_avg,
+STYPE = _int8,
+COMBINEFUNC = int4_avg_combine,
+PARALLEL = SAFE,
+INITCOND='{0,0}'
+);
+
+CREATE OR REPLACE AGGREGATE sys.avg(SMALLINT)(
+SFUNC = int2_avg_accum,
+FINALFUNC = int4int2_avg,
+STYPE = _int8,
+COMBINEFUNC = int4_avg_combine,
+PARALLEL = SAFE,
+INITCOND='{0,0}'
+);
+
+CREATE OR REPLACE AGGREGATE sys.avg(INT)(
+SFUNC = int4_avg_accum,
+FINALFUNC = int4int2_avg,
+STYPE = _int8,
+COMBINEFUNC = int4_avg_combine,
+PARALLEL = SAFE,
+INITCOND='{0,0}'
+);
+
+CREATE OR REPLACE AGGREGATE sys.avg(BIGINT) (
+SFUNC = int8_avg_accum,
+FINALFUNC = bigint_avg,
+STYPE = INTERNAL,
+COMBINEFUNC = int8_avg_combine,
+SERIALFUNC = int8_avg_serialize,
+DESERIALFUNC = int8_avg_deserialize,
+PARALLEL = SAFE
+);
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
