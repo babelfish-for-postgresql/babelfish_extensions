b6671a6f4bb1bf45ff0550315b2d0cb3d85a6c13
pratikzode <pratik.zode@gmail.com>
2023-01-17 23:40:32 +0000

    BABEL-3802 Bigint/Int/Smallint/Tinyint Power Function
    This Jira is a part of BABEL-736.
    At first in Babel, the Transact-SQL functions power always return float instead of returning the same type in SQL server.
    In this Jira we have added Bigint, Int, Smallint and Tinyint to power function.
    Have added the test case to show Bigint, Int, Smallint and Tinyint to power.
    Now this 3 is giving same output as SQL server.
    
    Task: BABEL-3802
    Signed-off-by: pratikzode <pratik.zode@gmail.com>

---
+++ test/JDBC/input/BABEL-3802-vu-cleanup.sql
@@ -0,0 +1,41 @@
+DROP TABLE BABEL_3802_vu_prepare_t_1
+GO
+
+DROP VIEW BABEL_3802_vu_prepare_t_2
+GO
+ 
+DROP VIEW BABEL_3802_vu_prepare_t_3
+GO
+
+DROP VIEW BABEL_3802_vu_prepare_t_4
+GO
+
+DROP PROCEDURE BABEL_3802_vu_prepare_t_p1
+GO
+
+DROP PROCEDURE BABEL_3802_vu_prepare_t_p2
+GO
+
+DROP PROCEDURE BABEL_3802_vu_prepare_t_p3
+GO
+
+DROP PROCEDURE BABEL_3802_vu_prepare_t_p4
+GO
+
+DROP PROCEDURE BABEL_3802_vu_prepare_t_p5
+GO
+
+DROP PROCEDURE BABEL_3802_vu_prepare_t_p6
+GO
+
+DROP VIEW BABEL_3802_vu_prepare_t_v1
+GO
+
+DROP VIEW BABEL_3802_vu_prepare_t_v2
+GO
+
+DROP VIEW BABEL_3802_vu_prepare_t_v3
+GO
+
+DROP VIEW BABEL_3802_vu_prepare_t_v4
+GO
---
+++ test/JDBC/input/BABEL-3802-vu-verify.sql
@@ -0,0 +1,41 @@
+SELECT power(a,b), power(b,b), power(c,b), power(d,b), power(e,b) from BABEL_3802_vu_prepare_t_1 ORDER BY 1
+GO
+
+SELECT * FROM BABEL_3802_vu_prepare_t_2
+GO
+
+SELECT * FROM BABEL_3802_vu_prepare_t_3
+GO
+
+SELECT * FROM BABEL_3802_vu_prepare_t_4
+GO
+
+EXECUTE BABEL_3802_vu_prepare_t_p1
+GO
+
+EXECUTE BABEL_3802_vu_prepare_t_p2
+GO
+
+EXECUTE BABEL_3802_vu_prepare_t_p3
+GO
+
+EXECUTE BABEL_3802_vu_prepare_t_p4
+GO
+
+EXECUTE BABEL_3802_vu_prepare_t_p5
+GO
+
+EXECUTE BABEL_3802_vu_prepare_t_p6
+GO
+
+SELECT * FROM BABEL_3802_vu_prepare_t_v1
+GO
+
+SELECT * FROM BABEL_3802_vu_prepare_t_v2
+GO
+
+SELECT * FROM BABEL_3802_vu_prepare_t_v3
+GO
+
+SELECT * FROM BABEL_3802_vu_prepare_t_v4
+GO
