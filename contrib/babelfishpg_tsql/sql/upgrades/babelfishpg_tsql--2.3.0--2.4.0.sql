4399d7efedba57c93915adf06f476fb37e2fea17
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2023-01-18 17:12:33 +0530

    Support storing and retrieving TSQL trigger definition in Babelfish (#1074)
    Description
    SSMS uses the views sys.sql_modules and sys.all_sql_modules to show the object definitions. This change is needed for the SSMS scripting so that users have a way to script the trigger definition in TSQL syntax which helps in DDL Export.
    
    Issues Resolved
    Task: BABEL-3681
    Signed-off-by: Shalini Lohia lshalini@amazon.com

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.3.0--2.4.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.3.0--2.4.0.sql
@@ -942,9 +942,8 @@ CREATE OR REPLACE VIEW sys.all_sql_modules_internal AS
 SELECT
   ao.object_id AS object_id
   , CAST(
-      CASE WHEN ao.type in ('P', 'FN', 'IN', 'TF', 'RF', 'IF') THEN COALESCE(f.definition, '')
+      CASE WHEN ao.type in ('P', 'FN', 'IN', 'TF', 'RF', 'IF', 'TR') THEN COALESCE(f.definition, '')
       WHEN ao.type = 'V' THEN COALESCE(bvd.definition, '')
-      WHEN ao.type = 'TR' THEN NULL
       ELSE NULL
       END
     AS sys.nvarchar(4000)) AS definition  -- Object definition work in progress, will update definition with BABEL-3127 Jira.
