ddad6881ad46dfd11e0550ffb708201f7f4d5d45
Sai Rohan Basa <bsrohan@amazon.com>
2023-01-19 08:30:26 +0000

    updated sp_linkedservers implementation

--- contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
+++ contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
@@ -2904,6 +2904,7 @@ BEGIN
 		location AS SRV_LOCATION,
 		catalog AS SRV_CAT 
 	FROM sys.servers
+	ORDER BY SRV_NAME
 END;
 $$ LANGUAGE 'pltsql';
 GRANT EXECUTE ON PROCEDURE sys.sp_linkedservers TO PUBLIC;
