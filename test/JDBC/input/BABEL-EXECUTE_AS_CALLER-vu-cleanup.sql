05718c50267c825d889afeb9a101be78b54b0083
Suthapalli-Ramya-satya-vasavi-srija <110654856+Suthapalli-Ramya-satya-vasavi-srija@users.noreply.github.com>
2023-01-19 23:56:20 +0530

    CREATE PROCEDURE with morethan 1 parameter and has WITH-clause is not working as expected
    Earlier syntax error is being raised when tried to create a procedure
    which contains more than 1 parameter and WITH-clause in it. This was
    happened because the remove of COMMA is not handled properly in case of
    create or alter procedure.
    
    This commit fixes the above issue by removing the COMMA in WITH-clause
    but not in the parameters.
    
    Task: BABEL-3843
    Signed-off-by: vasavi suthapalli <svasusri@amazon.com>

--- test/JDBC/input/BABEL-EXECUTE_AS_CALLER-vu-cleanup.sql
+++ test/JDBC/input/BABEL-EXECUTE_AS_CALLER-vu-cleanup.sql
@@ -1,7 +1,14 @@
+DROP TRIGGER babel_execute_as_caller_trigger4;
+DROP TRIGGER babel_execute_as_caller_trigger3;
+DROP TRIGGER babel_execute_as_caller_trigger2;
 DROP TRIGGER babel_execute_as_caller_trigger1;
+DROP PROCEDURE babel_execute_as_caller_procedure_6;
+DROP PROCEDURE babel_execute_as_caller_procedure_5;
+DROP PROCEDURE babel_execute_as_caller_procedure_4;
 DROP PROCEDURE babel_execute_as_caller_procedure_3;
 DROP PROCEDURE babel_execute_as_caller_procedure_2;
 DROP PROCEDURE babel_execute_as_caller_procedure_1;
+DROP FUNCTION babel_execute_as_caller_function_return_int_7;
 DROP FUNCTION babel_execute_as_caller_function_return_int_6;
 DROP FUNCTION babel_execute_as_caller_function_return_int_5;
 DROP FUNCTION babel_execute_as_caller_function_return_int_4;
