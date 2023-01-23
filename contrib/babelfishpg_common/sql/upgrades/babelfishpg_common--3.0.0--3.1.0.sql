fa4e547e081503155533428fdea7192e4aac39e0
Shlok Kumar Kyal <68871659+skkyal@users.noreply.github.com>
2023-01-24 00:10:23 +0530

    Set Volatility of VOLATILE system function to STABLE (#1109)
    There are 1053 system functions in the sys schema, of them 331 are volatile. All the VOLATILE system functions should be set to STABLE as this is a critical basis to enable index usage in queries (other conditions are needed as well, but function stability is a must).
    This commit made all T-SQL system functions as STABLE, except the functions whose test cases were missing or do not satisfy the criteria to be STABLE. Of 331 Volatile system functions, 194 functions were made STABLE.
    
    Task: BABEL-3790
    Signed-off-by: Shlok Kumar Kyal (skkyal@amazon.com)

---
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--3.0.0--3.1.0.sql
@@ -0,0 +1,32 @@
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '3.1.0'" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+/* set sys functions as STABLE */
+ALTER FUNCTION sys._round_fixeddecimal_to_int8(In arg sys.fixeddecimal) STABLE;
+ALTER FUNCTION sys._round_fixeddecimal_to_int4(In arg sys.fixeddecimal) STABLE;
+ALTER FUNCTION sys._round_fixeddecimal_to_int2(In arg sys.fixeddecimal) STABLE;
+ALTER FUNCTION sys._trunc_numeric_to_int8(In arg numeric) STABLE;
+ALTER FUNCTION sys._trunc_numeric_to_int4(In arg numeric) STABLE;
+ALTER FUNCTION sys._trunc_numeric_to_int2(In arg numeric) STABLE;
+ALTER FUNCTION sys.CHAR(x in int) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper(leftarg text, rightarg text) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper_outer(leftarg text, rightarg text) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper(leftarg sys.varchar, rightarg sys.varchar) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper(leftarg sys.nvarchar, rightarg sys.nvarchar) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper(leftarg sys.bpchar, rightarg sys.bpchar) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper(leftarg sys.nchar, rightarg sys.nchar) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper(leftarg sys.varchar, rightarg sys.nvarchar) STABLE;
+ALTER FUNCTION sys.babelfish_concat_wrapper(leftarg sys.nvarchar, rightarg sys.varchar) STABLE;
+ALTER FUNCTION sys.tinyintxor(leftarg sys.tinyint, rightarg sys.tinyint) STABLE;
+ALTER FUNCTION sys.int2xor(leftarg int2, rightarg int2) STABLE;
+ALTER FUNCTION sys.intxor(leftarg int4, rightarg int4) STABLE;
+ALTER FUNCTION sys.int8xor(leftarg int8, rightarg int8) STABLE;
+ALTER FUNCTION sys.bitxor(leftarg pg_catalog.bit, rightarg pg_catalog.bit) STABLE;
+ALTER FUNCTION sys.newid() STABLE;
+ALTER FUNCTION sys.NEWSEQUENTIALID() STABLE;
+ALTER FUNCTION sys.babelfish_typecode_list() STABLE;
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
