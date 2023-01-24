989194242be9c4ca0a78124a7c5a82148f1dc236
Walt Boettge <wboettge@amazon.com>
2023-01-24 21:08:55 +0000

    Add sp_set_session_context and SESSION_CONTEXT
    Adds support for the sp_set_session_context stored procedure and the
    SESSION_CONTEXT function. Any user can now store and load key-value
    pairs local to the current session.
    
    Task: BABEL-3800
    
    Signed-off-by: Walt Boettge <wboettge@amazon.com>

--- contrib/babelfishpg_tsql/src/tsqlUnsupportedFeatureHandler.cpp
+++ contrib/babelfishpg_tsql/src/tsqlUnsupportedFeatureHandler.cpp
@@ -1531,7 +1531,6 @@ const char *unsupported_sp_procedures[] = {
 	"sp_resetstatus",
 	"sp_sequence_get_range",
 	"sp_serveroption",
-	"sp_set_session_context",
 	"sp_setnetname",
 	"sp_settriggerorder",
 	"sp_spaceused",
