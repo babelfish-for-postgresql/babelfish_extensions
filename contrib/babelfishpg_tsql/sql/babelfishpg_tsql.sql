31a7c7eff0e565b1ebeff48cd7b4276c6211bd72
Walt Boettge <wboettge@amazon.com>
2023-01-26 20:37:12 +0000

    Clean up changes
    Make error formatting consistent. Make session_context always lowercase.
    
    Signed-off-by: Walt Boettge <wboettge@amazon.com>

--- contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
+++ contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
@@ -2548,7 +2548,7 @@ GRANT EXECUTE ON PROCEDURE sys.sp_set_session_context TO PUBLIC;
 
 CREATE OR REPLACE FUNCTION sys.session_context ("@key" sys.sysname)
 	RETURNS sys.SQL_VARIANT AS 'babelfishpg_tsql', 'SESSION_CONTEXT' LANGUAGE C;
-GRANT EXECUTE ON FUNCTION sys.SESSION_CONTEXT TO PUBLIC;
+GRANT EXECUTE ON FUNCTION sys.session_context TO PUBLIC;
 
 
 CREATE OR REPLACE VIEW sys.sp_sproc_columns_view
