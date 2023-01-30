9a5dfee35157bcf3f9423bf37fee48abad5f60d6
Dipesh Dhameliya <dddhamel@amazon.com>
2023-01-30 13:27:43 +0530

    Fixed test cases

--- test/JDBC/input/Test_user_from_win_login-vu-prepare.sql
+++ test/JDBC/input/Test_user_from_win_login-vu-prepare.sql
@@ -1,8 +1,2 @@
 create login [pnq\admin] from windows;
 GO
-
-exec babelfish_add_domain_mapping_entry 'def', 'def.internal'
-GO
-
-create login [def\test] from windows;
-GO
--- test/JDBC/input/Test_user_from_win_login-vu-verify.sql
+++ test/JDBC/input/Test_user_from_win_login-vu-verify.sql
@@ -4,23 +4,8 @@ GO
 select login_name from sys.babelfish_authid_user_ext where rolname = 'master_win_admin';
 GO
 
-create user win_test for login [def\test]
-GO
-
-select login_name from sys.babelfish_authid_user_ext where rolname = 'master_win_test';
-GO
-
-drop user win_test;
-GO
-
 drop user win_admin;
 GO
 
-drop login [def\test];
-GO
-
 drop login [pnq\admin];
-GO
-
-exec babelfish_truncate_domain_mapping_table;
 GO
