8aa8fa71b15da5a060cd677ba81adaf4af09fce3
Harsh Lunagariya <lunharsh@amazon.com>
2023-01-31 09:30:57 +0000

    Add dependency tests
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>

---
+++ test/JDBC/input/views/isc-schemata-vu-cleanup.sql
@@ -0,0 +1,11 @@
+DROP SCHEMA isc_schemata_vu_prepare_sch;
+GO
+
+DROP VIEW isc_schemata_vu_prepare_view
+GO
+
+DROP PROCEDURE isc_schemata_vu_prepare_proc
+GO
+
+DROP FUNCTION isc_schemata_vu_prepare_func
+GO
