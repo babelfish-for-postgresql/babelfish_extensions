4ec753f94b760e5f5923b8d8313b06bf1308d996
Jake Owen <jakeowen1@gmail.com>
2023-02-03 18:45:45 -0500

    TRY_CONVERT AND TRY_CAST must raise error for prohibted int literal casts/conversions (#1219)
    Currently TRY_CONVERT and TRY_CAST do not raise errors for prohibited
    conversions using int and float literals in Babelfish. This commit adds
    support for this form of error handling, as well as expressions
    
    Task: Babel-3748
    Signed-off-by: Jake Owen <owjco@amazon.com>

--- contrib/babelfishpg_tsql/src/backend_parser/gram-tsql-epilogue.y.c
+++ contrib/babelfishpg_tsql/src/backend_parser/gram-tsql-epilogue.y.c
@@ -301,17 +301,18 @@ TsqlFunctionTryCast(Node *arg, TypeName *typename, int location)
         }
         else
         {
-                /* Cast null to typename to take advantage of polymorphic types in Postgres */
-                Node *null_const = makeTypeCast(makeNullAConst(location), typename, location);
+                Node *targetType = makeTypeCast(makeNullAConst(location), typename, location);
                 List *args;
-                if(arg->type == T_TypeCast || arg->type == T_FuncCall)
+                switch(arg->type)
                 {
-                    args = list_make3(arg, null_const, makeIntConst(typmod, location));
-                }
-                else
-                {
-                    Node *arg_const = makeTypeCast(arg, SystemTypeName("text"), location);
-                    args = list_make3(arg_const, null_const, makeIntConst(typmod, location));
+                    case T_A_Const:
+                    case T_TypeCast:
+                    case T_FuncCall:
+                    case T_A_Expr:
+                        args = list_make3(arg, targetType, makeIntConst(typmod, location));
+                        break;
+                    default:
+                        args = list_make3(makeTypeCast(arg, makeTypeName("text"), location), targetType, makeIntConst(typmod, location));
                 }
 
                 result = (Node *) makeFuncCall(TsqlSystemFuncName("babelfish_try_cast_to_any"), args, COERCE_EXPLICIT_CALL, location);
