5d9e48fa69280183b8585699ad2138eaca92c3b5
Sumit Jaiswal <54075285+sumitj824@users.noreply.github.com>
2023-02-03 20:00:43 +0530

    Fix the default output style for date and time types in CONVERT function (#1203)
    Earlier when we were converting from DATE/TIME types to VARCHAR type using CONVERT function, The default output style was incorrect. 
    
    This commit fix the default output style for date and time types in CONVERT function.
    
    Task: BABEL-3938
    Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

---
+++ test/JDBC/input/BABEL-3938-vu-verify.sql
@@ -0,0 +1,32 @@
+SELECT * FROM babel_3938_vu_prepare_view1
+GO
+
+SELECT * FROM babel_3938_vu_prepare_view2
+GO
+
+EXEC babel_3938_vu_prepare_proc1
+GO
+
+EXEC babel_3938_vu_prepare_proc2
+GO
+
+SELECT * FROM babel_3938_vu_prepare_func1()
+GO
+
+SELECT * FROM babel_3938_vu_prepare_func2()
+GO
+
+SELECT * FROM babel_3938_vu_prepare_view3
+GO
+
+SELECT * FROM babel_3938_vu_prepare_view4
+GO
+
+SELECT * FROM babel_3938_vu_prepare_view5
+GO
+
+SELECT * FROM babel_3938_vu_prepare_view6
+GO
+
+SELECT * FROM babel_3938_vu_prepare_view7
+GO
