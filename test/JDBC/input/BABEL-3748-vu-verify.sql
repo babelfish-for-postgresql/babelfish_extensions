b43153cf23645ca74c68c57887b881aae01c147f
Jake Owen <jakeowen1@gmail.com>
2023-02-03 18:45:45 -0500

    TRY_CONVERT AND TRY_CAST must raise error for prohibted int literal casts/conversions (#1219)
    Currently TRY_CONVERT and TRY_CAST do not raise errors for prohibited
    conversions using int and float literals in Babelfish. This commit adds
    support for this form of error handling, as well as expressions
    
    Task: Babel-3748
    Signed-off-by: Jake Owen <owjco@amazon.com>

---
+++ test/JDBC/input/BABEL-3748-vu-verify.sql
@@ -0,0 +1,164 @@
+SELECT * FROM BABEL_3748_vu_prepare_v1
+GO
+DROP VIEW BABEL_3748_vu_prepare_v1
+GO
+
+EXEC BABEL_3748_vu_prepare_p1
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p1
+GO
+
+SELECT BABEL_3748_vu_prepare_f1()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f1()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v2
+GO
+DROP VIEW BABEL_3748_vu_prepare_v2
+GO
+
+EXEC BABEL_3748_vu_prepare_p2
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p2
+GO
+
+SELECT BABEL_3748_vu_prepare_f2()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f2()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v3
+GO
+DROP VIEW BABEL_3748_vu_prepare_v3
+GO
+
+EXEC BABEL_3748_vu_prepare_p3
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p3
+GO
+
+SELECT BABEL_3748_vu_prepare_f3()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f3()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v4
+GO
+DROP VIEW BABEL_3748_vu_prepare_v4
+GO
+
+EXEC BABEL_3748_vu_prepare_p4
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p4
+GO
+
+SELECT BABEL_3748_vu_prepare_f4()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f4()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v5
+GO
+DROP VIEW BABEL_3748_vu_prepare_v5
+GO
+
+EXEC BABEL_3748_vu_prepare_p5
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p5
+GO
+
+SELECT BABEL_3748_vu_prepare_f5()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f5()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v6
+GO
+DROP VIEW BABEL_3748_vu_prepare_v6
+GO
+
+EXEC BABEL_3748_vu_prepare_p6
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p6
+GO
+
+SELECT BABEL_3748_vu_prepare_f6()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f6()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v7
+GO
+DROP VIEW BABEL_3748_vu_prepare_v7
+GO
+
+EXEC BABEL_3748_vu_prepare_p7
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p7
+GO
+
+SELECT BABEL_3748_vu_prepare_f7()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f7()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v8
+GO
+DROP VIEW BABEL_3748_vu_prepare_v8
+GO
+
+EXEC BABEL_3748_vu_prepare_p8
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p8
+GO
+
+SELECT BABEL_3748_vu_prepare_f8()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f8()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v9
+GO
+DROP VIEW BABEL_3748_vu_prepare_v9
+GO
+
+EXEC BABEL_3748_vu_prepare_p9
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p9
+GO
+
+SELECT BABEL_3748_vu_prepare_f9()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f9()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v10
+GO
+DROP VIEW BABEL_3748_vu_prepare_v10
+GO
+
+EXEC BABEL_3748_vu_prepare_p10
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p10
+GO
+
+SELECT BABEL_3748_vu_prepare_f10()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f10()
+GO
+
+SELECT * FROM BABEL_3748_vu_prepare_v11
+GO
+DROP VIEW BABEL_3748_vu_prepare_v11
+GO
+
+EXEC BABEL_3748_vu_prepare_p11
+GO
+DROP PROCEDURE BABEL_3748_vu_prepare_p11
+GO
+
+SELECT BABEL_3748_vu_prepare_f11()
+GO
+DROP FUNCTION BABEL_3748_vu_prepare_f11()
+GO
