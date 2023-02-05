6a1b6dfaf18f8dd66d226004ec6188e219864814
Tim Chang <changti@amazon.com>
2023-02-05 20:44:38 +0000

    Support schema

--- contrib/babelfishpg_tsql/src/tsqlIface.cpp
+++ contrib/babelfishpg_tsql/src/tsqlIface.cpp
@@ -1709,8 +1709,16 @@ public:
 		is_function = true;
 		if (ctx->NEXT() && ctx->full_object_name())
 		{
-			std::string seq_name = ::getFullText(ctx->full_object_name());
+			TSqlParser::Full_object_nameContext *fctx = (TSqlParser::Full_object_nameContext *) ctx->full_object_name();
+			std::string seq_name = ::getFullText(fctx);
 			std::string nextval_string = "nextval('" + seq_name + "')";
+			if (fctx->schema)
+			{
+				TSqlParser::IdContext *sctx = fctx->schema;
+				TSqlParser::IdContext *octx = fctx->object_name;
+				// Need to directly use the backend schema name since nextval is a postgres function
+				nextval_string = "nextval('master_" + ::stripQuoteFromId(sctx) + '.' + ::stripQuoteFromId(octx) + "')";
+			}
 
 			rewritten_query_fragment.emplace(std::make_pair(ctx->NEXT()->getSymbol()->getStartIndex(), std::make_pair(::getFullText(ctx->NEXT()), "")));
 			rewritten_query_fragment.emplace(std::make_pair(ctx->VALUE()->getSymbol()->getStartIndex(), std::make_pair(::getFullText(ctx->VALUE()), "")));
