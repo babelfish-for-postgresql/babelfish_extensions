2180e27991be9f9b0f4a515d23f7ee53225a1309
Dipesh Dhameliya <dipeshdhameliya125@gmail.com>
2023-02-07 19:26:02 +0530

    Avoid dumping COLLATE default for const clause (#1222)
    Previously, During v2.3.0 release, we fixed the most of collatable data types such as varchar to have the correct default collation same as server_collation_name. This fix may lead to dump extra COLLATE "default" clause for const clause during upgrade from 14.x to 15.1 if server is upgrade to 14.6 via minor version upgrade first.
    
    To fix this, we have introduced new hook handle_const_collation_hook on engine side and it would be initialised to print_const_collation inside babelfishpg_common extension which would determine if COLLATE needs to be dumped or not for given const clause. 
    
    Task: BABEL-3895
    Engine PR - https://github.com/babelfish-for-postgresql/postgresql_modified_for_babelfish/pull/96
    Signed-off-by: Dipesh Dhameliya <dddhamel@amazon.com>

--- contrib/babelfishpg_common/src/babelfishpg_common.c
+++ contrib/babelfishpg_common/src/babelfishpg_common.c
@@ -10,6 +10,7 @@
 #include "parser/parse_target.h"
 #include "parser/scansup.h"  /* downcase_identifier */
 #include "utils/guc.h"
+#include "utils/ruleutils.h"
 
 #include "babelfishpg_common.h"
 #include "collation.h"
@@ -131,6 +132,8 @@ _PG_init(void)
 
 	prev_PreCreateCollation_hook = PreCreateCollation_hook;
 	PreCreateCollation_hook = BabelfishPreCreateCollation_hook;
+
+	get_tsql_const_collation_hook = get_tsql_const_collation;
 }
 void
 _PG_fini(void)
@@ -141,6 +144,7 @@ _PG_fini(void)
 	CLUSTER_COLLATION_OID_hook = prev_CLUSTER_COLLATION_OID_hook;
 	TranslateCollation_hook = prev_TranslateCollation_hook;
 	PreCreateCollation_hook = prev_PreCreateCollation_hook;
+	get_tsql_const_collation_hook = NULL;
 }
 
 common_utility_plugin *
