8a792e7d44ee72ffe458ace71ad402636b40df62
Nirmit Shah <44708230+shah-nirmit@users.noreply.github.com>
2023-02-08 12:28:14 +0530

    Fixed index_id columns for index_columns to match sys.indexes (#1250)
    Signed-off-by: Nirmit Shah <nirmisha@amazon.com>
    Co-authored-by: Nirmit Shah <nirmisha@amazon.com>

--- test/JDBC/input/views/sys-index_columns-vu-verify.sql
+++ test/JDBC/input/views/sys-index_columns-vu-verify.sql
@@ -13,5 +13,8 @@ GO
 SELECT count(*) FROM  sys.index_columns idx JOIN sys.tables tab ON idx.object_id = tab.object_id WHERE tab.name = 'sys_index_columns_vu_prepare_t2';
 GO
 
+SELECT count(*) from sys.index_columns ic, sys.indexes i where ic.object_id = i.object_id and ic.index_id = i.index_id and i.name like 'sys_index_columns_vu_prepare_i3%'
+GO
+
 SELECT count(*) FROM  sys.index_columns idx JOIN sys.tables tab ON idx.object_id = tab.object_id WHERE tab.name = 'sys_index_columns_vu_prepare_t3';
 GO
