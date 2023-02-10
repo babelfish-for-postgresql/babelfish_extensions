39e49e1d04b51800402142dbb1d1cb6e833330c2
Rishabh Tanwar <33982749+rishabhtanwar29@users.noreply.github.com>
2023-02-10 15:03:42 +0530

    Correctly mark Babelfish configuration tables (#45)
    In all previous Babelfish versions, we have incorrectly marked some extension
    configuration tables as follows:
     * 1. Table sys.babelfish_authid_user_ext has not been marked as config table.
     * 2. Table sys.babelfish_configurations has been marked as configuration table
    but it is not supposed to.
    
    Due to the above fact, we observe following issues on a restored server:
    1. sys.babelfish_authid_user_ext catalog table turns out to be empty due to
    which we see some obvious catalog inconsistencies.
    2. sys.babelfish_configurations shows duplicate entries as one entry come
    from extension's installation file and other comes from dump file.
    
    We fix this issue as follows:
    1. For old Babelfish versions, modified pg_dump to forcefully dump
    sys.babelfish_authid_user_ext table's data but skip dumping the data of
    sys.babelfish_configurations table. Introduced function `updateExtConfigArray`
    for this purpose.
    2. For new Babelfish versions, marked sys.babelfish_authid_user_ext table as
    configuration table using `pg_extension_config_dump` function and unmark
    sys.babelfish_configurations table using `pg_extension_config_remove`.
    
    Note that postgres has not exposed `pg_extension_config_remove` as SQL
    function so we have implemented a SQL wrapper function in the extension
    for it.
    
    * Added a github action framework to test pg_dump and restore which is quite
    similar to existing MVU framework and shares most of it's implementation.
    * Currently, we are testing pg_dump-restore from latest -> latest release and
    running almost all the MVU tests except few tests which we have temporarily
    disabled in `dump-restore-util` github action.
    * We will enable more pg_dump-restore paths in future as we fix some known
    issues.
    
    Task: BABEL-3825
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>

--- test/JDBC/input/babelfish_integrity_checker-vu-verify.sql
+++ test/JDBC/input/babelfish_integrity_checker-vu-verify.sql
@@ -6,3 +6,9 @@ GO
 
 EXEC babelfish_integrity_checker_vu_prepare_proc;
 GO
+
+-- List down all configuration tables of babelfishpg_tsql extension
+SELECT relname FROM pg_class WHERE oid IN
+	(SELECT unnest(extconfig) FROM pg_extension WHERE extname = 'babelfishpg_tsql')
+	ORDER BY relname;
+GO
