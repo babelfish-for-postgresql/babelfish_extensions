13ab5f4340e3476ffc1e1e3679951fdaf07c12cd
pratikzode <pratik.zode@gmail.com>
2023-02-13 20:26:12 +0000

    BABEL-3802 Error Fix for smallint
    As INT, we still error out on values well within the range of an INT.
    Eg
    1> SELECT POWER( CAST( 100 AS SMALLINT ), 3)
    2> go
    Msg 220, Level 16, State 1, Server persist-i-sksql-use1-20230207-15latest, Line 1
    smallint out of range
    
    Now its showing same output as SQL server.
    1> SELECT POWER( CAST( 100 AS SMALLINT ), 3)
    2> go
    power
    -----------
     1000000
    
    Task: BABEL-3802
    Signed-off-by: pratikzode <pratik.zode@gmail.com>

---
+++ test/JDBC/input/babel-3802-vu-verify.sql
@@ -0,0 +1,17 @@
+SELECT power(a,b), power(b,b), power(c,b), power(d,b), power(e,b) from babel_3802_vu_prepare_t_1 ORDER BY 1
+GO
+
+EXECUTE babel_3802_vu_prepare_t_p1
+GO
+
+EXECUTE babel_3802_vu_prepare_t_p2
+GO
+
+EXECUTE babel_3802_vu_prepare_t_p3
+GO
+
+SELECT * FROM babel_3802_vu_prepare_t_v1
+GO
+
+SELECT * FROM babel_3802_vu_prepare_t_v1
+GO
