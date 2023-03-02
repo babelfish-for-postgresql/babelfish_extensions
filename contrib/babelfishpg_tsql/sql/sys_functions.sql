9b7f11b099ce42373e2a40233bf193167638c2a1
pratikzode <pratik.zode@gmail.com>
2023-03-02 15:41:08 +0000

    BABEL-1199 Modify the function
    Task: BABEL-1199
    Signed-off-by: pratikzode <pratik.zode@gmail.com>

--- contrib/babelfishpg_tsql/sql/sys_functions.sql
+++ contrib/babelfishpg_tsql/sql/sys_functions.sql
@@ -1707,16 +1707,10 @@ LANGUAGE SQL IMMUTABLE STRICT PARALLEL SAFE;
 CREATE OR REPLACE FUNCTION sys.rowcount()
 RETURNS INT AS 'babelfishpg_tsql' LANGUAGE C STABLE;
 
-CREATE OR REPLACE FUNCTION Database_principal_id(user_name sysname DEFAULT NULL)
-RETURNS OID AS $$
-BEGIN
-    IF user_name IS NULL THEN
-        RETURN (SELECT oid FROM pg_roles WHERE rolname = current_user collate "C");
-    ELSE
-        RETURN sys.user_id(user_name collate "C");
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION Database_principal_id(IN user_name TEXT DEFAULT NULL)
+RETURNS OID
+AS 'babelfishpg_tsql', 'user_id'
+LANGUAGE C IMMUTABLE PARALLEL SAFE;
 
 CREATE OR REPLACE FUNCTION sys.error()
 	   RETURNS INT AS 'babelfishpg_tsql' LANGUAGE C STABLE;
--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.0.0--3.1.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.0.0--3.1.0.sql
@@ -842,16 +842,10 @@ CREATE OR REPLACE FUNCTION sys.radians(IN arg1 NUMERIC)
 RETURNS numeric  AS 'babelfishpg_tsql','numeric_radians' LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;
 GRANT EXECUTE ON FUNCTION sys.radians(NUMERIC) TO PUBLIC;
 
-CREATE OR REPLACE FUNCTION Database_principal_id(user_name sysname DEFAULT NULL)
-RETURNS OID AS $$
-BEGIN
-    IF user_name IS NULL THEN
-        RETURN (SELECT oid FROM pg_roles WHERE rolname = current_user collate "C");
-    ELSE
-        RETURN sys.user_id(user_name collate "C");
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION Database_principal_id(IN user_name TEXT DEFAULT NULL)
+RETURNS OID
+AS 'babelfishpg_tsql', 'user_id'
+LANGUAGE C IMMUTABLE PARALLEL SAFE;
 
 CREATE OR REPLACE VIEW sys.partitions AS
 SELECT
