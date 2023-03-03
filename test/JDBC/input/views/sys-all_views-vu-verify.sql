832a7697fe7c49e5e10a782da68779177b484a90
lshalini <lshalini@amazon.com>
2023-03-03 07:53:48 +0000

    Fix the column "is_ms_shipped" of the view "sys.all_views"
    The column "is_ms_shipped" of the view "sys.all_views" should be correctly set to 0 or 1 based on the view being a system view or a user created view.
    
    Currently, is_ms_shipped is hardcoded to "0" which means SSMS treats all the system views as User defined views which is incorrect.
    
    Task: BABEL-4040
    Signed-off-by: Shalini Lohia <lshalini@amazon.com>

--- test/JDBC/input/views/sys-all_views-vu-verify.sql
+++ test/JDBC/input/views/sys-all_views-vu-verify.sql
@@ -50,5 +50,26 @@ FROM sys.all_views
 WHERE name = 'sys_all_views_select_chk_option_vu_prepare'
 GO
 
+-- query a system view
+SELECT
+    name
+  , principal_id
+  , type
+  , type_desc
+  , create_date
+  , modify_date
+  , is_ms_shipped
+  , is_published
+  , is_schema_published
+  , is_replicated
+  , has_replication_filter
+  , has_opaque_metadata
+  , has_unchecked_assembly_data
+  , with_check_option
+  , is_date_correlation_view
+FROM sys.all_views
+WHERE name = 'all_views'
+GO
+
 SELECT COUNT(*) FROM sys.all_columns WHERE object_id = object_id('sys.all_views');
 GO
