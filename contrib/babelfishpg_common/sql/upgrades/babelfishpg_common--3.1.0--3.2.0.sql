06020dfda48412d7c1880b66efaca70b79e919f8
Zhenye Li <zhenye@amazon.com>
2023-03-06 19:26:09 +0000

    Drop CAST before change it to implicit
    Signed-off-by: Zhenye Li <zhenye@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--3.1.0--3.2.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--3.1.0--3.2.0.sql
@@ -3,7 +3,8 @@
 
 SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
 
-ALTER CAST (NUMERIC AS sys.BIT) WITH FUNCTION sys.numeric_bit (NUMERIC) AS IMPLICIT;
+DROP CAST IF EXISTS(NUMERIC AS sys.BIT);
+CREATE CAST (NUMERIC AS sys.BIT) WITH FUNCTION sys.numeric_bit (NUMERIC) AS IMPLICIT;
 
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
