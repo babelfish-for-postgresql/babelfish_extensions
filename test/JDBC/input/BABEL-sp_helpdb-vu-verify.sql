6522c6fecbe8a9d000ba1b16bab9b0becd195191
Yanjie Xu <yanjiex@amazon.com>
2023-03-10 16:34:11 -0800

    Made dbname arg in sp_heapdb case-insensitive
    Previously sp_helpdb in babelfish treats dbname as case-sensitive
    whereas tsql treats the dbname argument as case-insensitive. This
    commit implemented a str_to_lower function for dbname and resol-
    ved the case-senstive issue.
    
    Task: BABEL-3643
    Signed-off-by: Yanjie Xu <yanjiex@amazon.com>

--- test/JDBC/input/BABEL-sp_helpdb-vu-verify.sql
+++ test/JDBC/input/BABEL-sp_helpdb-vu-verify.sql
@@ -1,14 +1,27 @@
-SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb() WHERE name IN ('master', 'babel_sp_helpdb_db');
+SELECT name, compatibility_level FROM sys.babelfish_helpdb() WHERE name IN ('master', 'babel_sp_helpdb_db');
 GO
 
 -- Executing sp_helpdb with already existing dbname as an input
-SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('master');
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('master');
 GO
-SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('babel_sp_helpdb_db');
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('babel_sp_helpdb_db');
 GO
 
 -- Executing sp_helpdb with wrong input
-SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('abc');
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('abc');
 GO
-SELECT name, db_size, owner, status, compatibility_level FROM sys.babelfish_helpdb('  wrongInput');
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('  wrongInput');
 GO
+
+-- Executing sp_helpdb with a existing dbname but in mixed upper, lower cases as an input
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('MaSteR');
+GO
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('bAbeL_sP_helPdb_Db');
+GO
+
+-- Executing sp_helpdb with a existing dbname but end with trailing spaces as an input
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('MaSteR        ');
+GO
+SELECT name, compatibility_level FROM sys.babelfish_helpdb('bAbeL_sP_helPdb_Db ');
+GO
+
