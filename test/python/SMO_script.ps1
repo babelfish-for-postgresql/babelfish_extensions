ff70d0eb70d145bbf39124aeaf9ba855ed64cf08
Ashish Prasad <pashisht@amazon.com>
2023-03-16 19:14:39 +0000

    Fix script condition

--- test/python/SMO_script.ps1
+++ test/python/SMO_script.ps1
@@ -14,31 +14,14 @@ $SmoServer.ConnectionContext.set_Login($paramsArray[3])
 $SmoServer.ConnectionContext.set_Password($paramsArray[4])
 $db = $SmoServer.Databases[$paramsArray[2]]   
 
-$schm = "sys"
-$dtb = "sysdatabases"
-
 $Objects = $db.Tables                                                                
 $Objects += $db.Views         
 $Objects += $db.StoredProcedures
 $Objects += $db.UserDefinedFunctions
-$SubObjects += $db.Tables.Indexes
-$SubObjects += $db.Tables.Triggers
+$Objects += $db.Tables.Indexes
+$Objects += $db.Tables.Triggers
 
 foreach ($CurrentObject in $Objects) 
-{     
-    if ($CurrentObject.schema -ne $schm -and $CurrentObject.schema -ne $dtb -and $CurrentObject.schema -ne $null -and -not $CurrentObject.IsSystemObject )
-        { 
-            $Scripter = New-Object ('Microsoft.SqlServer.Management.Smo.Scripter') ($SmoServer)
-            $Scripter.Options.DriAll = $True;
-            $Scripter.Options.ScriptSchema = $True;
-            $Scripter.Options.ScriptData  = $False;
-            $Scripter.Options.NoCollation = $True;
-            $Scripter.Script($CurrentObject);
-            Write-Output "GO`n" 
-        }
-}
-
-foreach ($CurrentObject in $SubObjects) 
 {     
     if (-not $CurrentObject.IsSystemObject )
         { 
