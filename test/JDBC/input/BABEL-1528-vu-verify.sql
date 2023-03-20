9d9236b2590d6b54b4ebe7e630fe662aba56f63a
Satarupa Biswas <satarupb@amazon.com>
2023-03-20 16:57:43 +0000

    CASTing a DATE literal without time part to TIME datatype fails
    For time datatype PG considers only date value(ex.- '2012-02-23') as bad i/p format. so, above testcase is throwing error from engine code time_in() much before coming to TDS side.
    However, SQL Server allows casting a date literal string to a TIME datatype when the time component in the string is missing: o/p becomes 00:00:00.0000000
    Fix is to add change in time_in() so that it considers date format with missing time info like above one, as valid input when the dialect is TSQL.
    
    Task: BABEL-1528
    Signed-off-by: Satarupa Biswas satarupb@amazon.com

---
+++ test/JDBC/input/BABEL-1528-vu-verify.sql
@@ -0,0 +1,14 @@
+select CAST('2012-02-23' AS time)
+GO
+
+select * from BABEL_1528_vu_prepare_t1
+GO
+
+select BABEL_1528_vu_prepare_f1()
+GO
+
+exec BABEL_1528_vu_prepare_p1
+GO
+
+select * FROM BABEL_1528_vu_prepare_v1
+GO
