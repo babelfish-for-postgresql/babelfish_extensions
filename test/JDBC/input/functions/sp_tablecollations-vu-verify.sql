204d11ade1dbe1e98da869cbf03faad6be248edc
Aditya Verma <adiityav@amazon.com>
2023-03-22 12:35:22 +0000

    added test case to verify if column datatype after change is correct
    Signed-off-by: Aditya Verma <adiityav@amazon.com>

--- test/JDBC/input/functions/sp_tablecollations-vu-verify.sql
+++ test/JDBC/input/functions/sp_tablecollations-vu-verify.sql
@@ -1,3 +1,14 @@
 exec sp_tablecollations_100 'foo'
 GO
 
+select 
+    case when t.typname = 'sysname' then 'true' 
+        else 'false' end
+from
+    pg_attribute a left join 
+    pg_class c on a.attrelid = c.oid left join 
+    pg_type t on a.atttypid = t.oid 
+where 
+    c.relname = 'spt_tablecollations_view' 
+    and a.attname = 'name';
+GO
