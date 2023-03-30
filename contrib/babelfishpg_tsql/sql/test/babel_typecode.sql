ef87fafa9f76c0d8b3c2fd0a5436301a88672b59
pratikzode <73869399+pratikzode@users.noreply.github.com>
2023-03-30 08:27:24 -0700

    TES2_14 Moving Test to JDBC (#1367)
    * TES2_14 Moving Test to JDBC
    
    Moved
    babel_219,
    babel_collection,
    babel_datatype,
    babel_ddl,
    babel_delete1,
    babel_function,
    babel_like,
    babel_typecode,
    babel_uniqueidentifier
    babel_set_command
    babel_table_type
    babel_transaction
    babel_emoji
    to JDBC
    And removed it from babelfish_extensions/contrib/babelfishpg_tsql/sql/test directory
    
    Signed-off-by: pratikzode <pratik.zode@gmail.com>

--- contrib/babelfishpg_tsql/sql/test/babel_typecode.sql
+++
@@ -1,4 +0,0 @@
-SET babelfishpg_tsql.sql_dialect = 'tsql';
-
--- test typecode list sys table
-SELECT pg_namespace, pg_typname, tsql_typname, type_family_priority, priority, sql_variant_hdr_size FROM sys.babelfish_typecode_list();
