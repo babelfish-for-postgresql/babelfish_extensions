dc029e9909c27424ea230cbed66dd145f5e12ad6
Dipesh Dhameliya <dipeshdhameliya125@gmail.com>
2023-04-04 11:52:51 +0530

    Update the extension version to v2.5.0 and Fixed test case failure related to sp_databases (#1379)
    Output of the system procedure, sp_databases contains the size of the database which is subjected to vary. Hence, it was causing the test failures unexpectedly.
    
    This commit adds a minor improvement to the definition of the view sys.sp_databases_view to consider only physical relations to calculate the database size. We are temporarily disabling the test case for sp_databases to fix the test flakiness arising due to database size.
    
    This commit also updates the extension version to v2.5.0.
    
    Task: BABEL-4052
    Signed-off-by: Dipesh Dhameliya <dddhamel@amazon.com>

---
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.4.0--2.5.0.sql
@@ -0,0 +1,29 @@
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_tsql"" UPDATE TO '2.5.0'" to load this file. \quit
+
+-- add 'sys' to search path for the convenience
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+CREATE OR REPLACE VIEW sys.sp_databases_view AS
+	SELECT CAST(database_name AS sys.SYSNAME),
+	-- DATABASE_SIZE returns a NULL value for databases larger than 2.15 TB
+	CASE WHEN (sum(table_size)/1024.0) > 2.15 * 1024.0 * 1024.0 * 1024.0 THEN NULL
+		ELSE CAST((sum(table_size)/1024.0) AS int) END as database_size,
+	CAST(NULL AS sys.VARCHAR(254)) as remarks
+	FROM (
+		SELECT pg_catalog.pg_namespace.oid as schema_oid,
+		pg_catalog.pg_namespace.nspname as schema_name,
+		INT.name AS database_name,
+		coalesce(pg_relation_size(pg_catalog.pg_class.oid), 0) as table_size
+		FROM
+		sys.babelfish_namespace_ext EXT
+		JOIN sys.babelfish_sysdatabases INT ON EXT.dbid = INT.dbid
+		JOIN pg_catalog.pg_namespace ON pg_catalog.pg_namespace.nspname = EXT.nspname
+		LEFT JOIN pg_catalog.pg_class ON relnamespace = pg_catalog.pg_namespace.oid where pg_catalog.pg_class.relkind = 'r'
+	) t
+	GROUP BY database_name
+	ORDER BY database_name;
+GRANT SELECT on sys.sp_databases_view TO PUBLIC;
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "2.4.0"
+#define BABELFISH_VERSION_STR "2.5.0"
 #define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.7.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
