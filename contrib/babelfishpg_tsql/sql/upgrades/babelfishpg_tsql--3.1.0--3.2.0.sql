fc5360d5d54e96c7710b9b33218404b1dad36874
Deepakshi Mittal <depakshi@amazon.com>
2023-04-05 23:48:53 +0000

    Added explicit casting
    Task: BABEL-2476
    
    Signed-off-by: Deepakshi Mittal <depakshi@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.1.0--3.2.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.1.0--3.2.0.sql
@@ -1469,39 +1469,39 @@ CALL sys.babelfish_drop_deprecated_object('view', 'sys', 'database_principals_de
 -- SYSLOGINS
 CREATE OR REPLACE VIEW sys.syslogins
 AS SELECT 
-CAST(CAST(Base.principal_id as INT) as sys.varbinary(85)) AS sid,
-CAST(0 AS SYS.TINYINT) as status,
+CAST(Base.sid AS sys.varbinary(85)) AS sid,
+CAST(0 AS SYS.TINYINT) AS status,
 CAST(Base.create_date AS SYS.DATETIME) AS createdate,
 CAST(Base.modify_date AS SYS.DATETIME) AS updatedate,
 CAST(Base.create_date AS SYS.DATETIME) AS accdate,
-CAST(0 AS INT) as totcpu,
-CAST(0 AS INT) as totio,
-CAST(0 AS INT) as spacelimit,
-CAST(0 AS INT) as timelimit,
-CAST(0 AS INT) as resultlimit,
+CAST(0 AS INT) AS totcpu,
+CAST(0 AS INT) AS totio,
+CAST(0 AS INT) AS spacelimit,
+CAST(0 AS INT) AS timelimit,
+CAST(0 AS INT) AS resultlimit,
 CAST(Base.name AS sys.SYSNAME) AS name,
 CAST(Base.default_database_name AS SYS.SYSNAME) AS dbname,
 CAST(Base.default_language_name AS SYS.SYSNAME) AS default_language_name,
 CAST(Base.name AS SYS.NVARCHAR(128)) AS loginname,
 CAST(NULL AS SYS.NVARCHAR(128)) AS password,
-CAST(0 AS INT) as denylogin,
-CAST(1 AS INT) as hasaccess,
-CAST(0 AS INT) as isntname,
-CAST(0 AS INT) as isntgroup,
-CAST(0 AS INT) as isntuser,
+CAST(0 AS INT) AS denylogin,
+CAST(1 AS INT) AS hasaccess,
+CAST(0 AS INT) AS isntname,
+CAST(0 AS INT) AS isntgroup,
+CAST(0 AS INT) AS isntuser,
 CAST(
     CASE
         WHEN pg_has_role(CAST('sysadmin' AS TEXT), CAST(Base.name AS TEXT), 'MEMBER') = true THEN 1
         ELSE 0
     END
 AS INT) AS sysadmin,
-CAST(0 AS INT) as securityadmin,
-CAST(0 AS INT) as serveradmin,
-CAST(0 AS INT) as setupadmin,
-CAST(0 AS INT) as processadmin,
-CAST(0 AS INT) as diskadmin,
-CAST(0 AS INT) as dbcreator,
-CAST(0 AS INT) as bulkadmin
+CAST(0 AS INT) AS securityadmin,
+CAST(0 AS INT) AS serveradmin,
+CAST(0 AS INT) AS setupadmin,
+CAST(0 AS INT) AS processadmin,
+CAST(0 AS INT) AS diskadmin,
+CAST(0 AS INT) AS dbcreator,
+CAST(0 AS INT) AS bulkadmin
 FROM sys.server_principals AS Base;
 
 GRANT SELECT ON sys.syslogins TO PUBLIC;
