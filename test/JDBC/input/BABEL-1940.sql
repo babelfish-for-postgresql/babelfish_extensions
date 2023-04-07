96174994f22f98de7a9649eb5052b6dbc7eabcb7
Sumit Jaiswal <sumiji@amazon.com>
2023-04-07 11:16:19 +0000

    Added test for different collation
    Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

--- test/JDBC/input/BABEL-1940.sql
+++ test/JDBC/input/BABEL-1940.sql
@@ -33,5 +33,23 @@ GO
 SELECT CAST(a as VARCHAR(MAX)) FROM babel_1940_t1
 GO
 
+
+create table babel_1940_t2( a varchar(20) collate japanese_cs_as);
+GO
+
+insert into babel_1940_t2 values ('ｳ'), ('C'), ('ﾊﾟ'), ('３'), ('c'), ('ｲ'), ('Ｃ'),('ﾊ'),
+('1'), ('ｱ'),('パ'), ('b'), ('2'), ('B'),('１'), ('Ａ'),('ア'),('A'), ('a'),('AbC'), ('aBc');
+GO
+
+
+SELECT CONVERT(varbinary(MAX), a) FROM babel_1940_t2
+GO
+
+SELECT CONVERT(varchar(MAX), CONVERT(varbinary(MAX), a)) FROM babel_1940_t2
+GO
+
+DROP TABLE babel_1940_t2
+GO
+
 DROP TABLE babel_1940_t1
 GO
