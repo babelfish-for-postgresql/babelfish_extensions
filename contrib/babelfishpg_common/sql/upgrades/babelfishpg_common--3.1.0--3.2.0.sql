7a7e98f306f3a3b3c42d04495cd4f4d47a8b6af6
Satarupa Biswas <satarupb@amazon.com>
2023-04-07 07:03:55 +0000

    Adding changes and testcases for BABEL-879, BABEL-926, BABEL-2643
    BABEL-879 [Money Casts are not rounded correctly]
    BABEL-926 [Incorrect TRY_CAST behavior from various numeric types to tinyint]
    BABEL-2643 [Money type not working with choose() function in MSSQL]
    
    Task: BABEL-879, BABEL-926, BABEL-2643
    Signed-off-by: Satarupa Biswas (satarupb@amazon.com)

--- contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--3.1.0--3.2.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--3.1.0--3.2.0.sql
@@ -6,5 +6,17 @@ SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false)
 DROP CAST IF EXISTS(NUMERIC AS sys.BIT);
 CREATE CAST (NUMERIC AS sys.BIT) WITH FUNCTION sys.numeric_bit (NUMERIC) AS IMPLICIT;
 
+DROP CAST IF EXISTS(sys.VARCHAR AS sys.MONEY);
+
+DROP FUNCTION IF EXISTS sys.varchar2money(sys.VARCHAR) CASCADE;
+
+CREATE OR REPLACE FUNCTION sys.varchar2money(sys.VARCHAR)
+RETURNS sys.MONEY
+AS 'babelfishpg_common', 'varchar2money'
+LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE CAST (sys.VARCHAR AS sys.MONEY)
+WITH FUNCTION sys.varchar2money(sys.VARCHAR) AS IMPLICIT;
+
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
