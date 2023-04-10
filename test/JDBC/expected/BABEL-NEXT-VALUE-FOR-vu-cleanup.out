659b6bc7d021d008dacef469971f84901644a0e7
Jake Owen <owjco@amazon.com>
2023-04-10 18:52:01 +0000

    [BABEL-4098] Select Sequences - Gives error while in user created databases
    Signed-off-by: Jake Owen <owjco@amazon.com>

--- test/JDBC/expected/BABEL-NEXT-VALUE-FOR-vu-cleanup.out
+++ test/JDBC/expected/BABEL-NEXT-VALUE-FOR-vu-cleanup.out
@@ -6,3 +6,20 @@ DROP SEQUENCE babel_949.seq1;
 GO
 DROP SCHEMA babel_949;
 GO
+
+use babel_949_db;
+go
+
+DROP SEQUENCE babel_949_2.seq2;
+GO
+DROP SEQUENCE seq3;
+GO
+
+DROP SCHEMA babel_949_2;
+GO
+
+use master;
+go
+
+DROP DATABASE babel_949_db;
+go
--- test/JDBC/input/BABEL-NEXT-VALUE-FOR-vu-verify.sql
+++ test/JDBC/input/BABEL-NEXT-VALUE-FOR-vu-verify.sql
@@ -1,3 +1,6 @@
+use master;
+go
+
 SELECT * FROM table_949 ORDER BY table_key;
 GO
 
@@ -11,5 +14,14 @@ SELECT NEXT VALUE FOR null;
 GO
 
 SELECT NEXT VALUE FOR not_a_seq;
+GO
+
+use babel_949_db;
+go
+
+SELECT NEXT VALUE FOR babel_949_2.seq2
+GO
+
+SELECT NEXT VALUE FOR seq3
 GO
 
