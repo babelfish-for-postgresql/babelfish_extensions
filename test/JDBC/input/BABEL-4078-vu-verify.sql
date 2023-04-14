9960d00257724a9965975ccbbb5c586a92ee6663
Sumit Jaiswal <54075285+sumitj824@users.noreply.github.com>
2023-04-14 09:53:31 +0530

    Fix the output style for NVARCHAR datatype in CONVERT function (#1425)
    Earlier when we were converting from DATE/TIME types to NVARCHAR type using CONVERT function the output style was not getting into account as we have not handled the case for NVARCHAR type in TsqlFunctionConvert function correctly. This commit add changes to handle the case for NVARCHAR correctly.
    
    Task: BABEL-4078
    Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

---
+++ test/JDBC/input/BABEL-4078-vu-verify.sql
@@ -0,0 +1,50 @@
+SELECT * FROM babel_4078_vu_prepare_view1
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view11
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view2
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view22
+GO
+
+EXEC babel_4078_vu_prepare_proc1
+GO
+
+EXEC babel_4078_vu_prepare_proc11
+GO
+
+EXEC babel_4078_vu_prepare_proc2
+GO
+
+EXEC babel_4078_vu_prepare_proc22
+GO
+
+SELECT * FROM babel_4078_vu_prepare_func1()
+GO
+
+SELECT * FROM babel_4078_vu_prepare_func11()
+GO
+
+SELECT * FROM babel_4078_vu_prepare_func2()
+GO
+
+SELECT * FROM babel_4078_vu_prepare_func22()
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view3
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view4
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view5
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view6
+GO
+
+SELECT * FROM babel_4078_vu_prepare_view7
+GO
