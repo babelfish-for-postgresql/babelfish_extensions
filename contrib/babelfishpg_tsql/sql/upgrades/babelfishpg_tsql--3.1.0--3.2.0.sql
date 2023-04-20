931476a2c0b882f9993bfcaa66cb458a1cf41fb4
pratikzode <pratik.zode@gmail.com>
2023-04-20 07:55:00 +0000

    Modified ALTER FUNCTION sys.parsename(VARCHAR,INT)
    Signed-off-by: pratikzode <pratik.zode@gmail.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.1.0--3.2.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.1.0--3.2.0.sql
@@ -824,7 +824,7 @@ CREATE AGGREGATE sys.VARP(float8) (
 CREATE OR REPLACE FUNCTION sys.rowcount_big()
 RETURNS BIGINT AS 'babelfishpg_tsql' LANGUAGE C STABLE;
 
-ALTER FUNCTION sys.parsename(character varying,integer) RENAME TO parsename_deprecated_in_3_2_0;
+ALTER FUNCTION sys.parsename(VARCHAR,INT) RENAME TO parsename_deprecated_in_3_2_0;
 
 CREATE OR REPLACE FUNCTION sys.parsename(object_name character varying, object_piece int)
 RETURNS sys.SYSNAME
