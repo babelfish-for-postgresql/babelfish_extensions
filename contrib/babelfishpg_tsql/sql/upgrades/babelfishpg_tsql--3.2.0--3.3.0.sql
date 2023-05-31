c1527087d68ef58e2163e2c528c6d078a8a7692d
Ashish Prasad <pashisht@amazon.com>
2023-05-31 08:20:39 +0000

    Added missing drop and  reset search path command

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
@@ -32,3 +32,9 @@ $$
 LANGUAGE plpgsql;
 
 
+-- Drops the temporary procedure used by the upgrade script.
+-- Please have this be one of the last statements executed in this upgrade script.
+DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar, varchar);
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
