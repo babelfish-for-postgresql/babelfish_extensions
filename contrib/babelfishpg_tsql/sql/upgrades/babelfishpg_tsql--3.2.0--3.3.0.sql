a337e6aefdc0880bc2af8c2571a0fc4777acd447
Ashish Prasad <pashisht@amazon.com>
2023-06-09 05:46:07 +0000

    Typeproperty Function

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
@@ -40,6 +40,137 @@ LANGUAGE C IMMUTABLE STRICT;
 
 CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'parsename_deprecated_in_3_3_0');
 
+
+CREATE OR REPLACE FUNCTION typeproperty(
+    typename sys.VARCHAR,
+    property sys.VARCHAR
+    )
+RETURNS INT
+AS $$
+DECLARE
+    var_sc int;
+    schemaid int;
+    preci int;
+    schema_name VARCHAR;
+    type_name VARCHAR;
+    type_namee VARCHAR;
+    sys_id int;
+    testt VARCHAR;
+BEGIN
+
+    property := RTRIM(LOWER(COALESCE(property COLLATE "C",'')));
+
+    IF typename LIKE '%.%'  THEN
+    schema_name := RTRIM(lower(split_part(typename COLLATE "C", '.', 1)));
+    type_name :=  RTRIM(lower((split_part(typename COLLATE "C",'.', 2))));
+    ELSE
+    schema_name := 'dbo';
+    type_name := RTRIM(LOWER(typename));
+    END IF;
+
+
+    IF NOT EXISTS (SELECT ao.name FROM sys.types ao WHERE ao.name = type_name COLLATE sys.database_default)
+    THEN
+        RETURN NULL;
+    END IF;
+
+    IF NOT EXISTS (SELECT ao.name FROM sys.schemas ao WHERE ao.name = schema_name COLLATE sys.database_default OR schema_name = 'sys' OR schema_name = 'pg_catalog')
+    THEN
+        RETURN NULL ;
+    END IF;
+
+    IF NOT EXISTS (SELECT ty.name FROM sys.types ty WHERE ty.name = type_name COLLATE sys.database_default AND ty.is_user_defined = 0) THEN
+    schemaid := (SELECT sc.schema_id FROM sys.schemas sc WHERE sc.name = schema_name COLLATE sys.database_default);
+    ELSE
+        schemaid := (SELECT sc.schema_id FROM sys.types sc WHERE sc.name = type_name COLLATE sys.database_default);
+        IF schema_name = 'dbo'
+        THEN
+        schema_name := schema_name(schemaid);
+        END IF;
+    END IF;
+
+
+    if (SELECT schema_id(schema_name)) <> schemaid
+    THEN
+    RETURN NULL;
+    END IF;
+
+    sys_id := (SELECT CAST(dc.system_type_id AS INT) FROM sys.types dc WHERE dc.name = type_name COLLATE sys.database_default AND dc.schema_id = schemaid);
+    type_namee := (SELECT CAST(dc.name AS VARCHAR) FROM sys.types dc WHERE dc.system_type_id = sys_id AND dc.is_user_defined = 0);
+
+    IF property = 'allowsnull'
+    THEN
+        RETURN (
+            SELECT CAST( t1.is_nullable AS INT)
+            FROM sys.types t1
+            WHERE t1.name = type_name COLLATE sys.database_default AND t1.schema_id = schemaid );
+
+    ELSEIF property = 'precision'
+    THEN
+        preci := (SELECT CAST(dc.precision AS INT) FROM sys.types dc WHERE dc.name = type_name COLLATE sys.database_default AND dc.schema_id = schemaid);
+        IF sys_id = 0
+        THEN
+            RETURN preci;
+        END IF;
+
+        IF preci = 0
+        THEN
+            preci = (SELECT CAST(dc.prec AS INT) FROM sys.systypes dc WHERE dc.name = type_name COLLATE sys.database_default AND dc.uid = schemaid);
+            IF preci IS NULL
+            THEN
+                IF type_namee = 'image' or type_namee = 'text'
+                THEN
+                RETURN 2147483647;
+                ELSEIF type_namee = 'ntext'
+                THEN
+                RETURN 1073741823;
+                END IF;
+            END IF;
+            RETURN preci;
+        ELSE
+            RETURN preci;
+        END IF;        
+
+    ELSEIF property = 'scale'
+    THEN
+        preci := (SELECT CAST(dc.precision AS INT) FROM sys.types dc WHERE dc.name = type_namee COLLATE sys.database_default AND dc.system_type_id = sys_id);
+        IF sys_id = 0
+        THEN
+            RETURN preci;
+        END IF;
+        IF preci = 0 or type_namee = 'float' or type_namee = 'real' or type_namee = 'bit'
+        THEN
+            RETURN NULL;
+        ELSE
+            RETURN(SELECT CAST(dc.scale AS INT) FROM sys.types dc WHERE dc.name = type_name COLLATE sys.database_default AND dc.schema_id = schemaid);
+        END IF;
+    ELSEIF property = 'ownerid'
+    THEN
+        IF NOT EXISTS (SELECT ty.name FROM sys.types ty WHERE ty.name = type_name COLLATE sys.database_default AND ty.is_user_defined = 0) THEN
+        RETURN(SELECT CAST(dc.nspowner AS INT) FROM  pg_catalog.pg_namespace dc WHERE dc.oid = schemaid);
+        ELSE
+        RETURN 10;
+        END IF;
+
+    ELSEIF property = 'usesansitrim'
+    THEN
+        IF type_name::regtype IN ('bigint'::regtype, 'int'::regtype, 'smallint'::regtype,'tinyint'::regtype,
+            'numeric'::regtype, 'float'::regtype, 'real'::regtype, 'money'::regtype)
+        THEN
+            RETURN NULL;
+        ELSE
+            RETURN 1;
+        END IF;
+
+    END IF;
+
+    RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql STABLE;
+
+
+
 -- Drops the temporary procedure used by the upgrade script.
 -- Please have this be one of the last statements executed in this upgrade script.
 DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar, varchar);
