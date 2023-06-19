b23bfbe1b2a6a826e3689dff5be4b921368b6677
Kritika <kritikaq@amazon.com>
2023-06-19 17:52:34 +0000

    upgrade tests
    Signed-off-by: Kritika <kritikaq@amazon.com>

---
+++ test/JDBC/input/storedProcedures/Test-sp_execute_postgresql-vu-cleanup.sql
@@ -0,0 +1,5 @@
+DROP TABLE tmp_sp_execute_postgresql;
+GO
+
+DROP PROC test_sp_execute_postgresql_proc
+GO
---
+++ test/JDBC/input/storedProcedures/Test-sp_execute_postgresql-vu-prepare.sql
@@ -0,0 +1,11 @@
+-- create a temporary table
+CREATE TABLE tmp_sp_execute_postgresql(addStmt sys.nvarchar)
+GO
+
+
+-- CREATE PROC test_sp_execute_postgresql_proc
+-- AS
+-- BEGIN
+--     EXEC sp_execute_postgresql 
+-- END
+-- go
