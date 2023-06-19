ff3f51c0126178deacc18371f95d29a47715dc8c
Kritika <kritikaq@amazon.com>
2023-06-19 18:35:09 +0000

    upgrade tests
    Signed-off-by: Kritika <kritikaq@amazon.com>

--- test/JDBC/input/storedProcedures/Test-sp_execute_postgresql-vu-prepare.sql
+++ test/JDBC/input/storedProcedures/Test-sp_execute_postgresql-vu-prepare.sql
@@ -2,10 +2,3 @@
 CREATE TABLE tmp_sp_execute_postgresql(addStmt sys.nvarchar)
 GO
 
-
--- CREATE PROC test_sp_execute_postgresql_proc
--- AS
--- BEGIN
---     EXEC sp_execute_postgresql 
--- END
--- go
