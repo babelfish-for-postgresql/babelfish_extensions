a496c0f5db3272c027c396db38c062528aeea9f2
Robin Li <renyuli@amazon.com>
2023-07-07 15:46:37 +0000

    clean up merge conflict

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
@@ -51,92 +51,50 @@ LANGUAGE C STABLE PARALLEL SAFE;
 
 ALTER TABLE sys.babelfish_server_options ADD COLUMN IF NOT EXISTS connect_timeout INT;
 
-
-
--- This function performs string rewriting for the full text search CONTAINS predicate
--- in Babelfish
-
--- For example, a T-SQL query 
--- SELECT * FROM t WHERE CONTAINS(txt, '"good old days"')
--- is rewritten into a Postgres query 
--- SELECT * FROM t WHERE to_tsvector('fts_contains', txt) @@ to_tsquery('fts_contains', 'good <-> old <-> days')
--- In particular, the string constant '"good old days"' gets rewritten into 'good <-> old <-> days'
--- This function performs the string rewriting from '"good old days"' to 'good <-> old <-> days'
-
-CREATE OR REPLACE FUNCTION sys.babelfish_fts_contains_rewrite(IN phrase text)
-  RETURNS TEXT AS
-$$
-DECLARE
-  joined_text text;
-  word text;
-BEGIN
-  -- Initialize the joined_text variable
-  joined_text := '';
-
-  -- Strip leading and trailing spaces from the phrase
-  phrase := trim(phrase COLLATE "C") COLLATE "C";
-
-  -- no rewriting is needed if the query is a single word
-  IF position((' ' COLLATE C) IN (phrase COLLATE "C")) = 0 THEN
-    RETURN phrase;
-  END IF;
-
-  -- rewrite phrase queries 
-  -- '"word1 word2 word3"' is rewritten into 'word1<->word2<->word3'
-
-  -- Check if the phrase is surrounded by double quotes
-  IF position(('"' COLLATE "C") IN (phrase COLLATE "C") ) <> 1 OR position(('"' COLLATE "C") IN (reverse(phrase) COLLATE "C")) <> 1 THEN
-    RAISE EXCEPTION 'Phrase must be surrounded by double quotes';
-  END IF;
-
-  -- Strip the double quotes from the phrase
-  phrase := substring(phrase COLLATE "C", 2, length(phrase) - 2) COLLATE "C";
-
-  -- Strip leading and trailing spaces from the phrase
-  phrase := trim(phrase COLLATE "C") COLLATE "C";
-
-  -- Split the phrase into an array of words
-  FOREACH word IN ARRAY regexp_split_to_array(phrase COLLATE "C", '\s+' COLLATE "C") COLLATE "C" LOOP
-    -- Append the word to the joined_text variable
-    joined_text := joined_text || word || '<->';
-  END LOOP;
-
-  -- Remove the trailing "<->" from the joined_text
-  joined_text := substring(joined_text COLLATE "C", 1, length(joined_text) - 3) COLLATE "C";
-
-  -- Return the joined_text
-  RETURN joined_text;
-END;
-$$
-LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE; 
--- Removing IMMUTABLE PARALLEL SAFE will disallow parallel mode for full text search
-
-
-/*
- * tsql full-text search configurations for Babelfish
- */
-
-CREATE TEXT SEARCH DICTIONARY fts_contains_dict (
-    TEMPLATE = simple,
-    STOPWORDS = tsql_contains
-);
-
-COMMENT ON TEXT SEARCH DICTIONARY fts_contains_dict IS 'Babelfish T-SQL full text search CONTAINS dictionary';
-
-CREATE TEXT SEARCH CONFIGURATION fts_contains ( COPY = simple );
-
-COMMENT ON TEXT SEARCH CONFIGURATION fts_contains IS 'Babelfish T-SQL full text search CONTAINS configuration';
-
-ALTER TEXT SEARCH CONFIGURATION fts_contains
-    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart,
-                      word, hword, hword_part
-    WITH fts_contains_dict;
-
-
-
-
--- Reset search_path to not affect any subsequent scripts
-SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
+CREATE OR REPLACE VIEW sys.servers
+AS
+SELECT
+  CAST(f.oid as int) AS server_id,
+  CAST(f.srvname as sys.sysname) AS name,
+  CAST('' as sys.sysname) AS product,
+  CAST('tds_fdw' as sys.sysname) AS provider,
+  CAST((select string_agg(
+                  case
+                  when option like 'servername=%%' then substring(option, 12)
+                  else NULL
+                  end, ',')
+          from unnest(f.srvoptions) as option) as sys.nvarchar(4000)) AS data_source,
+  CAST(NULL as sys.nvarchar(4000)) AS location,
+  CAST(NULL as sys.nvarchar(4000)) AS provider_string,
+  CAST((select string_agg(
+                  case
+                  when option like 'database=%%' then substring(option, 10)
+                  else NULL
+                  end, ',')
+          from unnest(f.srvoptions) as option) as sys.sysname) AS catalog,
+  CAST(s.connect_timeout as int) AS connect_timeout,
+  CAST(s.query_timeout as int) AS query_timeout,
+  CAST(1 as sys.bit) AS is_linked,
+  CAST(0 as sys.bit) AS is_remote_login_enabled,
+  CAST(0 as sys.bit) AS is_rpc_out_enabled,
+  CAST(1 as sys.bit) AS is_data_access_enabled,
+  CAST(0 as sys.bit) AS is_collation_compatible,
+  CAST(1 as sys.bit) AS uses_remote_collation,
+  CAST(NULL as sys.sysname) AS collation_name,
+  CAST(0 as sys.bit) AS lazy_schema_validation,
+  CAST(0 as sys.bit) AS is_system,
+  CAST(0 as sys.bit) AS is_publisher,
+  CAST(0 as sys.bit) AS is_subscriber,
+  CAST(0 as sys.bit) AS is_distributor,
+  CAST(0 as sys.bit) AS is_nonsql_subscriber,
+  CAST(1 as sys.bit) AS is_remote_proc_transaction_promotion_enabled,
+  CAST(NULL as sys.datetime) AS modify_date,
+  CAST(0 as sys.bit) AS is_rda_server
+FROM pg_foreign_server AS f
+LEFT JOIN pg_foreign_data_wrapper AS w ON f.srvfdw = w.oid
+LEFT JOIN sys.babelfish_server_options AS s on f.srvname = s.servername
+WHERE w.fdwname = 'tds_fdw';
+GRANT SELECT ON sys.servers TO PUBLIC;
 
 CREATE OR REPLACE PROCEDURE sys.sp_testlinkedserver(IN "@servername" sys.sysname)
 AS 'babelfishpg_tsql', 'sp_testlinkedserver_internal' LANGUAGE C;
@@ -186,27 +144,6 @@ GRANT SELECT ON sys.shipped_objects_not_in_sys TO PUBLIC;
 DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar, varchar);
 
 
-/*
- * tsql full-text search configurations for Babelfish
- */
-
-CREATE TEXT SEARCH DICTIONARY fts_contains_dict (
-    TEMPLATE = simple,
-    STOPWORDS = tsql_contains
-);
-
-COMMENT ON TEXT SEARCH DICTIONARY fts_contains_dict IS 'Babelfish T-SQL full text search CONTAINS dictionary';
-
-CREATE TEXT SEARCH CONFIGURATION fts_contains ( COPY = simple );
-
-COMMENT ON TEXT SEARCH CONFIGURATION fts_contains IS 'Babelfish T-SQL full text search CONTAINS configuration';
-
-ALTER TEXT SEARCH CONFIGURATION fts_contains
-    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart,
-                      word, hword, hword_part
-    WITH fts_contains_dict;
-
-
 
 -- This function performs string rewriting for the full text search CONTAINS predicate
 -- in Babelfish
@@ -267,11 +204,6 @@ LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE;
 -- Removing IMMUTABLE PARALLEL SAFE will disallow parallel mode for full text search
 
 
--- Reset search_path to not affect any subsequent scripts
-SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
-
-
-
 /*
  * tsql full-text search configurations for Babelfish
  */
@@ -294,52 +226,14 @@ ALTER TEXT SEARCH CONFIGURATION fts_contains
 
 
 
-
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
 
-CREATE OR REPLACE PROCEDURE sys.sp_testlinkedserver(IN "@servername" sys.sysname)
-AS 'babelfishpg_tsql', 'sp_testlinkedserver_internal' LANGUAGE C;
-GRANT EXECUTE on PROCEDURE sys.sp_testlinkedserver(IN sys.sysname) TO PUBLIC;
 
-CREATE OR REPLACE PROCEDURE master_dbo.sp_testlinkedserver( IN "@servername" sys.sysname)
-AS 'babelfishpg_tsql', 'sp_testlinkedserver_internal'
-LANGUAGE C;
 
-ALTER PROCEDURE master_dbo.sp_testlinkedserver OWNER TO sysadmin;
 
-create or replace view sys.shipped_objects_not_in_sys AS
--- This portion of view retrieves information on objects that reside in a schema in one specfic database.
--- For example, 'master_dbo' schema can only exist in the 'master' database.
--- Internally stored schema name (nspname) must be provided.
-select t.name,t.type, ns.oid as schemaid from
-(
-  values
-    ('xp_qv','master_dbo','P'),
-    ('xp_instance_regread','master_dbo','P'),
-    ('sp_addlinkedserver', 'master_dbo', 'P'),
-    ('sp_addlinkedsrvlogin', 'master_dbo', 'P'),
-    ('sp_dropserver', 'master_dbo', 'P'),
-    ('sp_droplinkedsrvlogin', 'master_dbo', 'P'),
-    ('sp_testlinkedserver', 'master_dbo', 'P'),
-    ('fn_syspolicy_is_automation_enabled', 'msdb_dbo', 'FN'),
-    ('syspolicy_configuration', 'msdb_dbo', 'V'),
-    ('syspolicy_system_health_state', 'msdb_dbo', 'V')
-) t(name,schema_name, type)
-inner join pg_catalog.pg_namespace ns on t.schema_name = ns.nspname
 
-union all
 
--- This portion of view retrieves information on objects that reside in a schema in any number of databases.
--- For example, 'dbo' schema can exist in the 'master', 'tempdb', 'msdb', and any user created database.
-select t.name,t.type, ns.oid as schemaid from
-(
-  values
-    ('sysdatabases','dbo','V')
-) t (name, schema_name, type)
-inner join sys.babelfish_namespace_ext b on t.schema_name = b.orig_name
-inner join pg_catalog.pg_namespace ns on b.nspname = ns.nspname;
-GRANT SELECT ON sys.shipped_objects_not_in_sys TO PUBLIC;
 
 
 
