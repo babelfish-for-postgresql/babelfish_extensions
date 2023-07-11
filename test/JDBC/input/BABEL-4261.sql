1e1a736eb42f751611a0ad64f543a08532cfafe2
Robin Li <renyuli@amazon.com>
2023-07-11 17:09:03 +0000

    run tests

--- test/JDBC/input/BABEL-4261.sql
+++ test/JDBC/input/BABEL-4261.sql
@@ -6,7 +6,6 @@ select set_config('parallel_tuple_cost', 0, false);
 select set_config('min_parallel_table_scan_size', 0, false);
 GO
 
--- execution
 select a, count(*) from t_babel4261 group by a order by 2; -- should not crash
 GO
 
