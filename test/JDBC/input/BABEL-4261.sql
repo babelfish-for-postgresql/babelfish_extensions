85843cc1522f46e6de990cb4e6f95897b3499d64
Robin Li <renyuli@amazon.com>
2023-07-11 23:34:46 +0000

    add statistics profile to show it's a parellel plan

--- test/JDBC/input/BABEL-4261.sql
+++ test/JDBC/input/BABEL-4261.sql
@@ -6,7 +6,26 @@ select set_config('parallel_tuple_cost', 0, false);
 select set_config('min_parallel_table_scan_size', 0, false);
 GO
 
+-- show explicitly this is a parallel query plan
+select set_config('babelfishpg_tsql.explain_timing', 'off', false);
+GO
+
+select set_config('babelfishpg_tsql.explain_summary', 'off', false);
+GO
+
+SET BABELFISH_STATISTICS PROFILE ON
+GO
+
 select a, count(*) from t_babel4261 group by a order by 2; -- should not crash
+GO
+
+SET BABELFISH_STATISTICS PROFILE OFF
+GO
+
+select set_config('babelfishpg_tsql.explain_timing', 'on', false);
+GO
+
+select set_config('babelfishpg_tsql.explain_summary', 'on', false);
 GO
 
 DROP TABLE t_babel4261;
