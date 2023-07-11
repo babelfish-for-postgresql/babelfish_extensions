17279d9216ad71678ef77f40eef7c52b0a4f63ea
Robin Li <renyuli@amazon.com>
2023-07-11 15:24:35 +0000

    Fix backward scan flag for parallel queries
    Currently there is a bug in Babelfish that allows the backward scan flag
    to be set for parallel queries. However, backward scans cannot be
    parallelized. This bug results in assertion failures in the Postgres engine and
    leads Babelfish queries to crash. In this commit, we fix the issue by
    disallowing backward scan flag to be set when a query is in parallel
    mode. This issue occurs during the implementation of full text search in
    Babelfish. Fixing the issue unblocks full text search implementation.
    
    Signed-off-by: Robin Li <renyuli@amazon.com>

---
+++ test/JDBC/input/BABEL-4261.sql
@@ -0,0 +1,17 @@
+CREATE TABLE t_babel4261 (a int, b int);
+GO
+
+select set_config('parallel_setup_cost', 0, false);
+select set_config('parallel_tuple_cost', 0, false);
+select set_config('min_parallel_table_scan_size', 0, false);
+GO
+
+-- execution
+select a, count(*) from t_babel4261 group by a order by 2; -- should not crash
+GO
+
+DROP TABLE t_babel4261;
+GO
+
+
+-- quip doc : https://quip-amazon.com/MQ3PAadoDe2n/Full-text-search-parallel-query-backward-scan-issue
