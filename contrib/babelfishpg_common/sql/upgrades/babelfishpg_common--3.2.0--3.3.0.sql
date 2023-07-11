285686a289144a41289b6ec9f0441182e7758c79
Ashish Prasad <pashisht@amazon.com>
2023-07-11 10:18:36 +0000

    Add new upgrade script for babelfish_common

---
+++ contrib/babelfishpg_common/sql/upgrades/babelfishpg_common--3.2.0--3.3.0.sql
@@ -0,0 +1,9 @@
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '3.3.0'" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+ALTER DOMAIN sys.TIMESTAMP SET NOT NULL;
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
