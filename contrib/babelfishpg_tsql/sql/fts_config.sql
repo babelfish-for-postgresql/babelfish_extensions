c83531623ce6619c782e60e74ce9ccd294fc2bcd
Robin Li <renyuli@amazon.com>
2023-07-13 18:10:57 +0000

    add comment

--- contrib/babelfishpg_tsql/sql/fts_config.sql
+++ contrib/babelfishpg_tsql/sql/fts_config.sql
@@ -9,11 +9,11 @@ CREATE TEXT SEARCH DICTIONARY fts_contains_dict (
     STOPWORDS = tsql_contains
 );
 
-COMMENT ON TEXT SEARCH DICTIONARY fts_contains_dict IS 'Babelfish T-SQL full text search CONTAINS dictionary';
+COMMENT ON TEXT SEARCH DICTIONARY fts_contains_dict IS 'Babelfish T-SQL full text search CONTAINS dictionary (currently we only support American English)';
 
 CREATE TEXT SEARCH CONFIGURATION fts_contains ( COPY = simple );
 
-COMMENT ON TEXT SEARCH CONFIGURATION fts_contains IS 'Babelfish T-SQL full text search CONTAINS configuration';
+COMMENT ON TEXT SEARCH CONFIGURATION fts_contains IS 'Babelfish T-SQL full text search CONTAINS configuration (currently we only support American English)';
 
 ALTER TEXT SEARCH CONFIGURATION fts_contains
     ALTER MAPPING FOR asciiword, asciihword, hword_asciipart,
