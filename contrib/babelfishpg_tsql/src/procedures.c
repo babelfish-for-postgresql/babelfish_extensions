3d28d7f9a746a172e028397bbd519ec796bfb1d4
Sharu Goel <30777678+thephantomthief@users.noreply.github.com>
2023-07-19 11:30:47 +0530

    OPENQUERY for Linked Servers does not work when used from user created database (#1592)
    Description
    Previously the user mapping that got created as part of sp_addlinkedsrvlogin was only working in master database. In T-SQL the linked login is not specific to login unlike the user mappings in PG. To fix this we create the user mapping for the public role so that the linked login is not specific to any single role.
    
    Task: BABEL-4229
    
    Signed-off-by: Sharu Goel goelshar@amazon.com

--- contrib/babelfishpg_tsql/src/procedures.c
+++ contrib/babelfishpg_tsql/src/procedures.c
@@ -18,6 +18,7 @@
 #include "commands/prepare.h"
 #include "common/string.h"
 #include "executor/spi.h"
+#include "foreign/foreign.h"
 #include "fmgr.h"
 #include "funcapi.h"
 #include "hooks.h"
@@ -2292,11 +2293,12 @@ sp_addlinkedsrvlogin_internal(PG_FUNCTION_ARGS)
 	 * We prepare the following query to create a user mapping. This will
 	 * be executed using ProcessUtility():
 	 *
-	 * CREATE USER MAPPING FOR CURRENT_USER SERVER <servername> OPTIONS (username
-	 * 	'<remote server user name>', password '<remote server user password>')
+	 * CREATE USER MAPPING FOR PUBLIC SERVER <servername> OPTIONS
+	 * (username '<remote server user name>', password '<remote server user
+	 * password>')
 	 *
 	 */
-	appendStringInfo(&query, "CREATE USER MAPPING FOR CURRENT_USER SERVER \"%s\" ", servername);
+	appendStringInfo(&query, "CREATE USER MAPPING FOR PUBLIC SERVER \"%s\" ", servername);
 
 	/*
 	 * Add the relevant options
@@ -2362,20 +2364,37 @@ sp_droplinkedsrvlogin_internal(PG_FUNCTION_ARGS)
 
 	if (locallogin != NULL)
 		ereport(ERROR,
-						(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-							errmsg("Only @locallogin = NULL is supported. Configuring remote server access specific to local login is not yet supported")));
-	
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("Only @locallogin = NULL is supported. Configuring remote server access specific to local login is not yet supported")));
+
+	remove_trailing_spaces(servername);
+
+	/* Check if servername is valid */
+	get_foreign_server_oid(servername, false);
+
 	initStringInfo(&query);
 
 	/*
-	 * We prepare the following query to drop a linked server login. This will
+	 * We prepare the following queries to drop a linked server login. This will
 	 * be executed using ProcessUtility():
 	 *
-	 * DROP USER MAPPING FOR CURRENT_USER SERVER @SERVERNAME
-	 *
+	 * DROP USER MAPPING IF EXISTS FOR CURRENT_USER SERVER @SERVERNAME
+	 * DROP USER MAPPING IF EXISTS FOR PUBLIC SERVER @SERVERNAME
+	 * 
+	 * Linked logins were first implemented as PG USER MAPPINGs for the CURRENT_USER which
+	 * was not entirely correct because T-SQL linked logins are not user or login specific.
+	 * To address this we now create user mapping for the PG PUBLIC role internally.
+	 * 
+	 * To ensure sp_droplinkedsrvlogin works in accordance with both the older and newer
+	 * implementation of linked logins, we try to drop USER MAPPINGs for both the CURRENT_USER
+	 * and PUBLIC PG roles.
 	 */
-	appendStringInfo(&query, "DROP USER MAPPING FOR CURRENT_USER SERVER \"%s\"", servername);
+	appendStringInfo(&query, "DROP USER MAPPING IF EXISTS FOR CURRENT_USER SERVER \"%s\"", servername);
+	exec_utility_cmd_helper(query.data);
 
+	resetStringInfo(&query);
+
+	appendStringInfo(&query, "DROP USER MAPPING IF EXISTS FOR PUBLIC SERVER \"%s\"", servername);
 	exec_utility_cmd_helper(query.data);
 
 	if(locallogin)
