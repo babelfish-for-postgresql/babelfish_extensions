16c2dae98946e702047648d68321a6cf9ca3c317
Kushaalshroff1234@gmail.com <kushaal@amazon.com>
2023-07-21 09:03:19 +0000

    add upgrade tests

---
+++ test/JDBC/input/getdate-vu-verify.sql
@@ -0,0 +1,29 @@
+exec sysdatetime_dep_proc
+go
+
+exec sysdatetimeoffset_dep_proc
+go
+
+exec sysutcdatetime_dep_proc
+go
+
+exec getdate_dep_proc
+go
+
+exec getutcdate_dep_proc
+go
+
+Select * from sysdatetime_dep_view
+go
+
+select * from sysdatetimeoffset_dep_view
+go
+
+select * from sysutcdatetime_dep_view
+go
+
+select * from getdate_dep_view
+go
+
+select * from getutcdate_dep_view
+go
