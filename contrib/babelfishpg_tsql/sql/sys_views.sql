b72057a4c5daa104bee879a6539897891c0c0468
Ashish Prasad <56514722+hash-16@users.noreply.github.com>
2023-07-24 11:04:56 +0530

    Support scripting of multiple views (#1593)
    Currently scripting single view works fine but the scripting crashes when trying to script more than one view and  scripting one or more view with other objects.
    
    To support multiple views scripting , implemented following things:-
    
    1.  `SELECT (@@microsoftversion / 0x1000000) & 0xff AS [VersionMajor]`
    For the above query here we are doing division between int4 (@@microsoftversion) and BIGINT (0x1000000) and we are currently returning BIGINT  but  int4 is expected as an output due to which scripting fails. This happens here because we are currently hitting Datum int48div(PG_FUNCTION_ARGS) .
    So we created division operator for division between int4 and varbinary(hexadecimal) datatype which should return int ( 4 bytes) instead of BIGINT (8 bytes).
    3.  `select dep.referencing_id,dep.referenced_id from sys.sql_expression_dependencies as dep join #tempordering as t1 on dep.referenced_id = t1.ID join #tempordering as t2 on dep.referencing_id = t2.ID where dep.referenced_id != dep.referencing_id UNION select [object_id], [history_table_id] from sys.tables where [temporal_type] = 2`
    Added empty view sys.sql_expression_dependencies which was required by the above scripting query , though empty views works fine here . Have created a JIRA (BABEL-4288) for fully supporting it in future.
    
    Task: BABEL-3941
    Signed-off-by: Ashish Prasad <pashisht@amazon.com>

--- contrib/babelfishpg_tsql/sql/sys_views.sql
+++ contrib/babelfishpg_tsql/sql/sys_views.sql
@@ -2913,3 +2913,88 @@ SELECT
 FROM sys.indexes AS i
 INNER JOIN pg_catalog.pg_class AS c ON i.object_id = c."oid";
 GRANT SELECT ON sys.partitions TO PUBLIC;
+
+CREATE OR REPLACE VIEW sys.servers
+AS
+SELECT
+  CAST(f.oid as int) AS server_id,
+  CAST(f.srvname as sys.sysname) AS name,
+  CAST('' as sys.sysname) AS product,
+  CAST('tds_fdw' as sys.sysname) AS provider,
+  CAST((select string_agg(
+                  case
+                  when option like 'servername=%%' then substring(option, 12)
+                  else NULL
+                  end, ',')
+          from unnest(f.srvoptions) as option) as sys.nvarchar(4000)) AS data_source,
+  CAST(NULL as sys.nvarchar(4000)) AS location,
+  CAST(NULL as sys.nvarchar(4000)) AS provider_string,
+  CAST((select string_agg(
+                  case
+                  when option like 'database=%%' then substring(option, 10)
+                  else NULL
+                  end, ',')
+          from unnest(f.srvoptions) as option) as sys.sysname) AS catalog,
+  CAST(s.connect_timeout as int) AS connect_timeout,
+  CAST(s.query_timeout as int) AS query_timeout,
+  CAST(1 as sys.bit) AS is_linked,
+  CAST(0 as sys.bit) AS is_remote_login_enabled,
+  CAST(0 as sys.bit) AS is_rpc_out_enabled,
+  CAST(1 as sys.bit) AS is_data_access_enabled,
+  CAST(0 as sys.bit) AS is_collation_compatible,
+  CAST(1 as sys.bit) AS uses_remote_collation,
+  CAST(NULL as sys.sysname) AS collation_name,
+  CAST(0 as sys.bit) AS lazy_schema_validation,
+  CAST(0 as sys.bit) AS is_system,
+  CAST(0 as sys.bit) AS is_publisher,
+  CAST(0 as sys.bit) AS is_subscriber,
+  CAST(0 as sys.bit) AS is_distributor,
+  CAST(0 as sys.bit) AS is_nonsql_subscriber,
+  CAST(1 as sys.bit) AS is_remote_proc_transaction_promotion_enabled,
+  CAST(NULL as sys.datetime) AS modify_date,
+  CAST(0 as sys.bit) AS is_rda_server
+FROM pg_foreign_server AS f
+LEFT JOIN pg_foreign_data_wrapper AS w ON f.srvfdw = w.oid
+LEFT JOIN sys.babelfish_server_options AS s on f.srvname = s.servername
+WHERE w.fdwname = 'tds_fdw';
+GRANT SELECT ON sys.servers TO PUBLIC;
+
+CREATE OR REPLACE VIEW sys.linked_logins
+AS
+SELECT
+  CAST(u.srvid as int) AS server_id,
+  CAST(0 as int) AS local_principal_id,
+  CAST(0 as sys.bit) AS uses_self_credential,
+  CAST((select string_agg(
+                  case
+                  when option like 'username=%%' then substring(option, 10)
+                  else NULL
+                  end, ',')
+          from unnest(u.umoptions) as option) as sys.sysname) AS remote_name,
+  CAST(NULL as sys.datetime) AS modify_date
+FROM pg_user_mappings AS U
+LEFT JOIN pg_foreign_server AS f ON u.srvid = f.oid
+LEFT JOIN pg_foreign_data_wrapper AS w ON f.srvfdw = w.oid
+WHERE w.fdwname = 'tds_fdw';
+GRANT SELECT ON sys.linked_logins TO PUBLIC;
+
+CREATE OR REPLACE VIEW sys.sql_expression_dependencies
+AS
+SELECT
+    CAST(0 as INT) AS referencing_id,
+    CAST(0 as INT) AS referencing_minor_id,
+    CAST(0 as sys.TINYINT) AS referencing_class,
+    CAST('' as NVARCHAR(60)) AS referencing_class_desc,
+    CAST(0 as sys.BIT) AS is_schema_bound_reference,
+    CAST(0 as sys.TINYINT) AS referenced_class,
+    CAST('' as NVARCHAR(60)) AS referenced_class_desc,
+    CAST('' as SYSNAME) AS referenced_server_name,
+    CAST('' as SYSNAME) AS referenced_database_name,
+    CAST('' as SYSNAME) AS referenced_schema_name,
+    CAST('' as SYSNAME) AS referenced_entity_name,
+    CAST(0 as INT) AS referenced_id,
+    CAST(0 as INT) AS referenced_minor_id,
+    CAST(0 as sys.BIT) AS is_caller_dependent,
+    CAST(0 as sys.BIT) AS is_ambiguous
+WHERE FALSE;
+GRANT SELECT ON sys.sql_expression_dependencies TO PUBLIC;
