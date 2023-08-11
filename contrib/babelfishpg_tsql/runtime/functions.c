8620f5c097eb2e292eae9a07297a5454363bcf99
Sai Rohan Basa <bsrohan@amazon.com>
2023-08-11 03:50:27 +0000

    Initialise namespace_name to NULL

--- contrib/babelfishpg_tsql/runtime/functions.c
+++ contrib/babelfishpg_tsql/runtime/functions.c
@@ -2053,7 +2053,7 @@ bool is_ms_shipped(char *object_name, int type, Oid schema_id)
 {
 	int	i = 0;
 	bool	is_ms_shipped = false;
-	char	*namespace_name;
+	char	*namespace_name = NULL;
 	/*
 	 * This array contains information of objects that reside in a schema in one specfic database.
 	 * For example, 'master_dbo' schema can only exist in the 'master' database.
@@ -2130,7 +2130,7 @@ bool is_ms_shipped(char *object_name, int type, Oid schema_id)
 	for (i = 0; i < num_all_db_objects; i++)
 	{
 		char		*tempnspname = NULL;
-		bool		isNull;
+		bool		isNull = false;
 
 		if (is_ms_shipped)
 			break;
