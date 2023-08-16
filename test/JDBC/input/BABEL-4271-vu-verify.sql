f8f93d93bb98be5a460b538ef9cd69138fb1fe39
Vikash Prajapati <vikasprj@dev-dsk-vikasprj-1c-d637e1bd.eu-west-1.amazon.com>
2023-08-16 09:35:36 +0000

    Address comments
    Signed-off-by: Vikash Prajapati <vikasprj@amazon.com>

--- test/JDBC/input/BABEL-4271-vu-verify.sql
+++ test/JDBC/input/BABEL-4271-vu-verify.sql
@@ -11,7 +11,17 @@ select 1 where 'abc' LIKE '[a-c]bc' ESCAPE '';
 go
 select 1 where 'bbc' LIKE '[a-c]bc' ESCAPE '';
 go
-select a, b from testvikasprj where testvikasprj.a LIKE testvikasprj.b ESCAPE '';
+select a, b from babel_4271_vu_prepare_t1 where babel_4271_vu_prepare_t1.a LIKE babel_4271_vu_prepare_t1.b ESCAPE '';
+go
+SELECT a, '' from babel_4271_vu_prepare_t1 where babel_4271_vu_prepare_t1.a LIKE '' ESCAPE '';
+go
+SELECT a, 'abc' from babel_4271_vu_prepare_t1 where babel_4271_vu_prepare_t1.a LIKE '' ESCAPE '';
+go
+SELECT '', '' from babel_4271_vu_prepare_t1 where '' LIKE babel_4271_vu_prepare_t1.b ESCAPE '';
+go
+SELECT 'xy', b from babel_4271_vu_prepare_t1 where 'cbc' LIKE babel_4271_vu_prepare_t1.a ESCAPE '';
+go
+SELECT a, b from babel_4271_vu_prepare_t1 where '' LIKE '' ESCAPE '';
 go
 -- Test to check ESCAPE null case (ESCAPE null means no ESCAPE char used)
 select 1 where 'ABCD' LIKE 'AB[C]D' ESCAPE null;
@@ -26,5 +36,17 @@ select 1 where 'abc' LIKE '[a-c]bc' ESCAPE null;
 go
 select 1 where 'bbc' LIKE '[a-c]bc' ESCAPE null;
 go
-select a, b from testvikasprj where testvikasprj.a LIKE testvikasprj.b ESCAPE null;
+select a, b from babel_4271_vu_prepare_t1 where babel_4271_vu_prepare_t1.a LIKE babel_4271_vu_prepare_t1.b ESCAPE null;
+go
+SELECT a, 'abc' from babel_4271_vu_prepare_t1 where babel_4271_vu_prepare_t1.a LIKE 'abc' ESCAPE null;
+go
+SELECT a, '' from babel_4271_vu_prepare_t1 where babel_4271_vu_prepare_t1.a LIKE babel_4271_vu_prepare_t1.b ESCAPE null;
+go
+SELECT a, '' from babel_4271_vu_prepare_t1 where babel_4271_vu_prepare_t1.a LIKE '' ESCAPE null;
+go
+SELECT 'xy', b from babel_4271_vu_prepare_t1 where 'cbc' LIKE babel_4271_vu_prepare_t1.a ESCAPE null;
+go
+SELECT '', '' from babel_4271_vu_prepare_t1 where '' LIKE babel_4271_vu_prepare_t1.b ESCAPE null;
+go
+SELECT a, b from babel_4271_vu_prepare_t1 where '' LIKE '' ESCAPE null;
 go
