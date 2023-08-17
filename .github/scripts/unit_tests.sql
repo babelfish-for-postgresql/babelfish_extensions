8619c994e89bcfedab2f4601caaf5fb69772434e
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2023-08-17 06:35:38 +0000

    Enable unit testing framework for babelfish
    Signed-off-by: Shameem Ahmed <shameemahmed20apr2000@gmail.com>

---
+++ .github/scripts/unit_tests.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION check_all_unit_tests_passed()
+RETURNS VOID AS $$
+DECLARE
+  total_tests INTEGER;
+  passed_tests INTEGER;
+BEGIN
+  -- Get the total number of tests
+  SELECT COUNT(*) INTO total_tests
+  FROM babelfishpg_unit_run_tests(); 
+
+  -- Get the number of tests passed
+  SELECT COUNT(*) INTO passed_tests
+  FROM babelfishpg_unit_run_tests()
+  WHERE status = 'passed';
+
+  -- Throw an error if not all tests passed
+  IF total_tests <> passed_tests THEN
+    RAISE EXCEPTION 'Not all tests passed. Total tests: %, Passed tests: %', total_tests, passed_tests;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM check_all_unit_tests_passed();
