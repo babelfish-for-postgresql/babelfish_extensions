2b1b8e2e8ac76268b618b0c0f47fe049c97df593
Deepakshi Mittal <depakshi@amazon.com>
2023-08-23 17:36:14 +0000

    Corrected test case
    Task: BABEL-539
    
    Signed-off-by: Deepakshi Mittal <depakshi@amazon.com>

--- test/JDBC/input/BABEL_539-vu-verify.sql
+++ test/JDBC/input/BABEL_539-vu-verify.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS babel_539NewTable1;
 GO
 
 --calling internal function directly
-SELECT col1, IDENTITY_INTO(1, 1,1) as id_num INTO babel_539NewTempTable2 FROM babel_539OldTable;
+SELECT col1, IDENTITY_INTO_INT(23, 1,1) as id_num INTO babel_539NewTempTable2 FROM babel_539OldTable;
 GO
 
 SELECT sys.IDENTITY(23, 1);
@@ -17,19 +17,16 @@ GO
 SELECT IDENTITY(int, 21);
 GO
 
-SELECT sys.IDENTITY_INTO(23, 1, 1);
-GO
-
 SELECT sys.identity_into_int(23, 1, 1);
 GO
 
 SELECT sys.IDENTITY_INTO_SMALLINT(21, 1, 1);
 GO
 
-SELECT sys.IDENTITY_INTO_BIGINT(20, 1, 1);
+SELECT sys.IDENTITY_INTO_INT(23, 1, 1);
 GO
 
-SELECT sys.IDENTITY_INTO_INT(23, 1, 1);
+SELECT sys.IDENTITY_INTO_BIGINT(20, 1, 1);
 GO
 
 SELECT col1, IDENTITY(int,1,1) AS id_num INTO #babel_539NewTable1 FROM babel_539OldTable;
