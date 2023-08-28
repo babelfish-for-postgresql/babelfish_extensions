c417b0fdf6e25c8dc78b08f807de5dc225778d5c
Christophe <toph@toph.fr>
2023-08-28 18:07:38 +0200

    fixing column_id in view sys.index_columns
    column_id should be the id in the table, but it was the position in the index

--- contrib/babelfishpg_tsql/sql/sys_views.sql
+++ contrib/babelfishpg_tsql/sql/sys_views.sql
@@ -1962,13 +1962,14 @@ as
 select i.indrelid::integer as object_id
   , CAST(CASE WHEN i.indisclustered THEN 1 ELSE 1+row_number() OVER(PARTITION BY c.oid) END AS INTEGER) AS index_id
   , a.attrelid::integer as index_column_id
-  , a.attnum::integer as column_id
+  , col.attnum::integer as column_id
   , a.attnum::sys.tinyint as key_ordinal
   , 0::sys.tinyint as partition_ordinal
   , 0::sys.bit as is_descending_key
   , 1::sys.bit as is_included_column
 from pg_index as i
 inner join pg_catalog.pg_attribute a on i.indexrelid = a.attrelid
+inner join pg_catalog.pg_attribute col on col.attname = a.attname and col.attrelid = i.indrelid
 inner join pg_class c on i.indrelid = c.oid
 inner join sys.schemas sch on sch.schema_id = c.relnamespace
 where has_schema_privilege(sch.schema_id, 'USAGE')
