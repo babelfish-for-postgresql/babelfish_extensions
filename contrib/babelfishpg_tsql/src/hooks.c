eef909c4c45493970b5aa00972007863cab658b9
Vikash Prajapati <vikasprj@amazon.com>
2023-08-28 15:47:27 +0000

    Fix to not allow VIEW in OUTPUT INTO clause
    Signed-off-by: Vikash Prajapati <vikasprj@amazon.com>

--- contrib/babelfishpg_tsql/src/hooks.c
+++ contrib/babelfishpg_tsql/src/hooks.c
@@ -2324,6 +2324,14 @@ modify_insert_stmt(InsertStmt *stmt, Oid relid)
 	HeapTuple	tuple;
 	List	   *insert_col_list = NIL,
 			   *temp_col_list;
+	char		relkind = get_rel_relkind(relid);
+
+	if(relkind == RELKIND_VIEW || relkind == RELKIND_MATVIEW)
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("The target '%s' of the OUTPUT INTO clause cannot be a view or common table expression.", stmt->relation->relname)));
+	}
 
 	if (!output_into_insert_transformation)
 		return;
