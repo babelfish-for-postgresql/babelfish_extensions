cc8f5118d1da455c380729b3b99c3f29445847fd
Sumit Jaiswal <sumiji@amazon.com>
2023-08-29 11:26:00 +0000

    Added test cases with alias in qoutes
    Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

--- test/JDBC/input/BABEL_4012.sql
+++ test/JDBC/input/BABEL_4012.sql
@@ -8,7 +8,7 @@ INSERT INTO BABEL_4012_sch.table1 VALUES(1,4), (1,5);
 GO
 
 -- WITH ALIAS
-
+-- without qouted identifier in alias
 -- with schema name in set and update
 UPDATE BABEL_4012_sch.table1 SET BABEL_4012_sch.table1.a = 2 FROM BABEL_4012_sch.table1 AS t1
 GO
@@ -78,6 +78,81 @@ GO
 
 SELECT * FROM BABEL_4012_sch.table1;
 GO
+
+-- WITH ALIAS
+-- with qouted identifier in alias
+-- with schema name in set and update
+UPDATE BABEL_4012_sch.table1 SET BABEL_4012_sch.table1.a = 3 FROM BABEL_4012_sch.table1 AS "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- with schema name in set and not in update
+UPDATE table1 SET BABEL_4012_sch.table1.a = 2 FROM BABEL_4012_sch.table1 AS  "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- with schema name in set and alias in update
+UPDATE  "_t1 AbC か₰₨" SET BABEL_4012_sch.table1.a = 1 FROM BABEL_4012_sch.table1 AS  "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- with schema name in update and not in set
+UPDATE BABEL_4012_sch.table1 SET table1.a = 3 FROM BABEL_4012_sch.table1 AS  "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- without schema in update and set
+UPDATE table1 SET table1.a = 2 FROM BABEL_4012_sch.table1 AS  "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- without schema name in set and alias in update
+UPDATE  "_t1 AbC か₰₨" SET table1.a = 1 FROM BABEL_4012_sch.table1 AS  "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- using alias in set and schema in update
+UPDATE BABEL_4012_sch.table1 SET "_t1 AbC か₰₨".a = 3 FROM BABEL_4012_sch.table1 AS  "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- using alias in set and without schema in update
+UPDATE table1 SET "_t1 AbC か₰₨".a = 2 FROM BABEL_4012_sch.table1 AS "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- using alias in set and update
+UPDATE "_t1 AbC か₰₨" SET "_t1 AbC か₰₨".a = 1 FROM BABEL_4012_sch.table1 AS "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+-- using alias in set and update
+UPDATE "_t1 AbC か₰₨" SET "_t1 AbC か₰₨".a = 2 FROM BABEL_4012_sch.table1 AS "_t1 AbC か₰₨"
+GO
+
+SELECT * FROM BABEL_4012_sch.table1;
+GO
+
+
+
 
 
 -- WITHOUT ALIAS
