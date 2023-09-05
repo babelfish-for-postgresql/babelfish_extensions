687d173200db2dde0eabde2cec06430307ddc2c1
ANJU BHARTI <abanju@amazon.com>
2023-09-05 11:46:39 +0000

    set GUC parameter for alter table
    Signed-off-by: ANJU BHARTI <abanju@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.2.0--3.3.0.sql
@@ -862,11 +862,15 @@ IMMUTABLE;
 
 ALTER FUNCTION sys.replace (in input_string text, in pattern text, in replacement text) IMMUTABLE;
 
-ALTER TABLE sys.babelfish_sysdatabases ADD COLUMN IF NOT EXISTS orig_name TEXT NOT NULL COLLATE "C";;
+SET allow_system_table_mods = on;
+
+ALTER TABLE sys.babelfish_sysdatabases ADD COLUMN IF NOT EXISTS orig_name TEXT NOT NULL COLLATE "C";
 
 UPDATE sys.babelfish_sysdatabases SET orig_name = name WHERE orig_name IS NULL;
 
 ALTER TABLE sys.babelfish_sysdatabases ALTER COLUMN orig_name SET NOT NULL;
+
+RESET allow_system_table_mods;
 
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
