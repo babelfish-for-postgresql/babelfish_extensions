f56033e33d0d663083c0b1565346fc73f35fe54b
Anikait Agrawal <agraani@amazon.com>
2023-09-07 14:17:38 +0000

    Fixed latitude and srid value Ranges for Geeography Data Types
    Signed-off-by: Anikait Agrawal <agraani@amazon.com>

--- test/JDBC/input/datatypes/TestSpatialPoint-vu-prepare.sql
+++ test/JDBC/input/datatypes/TestSpatialPoint-vu-prepare.sql
@@ -79,9 +79,14 @@ INSERT INTO SPATIALPOINTGEOG_dt (location)
 VALUES ( geography::STGeomFromText('Point(1.0 2.0)', 4326) )
 GO
 
--- Positive Test for STGeomFromText with SRID 0
+-- Negative Test for STGeomFromText for Geography with SRID 0
 INSERT INTO SPATIALPOINTGEOG_dt (location)
 VALUES ( geography::STGeomFromText('Point(47.65100 -22.34900)', 0) )
+GO
+
+-- Negative Test for STGeomFromText for Geography when lat > 90 or < -90
+INSERT INTO SPATIALPOINTGEOG_dt (location)
+VALUES ( geography::STGeomFromText('Point(47.65100 -122.34900)', 4326) )
 GO
 
 -- Negative Test for STGeomFromText when SRID is not provided
@@ -100,13 +105,11 @@ GO
 -- GO
 
 -- Negative Test for STGeomFromText when SRID >= 10^6
--- SRID should be between 0 to 999999
 INSERT INTO SPATIALPOINTGEOG_dt (location)
 VALUES ( geography::STGeomFromText('Point(47.65100 -22.34900)', 1000000000 ) )
 GO
 
 -- Negative Test for STGeomFromText with SRID < 0
--- SRID should be between 0 to 999999
 INSERT INTO SPATIALPOINTGEOG_dt (location)
 VALUES ( geography::STGeomFromText('Point(47.65100 -22.34900)', -1) )
 GO
@@ -134,9 +137,19 @@ INSERT INTO SPATIALPOINTGEOG_dt (location)
 VALUES ( geography::STPointFromText('Point(1.0 2.0)', 4326) )
 GO
 
+-- Negative Test for STPointFromText for Geography when lat > 90 or < -90
+INSERT INTO SPATIALPOINTGEOG_dt (location)
+VALUES ( geography::STPointFromText('Point(47.65100 122.34900)', 4326) )
+GO
+
 -- Positive Test for Point with SRID 4326
 INSERT INTO SPATIALPOINTGEOG_dt (location)
 VALUES ( geography::Point(47.65100, -22.34900, 4326) )
+GO
+
+-- Negative Test for Point for Geography when lat > 90 or < -90
+INSERT INTO SPATIALPOINTGEOG_dt (location)
+VALUES ( geography::Point(147.65100, -22.34900, 4326) )
 GO
 
 CREATE TABLE SPATIALPOINT_dt (GeomColumn geometry, GeogColumn geography)
