6f47fe8566ee72d6e34062536dd318b34c658d1f
Ashish Prasad <pashisht@amazon.com>
2023-09-11 19:44:17 +0000

    Revert all query rewriting

--- test/JDBC/input/ATTIMEZONE.sql
+++ test/JDBC/input/ATTIMEZONE.sql
@@ -46,3 +46,24 @@ GO
 Select convert(datetime2,'2002-01-01 02:01:00.000 +00:00') AT TIME ZONE 'dgycgwycgqd'
 GO
 
+DECLARE @lvDate DATETIME, @lvDateUTC DATETIME
+SET @lvDate = '2021-01-01'
+SET @lvDateUTC = @lvDate AT TIME ZONE  'US Eastern Standard Time' AT TIME ZONE 'UTC'
+SELECT @lvDate, @lvDateUTC
+GO
+
+Select convert(datetime2,'2002-01-01 02:01:00.000 +00:00') aT TiMe ZoNE 'Eastern Standard Time'
+GO
+
+Select convert(datetime2,'2002-01-01 02:01:00.000 +00:00') aT   TiMe   ZoNE 'Eastern Standard Time'
+GO
+
+DROP TABLE IF EXISTS test
+Create table test( a datetime)
+insert into test(a) values(cast('1999-02-16' as datetime))
+GO
+DECLARE @testzone nvarchar(128) = 'Central European Standard Time';
+Select a AT TIME ZONE @testzone from test
+GO
+
+
