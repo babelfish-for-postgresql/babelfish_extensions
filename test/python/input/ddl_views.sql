fc811f71a6d3487fca089b825f269a76c6fb0dbf
Ashish Prasad <56514722+hash-16@users.noreply.github.com>
2023-09-28 14:00:49 +0530

    Update ddl_views.sql

--- test/python/input/ddl_views.sql
+++ test/python/input/ddl_views.sql
@@ -21,6 +21,26 @@ CREATE VIEW sys_all_views_dep_view_vu_prepare AS
 SELECT name, type, with_check_option FROM sys.all_views where object_id = object_id('sys_all_views_select_vu_prepare')
 GO
 
+CREATE LOGIN AbolrousHazem1   
+    WITH PASSWORD = '340$Uuxwp7Mcxo7Khy'; 
+	
+CREATE LOGIN AbolrousHazem2   
+    WITH PASSWORD = '340$UuxwpMcxo7Khy';  
+
+CREATE LOGIN AbolrousHazem3   
+    WITH PASSWORD = '340$Uuxwp7Mco7Khy'; 
+	
+
+
+CREATE USER AbolrousHazem1 FOR LOGIN AbolrousHazem1;  
+GO
+
+CREATE USER AbolrousHaze2m FOR LOGIN AbolrousHazem2;  
+GO
+
+CREATE USER AbolrousHazem3 FOR LOGIN AbolrousHazem3;  
+GO
+
 --DROP
 DROP VIEW IF EXISTS sys_all_views_select_vu_prepare
 GO
