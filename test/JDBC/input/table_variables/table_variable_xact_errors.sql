8e8e212366d773a1fb45b39ee71fba6c8a325efc
Kristian Lejao <1741885+lejaokri@users.noreply.github.com>
2023-10-06 09:57:20 -0700

    [extension] Add missing commits in 2_5 (#1890)
    * Removed incorrect check in table variable visibility functions (#1518)
    
    disallowing Table variables from being used in psql por
    
    Task: [NO-JIRA]
    
    Signed-off-by: Kristian Lejao <klejao@amazon.com>
    (cherry picked from commit 6690d7b4d6f60dc48491b3e5e8a4b6d741a622f2)
    
    * Introduce drop_relation_refcnt_hook to be called during drop table variable (#1574)
    
    when error handling does not properly close table variables on Error
    
    Tasks: BABEL-4225, BABEL-4226, BABEL-4227
    
    Signed-off-by: Kristian Lejao <klejao@amazon.com>
    (cherry picked from commit 99e804f973fbc591e57b1d6b7937c51f2b6e77b0)
    
    * Drop Table variables after dropping cursors because cursors (#1584) (#1589)
    
    During cleanup phase, drop table variable would drain the relation refcnt hence when we close the cursors that references the table variables would error out. Fix is to change the order of operation in the cleanup phase, drop cursor before drop table variables.
    
    Task: BABEL-4268, BABEL-4269
    
    Signed-off-by: Kristian Lejao <klejao@amazon.com>
    (cherry picked from commit f1ab4aac64bcc5fd2554da49852aebf894439b17)
    
    * Add null check in exec_eval_datum for table types (#1778)
    
    In cases of nested TVF, exec_eval_datum was returning an incorrect value of isnull for actual null values. We match the check in bbf_table_var_lookup to get the correct value.
    
    Task: BABEL-4337
    Signed-off-by: Tim Chang <changti@amazon.com>
    (cherry picked from commit 4c5e722af36544b90ba0a12b8df56110988dc512)
    
    * Drop Table Variables even when AbortCurTransaction is true (#1737)
    
    When there is an error, AbortCurTransaction could be set to true.
    During clean up phase, logic tries to drop table variables and as part of the drop,
    it acquires exclusive lock on the relation which eventually calls
    AssignTransactionId. This is not allowed in AssignTransactionId and error
    handling is not able to recover from there.
    
    It has been decided to temporarily set AbortCurTransaction=false in
    clean up phase in order for drop to continue.
    
    Task: BABEL-4336
    Signed-off-by: Kristian Lejao <klejao@amazon.com>
    (cherry picked from commit c36507239d2290d5200907687430e79b1dc3405c)
    
    * Update psqlodbc driver used in github action (#1863)
    
    * Update psqlodbc driver used in github action
    
    Signed-off-by: Alex Kasko <alex@staticlibs.net>
    (cherry picked from commit 2cb3d95b32310d05d38bc599e549a012d2515dda)
    
    ---------
    
    Co-authored-by: Tim Chang <timchang514@users.noreply.github.com>
    Co-authored-by: staticlibs <alex@staticlibs.net>

--- test/JDBC/input/table_variables/table_variable_xact_errors.sql
+++ test/JDBC/input/table_variables/table_variable_xact_errors.sql
@@ -26,6 +26,7 @@ GO
 
 -------------------------------------------------------------------------------
 -- Test 2: Procedure with Table Variables and THROW
+--         Test with PROC + Table Variable and error while relation is open
 -------------------------------------------------------------------------------
 CREATE PROC table_variable_throw_proc1 AS
 BEGIN
@@ -44,6 +45,25 @@ SELECT * FROM @tv
 GO
 
 DROP PROCEDURE table_variable_throw_proc1
+GO
+
+CREATE PROCEDURE tv_function_1
+AS
+BEGIN TRY
+    DECLARE @tv TABLE(c1 INT PRIMARY KEY, c2 INT)
+    INSERT INTO @tv VALUES(1, 10), (2, 20), (3, 30)
+    INSERT INTO @tv VALUES(3, 30) -- duplicate key, fail while table and index are open
+END TRY
+BEGIN CATCH
+    INSERT INTO @tv VALUES(3, 30) -- duplicate key, fail while table and index are open
+    SELECT * FROM @tv
+END CATCH;
+GO
+
+EXEC tv_function_1
+GO
+
+DROP PROCEDURE tv_function_1
 GO
 
 -------------------------------------------------------------------------------
@@ -91,3 +111,320 @@ GO
 DROP TYPE empDates
 GO
 
+-------------------------------------------------------------------------------
+-- BABEL-4225: Error inside function should not cause crash
+-------------------------------------------------------------------------------
+CREATE SCHEMA [Control]
+GO
+
+CREATE FUNCTION [Control].[csf_script_delete_row] (
+   @SetIDLocal  INT,
+   @InternalID  VARCHAR(64)
+) RETURNS VARCHAR(MAX)
+AS
+BEGIN
+   DECLARE @Template          VARCHAR(MAX);
+   DECLARE @Results           VARCHAR(MAX);
+   DECLARE @DeletePredicates  VARCHAR(MAX);
+   DECLARE @Ordinal           INT;   -- NOTE: In this circumstance, each column is either an update or a predicate but not both.
+   DECLARE @Columns
+   TABLE   ( Ordinal          INT IDENTITY(1, 1) NOT NULL,
+             ColumnPredicate  VARCHAR(MAX)           NULL,
+             PRIMARY KEY ( Ordinal )
+           );
+   SET @Template = 'DELETE [SchemaName].[TableName] WHERE DeleteConditionPredicates;';
+   INSERT INTO @Columns (ColumnPredicate )
+   SELECT '[' + LRV.ColumnName + '] ' + ISNULL(('= ' + NULLIF(LRV.Expression, 'NULL')), 'IS NULL')
+   FROM   [Control].[DataRow] AS RLR
+          INNER JOIN [Control].[DataRowValue] AS LRV
+          ON (RLR.SetID      = LRV.SetID         AND
+              RLR.InternalID = LRV.InternalIDRow    )
+          INNER JOIN [Control].[cvw_local_column_base] AS STC
+          ON (RLR.SchemaName = STC.SchemaName AND
+              RLR.TableName  = STC.TableName  AND
+              LRV.ColumnName = STC.ColumnName    )
+   WHERE  RLR.SetID      =  @SetIDLocal
+   AND    RLR.InternalID =  @InternalID
+   AND    LRV.MatchType  != 'N'
+   ORDER BY STC.ColumnID ASC;END; -- [Control].[csf_script_delete_row]
+go
+
+SELECT Control.csf_script_delete_row('1', ' ')
+GO
+
+DROP FUNCTION [Control].[csf_script_delete_row]
+GO
+
+DROP SCHEMA [Control]
+GO
+
+-------------------------------------------------------------------------------
+-- BABEL-4226: Error while table and index are open.
+-------------------------------------------------------------------------------
+
+CREATE FUNCTION dbo.foo() RETURNS @tab TABLE (a INT, b VARCHAR(MAX) NOT NULL) AS
+BEGIN
+        INSERT @tab(a, b) VALUES (1, NULL)
+        RETURN
+END
+GO
+
+SELECT * FROM dbo.foo()
+GO
+
+DROP FUNCTION dbo.foo()
+GO
+
+CREATE FUNCTION dbo.foo() RETURNS @tab TABLE (a INT PRIMARY KEY, b VARCHAR(MAX) NOT NULL) AS
+BEGIN
+        INSERT @tab(a, b) VALUES (1, NULL)
+        RETURN
+END
+GO
+
+SELECT * FROM dbo.foo()
+GO
+
+DROP FUNCTION dbo.foo()
+GO
+
+CREATE FUNCTION [dbo].[tv_function_1]() RETURNS @tab TABLE(a int, b int PRIMARY KEY) AS
+BEGIN
+    DECLARE @tv TABLE(c1 INT PRIMARY KEY, c2 INT)
+    INSERT INTO @tv VALUES(1, 10), (2, 20), (3, 30)
+    INSERT INTO @tab(a, b) SELECT c1, c2 FROM @tv
+    INSERT INTO @tab VALUES(4, 30) -- duplicate key, fail while table and index are open
+    INSERT INTO @tab VALUES(1, 2)
+    RETURN
+END
+GO
+
+SELECT * FROM [dbo].[tv_function_1]()
+GO
+
+DROP FUNCTION [dbo].[tv_function_1]
+GO
+
+-------------------------------------------------------------------------------
+-- BABEL-4227: Error should not cause crash
+-------------------------------------------------------------------------------
+CREATE FUNCTION LevenschteinDifference
+(
+@FirstString nVarchar(255), @SecondString nVarchar(255)
+)
+RETURNS int
+as begin
+Declare @PseudoMatrix table
+     (location int identity primary key,
+      firstorder int not null,
+      Firstch nchar(1),
+      secondorder int not null,
+      Secondch nchar(1),
+      Thevalue int not null default 0,
+      PreviousRowValues varchar(200)
+      )
+
+insert into @PseudoMatrix (firstorder, firstch, secondorder, secondch, TheValue )
+SELECT TheFirst.number,TheFirst.ch, TheSecond.number,TheSecond.ch,0
+  FROM
+   (SELECT number, SUBSTRING(@FirstString,number,1) AS ch
+    FROM numbers WHERE number <= LEN(@FirstString) union all Select 0,Char(0)) TheFirst
+  cross JOIN
+   (SELECT number, SUBSTRING(@SecondString,number,1) AS ch
+    FROM numbers WHERE number <= LEN(@SecondString) union all Select 0,Char(0)) TheSecond
+
+order by TheFirst.number, TheSecond.number
+
+Declare @current Varchar(255)
+Declare @previous Varchar(255)
+Declare @TheValue int
+Declare @Deletion int, @Insertion int, @Substitution int, @minim int
+Select @current='', @previous=''
+Update @PseudoMatrix
+    Set
+    @Deletion=@TheValue+1,
+    @Insertion=ascii(substring(@previous,secondorder+1,1))+1,
+    @Substitution=ascii(substring(@previous,(secondorder),1)) +1,
+    @minim=case when @Deletion<@Insertion then @Deletion else @insertion end,
+    @TheValue = Thevalue = case
+ when SecondOrder=0 then FirstOrder
+ When FirstOrder=0 then Secondorder
+     when FirstCh=SecondCh then ascii(substring(@previous,(secondorder),1))
+     else case when @Minim<@Substitution then @Minim else @Substitution end
+   end,
+    @Previous=PreviousRowValues=case when secondorder =0 then @current else @Previous end,
+    @current= case when secondorder =0 then char(@TheValue) else @Current+char(@TheValue) end
+return @TheValue
+End
+go
+
+SELECT dbo.LevenschteinDifference(NULL, NULL)
+GO
+
+SELECT dbo.LevenschteinDifference(' ', ' ')
+GO
+
+DROP FUNCTION dbo.LevenschteinDifference
+GO
+
+-------------------------------------------------------------------------------
+-- Other errors with sp_executesql
+-------------------------------------------------------------------------------
+
+CREATE procedure temp_table_sp_exec AS
+BEGIN
+    DECLARE @SQLString NVARCHAR(500);
+    SET @SQLString = N'declare @table_t1 table(a int); INSERT INTO @table_t1 values(1);SELECT * FROM @table_t1';
+    EXECUTE sp_executesql @SQLString;
+
+    SET @SQLString = N'declare @table_t1 table(a int NOT NULL); INSERT INTO @table_t1 values(NULL);SELECT * FROM @table_t1';
+    EXECUTE sp_executesql @SQLString;
+END;
+GO
+
+EXEC temp_table_sp_exec
+GO
+
+DROP PROCEDURE temp_table_sp_exec
+GO
+
+-------------------------------------------------------------------------------
+-- Drop type used by table variable
+-------------------------------------------------------------------------------
+
+CREATE TYPE typa FROM int
+GO
+
+CREATE TYPE typb FROM nvarchar(100)
+GO
+
+DECLARE @tv_3 TABLE(a typa, b typb)
+DROP TYPE typb
+
+INSERT INTO @tv_3 VALUES(1, 'Hello')
+SELECT * FROM @tv_3
+GO
+
+DROP TYPE typa
+GO
+
+DROP TYPE typb
+GO
+
+-------------------------------------------------------------------------------
+-- Cursor not explicitly closed
+-------------------------------------------------------------------------------
+
+DECLARE @STATION_INTS_TABLE TABLE (STATION_INT INT)
+DECLARE @v INT
+
+DECLARE CUR_NETWORK CURSOR LOCAL FOR SELECT STATION_INT FROM @STATION_INTS_TABLE
+OPEN CUR_NETWORK
+FETCH NEXT  FROM CUR_NETWORK INTO @v
+GO
+
+select 123
+GO
+
+CREATE FUNCTION [dbo].[WOSQL_BuildRevenueDetailOLUQuery]  ()
+RETURNS NVARCHAR(MAX) AS
+BEGIN
+    DECLARE @TSQL NVARCHAR(MAX)
+    DECLARE @STATION_INTS_TABLE TABLE (STATION_INT INT)
+    DECLARE @STATION_INT INT     SET @TSQL = ''
+    DECLARE CUR_NETWORK CURSOR LOCAL FOR
+        SELECT STATION_INT FROM @STATION_INTS_TABLE    OPEN CUR_NETWORK
+    FETCH NEXT FROM CUR_NETWORK INTO @STATION_INT
+
+    RETURN @TSQL
+END
+GO
+
+SELECT dbo.WOSQL_BuildRevenueDetailOLUQuery()
+GO
+
+SELECT dbo.WOSQL_BuildRevenueDetailOLUQuery()
+GO
+
+DROP FUNCTION [dbo].[WOSQL_BuildRevenueDetailOLUQuery]
+GO
+
+CREATE FUNCTION [dbo].[WOSQL_BuildRevenueDetailOLUQuery]  ()
+RETURNS NVARCHAR(MAX) AS
+BEGIN
+    DECLARE @TSQL NVARCHAR(MAX)
+    DECLARE @STATION_INTS_TABLE TABLE (STATION_INT INT)
+    DECLARE @STATION_INT INT     SET @TSQL = ''
+    DECLARE CUR_NETWORK CURSOR LOCAL FOR
+        SELECT STATION_INT FROM @STATION_INTS_TABLE    OPEN CUR_NETWORK
+    FETCH NEXT FROM CUR_NETWORK INTO @STATION_INT
+
+    THROW 51000, 'Throw error', 1;
+
+    RETURN @TSQL
+END
+GO
+
+SELECT dbo.WOSQL_BuildRevenueDetailOLUQuery()
+GO
+
+DROP FUNCTION [dbo].[WOSQL_BuildRevenueDetailOLUQuery]
+GO
+
+CREATE FUNCTION [dbo].[WOSQL_BuildRevenueDetailOLUQuery]  ()
+RETURNS NVARCHAR(MAX) AS
+BEGIN
+    DECLARE @TSQL NVARCHAR(MAX)
+    DECLARE @STATION_INTS_TABLE TABLE (STATION_INT INT PRIMARY KEY, C2 INT)
+    DECLARE @STATION_INT INT     SET @TSQL = ''
+
+    INSERT INTO @STATION_INTS_TABLE VALUES(1, 1)
+
+    DECLARE CUR_NETWORK CURSOR LOCAL FOR
+        SELECT STATION_INT FROM @STATION_INTS_TABLE
+
+    OPEN CUR_NETWORK
+    FETCH NEXT FROM CUR_NETWORK INTO @STATION_INT
+
+    INSERT INTO @STATION_INTS_TABLE VALUES(1, 1) -- duplicate key
+    RETURN @TSQL
+END
+GO
+
+SELECT dbo.WOSQL_BuildRevenueDetailOLUQuery()
+GO
+
+DROP FUNCTION [dbo].[WOSQL_BuildRevenueDetailOLUQuery]
+GO
+
+-------------------------------------------------------------------------------
+-- BABEL-4267: Error should not cause crash
+-------------------------------------------------------------------------------
+
+CREATE PROCEDURE usp_PopulateDiscount
+AS
+    DECLARE @Lookup TABLE (StartDate DATETIME NOT NULL)
+    INSERT INTO @Lookup SELECT GETDATE()
+    BEGIN TRANSACTION
+    DELETE trgt FROM Discount trgt           -- Discount does not exist
+    COMMIT
+go
+
+EXECUTE usp_PopulateDiscount
+go
+
+CREATE PROCEDURE test
+AS
+BEGIN TRY
+    DECLARE @tv1 TABLE(c1 INT PRIMARY KEY, b INT IDENTITY, c CHAR(15) DEFAULT 'Whoops!')
+    SELECT 1/0
+END TRY
+BEGIN CATCH
+    BEGIN TRANSACTION
+    INSERT INTO @tv1 VALUES(1, 3, 'Three')          -- invalid syntax, should do a clean shutdown
+    COMMIT
+END CATCH;
+GO
+
+exec test
+go
