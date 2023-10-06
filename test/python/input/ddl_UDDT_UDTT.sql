3b935ed9dd595675f1d35669c307bc37fef6245c
Ashish Prasad <pashisht@amazon.com>
2023-10-06 08:47:09 +0000

    Add test to ddl test framework

--- test/python/input/ddl_UDDT_UDTT.sql
+++ test/python/input/ddl_UDDT_UDTT.sql
@@ -2,8 +2,6 @@ DROP TYPE IF EXISTS test1
 GO
 DROP TYPE IF EXISTS test2
 GO
-DROP TYPE IF EXISTS shc_test.test3
-GO
 DROP TYPE IF EXISTS test4
 GO
 DROP TYPE IF EXISTS LocationTableType
@@ -11,6 +9,10 @@ GO
 DROP TYPE IF EXISTS InventoryItem
 GO
 DROP TYPE IF EXISTS shc_test.LocationTableType
+GO
+DROP TABLE IF EXISTS t_udd
+GO
+DROP TYPE IF EXISTS shc_test.test3
 GO
 DROP SCHEMA IF EXISTS shc_test
 GO
@@ -24,11 +26,14 @@ GO
 CREATE SCHEMA shc_test
 GO
 
-Create type shc_test.test3 FROM varchar(11) NOT NULL ;
+Create type shc_test.test3 FROM int NOT NULL ;
 GO
 
 Create type test4 from numeric(15,4)
 GO
+
+create table t_udd( a shc_test.test3);
+go
 
 CREATE TYPE LocationTableType AS TABLE   
     ( LocationName VARCHAR(50)  
@@ -56,8 +61,6 @@ DROP TYPE IF EXISTS test1
 GO
 DROP TYPE IF EXISTS test2
 GO
-DROP TYPE IF EXISTS shc_test.test3
-GO
 DROP TYPE IF EXISTS test4
 GO
 DROP TYPE IF EXISTS LocationTableType
@@ -66,5 +69,10 @@ DROP TYPE IF EXISTS InventoryItem
 GO
 DROP TYPE IF EXISTS shc_test.LocationTableType
 GO
+DROP TABLE IF EXISTS t_udd
+GO
+DROP TYPE IF EXISTS shc_test.test3
+GO
 DROP SCHEMA IF EXISTS shc_test
 GO
+
