5981054f491b7ee0d04d9551c6b3d6989c37441a
Deepakshi Mittal <depakshi@amazon.com>
2023-10-09 19:57:34 +0000

    Fix failing test cases in github, added to upadte schedule
    Task: BABEL-2170
    
    Signed-off-by: Deepakshi Mittal <depakshi@amazon.com>

--- test/JDBC/expected/BABEL-2170-vu-cleanup.out
+++ test/JDBC/expected/BABEL-2170-vu-cleanup.out
@@ -23,41 +23,10 @@ GO
 DROP TABLE IF EXISTS babel_2170_vu_employees;
 GO
 
--- schema_2170 object cleanup
-DROP TRIGGER IF EXISTS schema_2170.babel_2170_vu_employees_view_iot_update;
-GO
-
-DROP VIEW IF EXISTS schema_2170.babel_2170_vu_employees_view;
-GO
-
-DROP TABLE IF EXISTS schema_2170.babel_2170_vu_employees;
-GO
--- clean  all objects in second database
-USE db2_BABEL2170;
-GO
-
-DROP TRIGGER IF EXISTS babel_2170_vu_employees_view_iot_insert;
-GO
-
-DROP TRIGGER IF EXISTS babel_2170_vu_employees_view_iot_update;
-GO
-
-DROP TRIGGER IF EXISTS babel_2170_vu_employees_view_iot_delete;
-GO
-
-DROP VIEW IF EXISTS babel_2170_vu_employees_view;
-GO
-
-DROP TABLE IF EXISTS babel_2170_vu_employees;
-GO
-
 USE MASTER; 
 GO
 
-DROP DATABASE db1_BABEL2170;
-GO
-
-DROP DATABASE db2_BABEL2170;
+DROP DATABASE IF EXISTS db1_BABEL2170;
 GO
 
 SELECT name FROM sys.sysobjects WHERE name LIKE 'babel_2170%' ORDER BY name;
--- test/JDBC/input/triggers/BABEL-2170-vu-verify.sql
+++ test/JDBC/input/triggers/BABEL-2170-vu-verify.sql
@@ -84,7 +84,7 @@ GO
 
 -- Test Instead of Update trigger in same db but cross schema 
 
--- cleanup default dbo schema IO upadte trigger
+-- Cleanup default dbo schema IO Update trigger
 DROP TRIGGER IF EXISTS [dbo].[babel_2170_vu_employees_view_iot_update];
 GO
 
@@ -106,16 +106,60 @@ GO
 UPDATE [dbo].[babel_2170_vu_employees_view] SET MonthSalary = MonthSalary +1 WHERE EmployeeID = 3;
 GO
 
-DROP TRIGGER IF EXISTS [schema_2170].[babel_2170_vu_employees_view_iot_update];
-GO
-
 DROP TRIGGER IF EXISTS[dbo].[babel_2170_vu_employees_view_iot_update];
 GO
 
--- create same name Instead of Insert trigger in second db to test Cross db behavior
+-- schema_2170 object cleanup
+DROP TRIGGER IF EXISTS [schema_2170].babel_2170_vu_employees_view_iot_update;
+GO
+
+DROP VIEW IF EXISTS [schema_2170].babel_2170_vu_employees_view;
+GO
+
+DROP TABLE IF EXISTS [schema_2170].babel_2170_vu_employees;
+GO
+
+DROP SCHEMA IF EXISTS schema_2170;
+GO
+
+-- test multi-db mode
+SELECT set_config('role', 'jdbc_user', false);
+GO
+
+SELECT set_config('babelfishpg_tsql.migration_mode', 'multi-db', false);
+GO
+
+CREATE DATABASE db2_BABEL2170;
+GO
+
 USE db2_BABEL2170;
 GO
 
+CREATE TABLE babel_2170_vu_employees
+(
+    EmployeeID      int NOT NULL,
+    EmployeeName    VARCHAR(50),
+    EmployeeAddress VARCHAR(50),
+    MonthSalary     NUMERIC(10, 2)
+)
+GO
+
+INSERT INTO babel_2170_vu_employees VALUES(1, 'amber', '1st Street', '1000');
+INSERT INTO babel_2170_vu_employees VALUES(2, 'angel', '1st Street', '2000');
+INSERT INTO babel_2170_vu_employees VALUES(3, 'ana', '1st Street', '3000');
+INSERT INTO babel_2170_vu_employees VALUES(4, 'adam', '1st Street', '4000');
+GO
+
+CREATE VIEW babel_2170_vu_employees_view AS
+SELECT EmployeeID,
+       EmployeeName,
+       EmployeeAddress,
+       MonthSalary
+FROM babel_2170_vu_employees
+WHERE EmployeeName LIKE 'a%';
+GO
+
+-- create same name Instead of Insert trigger in second db to test Cross db behavior
 CREATE TRIGGER babel_2170_vu_employees_view_iot_insert ON babel_2170_vu_employees_view
 INSTEAD OF INSERT
 AS
@@ -132,4 +176,26 @@ SELECT EmployeeID, EmployeeName, EmployeeAddress, MonthSalary FROM babel_2170_vu
 GO
 
 SELECT EmployeeID, EmployeeName, EmployeeAddress, MonthSalary FROM babel_2170_vu_employees_view WHERE EmployeeID =5;
+GO
+
+-- clean  all objects in second database
+DROP TRIGGER IF EXISTS babel_2170_vu_employees_view_iot_insert;
+GO
+
+DROP TRIGGER IF EXISTS babel_2170_vu_employees_view_iot_update;
+GO
+
+DROP TRIGGER IF EXISTS babel_2170_vu_employees_view_iot_delete;
+GO
+
+DROP VIEW IF EXISTS babel_2170_vu_employees_view;
+GO
+
+DROP TABLE IF EXISTS babel_2170_vu_employees;
+GO
+
+USE MASTER;
+GO
+
+DROP DATABASE IF EXISTS db2_BABEL2170;
 GO
