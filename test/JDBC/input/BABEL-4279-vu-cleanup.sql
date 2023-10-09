fc1e7c234f5a1dd103cf925a03261aa3ce269f43
Riya Jain <riyaajn@amazon.com>
2023-10-09 19:49:09 +0000

    Added test cases tfor both upgrade and jdbc.
    Signed-off-by: Riya Jain <riyaajn@amazon.com>

--- test/JDBC/input/BABEL-4279-vu-cleanup.sql
+++ test/JDBC/input/BABEL-4279-vu-cleanup.sql
@@ -5,19 +5,40 @@ GO
 DROP VIEW test_babel_4279_v2;
 GO
 
+DROP VIEW test_babel_4279_v3;
+GO
+
 DROP TABLE test_babel_4279_t1;
 GO
 
-DROP VIEW test_babel_4279_v3;
+USE ["test_babel_4279_d.1"];
+GO
+
+DROP VIEW test_babel_4279_sv1;
+GO
+
+DROP TABLE test_babel_4279_s1.test_babel_4279_st1;
+GO
+
+DROP SCHEMA test_babel_4279_s1;
+GO
+
+USE MASTER;
+GO
+
+DROP DATABASE ["test_babel_4279_d.1"];
 GO
 
 DROP VIEW test_babel_4279_v4;
 GO
 
+DROP VIEW test_babel_4279_v5;
+GO
+
 DROP TABLE test_babel_4279_t2;
 GO
 
-DROP VIEW test_babel_4279_v5;
+DROP VIEW test_babel_4279_v6;
 GO
 
 DROP TABLE "tngdf'".[sc,sdg"fdsngjds'];
@@ -26,13 +47,13 @@ GO
 DROP SCHEMA "tngdf'";
 GO
 
-DROP VIEW test_babel_4279_v6;
+DROP VIEW test_babel_4279_v7;
 GO
 
 DROP TABLE test_babel_4279_t3;
 GO
 
-DROP VIEW test_babel_4279_v7;
+DROP VIEW test_babel_4279_v8;
 GO
 
 DROP TABLE test_babel_4279_t4;
