ad1178876e59eac9df0c0011aa4c1a79867d4d38
Parikshit Sarode <pasarode@amazon.com>
2023-10-11 12:14:01 +0000

    implemented babelfish_concat_wrapper in c

--- contrib/babelfishpg_common/src/varbinary.c
+++ contrib/babelfishpg_common/src/varbinary.c
@@ -77,7 +77,7 @@ PG_FUNCTION_INFO_V1(float4binary);
 PG_FUNCTION_INFO_V1(float8binary);
 PG_FUNCTION_INFO_V1(binaryfloat4);
 PG_FUNCTION_INFO_V1(binaryfloat8);
-
+PG_FUNCTION_INFO_V1(babelfish_concat_wrapper);
 
 /*****************************************************************************
  *	 USER I/O ROUTINES														 *
@@ -586,6 +586,38 @@ rowversionbinary(PG_FUNCTION_ARGS)
 	memcpy(rp, data, len);
 
 	PG_RETURN_BYTEA_P(source);
+}
+
+Datum
+babelfish_concat_wrapper(PG_FUNCTION_ARGS)
+{
+	text *t1 = (PG_GETARG_TEXT_PP(0));
+	text *t2 = (PG_GETARG_TEXT_PP(1));
+
+	char *str1 = text_to_cstring(t1);
+	char *str2 = text_to_cstring(t2);
+
+	int len1 = VARSIZE_ANY_EXHDR(t1);
+	int len2 = VARSIZE_ANY_EXHDR(t2);
+
+	text *result_text = (text *) palloc(VARHDRSZ+len1+len2);
+
+	// Check if the first string is NULL
+    if (PG_ARGISNULL(0))
+    {
+        if (PG_ARGISNULL(1))
+            PG_RETURN_NULL(); // If both are NULL, return NULL
+        else
+            PG_RETURN_TEXT_P(PG_GETARG_TEXT_P(1)); // If only the first string is NULL, return the second string
+    }
+    else if (PG_ARGISNULL(1))
+    {
+        PG_RETURN_TEXT_P(PG_GETARG_TEXT_P(0)); // If only the second string is NULL, return the first string
+    }
+
+    SET_VARSIZE(result_text, VARHDRSZ+len1+len2);
+    snprintf(VARDATA(result_text), VARHDRSZ+len1+len2 + 1, "%s%s", str1, str2);
+    PG_RETURN_TEXT_P(result_text);
 }
 
 Datum
