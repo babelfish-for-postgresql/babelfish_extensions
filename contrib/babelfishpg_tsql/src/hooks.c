b2ae5ba78bb3e2bec3a7a912f5466c2a27e9bc5a
Riya Jain <75467028+riyajain39@users.noreply.github.com>
2023-10-12 14:00:39 +0530

    Fix the issue of preserving case of column names in SELECT corrname.colname. (#1866)
    This commit fixes preserving the original case spelling of column names while selecting named column with a
    correlation name.
    
    Issue: When selecting named column with a correlation name, the lowercase name is always returned. The case of
    column name is not preserved. For instance, select ABC from table, here ABC is the column name. In this case column
    name becomes downcased. To address this issue, I've made some code modifications to ensure that the column name
    is handled correctly, whether it's truncated or its case needs to be preserved.
    
    Task: BABEL-4279
    Signed-off-by: Riya Jain <riyaajn@amazon.com>

--- contrib/babelfishpg_tsql/src/hooks.c
+++ contrib/babelfishpg_tsql/src/hooks.c
@@ -1626,6 +1626,9 @@ pre_transform_target_entry(ResTarget *res, ParseState *pstate,
 		int			alias_len = 0;
 		const char *colname_start;
 		const char *identifier_name = NULL;
+		int			open_square_bracket = 0;
+		int			double_quotes = 0;
+		const char *last_dot;
 
 		if (res->name == NULL && res->location != -1 &&
 			IsA(res->val, ColumnRef))
@@ -1638,11 +1641,68 @@ pre_transform_target_entry(ResTarget *res, ParseState *pstate,
 			 * sourcetext
 			 */
 			if (list_length(cref->fields) == 1 &&
-				IsA(linitial(cref->fields), String))
+				IsA(linitial(cref->fields), String)) 
 			{
 				identifier_name = strVal(linitial(cref->fields));
 				alias_len = strlen(identifier_name);
 				colname_start = pstate->p_sourcetext + res->location;
+			}
+			/*
+			 * This condition will preserve the case of column name when there are more than
+			 * one cref->fields. For instance, Queries like 
+			 * 1. select [database].[schema].[table].[column] from table.
+			 * 2. select [schema].[table].[column] from table.
+			 * 3. select [t].[column] from table as t
+			 * Case 1: Handle the cases when column name is passed with no delimiters
+			 * For example, select ABC from table
+			 * Case 2: Handle the cases when column name is delimited with dq.
+			 * In such cases, we are checking if no. of dq are even or not. When dq are odd,
+			 * we are not tracing number of sqb and sq within dq.
+			 * For instance, Queries like select "AF bjs'vs] " from table.
+			 * Case 3: Handle the case when column name is delimited with sqb. When number of sqb
+			 * are zero, it means we are out of sqb.
+			 */
+			if(list_length(cref->fields) > 1 &&
+				IsA(llast(cref->fields), String))
+			{
+				identifier_name = strVal(llast(cref->fields));
+				alias_len = strlen(identifier_name);
+				colname_start = pstate->p_sourcetext + res->location;
+				while(true)
+				{	
+					if(open_square_bracket == 0 && *colname_start == '"')
+					{
+						double_quotes++;
+					}
+					/* To check how many open sqb are present in sourcetext. */
+					else if(double_quotes % 2 == 0 && *colname_start == '[')
+					{
+						open_square_bracket++;
+					}
+					else if(double_quotes % 2 == 0 && *colname_start == ']')
+					{
+						open_square_bracket--;
+					}
+					/*
+					 * last_dot pointer is to trace the last dot in the sourcetext,
+					 * as last dot indicates the starting of column name.
+					 */
+					else if(open_square_bracket == 0 && double_quotes % 2 == 0 && *colname_start == '.')
+					{
+						last_dot = colname_start;
+					}
+					/* 
+					 * If there is no open sqb, there are even no. of sq or dq and colname_start is at
+					 * space or comma, it means colname_start is at the end of column name.
+					 */
+					else if(open_square_bracket == 0 && double_quotes % 2 == 0 && (*colname_start == ' ' || *colname_start == ','))
+					{
+						last_dot++;
+						colname_start = last_dot;
+						break;
+					}
+					colname_start++;
+				}
 			}
 		}
 		else if (res->name != NULL && res->name_location != -1)
