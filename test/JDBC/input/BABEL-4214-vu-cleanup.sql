e8f6d6bd6cbabfc95e0dc2f4f5c9ec69171fbeb7
Deepakshi Mittal <depakshi@amazon.com>
2023-10-19 22:27:42 +0000

    Modified prepare and cleanup scripts
    Task: BABEL-4214
    
    Signed-off-by: Deepakshi Mittal <depakshi@amazon.com>

--- test/JDBC/input/BABEL-4214-vu-cleanup.sql
+++ test/JDBC/input/BABEL-4214-vu-cleanup.sql
@@ -1,2 +1,11 @@
-DROP VIEW babel_4214_datetime_to_int_view
+DROP PROC IF EXISTS babel_4214_datetime_to_int_proc
+GO
+
+DROP FUNCTION IF EXISTS babel_4214_datetime_to_int_function
+GO
+
+DROP PROC IF EXISTS babel_4214_smalldatetime_to_int_proc
+GO
+
+DROP FUNCTION IF EXISTS babel_4214_smalldatetime_to_int_function
 GO
