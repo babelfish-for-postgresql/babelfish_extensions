57f84b9eb7020051fe4c84840f33493ed5d0d5ed
Tanzeel Khan <tzlkhan@amazon.com>
2023-10-19 14:19:25 +0000

    retrigger tests
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- contrib/babelfishpg_tsql/src/guc.c
+++ contrib/babelfishpg_tsql/src/guc.c
@@ -1202,7 +1202,6 @@ define_custom_variables(void)
 							 PGC_SUSET,
 							 GUC_NO_SHOW_ALL | GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_AUTO_FILE,
 							 NULL, NULL, NULL);
-
 }
 
 int			escape_hatch_storage_options = EH_IGNORE;
