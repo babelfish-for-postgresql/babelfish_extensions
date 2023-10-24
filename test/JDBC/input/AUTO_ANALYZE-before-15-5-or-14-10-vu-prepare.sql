b5fb8d0ba38b78c1f8b7d8b84a225ef750b14d7d
Sumit Jaiswal <sumiji@amazon.com>
2023-10-24 10:48:34 +0000

    Add upgrade test make sure analyze is working Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

---
+++ test/JDBC/input/AUTO_ANALYZE-before-15-5-or-14-10-vu-prepare.sql
@@ -0,0 +1,18 @@
+-- function to list all the babelfish catalogs that has not been analyzed manually
+CREATE FUNCTION auto_analyze_vu_prepare_catalogs_have_not_been_analyzed()
+RETURNS TABLE 
+AS RETURN
+(SELECT relname FROM pg_stat_all_tables WHERE schemaname = 'sys' and last_analyze IS NULL)
+GO
+
+-- function to list all the babelfish catalogs that has been analyzed manually
+CREATE FUNCTION auto_analyze_vu_prepare_catalogs_have_been_analyzed()
+RETURNS TABLE 
+AS RETURN
+(SELECT relname FROM pg_stat_all_tables WHERE schemaname = 'sys' and last_analyze IS NOT NULL)
+GO
+
+
+-- will return null as none of the catalog has been analyzed
+SELECT * FROM auto_analyze_vu_prepare_catalogs_have_been_analyzed()
+GO
