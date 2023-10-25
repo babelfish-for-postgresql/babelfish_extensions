fbd6b49ae64bea2a576ec409aa6589efe79dee98
Sumit Jaiswal <sumiji@amazon.com>
2023-10-25 12:59:14 +0000

    only list in case of failure
    Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

--- test/JDBC/input/AUTO_ANALYZE-before-15-5-or-14-10-vu-verify.sql
+++ test/JDBC/input/AUTO_ANALYZE-before-15-5-or-14-10-vu-verify.sql
@@ -3,8 +3,3 @@
 -- will return NULL in this case
 SELECT relname FROM pg_stat_all_tables WHERE schemaname = 'sys' and last_analyze IS NULL order by relname
 GO
-
-
--- list all the babel catalogs that has been analyzed manually during extension upgrade
-SELECT relname FROM pg_stat_all_tables WHERE schemaname = 'sys' and last_analyze IS NOT NULL order by relname
-Go
