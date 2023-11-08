75ee1d3b00d8c4c8a0d366fa89cd278c684cb860
Xiaohui Fanhe <hexiaohu@amazon.com>
2023-11-08 14:04:57 +0000

    Remove flaky index scan of index on two columns
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

--- test/JDBC/input/ddl/babel_index_nulls_order-before-15-5-vu-verify.sql
+++ test/JDBC/input/ddl/babel_index_nulls_order-before-15-5-vu-verify.sql
@@ -54,30 +54,12 @@ go
 SELECT TOP 1 b FROM babel_index_nulls_order_before_15_5_tbl WHERE b > 'sss' ORDER BY b DESC
 go
 
-SET babelfish_showplan_all OFF
-go
-SELECT set_config('enable_indexonlyscan', 'off', false)
-go
-SET babelfish_showplan_all ON
-go
-
-SELECT TOP 1 a, b FROM babel_index_nulls_order_before_15_5_tbl WHERE a = 3 AND b <= 'sss' ORDER BY b DESC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_before_15_5_tbl WHERE a = 3 AND b <= 'sss' ORDER BY b
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_before_15_5_tbl WHERE a > 5 AND b = 'xyz' ORDER BY a DESC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_before_15_5_tbl WHERE a > 5 AND b = 'xyz' ORDER BY a
-go
-
 -- Reset
 SET babelfish_showplan_all OFF
 go
 SELECT set_config('enable_seqscan', 'on', false)
 go
 SELECT set_config('enable_bitmapscan', 'on', false)
-go
-SELECT set_config('enable_indexonlyscan', 'on', false)
 go
 
 -- Recreate indexes
