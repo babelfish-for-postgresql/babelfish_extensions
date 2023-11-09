015e992a07f7b479c89ece66fd2733c787e4ead8
Xiaohui Fanhe <hexiaohu@amazon.com>
2023-11-09 00:07:21 +0000

    Add query plan hints
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

--- test/JDBC/input/ddl/babel_index_nulls_order-vu-verify.sql
+++ test/JDBC/input/ddl/babel_index_nulls_order-vu-verify.sql
@@ -24,57 +24,6 @@ go
 SELECT * FROM babel_index_nulls_order_ab_v6
 go
 
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a <= 5 OR a IS NULL ORDER BY a DESC
-go
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a > 5 OR a IS NULL ORDER BY a ASC
-go
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a > 5 OR a IS NULL ORDER BY a
-go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b <= 'sss' OR b IS NULL ORDER BY b DESC
-go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b > 'sss' OR b IS NULL ORDER BY b ASC
-go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b > 'sss' OR b IS NULL ORDER BY b
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') OR b IS NULL ORDER BY b DESC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') OR b IS NULL ORDER BY b ASC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') OR b IS NULL ORDER BY b
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') OR a IS NULL ORDER BY a DESC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') OR a IS NULL ORDER BY a ASC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') OR a IS NULL ORDER BY a
-go
-
--- Funcationality and query plans
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a <= 5 ORDER BY a DESC
-go
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a > 5 ORDER BY a ASC
-go
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a > 5 ORDER BY a
-go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b <= 'sss' ORDER BY b DESC
-go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b > 'sss' ORDER BY b ASC
-go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b > 'sss' ORDER BY b
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') ORDER BY b DESC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') ORDER BY b ASC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') ORDER BY b
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') ORDER BY a DESC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') ORDER BY a ASC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') ORDER BY a
-go
-
 -- Check query plans, all aggregations should be optimized
 -- into LIMIT + index scan.
 SELECT set_config('babelfishpg_tsql.enable_pg_hint', 'on', false)
@@ -88,29 +37,23 @@ go
 SET babelfish_showplan_all ON
 go
 
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a <= 5 ORDER BY a DESC
+SELECT TOP 1 a FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_asc_idx_a)) WHERE a <= 5 ORDER BY a DESC
 go
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a > 5 ORDER BY a ASC
+SELECT TOP 1 a FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_asc_idx_a)) WHERE a > 5 ORDER BY a ASC
 go
-SELECT TOP 1 a FROM babel_index_nulls_order_tbl WHERE a > 5 ORDER BY a
+SELECT TOP 1 a FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_asc_idx_a)) WHERE a > 5 ORDER BY a
 go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b <= 'sss' ORDER BY b DESC
+SELECT TOP 1 b FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_desc_idx_b)) WHERE b <= 'sss' ORDER BY b DESC
 go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b > 'sss' ORDER BY b ASC
+SELECT TOP 1 b FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_desc_idx_b)) WHERE b > 'sss' ORDER BY b ASC
 go
-SELECT TOP 1 b FROM babel_index_nulls_order_tbl WHERE b > 'sss' ORDER BY b
+SELECT TOP 1 b FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_desc_idx_b)) WHERE b > 'sss' ORDER BY b
 go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') ORDER BY b DESC
+SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_default_idx_ab)) WHERE (a = 3 AND b <= 'sss') ORDER BY b DESC
 go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') ORDER BY b ASC
+SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_default_idx_ab)) WHERE (a = 3 AND b <= 'sss') ORDER BY b ASC
 go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a = 3 AND b <= 'sss') ORDER BY b
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') ORDER BY a DESC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') ORDER BY a ASC
-go
-SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WHERE (a > 5 AND b = 'xyz') ORDER BY a
+SELECT TOP 1 a, b FROM babel_index_nulls_order_tbl WITH (INDEX (babel_index_nulls_order_default_idx_ab))  WHERE (a = 3 AND b <= 'sss') ORDER BY b
 go
 
 -- Reset
