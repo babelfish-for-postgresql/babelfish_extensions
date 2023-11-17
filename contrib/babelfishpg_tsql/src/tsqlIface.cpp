bf3844d8b6c6e2c9d84118e788a682158cadf501
Shalini Lohia <lshalini@amazon.com>
2023-11-17 18:24:31 +0000

    Use pstrdup

--- contrib/babelfishpg_tsql/src/tsqlIface.cpp
+++ contrib/babelfishpg_tsql/src/tsqlIface.cpp
@@ -5529,7 +5529,7 @@ makeGrantdbStatement(TSqlParser::Security_statementContext *ctx)
 					}
 					if (prin->PUBLIC())
 					{
-						char *grantee_name = (char *)"public";
+						char *grantee_name = pstrdup("public");//(char *)"public";
 						grantee_list = lappend(grantee_list, grantee_name);
 					}
 				}
@@ -5562,7 +5562,7 @@ makeGrantdbStatement(TSqlParser::Security_statementContext *ctx)
 					}
 					if (prin->PUBLIC())
 					{
-						char *grantee_name = (char *)"public";
+						char *grantee_name = pstrdup("public");
 						grantee_list = lappend(grantee_list, grantee_name);
 					}
 				}
@@ -5597,7 +5597,7 @@ makeGrantdbStatement(TSqlParser::Security_statementContext *ctx)
 				}
 				if (prin->PUBLIC())
 				{
-					char *grantee_name = (char *)"public";
+					char *grantee_name = pstrdup("public");
 					grantee_list = lappend(grantee_list, grantee_name);
 				}
 			}
@@ -5654,7 +5654,7 @@ makeGrantdbStatement(TSqlParser::Security_statementContext *ctx)
 				}
 				if (prin->PUBLIC())
 				{
-					char *grantee_name = (char *)"public";
+					char *grantee_name = pstrdup("public");
 					grantee_list = lappend(grantee_list, grantee_name);
 				}
 			}
