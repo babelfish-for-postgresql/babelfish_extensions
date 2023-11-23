7ff89f2085a601b632be980170efa03044098320
Rohit bhagat <65351720+rohit01010@users.noreply.github.com>
2023-11-23 11:10:42 +0530

    Fix failure in MVU with non-default server collation name (#2053)
    Currently, in upgrade scripts, server_collation_name is set to restored_server_collation_name in babelfishpg_common upgrade scripts only and then GUC restored_server_collation_name is being reset. Now this server_collation_name is being set only for that connection, when a new connection is created server_collation_name will contain the default value. This will cause issue when different connections are used to update babelfishpg_common and babelfishpg_tsql, as the server_collation_name contains default value in connection in which babelfishpg_tsql is getting updated, but expected value of server_collation_name is the value of restored_server_collation_name.
    
    Due to this, a failure is occurring in Major Version upgrade from 14.x to 15.x, when different connections are used to upgrade babelfishpg_common and babelfishpg_tsql extension and server collation name is set to non-default value in base version.
    
    This PR will resolve this issue by setting the server_collation_name to restored_server_collation_name in babelfishpg_tsql upgrade scripts as well.
    
    Cherry-picked from b56dca61436
    Signed-off-by: Rohit Bhagat rohitbgt@amazon.com

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.6.0--3.0.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--2.6.0--3.0.0.sql
@@ -4,6 +4,26 @@
 -- add 'sys' to search path for the convenience
 SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
 
+CREATE OR REPLACE FUNCTION sys.babelfish_update_server_collation_name() RETURNS VOID
+LANGUAGE C
+AS 'babelfishpg_common', 'babelfish_update_server_collation_name';
+
+SELECT sys.babelfish_update_server_collation_name();
+
+DROP FUNCTION sys.babelfish_update_server_collation_name();
+
+-- reset babelfishpg_tsql.restored_server_collation_name GUC
+do
+language plpgsql
+$$
+    declare
+        query text;
+    begin
+        query := pg_catalog.format('alter database %s reset babelfishpg_tsql.restored_server_collation_name', CURRENT_DATABASE());
+        execute query;
+    end;
+$$;
+
 -- Created to to fetch default collation Oid which is being used to set collation of system objects
 CREATE OR REPLACE FUNCTION sys.babelfishpg_tsql_get_babel_server_collation_oid() RETURNS OID
 LANGUAGE C
