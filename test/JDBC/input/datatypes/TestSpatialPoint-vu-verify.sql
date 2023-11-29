7e5436f0a6ce429905b5ff593e168788b892ba9f
Anikait Agrawal <agraani@amazon.com>
2023-11-29 11:38:49 +0000

    Added Test Cases
    Signed-off-by: Anikait Agrawal <agraani@amazon.com>

--- test/JDBC/input/datatypes/TestSpatialPoint-vu-verify.sql
+++ test/JDBC/input/datatypes/TestSpatialPoint-vu-verify.sql
@@ -1,29 +1,29 @@
 DECLARE @point geometry;
 SET @point = geometry::STPointFromText('POINT(-122.34900 47.65100)', 4326);
-SELECT @point.STAsText();
+SELECT STAsText(@point);
 Go
 
 DECLARE @point geometry;
 SET @point = geometry::POINT(22.34900, -47.65100, 4326);
-SELECT @point.STAsText();
+SELECT STAsText(@point);
 Go
 
 DECLARE @point geometry;
 SET @point = geometry::STGeomFromText('POINT(-122.34900 47.65100)', 4326);
-SELECT @point.stx;
-SELECT @point.sty;
+SELECT stx(@point);
+SELECT sty(@point);
 Go
 
 DECLARE @point geometry;
 SET @point = geometry::POINT(22.34900, -47.65100, 4326);
-SELECT @point.stx;
-SELECT @point.sty;
+SELECT stx(@point);
+SELECT sty(@point);
 Go
 
 DECLARE @point1 geometry, @point2 geometry;
 SET @point1 = geometry::STPointFromText('POINT(-122.34900 47.65100)', 4326);
 SET @point2 = geometry::STGeomFromText('POINT(-122.35000 47.65000)', 4326);
-SELECT @point1.STDistance(@point2);
+SELECT STDistance(@point1, @point2);
 Go
 
 DECLARE @point geometry;
@@ -41,6 +41,19 @@ go
 -- Currently it is not supported
 -- TODO: Need to support it and make it similar to TSQL
 CREATE VIEW CoordsFromGeom AS SELECT location.STX, location.STY AS Coordinates FROM SPATIALPOINTGEOM_dt;
+GO
+
+-- TODO: Need to throw proper error message
+SELECT dbo.SPATIALPOINTGEOM_dt.location.STX from SPATIALPOINTGEOM_dt;
+GO
+
+-- TODO: Need to throw proper error message
+SELECT master.dbo.SPATIALPOINTGEOM_dt.location.STX from SPATIALPOINTGEOM_dt;
+GO
+
+-- Currently it is not supported
+-- TODO: Need to support it and make it similar to TSQL
+SELECT SPATIALPOINTGEOM_dt.location.STAsText() from SPATIALPOINTGEOM_dt;
 GO
 
 -- Currently it is not supported
@@ -70,6 +83,9 @@ SET @point1 = geometry::POINT(22.34900, -47.65100, 4326);;
 SET @point2 = 'Test_String';
 SELECT @point1.STDistance(@point2);
 Go
+
+SELECT location.LAT from SPATIALPOINTGEOM_dt;
+GO
 
 -- Currently it is not supported
 -- TODO: Need to support it and make it similar to TSQL
@@ -209,30 +225,30 @@ GO
 
 DECLARE @point geography;
 SET @point = geography::STGeomFromText('POINT(-122.34900 47.65100)', 4326);
-SELECT @point.STAsText();
+SELECT STAsText(@point);
 Go
 
 DECLARE @point geography;
 SET @point = geography::POINT(22.34900, -47.65100, 4326);
-SELECT @point.STAsText();
+SELECT STAsText(@point);
 Go
 
 DECLARE @point geography;
 SET @point = geography::STPointFromText('POINT(-122.34900 47.65100)', 4326);
-SELECT @point.long;
-SELECT @point.lat;
+SELECT long(@point);
+SELECT lat(@point);
 Go
 
 DECLARE @point geography;
 SET @point = geography::POINT(22.34900, -47.65100, 4326);
-SELECT @point.long;
-SELECT @point.lat;
+SELECT long(@point);
+SELECT lat(@point);
 Go
 
 DECLARE @point1 geography, @point2 geography;
 SET @point1 = geography::STPointFromText('POINT(-122.34900 47.65100)', 4326);
 SET @point2 = geography::STGeomFromText('POINT(-122.35000 47.65000)', 4326);
-SELECT @point1.STDistance(@point2);
+SELECT STDistance(@point1, @point2);
 Go
 
 DECLARE @point geography;
@@ -259,6 +275,53 @@ SET @LAT = geography::STGeomFromText('POINT(-22.34900 47.65100)', 4326);
 select geography::Point(@LAT.LONG, @LAT.LAT, 4326).STAsText(), geography::Point(@LAT.LONG, @LAT.LAT, 4326).STAsBinary(), geography::Point(@LAT.LONG, @LAT.LAT, 4326).STDistance(geography::Point(@LAT.LONG, @LAT.LAT, 4326));
 go
 
+CREATE TABLE SpatialData
+(
+    ID INT PRIMARY KEY,
+    SpatialLocation GEOGRAPHY
+);
+GO
+
+INSERT INTO SpatialData (ID, SpatialLocation)
+VALUES
+    (1, geography::Point(1, 2, 4326)),
+    (2, geography::Point(3, 4, 4326)),
+    (3, geography::Point(5, 6, 4326)),
+    (4, geography::Point(7, 8, 4326)),
+    (5, geography::Point(9, 10, 4326));
+GO
+
+SELECT
+    SpatialData.ID,
+    SPATIALPOINTGEOG_dt.location.LAT,
+    SpatialLocation.STDistance(SPATIALPOINTGEOG_dt.location)
+FROM
+    SpatialData
+JOIN
+    SPATIALPOINTGEOG_dt ON SPATIALPOINTGEOG_dt.location.long - SpatialData.SpatialLocation.lat <= 10;
+GO
+
+WITH RegionLocations AS (
+    SELECT
+        SpatialData.ID,
+        SPATIALPOINTGEOG_dt.location.LAT
+    FROM
+        SpatialData
+    JOIN
+        SPATIALPOINTGEOG_dt ON SPATIALPOINTGEOG_dt.location.long - SpatialData.SpatialLocation.lat <= 10
+)
+SELECT
+    lat,
+    COUNT(ID) AS LocationCount
+FROM
+    RegionLocations
+GROUP BY
+    lat;
+GO
+
+DROP TABLE SpatialData;
+GO
+
 -- Null test for Geospatial functions
 DECLARE @point1 geography, @point2 geography, @point3 geography;
 SET @point1 = geography::STPointFromText(null, 4326);
@@ -279,6 +342,9 @@ SET @point1 = geography::POINT(22.34900, -47.65100, 4326);
 SET @point2 = 'Test_String';
 SELECT @point2.STDistance(@point1);
 Go
+
+SELECT location.STY from SPATIALPOINTGEOG_dt;
+GO
 
 SELECT * FROM TextFromGeog;
 GO
