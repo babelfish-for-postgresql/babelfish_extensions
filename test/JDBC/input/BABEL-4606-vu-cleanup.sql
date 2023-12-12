91ba8009182589253dc0646cb3c20aaccc7206ee
Zhibai Song <songzhibai1234@gmail.com>
2023-12-12 08:59:59 -0800

    Fix the issue that inserted table refered in update join will cause crash (#2140)
    Previously when we implement update join, we missed a corner case that
    one of the join relations can be a named tuple store instead of RTE
    relation. And it'll lead analyzer to wrongly set resultRelation for the
    Query.
    This fix resolved the corner case by add a if condition to exclude
    RTE_NAMEDTUPLESTORE for being resultRelation.
    
    Task: BABEL-4606
    Signed-off-by: Zhibai Song <szh@amazon.com>

---
+++ test/JDBC/input/BABEL-4606-vu-cleanup.sql
@@ -0,0 +1,18 @@
+drop trigger babel_4606_trigger
+go
+
+drop table babel_4606
+go
+
+drop trigger babel_4606_2_trigger
+go
+
+drop table babel_4606_2
+go
+
+drop trigger babel_4606_3_trigger
+go
+
+drop table babel_4606_3
+go
+
