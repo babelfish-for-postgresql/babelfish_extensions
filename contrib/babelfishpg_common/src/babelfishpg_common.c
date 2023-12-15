8943e588d22af07d869a45ab23238dd1ed9d8118
Rohit Bhagat <rohitbgt@amazon.com>
2023-12-15 05:30:26 +0000

    added a rendezvous variable for TsqlUTF8LengthInUTF16 function
    Signed-off-by: Rohit Bhagat <rohitbgt@amazon.com>

--- contrib/babelfishpg_common/src/babelfishpg_common.c
+++ contrib/babelfishpg_common/src/babelfishpg_common.c
@@ -190,6 +190,7 @@ get_common_utility_plugin(void)
 		common_utility_plugin_var.TdsGetVariantBaseType = &TdsGetVariantBaseType;
 		common_utility_plugin_var.lookup_tsql_datatype_oid = &lookup_tsql_datatype_oid;
 		common_utility_plugin_var.GetUTF8CodePoint = &GetUTF8CodePoint;
+		common_utility_plugin_var.TsqlUTF8LengthInUTF16 = &TsqlUTF8LengthInUTF16;
 	}
 	return &common_utility_plugin_var;
 }
