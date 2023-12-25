bb39e83cca415a6f84cbd9617b15570eef91bf3e
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2023-12-25 14:39:56 +0530

    Restrict privileges from Unauthorised TSQL logins (#2162)
    Avoid granting CREATEROLE and CREATEDB privilege to non-sysadmins logins
    Manage CREATEDB/CREATEROLE privileges as part of grant/revoke membership to/from sysadmin via TDS Port only. Though the grant sysadmin to user works from psql endpoint for superuser, it will not add CREATEDB/CREATEROLE privileges. If a TSQL user wants to have the sysadmin membership and CREATEDB /CREATEROLE privileges, it should alter the server role via TDS port.
    Issues Resolved
    Any unprivileged Babelfish role should not grant/revoke sysadmin role or non-Babelfish roles to itself and to others from the PG port.
    Any unprivileged Babelfish role should not drop any role via PG port.
    Any unprivileged Babelfish role should not alter any role via PG port.
    Any unprivileged Babelfish role should not create any role via PG port.
    Restrict PG user to "grant sysadmin to user" to any user via PG port.
    
    Task: BABEL-4573, BABEL-4574
    
    Signed-off-by: Shalini Lohia lshalini@amazon.com

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.4.0--3.5.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.4.0--3.5.0.sql
@@ -118,6 +118,12 @@ AS 'babelfishpg_tsql', 'session_context'
 LANGUAGE C;
 GRANT EXECUTE ON FUNCTION sys.session_context TO PUBLIC;
 
+-- Update existing logins to remove createrole privilege
+CREATE OR REPLACE PROCEDURE sys.bbf_remove_createrole_from_logins()
+LANGUAGE C
+AS 'babelfishpg_tsql', 'remove_createrole_from_logins';
+CALL sys.bbf_remove_createrole_from_logins();
+
 -- === DROP deprecated functions (if exists)
 DO $$
 DECLARE
