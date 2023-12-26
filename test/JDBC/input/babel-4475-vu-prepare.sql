21347484f160f76119ebd94d4b999c6b2acf0589
Sai Rohan Basa <bsrohan@amazon.com>
2023-12-26 09:33:41 +0000

    Updated test files

--- test/JDBC/input/babel-4475-vu-prepare.sql
+++ test/JDBC/input/babel-4475-vu-prepare.sql
@@ -6,3 +6,6 @@ go
 
 create view babel_4475_v2 as select cast(cast('ab' as rowversion) as varchar(2))
 go
+
+EXEC sp_babelfish_configure 'babelfishpg_tsql.escape_hatch_rowversion', 'strict';
+go
--- test/JDBC/input/babel-4475-vu-verify.sql
+++ test/JDBC/input/babel-4475-vu-verify.sql
@@ -1,7 +1,13 @@
 select * from babel_4475_v1
 go
 
+EXEC sp_babelfish_configure 'babelfishpg_tsql.escape_hatch_rowversion', 'ignore';
+go
+
 select * from babel_4475_v2
+go
+
+EXEC sp_babelfish_configure 'babelfishpg_tsql.escape_hatch_rowversion', 'strict';
 go
 
 select definition from sys.sql_modules where object_id = object_id('babel_4475_v1')
