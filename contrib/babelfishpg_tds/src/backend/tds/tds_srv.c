af36152757443ccc0cd5b5331fa588ea2986e529
HarshLunagariya <40052763+HarshLunagariya@users.noreply.github.com>
2023-12-27 16:41:18 +0530

    Fix null port->gss->princ and emit principal in logs in case of GSS authentication (#2142)
    Earlier, GSS authenticated backend was not reporting GSS specific information like authenticated, encrypted and
    principal name while logging the connections. Even pg_stat_gssapi was showing principal as NULL since princ field in
    port->gss->princ was uninitialised.
    
    This commit resolves above issue by initialising the port->gss->princ at the time of authentication and including
    authenticated, encrypted and principal name field in connection logs if GSS authentication is enabled. It cherry-picks
    some changes from https://git.postgresql.org/gitweb/
    p=postgresql.git;a=commitdiff;h=622ae4621ece72a9f64b5602c74d7aaf373c1631 and
    https://git.postgresql.org/gitweb/?p=postgresql.git;a=commitdiff;h=126cdaf47af275f76b2f2ddb023bfdc6f018ae30 in
    the TDS extension.
    
    Task: BABEL-4660
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>

--- contrib/babelfishpg_tds/src/backend/tds/tds_srv.c
+++ contrib/babelfishpg_tds/src/backend/tds/tds_srv.c
@@ -340,6 +340,25 @@ pe_authenticate(Port *port, const char **username)
 
 		appendStringInfo(&logmsg, _(" Tds Version=0x%X."), GetClientTDSVersion());
 
+#ifdef ENABLE_GSS
+		if (port->gss)
+		{
+			const char *princ = be_gssapi_get_princ(port);
+
+			if (princ)
+				appendStringInfo(&logmsg,
+								 _(" GSS (authenticated=%s, encrypted=%s, principal=%s)"),
+								 be_gssapi_get_auth(port) ? _("yes") : _("no"),
+								 be_gssapi_get_enc(port) ? _("yes") : _("no"),
+								 princ);
+			else
+				appendStringInfo(&logmsg,
+								 _(" GSS (authenticated=%s, encrypted=%s)"),
+								 be_gssapi_get_auth(port) ? _("yes") : _("no"),
+								 be_gssapi_get_enc(port) ? _("yes") : _("no"));
+		}
+#endif
+
 		ereport(LOG, errmsg_internal("%s", logmsg.data));
 		pfree(logmsg.data);
 	}
