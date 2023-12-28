7e2c037c1b656daff3fe271c7705734e24453559
Sandeep Kumawat <skumwt@amazon.com>
2023-12-28 13:05:26 +0000

    remove buf.len > varchar_max condition in tds sender function
    Signed-off-by: Sandeep Kumawat <skumwt@amazon.com>

--- contrib/babelfishpg_tds/src/backend/tds/tdstypeio.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdstypeio.c
@@ -2782,9 +2782,6 @@ TdsSendTypeVarchar(FmgrInfo *finfo, Datum value, void *vMetaData)
 	}
 	else
 	{
-		/* We can store upto 2GB (2^31 - 1 bytes) for the varchar(max). */
-		if (unlikely(actualLen > VARCHAR_MAX))
-			elog(ERROR, "Number of bytes required for the field of varchar(max) exeeds 2GB");
 		TDSInstrumentation(INSTR_TDS_DATATYPE_VARCHAR_MAX);
 
 		rc = TdsSendPlpDataHelper(destBuf, actualLen);
@@ -2980,9 +2977,6 @@ TdsSendTypeNVarchar(FmgrInfo *finfo, Datum value, void *vMetaData)
 	}
 	else
 	{
-		/* We can store upto 2GB (2^31 - 1 bytes) for the nvarchar(max). */
-		if (unlikely(buf.len > VARCHAR_MAX))
-			elog(ERROR, "Number of bytes required for the field of nvarchar(max) exeeds 2GB");
 		TDSInstrumentation(INSTR_TDS_DATATYPE_NVARCHAR_MAX);
 
 		rc = TdsSendPlpDataHelper(buf.data, buf.len);
