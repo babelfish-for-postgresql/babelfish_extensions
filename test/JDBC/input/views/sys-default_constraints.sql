dd12f8757e62e2e82589149c0c0f9bbe9b40013d
Sandeep Kumawat <2025sandeepkumawat@gmail.com>
2024-01-04 16:15:27 +0530

    Fix case-sensitive semantics of some catalog columns (#2111)
    Fix case-sensitive semantics of some catalog columns -
    some sys catalogs listed below have default collation of postgres because of wrong data-type (pg_catalog) hence the default collation of these catalog columns shows case-sensitive behaviour which shouldn't be the case in Babelfish.
    
    List of views getting fixed - {sys.stats, sys.data_spaces, sys.dm_exec_connections, sys.syscolumns, sys.system_objects, sys.xml_indexes, sys.sysprocesses, sys.sysforeignkeys, sys.check_constraints, sys.default_constraints, sys.key_constraints, sys.foreign_keys, sys.views, sys.tables, sys.types, sys.sp_special_columns_view, sys.sp_sproc_columns_view, sys.assembly_types, sys.systypes, sys.table_types, sys.all_parameters}
    
    Task: BABEL-4529
    Signed-off-by: Sandeep Kumawat <skumwt@amazon.com>

--- test/JDBC/input/views/sys-default_constraints.sql
+++ test/JDBC/input/views/sys-default_constraints.sql
@@ -10,6 +10,12 @@ GO
 SELECT definition FROM sys.default_constraints where name LIKE '%sys_default_definitions%'
 GO
 
+SELECT COUNT(*) FROM sys.default_constraints where name LIKE '%sys_default_definitions%' and type='D';
+GO
+
+SELECT COUNT(*) FROM sys.default_constraints where name LIKE '%sys_default_definitions%' and type='d';
+GO
+
 SELECT COUNT(*) FROM sys.all_columns WHERE object_id = object_id('sys.default_constraints');
 GO
 
