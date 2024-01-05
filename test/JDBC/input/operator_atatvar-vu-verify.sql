d531d921f3ca28c2a1b75c0eb36e818b84b8b563
robverschoor <rcv@amazon.com>
2024-01-05 14:35:44 +0000

    Handle operators adjacent to @@variables

---
+++ test/JDBC/input/operator_atatvar-vu-verify.sql
@@ -0,0 +1,122 @@
+if 1=@@max_precision select 'yes' else select 'no'
+go
+if 1<>@@max_precision select 'yes' else select 'no'
+go
+if 1< >@@max_precision select 'yes' else select 'no'
+go
+if 1!=@@max_precision select 'yes' else select 'no'
+go
+if 1! =@@max_precision select 'yes' else select 'no'
+go
+if 1>@@max_precision select 'yes' else select 'no'
+go
+if 1<@@max_precision select 'yes' else select 'no'
+go
+if 1>=@@max_precision select 'yes' else select 'no'
+go
+if 1> =@@max_precision select 'yes' else select 'no'
+go
+if 1<=@@max_precision select 'yes' else select 'no'
+go
+if 1< =@@max_precision select 'yes' else select 'no'
+go
+if 1!>@@max_precision select 'yes' else select 'no'
+go
+if 1! >@@max_precision select 'yes' else select 'no'
+go
+if 1! <@@max_precision select 'yes' else select 'no'
+go
+if 1!<@@max_precision select 'yes' else select 'no'
+go
+if case when 1! <@@max_precision then 1 else 0 end=@@max_precision select 'yes' else select 'no'
+go
+if case when 1>@@max_precision then 1 else 0 end>@@max_precision select 'yes' else select 'no'
+go
+if case when 1=@@max_precision then 1 else 0 end!=@@max_precision select 'yes' else select 'no'
+go
+execute('if case when 1=@@max_precision then 1 else 0 end > 0 select ''yes'' else select ''no''')
+go
+select 1 where 1<@@max_precision 
+go
+select 1 where 1!=@@max_precision 
+go
+select 1 where 1<>@@max_precision 
+go
+select 1 where 1< >@@max_precision 
+go
+select 1 where 1!>@@max_precision 
+go
+select 1 where 1! >@@max_precision 
+go
+
+if 1 = @@max_precision select 'yes' else select 'no'
+go
+
+declare @v int = 1 if 1=@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1<@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1>@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1<>@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1!=@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1 !=@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1 ! =@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1!>@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1!<@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1=@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1<@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1>@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1< >@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1! =@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1! >@v select 'yes' else select 'no'
+go
+declare @v int = 1 if 1! <@v select 'yes' else select 'no'
+go
+
+select c=@@max_precision where 0< >@@max_precision
+go
+
+select * from v1_operator_atatvar where 0! <@@max_precision
+go
+
+select * from v1_operator_atatvar where case when 1!=@@max_precision then 1 else @@max_precision end=@@max_precision
+go
+
+declare @v int
+set @v=1
+set @v+=@v
+select @v=@v+1
+select @v+=@v
+select @v
+go
+
+execute p2_operator_atatvar
+go
+
+p2_operator_atatvar
+go
+
+execute p3_operator_atatvar @p=@@max_precision
+go
+
+p3_operator_atatvar @p=@@max_precision
+go
+
+execute('p3_operator_atatvar @p=@@max_precision')
+go
+
+execute('execute p3_operator_atatvar @p=@@max_precision')
+go
+
