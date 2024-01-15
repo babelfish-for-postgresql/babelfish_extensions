dfcf5d760c9cb4efbeb649ffc454f0d228307c81
Rohit bhagat <65351720+rohit01010@users.noreply.github.com>
2024-01-15 17:42:23 +0530

    Implemented sys.availability_replicas and sys.availability_groups as an empty views (#2184) (#2252)
    This PR implements sys.availability_replicas and sys.availability_groups as an empty views to support Object Explorer Database enumeration with SSMS version 19.2. Complete support will be added in future.
    Tested this PR changes with SSMS version 19.2
    
    Issues Resolved
    BABEL-4636
    
    Signed-off-by: Rohit Bhagat <rohitbgt@amazon.com>

---
+++ test/JDBC/input/views/sys_availability_groups-vu-verify.sql
@@ -0,0 +1,11 @@
+SELECT * FROM sys.availability_groups
+GO
+
+SELECT * FROM sys_availability_groups_test_view
+GO
+
+EXEC sys_availability_groups_test_proc
+GO
+
+SELECT sys_availability_groups_test_func()
+GO
