cbfda9411497b89fdac155ff53ba4fbd3fdfeeba
Nirmit Shah <nirmisha@amazon.com>
2024-01-18 05:52:00 +0000

    Refactor the nullmap size logic and add rowstat column to size calc
    Signed-off-by: Nirmit Shah <nirmisha@amazon.com>

--- contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
@@ -2773,7 +2773,13 @@ TdsPrintTup(TupleTableSlot *slot, DestReceiver *self)
 		 * NBCROW (0xD2). Count the number of nullable columns and build the
 		 * null bitmap just in case while we are at it.
 		 */
-		nullMapSize = (natts + 7) / 8;
+
+		if (sendRowStat)
+			/* Extra bit for the ROWSTAT column */
+			nullMapSize = (natts >> 3) + 1;
+		else
+			nullMapSize = ((natts - 1) >> 3) + 1;
+
 		nullMap = palloc0(nullMapSize);
 		MemSet(nullMap, 0, nullMapSize * sizeof(int8_t));
 		for (attno = 0; attno < natts; attno++)
