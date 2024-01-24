f000034cccb17672c493e7ce0ee83ec9b4631cd7
Sai Rohan Basa <bsrohan@amazon.com>
2024-01-24 04:32:34 +0000

    Added upgrade tests

---
+++ test/JDBC/input/babel-3254-vu-cleanup.sql
@@ -0,0 +1,17 @@
+drop procedure babel_3254_p1
+go
+
+drop procedure babel_3254_p2
+go
+
+drop procedure babel_3254_p3
+go
+
+drop procedure babel_3254_p4
+go
+
+drop procedure babel_3254_p5
+go
+
+drop procedure babel_3254_p6
+go
---
+++ test/JDBC/input/babel-3254-vu-verify.sql
@@ -0,0 +1,18 @@
+sp_procedure_params_100_managed 'babel_3254_p1'
+go
+
+sp_procedure_params_100_managed 'babel_3254_p2'
+go
+
+sp_procedure_params_100_managed 'babel_3254_p3'
+go
+
+sp_procedure_params_100_managed 'babel_3254_p4'
+go
+
+sp_procedure_params_100_managed 'babel_3254_p5'
+go
+
+sp_procedure_params_100_managed 'babel_3254_p6'
+go
+
