18aab635718425c5a7da55068b39abbd80a14a09
Sai Rohan Basa <bsrohan@amazon.com>
2024-01-25 13:23:26 +0000

    Added more tests

--- test/JDBC/input/babel-3254-vu-verify.sql
+++ test/JDBC/input/babel-3254-vu-verify.sql
@@ -16,3 +16,39 @@ go
 sp_procedure_params_100_managed 'babel_3254_p6'
 go
 
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p1', @group_number = 2
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p2', @group_number = 3
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p3', @group_number = 4
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p4', @group_number = 5
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p5', @group_number = 6
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p1', @parameter_name = '@a'
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p2', @parameter_name = '@b'
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p3', @parameter_name = '@c'
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p4', @parameter_name = '@d'
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p5', @parameter_name = '@e'
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p6', @parameter_name = '@f'
+go
+
+sp_procedure_params_100_managed @procedure_name = 'babel_3254_p6', @procedure_schema = 'dbo', @parameter_name = '@a'
+go
+
