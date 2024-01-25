0429ba1a2dd946f292156a8c025d1eabbd648574
Tim Chang <changti@amazon.com>
2024-01-25 19:44:41 +0000

    Added some extra index tests

--- test/JDBC/input/BABEL-4554-vu-prepare.sql
+++ test/JDBC/input/BABEL-4554-vu-prepare.sql
@@ -3,5 +3,42 @@
 CREATE TABLE #babel_4554_temp_table(a varchar(4000), b nvarchar(MAX), c sysname)
 GO
 
+-- 3: table, toast, index on toast
 SELECT COUNT(*) FROM sys.babelfish_get_enr_list()
+GO
+
+DROP TABLE #babel_4554_temp_table
+GO
+
+-- 4: table, toast, index on toast, pkey
+CREATE TABLE #babel_4554_temp_table_2(a sysname primary key, b nvarchar(MAX))
+GO
+
+SELECT COUNT(*) FROM sys.babelfish_get_enr_list()
+GO
+
+-- 1: index
+CREATE INDEX #babel_4554_idx1 ON #babel_4554_temp_table_2(b)
+GO
+
+SELECT COUNT(*) FROM sys.babelfish_get_enr_list()
+GO
+
+DROP INDEX #babel_4554_idx1 ON #babel_4554_temp_table_2
+GO
+
+DROP TABLE #babel_4554_temp_table_2
+GO
+
+-- Verify that non-ENR tables don't put their toasts in ENR.
+CREATE TYPE babel_4554_type FROM INT
+GO
+
+CREATE TABLE #babel_4554_temp_table_not_enr(a babel_4554_type, b nvarchar(MAX))
+GO
+
+SELECT COUNT(*) FROM sys.babelfish_get_enr_list()
+GO
+
+DROP TABLE #babel_4554_temp_table_not_enr
 GO
