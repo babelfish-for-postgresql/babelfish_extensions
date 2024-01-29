214ea5831d3077bb900ac1753609364a3edae4c9
Rishabh Tanwar <33982749+rishabhtanwar29@users.noreply.github.com>
2024-01-29 16:01:19 +0530

    Fix issues with datatypes for Logical Replication (#2268)
    This commit fixes following issues:
    
    1. (Var)binary columns are not replicated correctly since input function considers
    the value as a raw ASCII string although the value is actually already in binary format.
    Fixed this by using IsLogicalWorker() with existing TSQLHexConstTypmod
    check in varbinaryin function to correctly take binary input sent by the publisher.
    2. SQL_VARIANT columns are replicated correctly as far as the data contents are
    concerned, but the meta data is lost in the process which is because sql_variant output
    function returns the value in the respective datatype in string format which when fed
    back to the sql_variant input function gets treated as a varchar value.
    Fixed this by replicating sql_variant values as bytea. We will use MyReplicationSlot
    and MyWalSnd in sqlvariantout function on publisher and IsLogicalWorker() in
    sqlvariantin function on subscriber to identify if we are in logical replication context.
    3. On the subscriber side, IsLogicalWorker() is sufficient for native PG applyworker
    but will not work with external providers like pglogical, so will rely on SessionReplicationRole
    being replica since most of the providers seem to set this GUC.
    
    Additionally, this adds support to run logical replication tests in github action along with
    JDBC framework.
    
    Task: BABEL-3427, BABEL-4049
    Signed-off-by: Rishabh Tanwar ritanwar@amazon.com

--- .github/scripts/create_extension.sql
+++ .github/scripts/create_extension.sql
@@ -8,6 +8,7 @@ GRANT ALL ON SCHEMA sys to :user;
 ALTER USER :user CREATEDB;
 ALTER SYSTEM SET babelfishpg_tsql.database_name = :db;
 ALTER SYSTEM SET babelfishpg_tsql.migration_mode = :'migration_mode';
+ALTER SYSTEM SET babelfishpg_tds.port = :tsql_port;
 
 \if :parallel_query_mode
     ALTER SYSTEM SET parallel_setup_cost = 0;
