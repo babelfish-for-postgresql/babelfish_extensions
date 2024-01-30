3c80d6d45b645300d771f701df122856807dcac9
Anikait Agrawal <54908236+Anikait143@users.noreply.github.com>
2024-01-30 20:46:40 +0530

    Fixed flaky test cases by adding order by clause (#2315) (#2317)
    This commit contains fixes for sys-assembly_types test cases for missing order-by clauses
    Also Fixed Geospatial Tests for missing order-by clauses
    
    Signed-off-by: Anikait Agrawal agraani@amazon.com

--- test/JDBC/input/views/sys-assembly_types-vu-prepare.sql
+++ test/JDBC/input/views/sys-assembly_types-vu-prepare.sql
@@ -1,9 +1,9 @@
 CREATE VIEW sys_assembly_types_view_vu_prepare AS
-SELECT name, principal_id, max_length, precision, scale, collation_name, is_nullable, is_user_defined, is_assembly_type, default_object_id, rule_object_id, assembly_id, assembly_class, is_binary_ordered, is_fixed_length, prog_id, assembly_qualified_name, is_table_type FROM sys.assembly_types
+SELECT name, principal_id, max_length, precision, scale, collation_name, is_nullable, is_user_defined, is_assembly_type, default_object_id, rule_object_id, assembly_id, assembly_class, is_binary_ordered, is_fixed_length, prog_id, assembly_qualified_name, is_table_type FROM sys.assembly_types ORDER BY name DESC
 GO
 
 CREATE PROC sys_assembly_types_proc_vu_prepare AS
-SELECT name, principal_id, max_length, precision, scale, collation_name, is_nullable, is_user_defined, is_assembly_type, default_object_id, rule_object_id, assembly_id, assembly_class, is_binary_ordered, is_fixed_length, prog_id, assembly_qualified_name, is_table_type FROM sys.assembly_types
+SELECT name, principal_id, max_length, precision, scale, collation_name, is_nullable, is_user_defined, is_assembly_type, default_object_id, rule_object_id, assembly_id, assembly_class, is_binary_ordered, is_fixed_length, prog_id, assembly_qualified_name, is_table_type FROM sys.assembly_types ORDER BY name DESC
 GO
 
 CREATE FUNCTION sys_assembly_types_func_vu_prepare()
