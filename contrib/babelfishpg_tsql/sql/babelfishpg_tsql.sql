87693e49355983fef55b6235f7049fbe96f70f0e
Sai Rohan Basa <108261379+basasairohan@users.noreply.github.com>
2024-02-01 18:05:22 +0530

    Handle empty procedure_schema case in sp_procedure_params_100_managed procedure (#2327)
    Description
    
    If the procedure_schema argument in the sp_procedure_params_100_managed procedure is empty, we should return procedure metadata from the user's default schema. But, currently we return no rows in this case.
    
    This commit resolves the above issue.
    Issues Resolved
    
    BABEL-3254
    
    Signed-off-by: Sai Rohan Basa bsrohan@amazon.com

--- contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
+++ contrib/babelfishpg_tsql/sql/babelfishpg_tsql.sql
@@ -3666,7 +3666,7 @@ CREATE OR REPLACE PROCEDURE sys.sp_procedure_params_100_managed(IN "@procedure_n
                                                                 IN "@parameter_name" sys.sysname DEFAULT NULL)
 AS $$
 BEGIN
-	IF @procedure_schema IS NULL
+	IF @procedure_schema IS NULL OR @procedure_schema = ''
 		BEGIN
 			SELECT @procedure_schema = default_schema_name from sys.babelfish_authid_user_ext WHERE orig_username = user_name() AND database_name = db_name();
 		END
