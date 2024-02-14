39f248cda48867bd3125bface406254ce146448c
Shalini Lohia <lshalini@amazon.com>
2024-02-14 16:47:36 +0000

    revert change from upgrade script

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.4.0--3.5.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.4.0--3.5.0.sql
@@ -2280,17 +2280,6 @@ END;
 $$ LANGUAGE pltsql;
 GRANT EXECUTE ON PROCEDURE sys.sp_procedure_params_100_managed TO PUBLIC;
 
--- BABELFISH_SCHEMA_PERMISSIONS
-CREATE TABLE IF NOT EXISTS sys.babelfish_schema_permissions (
-  dbid smallint NOT NULL,
-  schema_name sys.NVARCHAR(128) NOT NULL COLLATE sys.database_default,
-  object_name sys.NVARCHAR(128) NOT NULL COLLATE sys.database_default,
-  permission INT NOT NULL,
-  grantee sys.NVARCHAR(128) NOT NULL COLLATE sys.database_default,
-  object_type CHAR(1) NOT NULL COLLATE sys.database_default,
-  PRIMARY KEY(dbid, schema_name, object_name, grantee, object_type)
-);
-
 create or replace view sys.all_objects as
 select 
     name collate sys.database_default
