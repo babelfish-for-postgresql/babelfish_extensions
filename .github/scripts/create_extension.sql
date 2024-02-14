5f7acfcf2470da13234cd6a7d5e05eaff7d9a808
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2024-02-14 13:35:26 +0530

    first commit rebased and forced push (#2352)
    In babelfish there could be cases where a procedure rollbacks/deletes it self during execution.
    For example
    
    BEGIN TRAN
    CREATE PROCEDURE poc1 AS ROLLBACK;
    EXEC p;
    
    Procedure 'proc1' does not exists after its execution since the transaction in which it was created
    was rolled back by the procedure itself.
    Now while executing rollback inside such procedures, the pg_stat entry for this proc is also deleted.
    When we return to fmgr to end the proc invocation, we will overwrite on this freed block of memory.
    In future, when alloc tries to allocate this freed block if memory to some other process,
    it may hit an assert since this block was corrupted.
    
    To avoid this, we add an additional check before writing on the pg_stat entry post invocation.
    Check involves simply looking for a local cache pg_stat entry for given key.
    
    Note* PG does not need this check since such a case is not possible there.
    Performance Effect
    
    Use the simplest of procedures to avoid diluting the performance changes and get the worst case scenario.
    Before & after numbers are taken as average of 4 runs.
    
    CREATE PROCEDURE proc1 AS SELECT 1;
    CREATE PROCEDURE proc2 @in INT AS SELECT @in + 1;
    
    CASE 1
    
    DECLARE @i INT = 0;
    DECLARE @dt DATETIME = getdate();
    WHILE (@i < 1000000)
    BEGIN
        EXEC proc1;
        SET @i = @i + 1;
    END;
    DECLARE @duration_ms int;
    SET @duration_ms = DATEDIFF(ms, @dt, getdate());
    SELECT @duration_ms
    GO
    
    Before Fix:
    track_functions --> disabled: 29189 ms enabled: 29367.75 Change: +0.62%
    After Fix:
    track_functions --> disabled: 28261 ms enabled: 28266 Change: ~0%
    
    CASE 2
    
    DECLARE @i INT = 0;
    DECLARE @dt DATETIME = getdate();
    WHILE (@in < 1000000)
    BEGIN
        EXEC proc2 1;
        SET @i = @i + 1;
    END;
    DECLARE @duration_ms int;
    SET @duration_ms = DATEDIFF(ms, @dt, getdate());
    SELECT @duration_ms
    GO
    
    Before Fix:
    track_functions --> disabled: 34730.25 ms enabled: 34732.25 Change: ~0%
    After Fix:
    track_functions --> disabled: 33918.75 ms enabled: 34075.5 Change: +0.4%
    
    Tests show no visible change in performance.
    Engine PR: babelfish-for-postgresql/postgresql_modified_for_babelfish#305
    Extension PR: #2352
    Issues Resolved
    
    [BABEL-4740]
    Sign Off
    
    Signed-off-by: Tanzeel Khan tzlkhan@amazon.com

--- .github/scripts/create_extension.sql
+++ .github/scripts/create_extension.sql
@@ -9,6 +9,7 @@ ALTER USER :user CREATEDB;
 ALTER SYSTEM SET babelfishpg_tsql.database_name = :db;
 ALTER SYSTEM SET babelfishpg_tsql.migration_mode = :'migration_mode';
 ALTER SYSTEM SET babelfishpg_tds.port = :tsql_port;
+ALTER SYSTEM SET track_functions = 'pl';
 
 \if :parallel_query_mode
     ALTER SYSTEM SET parallel_setup_cost = 0;
