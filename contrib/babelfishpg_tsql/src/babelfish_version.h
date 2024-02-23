938b6fc9f7ba5f66f9a7bc8455f1b70b6b7531c3
Anikait Agrawal <54908236+Anikait143@users.noreply.github.com>
2024-02-23 21:47:33 +0530

    Initial commit for Babelfish v4.2 (#2388)
    This commit bring the following changes:
    
    * Bumped babelfishpg_tsql and babelfishpg_common versions to 4.2.0 and added upgrade scripts for both.
    * Added upgrade paths from 14.latest, 15.latest and 16.2 to current versions.
    * Updated version branch template to include new branches.
    
    Signed-off-by: Anikait Agrawal agraani@amazon.com

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "4.1.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.2.0.0"
+#define BABELFISH_VERSION_STR "4.2.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.3.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
