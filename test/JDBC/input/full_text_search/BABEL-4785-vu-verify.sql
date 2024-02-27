b6dec710de94bfcbc26813685c244ac5b0b0719f
Roshan Kanwar <rkanwar0708@gmail.com>
2024-02-27 18:34:16 +0000

    fix build errors

--- test/JDBC/input/full_text_search/BABEL-4785-vu-verify.sql
+++ test/JDBC/input/full_text_search/BABEL-4785-vu-verify.sql
@@ -1,5 +1,5 @@
-SELECT schemaname, indexname FROM pg_indexes WHERE tablename='table.test' AND indexname LIKE 'ft_index%';
+SELECT schemaname, indexname FROM pg_indexes WHERE tablename='table.test' AND indexname LIKE 'ft_index%' ORDER BY schemaname;
 GO
 
-SELECT schemaname, indexname FROM pg_indexes WHERE tablename='test_table' AND indexname LIKE 'ft_index%';
+SELECT schemaname, indexname FROM pg_indexes WHERE tablename='test_table' AND indexname LIKE 'ft_index%' ORDER BY schemaname;
 GO
