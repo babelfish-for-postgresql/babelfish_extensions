a87ff4b6f703c057fad7f22fd6da0d9e027dd04e
shalinilohia50 <46928246+shalinilohia50@users.noreply.github.com>
2024-03-11 16:52:31 +0530

    When creating a user, verify its login is not already sysadmin (#2418)
    Description
    
        Creation of a user should be blocked when its login is already sysadmin.
        Create user gives the error "errstart was not called"
    
    Issues Resolved
    
    Task: BABEL-4430, BABEL-4652
    Signed-off-by: Shalini Lohia lshalini@amazon.com

--- contrib/babelfishpg_tsql/src/pl_handler.c
+++ contrib/babelfishpg_tsql/src/pl_handler.c
@@ -2797,6 +2797,10 @@ bbf_ProcessUtility(PlannedStmt *pstmt,
 													(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 													 errmsg("Windows login is not supported in babelfish")));
 									}
+									/* If login is a member of sysadmin, creating user for that login should not be allowed. */
+									if (has_privs_of_role(get_role_oid(login->rolename, false), get_sysadmin_oid()))
+										ereport(ERROR, (errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+														errmsg("Cannot create user for sysadmin role.")));
 								}
 							}
 
