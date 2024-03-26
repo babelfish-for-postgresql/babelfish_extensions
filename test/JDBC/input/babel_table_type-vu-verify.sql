ad82cec2ba15eefd3f2c2be47c925c906f8e1ecd
Jason Teng <jasonten@amazon.com>
2024-03-26 20:41:35 +0000

    Fix upgrade script function names and remove duplicate tests from non-upgrade script
    Signed-off-by: Jason Teng <jasonten@amazon.com>

--- test/JDBC/input/babel_table_type-vu-verify.sql
+++ test/JDBC/input/babel_table_type-vu-verify.sql
@@ -1,26 +1,26 @@
-SELECT * FROM a_very_long_function_name_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1);
+SELECT * FROM babel_table_type_vu_a_very_long_function_name_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1);
 GO
 
-SELECT * FROM a_short_function_name(1);
+SELECT * FROM babel_table_type_vu_a_short_function_name(1);
 GO
 
-SELECT * FROM a_very_long_function_name_bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb(1);
+SELECT * FROM babel_table_type_vu_a_very_long_function_name_bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb(1);
 GO
 
-SELECT * FROM a_medium_function_name_aaaaaaaaaaaaaaaaa(1);
+SELECT * FROM babel_table_type_vu_a_medium_function_name_aaaaaaaaaaaaaaaaa(1);
 GO
 
-SELECT * FROM a_nested_function(1);
+SELECT * FROM babel_table_type_vu_a_nested_function(1);
 GO
 
 -- cleanup
-DROP FUNCTION a_nested_function;
+DROP FUNCTION babel_table_type_vu_a_nested_function;
 GO
-DROP FUNCTION a_very_long_function_name_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
+DROP FUNCTION babel_table_type_vu_a_very_long_function_name_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
 GO
-DROP FUNCTION a_short_function_name;
+DROP FUNCTION babel_table_type_vu_a_short_function_name;
 GO
-DROP FUNCTION a_very_long_function_name_bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb;
+DROP FUNCTION babel_table_type_vu_a_very_long_function_name_bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb;
 GO
-DROP FUNCTION a_medium_function_name_aaaaaaaaaaaaaaaaa;
+DROP FUNCTION babel_table_type_vu_a_medium_function_name_aaaaaaaaaaaaaaaaa;
 GO
