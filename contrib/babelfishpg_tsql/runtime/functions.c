b6bc336d251f42e188f41cc69ffd381027682c0a
Tim Chang <timchang514@users.noreply.github.com>
2024-04-02 17:15:43 -0400

    Add tests for parent queryEnv in certain ENR functions (#2395) (#2459)
    * Add tests for parent queryEnv in certain ENR functions
    
    
    
    * Add flag to control search; fix test cases
    
    * Rerun test
    
    * Fix error in test output
    
    * Rerun tests
    
    * Fix test output
    
    ---------
    
    Signed-off-by: Tim Chang <changti@amazon.com>

--- contrib/babelfishpg_tsql/runtime/functions.c
+++ contrib/babelfishpg_tsql/runtime/functions.c
@@ -2085,7 +2085,7 @@ object_id(PG_FUNCTION_ARGS)
 				 * search in list of ENRs registered in the current query
 				 * environment by name
 				 */
-				EphemeralNamedRelation enr = get_ENR(currentQueryEnv, object_name);
+				EphemeralNamedRelation enr = get_ENR(currentQueryEnv, object_name, true);
 
 				if (enr != NULL && enr->md.enrtype == ENR_TSQL_TEMP)
 				{
@@ -2155,7 +2155,7 @@ object_id(PG_FUNCTION_ARGS)
 			 * search in list of ENRs registered in the current query
 			 * environment by name
 			 */
-			EphemeralNamedRelation enr = get_ENR(currentQueryEnv, object_name);
+			EphemeralNamedRelation enr = get_ENR(currentQueryEnv, object_name, true);
 
 			if (enr != NULL && enr->md.enrtype == ENR_TSQL_TEMP)
 			{
