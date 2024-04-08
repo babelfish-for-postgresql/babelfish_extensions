db87172e2d4d5818089f65d8d2c1bfce628183f4
Dipesh Dhameliya <dddhamel@amazon.com>
2024-04-08 17:51:00 +0530

    Fixed test

--- test/JDBC/input/babel_test_int4_numeric_oper-vu-prepare.sql
+++ test/JDBC/input/babel_test_int4_numeric_oper-vu-prepare.sql
@@ -13,143 +13,143 @@ CREATE INDEX test_int_numeric_vu_idx on test_int_numeric_vu(a);
 GO
 
 -- tsql
-create view test_int_numeric_v1 as
+create procedure test_int_numeric_p1 as
 select count(*) from test_int_numeric_vu where a IS NULL;
 GO
 
 -- seq scan
-create view test_int_numeric_v2 as
+create procedure test_int_numeric_p2 as
 select count(*) from test_int_numeric_vu where a <> 5.0;
 GO
 
-create view test_int_numeric_v3 as
+create procedure test_int_numeric_p3 as
 select count(*) from test_int_numeric_vu where 5.0 <> a;
 GO
 
 -- index scan on < and >
-create view test_int_numeric_v4 as
+create procedure test_int_numeric_p4 as
 select count(*) from test_int_numeric_vu where a < 5.0;
 GO
 
-create view test_int_numeric_v5 as
+create procedure test_int_numeric_p5 as
 select count(*) from test_int_numeric_vu where 5.0 > a;
 GO
 
-create view test_int_numeric_v6 as
+create procedure test_int_numeric_p6 as
 select count(*) from test_int_numeric_vu where a < -2147483648.0;
 GO
 
-create view test_int_numeric_v7 as
+create procedure test_int_numeric_p7 as
 select count(*) from test_int_numeric_vu where -2147483648.0 > a;
 GO
 
-create view test_int_numeric_v8 as
+create procedure test_int_numeric_p8 as
 select count(*) from test_int_numeric_vu where a <= 5.0;
 GO
 
-create view test_int_numeric_v9 as
+create procedure test_int_numeric_p9 as
 select count(*) from test_int_numeric_vu where 5.0 >= a;
 GO
 
-create view test_int_numeric_v10 as
+create procedure test_int_numeric_p10 as
 select count(*) from test_int_numeric_vu where a > 999995.0;
 GO
 
-create view test_int_numeric_v11 as
+create procedure test_int_numeric_p11 as
 select count(*) from test_int_numeric_vu where 999995.0 < a;
 GO
 
-create view test_int_numeric_v12 as
+create procedure test_int_numeric_p12 as
 select count(*) from test_int_numeric_vu where a >= 999995.0;
 GO
 
-create view test_int_numeric_v13 as
+create procedure test_int_numeric_p13 as
 select count(*) from test_int_numeric_vu where 999995.0 <= a;
 GO
 
-create view test_int_numeric_v14 as
+create procedure test_int_numeric_p14 as
 select count(*) from test_int_numeric_vu where a > 2147483647.0;
 go
 
-create view test_int_numeric_v15 as
+create procedure test_int_numeric_p15 as
 select count(*) from test_int_numeric_vu where 2147483647.0 < a;
 go
 
 -- seq scan on < and >
-create view test_int_numeric_v16 as
+create procedure test_int_numeric_p16 as
 select count(*) from test_int_numeric_vu where a > 5.0;
 GO
 
-create view test_int_numeric_v17 as
+create procedure test_int_numeric_p17 as
 select count(*) from test_int_numeric_vu where 5.0 < a;
 GO
 
 -- index scan for BETWEEN
-create view test_int_numeric_v18 as
+create procedure test_int_numeric_p18 as
 select count(*) from test_int_numeric_vu where a between 5.0 and 10.0;
 GO
 
 -- seq scan for BETWEEN
-create view test_int_numeric_v19 as
+create procedure test_int_numeric_p19 as
 select count(*) from test_int_numeric_vu where a between 5.0 and 999999.0;
 GO
 
 -- mix of int op numeric and int op int
-create view test_int_numeric_v20 as
+create procedure test_int_numeric_p20 as
 select count(*) from test_int_numeric_vu where (a between 5.0 and 999999.0) and a = 10;
 GO
 
-create view test_int_numeric_v21 as
+create procedure test_int_numeric_p21 as
 select count(*) from test_int_numeric_vu where a > 5.0 and a < 7;
 Go
 
-create view test_int_numeric_v22 as
+create procedure test_int_numeric_p22 as
 select count(*) from test_int_numeric_vu where 5.0 < a and 7 > a;
 Go
 
 -- shouldn't be any regression on int op int operators
 
 -- seq scan
-create view test_int_numeric_v23 as
+create procedure test_int_numeric_p23 as
 select count(*) from test_int_numeric_vu where a <> 5;
 GO
 
 -- index scan on < and >
-create view test_int_numeric_v24 as
+create procedure test_int_numeric_p24 as
 select count(*) from test_int_numeric_vu where a < 5;
 GO
 
-create view test_int_numeric_v25 as
+create procedure test_int_numeric_p25 as
 select count(*) from test_int_numeric_vu where a < -2147483648;
 GO
 
-create view test_int_numeric_v26 as
+create procedure test_int_numeric_p26 as
 select count(*) from test_int_numeric_vu where a <= 5;
 GO
 
-create view test_int_numeric_v27 as
+create procedure test_int_numeric_p27 as
 select count(*) from test_int_numeric_vu where a > 999995;
 GO
 
-create view test_int_numeric_v28 as
+create procedure test_int_numeric_p28 as
 select count(*) from test_int_numeric_vu where a >= 999995;
 GO
 
-create view test_int_numeric_v29 as
+create procedure test_int_numeric_p29 as
 select count(*) from test_int_numeric_vu where a > 2147483647;
 go
 
 -- seq scan on < and >
-create view test_int_numeric_v30 as
+create procedure test_int_numeric_p30 as
 select count(*) from test_int_numeric_vu where a > 5;
 GO
 
 -- index scan for BETWEEN
-create view test_int_numeric_v31 as
+create procedure test_int_numeric_p31 as
 select count(*) from test_int_numeric_vu where a between 5 and 10;
 GO
 
 -- seq scan for BETWEEN
-create view test_int_numeric_v32 as
+create procedure test_int_numeric_p32 as
 select count(*) from test_int_numeric_vu where a between 5 and 999999;
 GO
