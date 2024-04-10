f090b8853111ed6b6a60845748aefc0ab5aa6a93
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2024-04-10 20:16:47 +0530

    Fix scripting of tables with index on non first columns (#2424) (#2479)
    When exporting ddl_script for tables with indexes/constraints on columns which are not first in the table definition, wrong table definition in scripted. This is because the scripting tool uses sys.index_columns underneath and we are not returning the correct output currently. We also need to fix the index_id in sys.indexes since index_id is unique id for each index per table. index_id in sys.indexes and sys.index_columns should be same. Also fix sys.generated columns. Currently returns duplicates entries when two tables have same name generated columns.
    
    Cherry Picked From
    #2424
    
    Issues Resolved
    [BABEL-4817],[BABEL-4853]
    
    Signed-off-by: Tanzeel Khan tzlkhan@amazon.com

--- test/python/input/ddl_tables_index.sql
+++ test/python/input/ddl_tables_index.sql
@@ -24,6 +24,8 @@ DROP TABLE IF EXISTS test_null
 GO
 DROP TABLE IF EXISTS test_upper
 GO
+DROP TABLE IF EXISTS babel_4817_t1, babel_4817_t2, babel_4817_t3, babel_4817_t4, babel_4817_t5;
+GO
 Create table table_unique (a int NOT NULL UNIQUE)
 GO
 Create table table_primary (a int NOT NULL , b int NOT NULL,c int, PRIMARY KEY(a) )
@@ -66,8 +68,64 @@ Create index test_index on test_upper(a)
 GO
 Create index test_comp_index on table_unique(a)
 GO
+
+CREATE TABLE babel_4817_t1 (col1 INT, col2 INT NOT NULL, col3 AS col1*col2, col4 INT, col5 INT, col6 INT, col7 INT, col8 INT NOT NULL, PRIMARY KEY NONCLUSTERED(col3, col7 DESC), UNIQUE (col2 DESC, col8));
+GO
+CREATE INDEX babel_4817_t1_idx_1 ON dbo.babel_4817_t1 (col5) INCLUDE (col1);
+GO
+
+CREATE TABLE babel_4817_t2 (col1 INT, col2 INT NOT NULL, col3 AS col1*col2, col4 INT, col5 INT, col6 INT, col7 INT NOT NULL IDENTITY, col8 INT NOT NULL)
+GO
+ALTER TABLE babel_4817_t2 ADD CONSTRAINT babel_4817_t2_pk PRIMARY KEY NONCLUSTERED (col3, col7);
+GO
+CREATE UNIQUE NONCLUSTERED INDEX babel_4817_t2_unique_index ON dbo.babel_4817_t2 (col2 DESC, col8) INCLUDE (col4);
+GO
+CREATE NONCLUSTERED INDEX babel_4817_t2_idx ON dbo.babel_4817_t2 (col5) INCLUDE (col1);
+GO
+CREATE TABLE babel_4817_t3 (col1 INT IDENTITY, col2 INT, col3 INT, col4 INT, col5 AS col1*col2, col6 VARCHAR(30))
+GO
+ALTER TABLE babel_4817_t3 DROP COLUMN col4
+GO
+ALTER TABLE babel_4817_t3 ADD col4 INT
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[babel_4817_t4](
+    [id] [int] NULL,
+    [filename] [varchar](200) NOT NULL,
+    [commited_dt] [datetime] NOT NULL,
+    [commited_sql] [ntext] NULL,
+PRIMARY KEY CLUSTERED 
+(
+    [filename] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[babel_4817_t5]
+(
+  [id] [int] NOT NULL,
+  [filename] [varchar](200) NOT NULL,
+  [commited_dt] [datetime] NOT NULL,
+  [commited_sql] [ntext] NULL,
+  PRIMARY KEY CLUSTERED
+(
+              [filename] ASC, [ID] DESC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX ix_test ON [dbo].[babel_4817_t5] ([filename]) INCLUDE ([id],[commited_dt])
+GO
+
 --DROP
 
+DROP TABLE IF EXISTS babel_4817_3, babel_4817_4;
+GO
+DROP TABLE IF EXISTS babel_4817_t1, babel_4817_t2, babel_4817_t3, babel_4817_t4, babel_4817_t5;
+GO
 DROP TABLE IF EXISTS table_check
 GO
 DROP TABLE IF EXISTS table_foreign 
