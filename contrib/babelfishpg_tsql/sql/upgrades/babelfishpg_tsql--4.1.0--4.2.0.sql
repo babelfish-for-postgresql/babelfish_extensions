9c2b64a30ee4a088cde7fa5a329b416a3adf3a2e
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2024-04-10 19:18:58 +0530

    Update babelfishpg_tsql--4.1.0--4.2.0.sql
    fix merge conflict errors

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.1.0--4.2.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.1.0--4.2.0.sql
@@ -526,17 +526,6 @@ order by object_id, type_desc;
 GRANT SELECT ON sys.indexes TO PUBLIC;
 
 
--- Drops the temporary procedure used by the upgrade script.
--- Please have this be one of the last statements executed in this upgrade script.
-DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar, varchar);
-
--- After upgrade, always run analyze for all babelfish catalogs.
-CALL sys.analyze_babelfish_catalogs();
-
--- Reset search_path to not affect any subsequent scripts
-SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
-
-
 CREATE OR REPLACE FUNCTION sys.sp_tables_internal(
 	in_table_name sys.nvarchar(384) = '',
 	in_table_owner sys.nvarchar(384) = '', 
