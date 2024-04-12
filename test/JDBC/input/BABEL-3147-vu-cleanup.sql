df18bc86e659c16e8e987a0ff520f8e269971536
Rohit Bhagat <rohitbgt@amazon.com>
2024-04-12 13:47:53 +0000

    Fix Convert() to return correct date format with char and nchar (#2448)
    Earlier when we were converting from DATE/TIME types to CHAR or NCHAR type using CONVERT function the output
    style was not getting into account as we have not handled the case for CHAR and NCHAR type in TsqlFunctionConvert
    function correctly. This commit add changes to handle the case for CHAR and NCHAR correctly.
    
    Task: BABEL-4641
    Signed-off-by: Rohit Bhagat <rohitbgt@amazon.com>

--- test/JDBC/input/BABEL-3147-vu-cleanup.sql
+++ test/JDBC/input/BABEL-3147-vu-cleanup.sql
@@ -1,3 +1,4 @@
+-- error should not have been thrown, should be fixed under BABEL-1710
 DROP TABLE BABEL_3147_vu_prepare_t_1
 DROP TABLE BABEL_3147_vu_prepare_t_2
 DROP TABLE BABEL_3147_vu_prepare_t_3
