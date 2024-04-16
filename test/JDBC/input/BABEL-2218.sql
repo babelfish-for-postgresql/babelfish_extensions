213d3c1895744da3c6ee6f64f9ea36b1a6fdfcb3
Shard Gupta <shardga@amazon.com>
2024-04-16 11:57:17 +0000

    Add tests for procedure and triggers
    Signed-off-by: Shard Gupta <shardga@amazon.com>

--- test/JDBC/input/BABEL-2218.sql
+++ test/JDBC/input/BABEL-2218.sql
@@ -43,25 +43,25 @@ GO
 CREATE FUNCTION f_getval()RETURNS INTEGER
 AS
 BEGIN
-DECLARE temp_cursor CURSOR FOR SELECT a FROM t2218
+  DECLARE temp_cursor CURSOR FOR SELECT c1 FROM t2218
   OPEN temp_cursor
   FETCH NEXT FROM temp_cursor
   CLOSE temp_cursor
   RETURN 1
-  END
+END
 go
 
 -- cursor for select work if the destination(INTO @variable) is provided inside a function BABEL-4586
 CREATE FUNCTION f_getval() RETURNS INTEGER
 AS
 BEGIN
-DECLARE @my_var int
-DECLARE temp_cursor CURSOR FOR SELECT a FROM t2218
+  DECLARE @my_var int
+  DECLARE temp_cursor CURSOR FOR SELECT c1 FROM t2218
   OPEN temp_cursor
   FETCH NEXT FROM temp_cursor INTO @my_var
   CLOSE temp_cursor
   RETURN @my_var
-  END
+END
 go
 
 DROP FUNCTION f_getval;
@@ -71,14 +71,14 @@ GO
 CREATE FUNCTION f_getval() RETURNS INTEGER
 AS
 BEGIN
-DECLARE @my_var int
-DECLARE temp_cursor CURSOR FOR SELECT a FROM t2218
+  DECLARE @my_var int
+  DECLARE temp_cursor CURSOR FOR SELECT c1 FROM t2218
   OPEN temp_cursor
   FETCH NEXT FROM temp_cursor INTO @my_var
   FETCH NEXT FROM temp_cursor INTO @my_var
   CLOSE temp_cursor
   RETURN @my_var
-  END
+END
 go
 
 DROP FUNCTION f_getval;
@@ -88,15 +88,65 @@ GO
 CREATE FUNCTION f_getval() RETURNS INTEGER
 AS
 BEGIN
-DECLARE @my_var int
-DECLARE temp_cursor CURSOR FOR SELECT a FROM t2218
+  DECLARE @my_var int
+  DECLARE temp_cursor CURSOR FOR SELECT c1 FROM t2218
   OPEN temp_cursor
   FETCH NEXT FROM temp_cursor INTO @my_var
   FETCH NEXT FROM temp_cursor
   CLOSE temp_cursor
   RETURN @my_var
-  END
-go
+END
+GO
 
-DROP TABLE t2218;
+-- cursor for select should work for procedure
+CREATE PROCEDURE proc_with_cursor_fetch
+AS
+BEGIN
+  DECLARE temp_cursor CURSOR FOR SELECT c1 FROM t2218
+  OPEN temp_cursor
+  FETCH NEXT FROM temp_cursor
+  CLOSE temp_cursor
+END
+GO
+
+DROP PROCEDURE proc_with_cursor_fetch;
+GO
+
+-- cursor for select should work for procedure with INTO
+CREATE PROCEDURE proc_with_cursor_fetch
+AS
+BEGIN
+  DECLARE temp_cursor CURSOR FOR SELECT c1 FROM t2218
+  DECLARE @my_var int
+  OPEN temp_cursor
+  FETCH NEXT FROM temp_cursor INTO @my_var
+  CLOSE temp_cursor
+END
+GO
+
+DROP PROCEDURE proc_with_cursor_fetch;
+GO
+
+CREATE TRIGGER trg1 ON t2218 AFTER INSERT AS
+  DECLARE temp_cursor CURSOR FOR SELECT c1 FROM t2218
+  DECLARE @my_var int
+  OPEN temp_cursor
+  FETCH NEXT FROM temp_cursor INTO @my_var
+  CLOSE temp_cursor
+GO
+
+CREATE TRIGGER trg2 ON t2218 AFTER INSERT AS
+  DECLARE temp_cursor1 CURSOR FOR SELECT c1 FROM t2218
+  OPEN temp_cursor1
+  FETCH NEXT FROM temp_cursor1
+  CLOSE temp_cursor1
+GO
+
+-- Trigger after insert
+INSERT INTO t2218 VALUES (2218);
+GO
+
+DROP TRIGGER trg1
+DROP TRIGGER trg2
+DROP TABLE t2218
 GO
