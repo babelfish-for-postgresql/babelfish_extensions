65f1e4e7ea95add591fce28b5d779e2dcae4cc12
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2024-04-22 11:07:56 +0530

    Support unique constraints on nullable column with nulls not distinct (#2392)
    Currently babelfish has supported UNIQUE indexes/constraints under the escape hatch 'escape_hatch_unique_constraint'. This is because the current implementation allows duplicate NULL values in a column even when there is a UNIQUE constraint on it.
    While the correct expected behaviour for pltsql UNIQUE indexes should be to treat NULL values as NOT DISTINCT. So only one NULL value should be allowed in UNIQUE index. (reject duplicate nulls in column)
    We use PGs null not distinct option on column definition to implement this. So pltsql UNIQUE COLUMN CONSTRAINT and CREATE UNIQUE INDEX would implicitly have the nulls not distinct option and only single NULL value will be allowed.
    
    For UNIQUE COLUMN CONSTRAINT we re-write the query string in ANTLR parser while for CREATE INDEX we use the bison parser.
    
    This PR also includes fix for NULLS ordering for UNIQUE indexes created through table or column constraints. Refer engine PR for more information.
    Also INLINE INDEX is not currently supported and should throw unsupported error. Test cases added in BABEL-UNSUPPORTED
    Remove the escape hatch 'escape_hatch_unique_constraint' since it is no longer needed.
    Engine PR: babelfish-for-postgresql/postgresql_modified_for_babelfish#310
    Extension PR: #2392
    Issues Resolved
    
    [BABEL-3571]
    
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

---
+++ test/JDBC/input/BABEL_3571-before-15-5-vu-verify.sql
@@ -0,0 +1,237 @@
+-- deprecated escape hatch should not throw an error but print a simple message
+CREATE TABLE babel_3571_t (id INT)
+GO
+
+EXEC sp_babelfish_configure 'escape_hatch_unique_constraint', 'ignore';
+INSERT INTO babel_3571_t VALUES (1);
+GO
+
+CREATE PROC babel_3571_p
+AS
+BEGIN
+    EXEC sp_babelfish_configure 'babelfishpg_tsql.escape_hatch_unique_constraint', 'ignore', 'server';
+    INSERT INTO babel_3571_t VALUES (1);
+END
+GO
+
+EXEC babel_3571_p
+GO
+
+SELECT * FROM babel_3571_t
+GO
+
+DROP PROC babel_3571_p
+GO
+
+DROP TABLE babel_3571_t
+GO
+
+INSERT INTO babel_3571_11 VALUES (-1, '-a', 0x0001), (1, '+a', 0x1001), (NULL, '', NULL), (0, NULL, NULL)
+GO
+
+INSERT INTO babel_3571_11 VALUES (NULL, 'random', 0x9999)
+GO
+
+INSERT INTO babel_3571_11 VALUES (9999, NULL, 0x9999)
+GO
+
+INSERT INTO babel_3571_11 VALUES (9999, 'random', NULL)
+GO
+
+
+INSERT INTO babel_3571_12 VALUES (-1, '-a', 0x0001), (1, '+a', 0x1001), (NULL, '', NULL), (0, NULL, NULL)
+GO
+
+INSERT INTO babel_3571_12 VALUES (NULL, '', 0x9999)
+GO
+
+INSERT INTO babel_3571_12 VALUES (0, NULL, 0x9999)
+GO
+
+INSERT INTO babel_3571_12 VALUES (NULL, NULL, NULL)
+GO
+
+INSERT INTO babel_3571_12 VALUES (NULL, NULL, 0x9999)
+GO
+
+
+INSERT INTO babel_3571_13 VALUES (-1, '-a', 0x0001), (1, '+a', 0x1001), (NULL, '', NULL), (0, NULL, NULL)
+GO
+
+INSERT INTO babel_3571_13 VALUES (NULL, '', 0x9999)
+GO
+
+INSERT INTO babel_3571_13 VALUES (0, NULL, 0x9999)
+GO
+
+INSERT INTO babel_3571_13 VALUES (NULL, NULL, NULL)
+GO
+
+INSERT INTO babel_3571_13 VALUES (NULL, NULL, 0x9999)
+GO
+
+SELECT indexdef FROM pg_indexes 
+WHERE tablename LIKE '%babel_3571%'
+ORDER BY indexdef
+GO
+
+DROP TABLE babel_3571_11, babel_3571_12, babel_3571_13
+GO
+
+-- Repeat same tests but with more table/column specification
+-- columsn with not null specification should reject NULL
+INSERT INTO babel_3571_21 VALUES (-1, '-a', 0x0001), (1, '+a', 0x1001), (2, '', 0x0002), (0, 'something', 0x0003)
+GO
+
+INSERT INTO babel_3571_21 VALUES (NULL, 'random', 0x9999)
+GO
+
+INSERT INTO babel_3571_21 VALUES (9999, NULL, 0x9999)
+GO
+
+INSERT INTO babel_3571_21 VALUES (9999, 'random', NULL)
+GO
+
+INSERT INTO babel_3571_21 VALUES (9999, 'random', 0x1234)
+GO
+
+INSERT INTO babel_3571_21 VALUES (8888, 'random_again', 0x1234)
+GO
+
+
+INSERT INTO babel_3571_22 VALUES (-1, '-a', 0x0001), (1, '+a', 0x1001), (NULL, '', 0x0002), (0, NULL, 0x0003)
+GO
+
+INSERT INTO babel_3571_22 VALUES (NULL, '', 0x9999)
+GO
+
+INSERT INTO babel_3571_22 VALUES (0, NULL, 0x9999)
+GO
+
+INSERT INTO babel_3571_22 VALUES (1234, 'somethingrandom', NULL)
+GO
+
+INSERT INTO babel_3571_22 VALUES (1234, 'somethingrandom', 0x1234)
+GO
+
+INSERT INTO babel_3571_22 VALUES (4321, 'somethingrandomagain', 0x1234)
+GO
+
+
+INSERT INTO babel_3571_23 VALUES (-1, '-a', 0x0001), (1, '+a', 0x1001), (NULL, '', 0x0002), (0, NULL, 0x0003)
+GO
+
+INSERT INTO babel_3571_23 VALUES (NULL, '', 0x9999)
+GO
+
+INSERT INTO babel_3571_23 VALUES (9999, NULL, 0x9999)
+GO
+
+INSERT INTO babel_3571_23 VALUES (0, 'somethingagain', NULL)
+GO
+
+INSERT INTO babel_3571_23 VALUES (0, 'somethingagain', 0x1234)
+GO
+
+INSERT INTO babel_3571_23 VALUES (8888, 'newsomething', 0x1234)
+GO
+
+
+-- drop constraint
+ALTER TABLE babel_3571_23 DROP CONSTRAINT babel_3571_23_unique_idx
+GO
+
+INSERT INTO babel_3571_23 VALUES (NULL, NULL, 0X91)
+INSERT INTO babel_3571_23 VALUES (1, '+a', 0X92)
+GO
+
+INSERT INTO babel_3571_23 VALUES (NULL, NULL, 0X0001)
+GO
+
+
+ALTER TABLE babel_3571_23 DROP CONSTRAINT babel_3571_23_pkey
+
+INSERT INTO babel_3571_23 VALUES (NULL, NULL, 0X0001)
+GO
+
+SELECT indexdef FROM pg_indexes 
+WHERE tablename LIKE '%babel_3571%'
+ORDER BY indexdef
+GO
+
+DROP TABLE babel_3571_21, babel_3571_22, babel_3571_23
+GO
+
+
+-- CREATE TABLE WITH CONSTRAINT -- DROP COLUMN -- ADD COLUMN WITHOUT CONTRAINT
+INSERT INTO babel_3571_31 VALUES (1,1), (1,1), (NULL, NULL), (NULL, NULL);
+INSERT INTO babel_3571_32 VALUES (1,1), (1,1), (NULL, NULL), (NULL, NULL);
+INSERT INTO babel_3571_33 VALUES (1,1), (1,1), (NULL, NULL), (NULL, NULL);
+GO
+
+SELECT indexdef FROM pg_indexes 
+WHERE tablename LIKE '%babel_3571%'
+ORDER BY indexdef
+GO
+
+DROP TABLE babel_3571_31, babel_3571_32, babel_3571_33
+GO
+
+-- Test with identity column
+INSERT INTO babel_3571_41 VALUES (1), (2), (3)
+INSERT INTO babel_3571_42 VALUES (1), (2), (3)
+INSERT INTO babel_3571_43 VALUES (1), (2), (3)
+GO
+
+SET IDENTITY_INSERT dbo.babel_3571_41 ON;
+GO
+
+INSERT INTO babel_3571_41 (id1, id2) VALUES (1,10)
+GO
+INSERT INTO babel_3571_41 (id1, id2) VALUES (NULL,10)
+GO
+
+INSERT INTO babel_3571_41 (id1, id2) VALUES (4,10)
+INSERT INTO babel_3571_41 (id1, id2) VALUES (5, NULL)
+GO
+
+SET IDENTITY_INSERT dbo.babel_3571_41 OFF;
+GO
+
+SET IDENTITY_INSERT dbo.babel_3571_42 ON;
+GO
+
+INSERT INTO babel_3571_42 (id1, id2) VALUES (1,10)
+GO
+INSERT INTO babel_3571_42 (id1, id2) VALUES (NULL,10)
+GO
+
+INSERT INTO babel_3571_42 (id1, id2) VALUES (4,10)
+INSERT INTO babel_3571_42 (id1, id2) VALUES (5, NULL)
+GO
+
+SET IDENTITY_INSERT dbo.babel_3571_42 OFF;
+GO
+
+SET IDENTITY_INSERT dbo.babel_3571_43 ON;
+GO
+
+INSERT INTO babel_3571_43 (id1, id2) VALUES (1,10)
+GO
+INSERT INTO babel_3571_43 (id1, id2) VALUES (NULL,10)
+GO
+
+INSERT INTO babel_3571_43 (id1, id2) VALUES (4,10)
+INSERT INTO babel_3571_43 (id1, id2) VALUES (5, NULL)
+GO
+
+SET IDENTITY_INSERT dbo.babel_3571_43 OFF;
+GO
+
+SELECT indexdef FROM pg_indexes 
+WHERE tablename LIKE '%babel_3571%'
+ORDER BY indexdef
+GO
+
+DROP TABLE babel_3571_41, babel_3571_42, babel_3571_43
+GO
---
+++ test/JDBC/input/babel_index_nulls_order-15_5-or-15_6-or-16_1-or-16_2-vu-cleanup.sql
@@ -0,0 +1,27 @@
+DROP VIEW babel_index_nulls_order_a_v1
+go
+DROP VIEW babel_index_nulls_order_a_v2
+go
+DROP VIEW babel_index_nulls_order_a_v3
+go
+DROP VIEW babel_index_nulls_order_b_v1
+go
+DROP VIEW babel_index_nulls_order_b_v2
+go
+DROP VIEW babel_index_nulls_order_b_v3
+go
+DROP VIEW babel_index_nulls_order_ab_v1
+go
+DROP VIEW babel_index_nulls_order_ab_v2
+go
+DROP VIEW babel_index_nulls_order_ab_v3
+go
+DROP VIEW babel_index_nulls_order_ab_v4
+go
+DROP VIEW babel_index_nulls_order_ab_v5
+go
+DROP VIEW babel_index_nulls_order_ab_v6
+go
+
+DROP TABLE babel_index_nulls_order_tbl
+go
