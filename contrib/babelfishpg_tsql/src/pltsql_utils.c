65c66b2588dc3f094bf9d34dc711b2be789b8b72
Tim Chang <changti@amazon.com>
2024-05-22 17:08:40 +0000

    Removed extra diff
    Signed-off-by: Tim Chang <changti@amazon.com>

--- contrib/babelfishpg_tsql/src/pltsql_utils.c
+++ contrib/babelfishpg_tsql/src/pltsql_utils.c
@@ -20,7 +20,6 @@
 #include "utils/fmgroids.h"
 #include "utils/catcache.h"
 #include "utils/acl.h"
-#include "utils/queryenvironment.h"
 #include "access/table.h"
 #include "access/genam.h"
 #include "catalog.h"
