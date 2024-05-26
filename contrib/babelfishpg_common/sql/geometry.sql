c48a3265202294cd39c90d3a53fefa32af745088
Anikait Agrawal <54908236+Anikait143@users.noreply.github.com>
2024-05-26 13:23:08 +0530

    Fix Casting issue from Geometry to Bytea/Varbinary with SRID 0 (#2615)
    For Geometry with SRID = 0, when we CAST from geometry to bytea/varbinary, PostGIS considers SRID 0 as SRID UNKNOWN, which prevents it from adding SRID bytes in the resulting varlena(bytea) format, producing incorrect result in babelfish. To handle this we have added an additional wrapper check to identify SRID 0 and then we produce the result in the driver expected format manually by prepending the SRID bytes.
    
    # Input
    select cast(geometry::STGeomFromText('Point(47.65100 -22.34900)', 0) AS varbinary)
    
    # previous output (wrong output)               
    geometry__stgeomfromtext              
    --------------------------------------------------
    0x17D9CEF7010C53D34740D34D6210585936C0
    
    # current output (correct output)
    geometry__stgeomfromtext                                      
    -------------------------------------------------------------
    0x00000000010C17D9CEF753D34740D34D6210585936C0
    
    # Input
    select cast(geometry::STGeomFromText('Point(47.65100 -22.34900)', 0) AS bytea)
    
    # previous output (wrong output)               
    geometry__stgeomfromtext              
    --------------------------------------------------
    0x17D9CEF7010C53D34740D34D6210585936C0
    
    # current output (correct output)
    geometry__stgeomfromtext                                      
    -------------------------------------------------------------
    0x00000000010C17D9CEF753D34740D34D6210585936C0
    
    Task: BABEL-4986
    
    Signed-off-by: Anikait Agrawal agraani@amazon.com

--- contrib/babelfishpg_common/sql/geometry.sql
+++ contrib/babelfishpg_common/sql/geometry.sql
@@ -193,10 +193,23 @@ CREATE OR REPLACE FUNCTION sys.bytea(sys.GEOMETRY)
     AS $$
     DECLARE
         byte bytea;
+        srid_flag text;
     BEGIN
         byte := (SELECT sys.bytea_helper($1));
-        byte := substring(byte from 6);
-        byte := substring(byte from 1 for 4) || E'\\x010c' || substring(byte from 5);
+        -- Checking the Geometry type currently we support only POINT type -> type = 1
+        IF encode(substring(byte from 2 for 3), 'hex') = encode(E'\\x010000', 'hex') THEN
+            srid_flag := encode(substring(byte from 5 for 1), 'hex');
+            -- Check if the given geometry has SRID flag
+            IF srid_flag = encode(E'\\x20', 'hex') THEN
+                byte := substring(byte from 6);
+                byte := substring(byte from 1 for 4) || E'\\x010c' || substring(byte from 5);
+            ELSEIF srid_flag = encode(E'\\x00', 'hex') AND LENGTH(byte) = 21 THEN
+                -- Signifies SRID = 0, pass the driver expected wkb manually
+                byte := substring(byte from 6); -- contains only wkb point coords
+                -- prepend 4 byte SRID (00000000) + 2 bytes (type -> 010C for point)
+                byte := E'\\x00000000010c' || substring(byte from 1);
+            END IF;
+        END IF;
         RETURN byte;
     END;
     $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;
