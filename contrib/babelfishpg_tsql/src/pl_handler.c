e76669eaacd5d804efc0005af4918d00b3f9c809
Sumit Jaiswal <sumiji@amazon.com>
2024-05-28 11:12:25 +0000

    minor fix Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

--- contrib/babelfishpg_tsql/src/pl_handler.c
+++ contrib/babelfishpg_tsql/src/pl_handler.c
@@ -525,7 +525,10 @@ pltsql_pre_parse_analyze(ParseState *pstate, RawStmt *parseTree)
 					relid == namespace_ext_oid ||
 					relid == bbf_view_def_oid ||
 					relid == bbf_extended_properties_oid ||
-					relid == bbf_schema_perms_oid)
+					relid == bbf_schema_perms_oid ||
+					relid == bbf_partition_function_oid ||
+					relid == bbf_partition_scheme_oid ||
+					relid == bbf_partition_depend_oid)
 				{
 					ResTarget	*col = NULL;
 					A_Const 	*dbidValue = NULL;
