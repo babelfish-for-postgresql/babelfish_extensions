6c09e4db3663c4d3e5073729c16f0081af254df4
Jake Owen <jakeowen1@gmail.com>
2024-07-02 15:53:16 -0400

    Add test files for tsql_openjson_with_get_subjsonb engine crash fix (#2707)
    This commit adds test files for the engine fix of the crash in tsql_openjson_with_get_subjsonb.
    
    Task: BABEL-3820
    Signed-off-by: Jake Owen <owjco@amazon.com>

---
+++ test/JDBC/input/openjson/BABEL-3820-vu-cleanup.sql
@@ -0,0 +1,33 @@
+drop procedure openjson_3820_p1
+go
+
+drop procedure openjson_3820_p2
+go
+
+drop procedure openjson_3820_p3
+go
+
+drop procedure openjson_3820_p4
+go
+
+drop procedure openjson_3820_p5
+go
+
+drop procedure openjson_3820_p6
+go
+
+drop procedure openjson_3820_p7
+go
+
+drop procedure openjson_3820_p8
+go
+
+drop procedure openjson_3820_p9
+go
+
+
+drop procedure openjson_3820_p10
+go
+
+drop procedure openjson_3820_p11
+go
