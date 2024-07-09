7d11f323c7aa9e05df56cdfb160242599e2d1fb3
Dipesh Dhameliya <dipeshdhameliya125@gmail.com>
2024-07-09 20:35:17 +0530

    Fix protocol violation error happening due to Const node under append node (#2720)
    Previously, certain driver was running into an protocol violation error when Const node in target list appears under
    Append node. This was happening because resolve_numeric_typmod_from_exp was not implemented to handle Const
    node properly for decimal data types. This commit fixes that issue by properly handling decimal data type for Const
    node under Append node.
    
    Task: BABEL-5086
    Signed-off-by: Dipesh Dhameliya <dddhamel@amazon.com>

--- test/JDBC/input/babel_numeric.sql
+++ test/JDBC/input/babel_numeric.sql
@@ -61,3 +61,196 @@ go
 
 drop table t1;
 go
+
+select * from 
+(
+    select cast(1.23 as decimal(18,2)) as col
+    union all
+    select cast(1.23 as decimal(7,2)) as col
+) dummy order by col;
+go
+
+select * from 
+(
+    select cast(NULL as decimal(18,2)) as col
+    union all
+    select cast(1.23 as decimal(7,2)) as col
+) dummy order by col;
+go
+
+select * from 
+(
+    select cast(9999999999999999.99 as decimal(18,2)) as col
+    union all
+    select cast(99999.99 as decimal(7,2)) as col
+) dummy order by col;
+go
+
+create type decimal_18_2 from decimal(18,2);
+go
+
+create type decimal_7_2 from decimal(7,2);
+go
+
+select * from 
+(
+    select cast(1.23 as decimal_18_2) as col
+    union all
+    select cast(1.23 as decimal_7_2) as col
+) dummy order by col;
+go
+
+select * from 
+(
+    select cast(1.23 as decimal_18_2) as col
+    union all
+    select cast(NULL as decimal_7_2) as col
+) dummy order by col;
+go
+
+select * from 
+(
+    select cast(9999999999999999.99 as decimal_18_2) as col
+    union all
+    select cast(99999.99 as decimal_7_2) as col
+) dummy order by col;
+go
+
+create table babel_5086_t1 (a decimal(18,2), b decimal(7,2), c decimal_18_2, d decimal_7_2);
+go
+
+insert into babel_5086_t1 values (1.23, 1.23, 1.23, 1.23);
+insert into babel_5086_t1 values (9999999999999999.99, NULL, 9999999999999999.99, NULL);
+insert into babel_5086_t1 values (NULL, 99999.99, NULL, 99999.99);
+go
+
+select * from 
+(
+    select a as col from babel_5086_t1
+    union all
+    select b as col from babel_5086_t1
+) dummy order by col;
+go
+
+select * from 
+(
+    select c as col from babel_5086_t1
+    union all
+    select d as col from babel_5086_t1
+) dummy order by col;
+go
+
+select * from 
+(
+    select a as col from babel_5086_t1
+    union all
+    select b as col from babel_5086_t1
+    union all
+    select c as col from babel_5086_t1
+    union all
+    select d as col from babel_5086_t1
+) dummy order by col;
+go
+
+select * from 
+(
+    select a as col from babel_5086_t1
+    union all
+    select c as col from babel_5086_t1
+) dummy order by col;
+go
+
+select * from 
+(
+    select b as col from babel_5086_t1
+    union all
+    select d as col from babel_5086_t1
+) dummy order by col;
+go
+
+create type numeric_18_2 from numeric(18,2);
+go
+
+create type numeric_7_2 from numeric(7,2);
+go
+
+select * from 
+(
+    select cast(1.23 as numeric_18_2) as col
+    union all
+    select cast(1.23 as numeric_7_2) as col
+) dummy order by col;
+go
+
+select * from 
+(
+    select cast(12344.234 as numeric_18_2) as col
+    union all
+    select cast(1.23 as numeric_7_2) as col
+) dummy order by col;
+go
+
+create table babel_5086_t2 (a numeric(18,2), b numeric(7,2), c numeric_18_2, d numeric_7_2);
+go
+
+insert into babel_5086_t2 values (1.23, 1.23, 1.23, 1.23);
+insert into babel_5086_t2 values (9999999999999999.99, NULL, 9999999999999999.99, NULL);
+insert into babel_5086_t2 values (NULL, 99999.99, NULL, 99999.99);
+go
+
+select * from 
+(
+    select a as col from babel_5086_t2 
+    union all
+    select b as col from babel_5086_t2
+) dummy order by col;
+go
+
+select * from 
+(
+    select c as col from babel_5086_t2
+    union all
+    select d as col from babel_5086_t2
+) dummy order by col;
+go
+
+select * from 
+(
+    select a as col from babel_5086_t2
+    union all
+    select b as col from babel_5086_t2
+    union all
+    select c as col from babel_5086_t2
+    union all
+    select d as col from babel_5086_t2
+) dummy order by col;
+go
+
+select * from 
+(
+    select a as col from babel_5086_t2
+    union all
+    select c as col from babel_5086_t2
+) dummy order by col;
+go
+
+
+select * from 
+(
+    select b as col from babel_5086_t2
+    union all
+    select d as col from babel_5086_t2
+) dummy order by col;
+go
+
+drop  table babel_5086_t1;
+go
+
+drop  table babel_5086_t2;
+go
+
+drop type decimal_18_2;
+drop type decimal_7_2;
+drop type numeric_18_2;
+drop type numeric_7_2;
+go
