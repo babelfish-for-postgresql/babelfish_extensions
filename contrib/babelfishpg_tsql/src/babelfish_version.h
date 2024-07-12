a391d96c1ced8d24f90ad11d83216780addffc48
Sharu Goel <30777678+thephantomthief@users.noreply.github.com>
2024-07-12 10:44:42 +0530

    Update babelfish_version.h

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "4.1.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.2.0.0"
+#define BABELFISH_VERSION_STR "4.1.1"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.2.1.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
