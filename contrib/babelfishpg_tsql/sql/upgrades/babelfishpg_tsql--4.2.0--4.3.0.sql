97e9f2039181e7bd04da2cc09d2761979aa12790
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2024-07-26 15:08:53 +0530

    Support strpos & replace for non deterministic collation (#2716)
    Support strpos, charindex and replace for Accent insensitive collations in Babelfish. We use ICU functions such as
    usearch and ucol_strcoll to implement this support.
    
    sys.strpos()/ sys.charindex(): we use usearch to find the first available match in source string. If collation is CS_AS then
    we also double check the match using ucol_strcoll. This is needed since usearch can return false positive for case
    sensitive search when collation is colstrength primary. If ucol_strcoll fails for the match, we skip to the next match.
    
    sys.replace: Similar to strpos we find all the matches using usearch, double check it using ucol_strcoll for CS_AS. For
    the matched portion of the string we copy the replacement string instead while copying the remaining portion from the
    original source string.
    
    Engine PR: babelfish-for-postgresql/postgresql_modified_for_babelfish#402
    Task: BABEL-4850
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.2.0--4.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.2.0--4.3.0.sql
@@ -328,6 +328,52 @@ WHERE
   ps.dbid = sys.db_id();
 GRANT SELECT ON sys.partition_schemes TO PUBLIC;
 
+CREATE OR REPLACE FUNCTION sys.is_collated_ai(IN input_string TEXT) RETURNS BOOL
+AS 'babelfishpg_tsql', 'is_collated_ai_internal'
+LANGUAGE C VOLATILE PARALLEL SAFE;
+
+
+CREATE OR REPLACE FUNCTION sys.replace (in input_string text, in pattern text, in replacement text) returns TEXT as
+$body$
+begin
+   if pattern is null or replacement is null then
+       return null;
+   elsif pattern = '' then
+       return input_string;
+   elsif sys.is_collated_ai(input_string) then
+       return pg_catalog.replace(input_string, pattern, replacement);
+   elsif sys.is_collated_ci_as(input_string) then
+       return regexp_replace(input_string, '***=' || pattern, replacement, 'ig');
+   else
+       return regexp_replace(input_string, '***=' || pattern, replacement, 'g');
+   end if;
+end
+$body$
+LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION sys.charindex(expressionToFind PG_CATALOG.TEXT,
+										 expressionToSearch PG_CATALOG.TEXT,
+										 start_location INTEGER DEFAULT 0)
+RETURNS INTEGER AS
+$BODY$
+SELECT
+CASE
+WHEN expressionToFind = '' THEN
+    0
+WHEN start_location <= 0 THEN
+	strpos(expressionToSearch, expressionToFind)
+ELSE
+	CASE
+	WHEN strpos(substr(expressionToSearch, start_location), expressionToFind) = 0 THEN
+		0
+	ELSE
+		strpos(substr(expressionToSearch, start_location), expressionToFind) + start_location - 1
+	END
+END;
+$BODY$
+STRICT
+LANGUAGE SQL IMMUTABLE;
+
 create or replace function sys.get_tds_id(
 	datatype sys.varchar(50)
 )
