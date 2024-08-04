873d8fb30b42abfede0e4076ec527de360724127
Shalini Lohia <lshalini@amazon.com>
2024-08-04 06:30:26 +0000

    Update views sys.triggers, sys.objects, sys.all_objects to show the correct trigger oid

--- contrib/babelfishpg_tsql/runtime/functions.c
+++ contrib/babelfishpg_tsql/runtime/functions.c
@@ -2128,6 +2128,12 @@ object_id(PG_FUNCTION_ARGS)
 					 !strcmp(object_type, "pc") || !strcmp(object_type, "tf") || !strcmp(object_type, "rf") ||
 					 !strcmp(object_type, "x"))
 			{
+				if (!strcmp(object_type, "p") && tsql_get_trigger_oid(object_name, schema_oid, user_id))
+					//PG_RETURN_NULL();
+					{
+						PG_RETURN_NULL();
+					}
+				
 				/* search in pg_proc by name and schema oid */
 				result = tsql_get_proc_oid(object_name, schema_oid, user_id);
 			}
