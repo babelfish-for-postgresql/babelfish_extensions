6d51b3dc9ecf2a2a38ada8b2ebf0b2da4a46002e
Shalini Lohia <lshalini@amazon.com>
2024-08-08 06:19:27 +0000

    Add automated tests which validates oid returned from sys.triggers, sys.events, sys.objects, sys.all_objects same as object_id

--- test/JDBC/input/views/sys_all_objects-vu-verify.sql
+++ test/JDBC/input/views/sys_all_objects-vu-verify.sql
@@ -1,2 +1,6 @@
 SELECT name, type, type_desc from sys.all_objects  where name like 'sys_all_objects_vu_prepare_1%' order by name;
 GO
+
+select case when object_id([name]) = object_id then 'equal' else 'not equal' end
+from sys.all_objects where [name] = 'sys_all_objects_vu_prepare_1_3'
+go
