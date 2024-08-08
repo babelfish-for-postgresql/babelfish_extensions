aa6df5ead9286ebe130aa79f280522b379b4d42e
Jake Owen <owjco@amazon.com>
2024-08-08 20:39:36 +0000

    Add tests
    Signed-off-by: Jake Owen <owjco@amazon.com>

--- test/JDBC/input/alter/alter-procedure-vu-verify.sql
+++ test/JDBC/input/alter/alter-procedure-vu-verify.sql
@@ -219,6 +219,7 @@ go
 select ROUTINE_NAME, ROUTINE_BODY, ROUTINE_DEFINITION from information_schema.routines where SPECIFIC_NAME LIKE 'alter_proc_p4';
 go
 
+-- Test Case: confirm procedure altered in 'alter-procedure-vu-prepare' is properly updated
 exec alter_proc_p5 @dateParam = '2000-01-01'
 go
 
