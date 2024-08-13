ab4000ef1c3b1dfc2283f6cc874ce3f448992492
yashneet vinayak <yashneet@amazon.com>
2024-08-13 06:33:38 +0000

    updating tests sys-syscolumns
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

--- test/JDBC/input/views/sys-syscolumns.sql
+++ test/JDBC/input/views/sys-syscolumns.sql
@@ -154,6 +154,162 @@ go
 DROP PROCEDURE syscolumns_demo_proc3
 go
 
+-- sla 13000
+create database db1 COLLATE BBF_Unicode_CP1_CI_AI;
+go
+
+use db1;
+go
+
+-- create helper function to get datatype name given oid
+CREATE FUNCTION OidToDataType(@Oid integer)
+RETURNS VARCHAR(50)
+AS
+BEGIN
+        DECLARE @datatype VARCHAR(50);
+        SET @datatype = (SELECT typname from pg_type where oid = @Oid);
+        RETURN @datatype;
+END;
+GO
+
+-- create helper function to get procedure/table name given oid
+CREATE FUNCTION OidToObject(@Oid integer)
+RETURNS VARCHAR(50)
+AS
+BEGIN
+        DECLARE @object_name VARCHAR(50);
+
+        SET @object_name = (SELECT relname from pg_class where oid = @Oid);
+
+        IF (@object_name is null)
+        BEGIN
+                SET @object_name = (SELECT proname from pg_proc where oid = @Oid);
+        END
+
+        RETURN @object_name
+END;
+GO
+
+-- create helper function to get collation name given oid
+CREATE FUNCTION OidToCollation(@Oid integer)
+RETURNS VARCHAR(50)
+AS
+BEGIN
+        DECLARE @collation VARCHAR(50);
+        SET @collation = (SELECT collname from pg_collation where oid = @Oid);
+        RETURN @collation;
+END;
+GO
+
+-- Setup some procedures and tables
+create procedure syscolumns_demo_proc1 @firstparam NVARCHAR(50) as select 1
+GO
+
+create procedure syscolumns_demo_proc2 @firstparam NVARCHAR(50), @secondparam VARCHAR(50) OUT as select 2
+GO
+
+create table syscolumns_demo_table (col_a int, col_b bigint, col_c char(10), col_d numeric(5,4))
+GO
+
+select name, OidToObject(id), OidToDataType(xtype), typestat, length from sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+select name, OidToObject(id), OidToDataType(xtype), typestat, length from dbo.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+select colid, cdefault, domain, number from sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+select colid, cdefault, domain, number from dbo.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+select OidToCollation(collationid), status, OidToDataType(type), prec, scale from sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+select OidToCollation(collationid), status, OidToDataType(type), prec, scale from dbo.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+select iscomputed, isoutparam, isnullable, collation from sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+select iscomputed, isoutparam, isnullable, collation from dbo.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c' or name = 'col_d' order by OidToObject(id) asc, name
+GO
+
+SELECT COUNT(*) FROM sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+SELECT COUNT(*) FROM dbo.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+use master;
+go
+
+create procedure syscolumns_demo_proc3 @thirdparam NVARCHAR(50) as select 3;
+go
+
+SELECT COUNT(*) FROM sys.syscolumns where name = '@thirdparam'
+go
+
+-- syscolumns should also exist in dbo schema
+SELECT COUNT(*) FROM dbo.SySCOluMNs where name = '@thirdparam';
+go
+
+SELECT COUNT(*) FROM db1.sys.SySCOluMNs where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+-- In case of cross-db, syscolumns should also exist in dbo schema
+-- Cross-DB system view query is not currently supported in Babelfish.
+SELECT COUNT(*) FROM db1.DbO.SySCOluMNs where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+-- should not be visible here
+SELECT COUNT(*) FROM db1.sys.SySCOluMNs where name = '@thirdparam';
+GO
+
+SELECT COUNT(*) FROM db1.dbo.SySCOluMNs where name = '@thirdparam';
+GO
+
+-- should not be visible here
+SELECT COUNT(*) FROM sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+SELECT COUNT(*) FROM dbo.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+use db1;
+go
+
+SELECT COUNT(*) FROM sys.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+SELECT COUNT(*) FROM dbo.syscolumns where name = '@firstparam' or name = '@secondparam' or name = 'col_a' or name = 'col_b' or name = 'col_c'
+go
+
+-- should not be visible here
+SELECT COUNT(*) FROM sys.syscolumns where name = '@thirdparam'
+go
+
+SELECT COUNT(*) FROM dbo.syscolumns where name = '@thirdparam'
+go
+
+-- Cleanup
+DROP FUNCTION OidToDataType
+DROP FUNCTION OidToObject
+DROP FUNCTION OidToCollation
+DROP PROCEDURE syscolumns_demo_proc1
+DROP PROCEDURE syscolumns_demo_proc2
+DROP TABLE syscolumns_demo_table
+GO
+
+use master;
+go
+
+drop database db1;
+go
+
+DROP PROCEDURE syscolumns_demo_proc3
+go
+
 -- Tests for sys.columns catalog view
 -- Test precision and scale for all numeric datatypes
 create table t1(a int, b float, c bigint, d numeric, e smallint, f tinyint, g decimal, h money, i smallmoney);
