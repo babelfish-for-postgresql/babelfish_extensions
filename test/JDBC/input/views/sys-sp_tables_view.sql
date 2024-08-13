8d6c82cfbc7e80d249606a766d8cc11749d3ad85
yashneet vinayak <yashneet@amazon.com>
2024-08-13 06:32:01 +0000

    updating tests sys-sp_tables_view
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

--- test/JDBC/input/views/sys-sp_tables_view.sql
+++ test/JDBC/input/views/sys-sp_tables_view.sql
@@ -51,3 +51,57 @@ GO
 
 DROP DATABASE db1
 GO
+
+CREATE DATABASE db1 COLLATE BBF_Unicode_CP1_CI_AI;
+GO
+
+USE db1
+GO
+
+create table tbl_1 (a int)
+GO
+
+select count(*) from sys.sp_tables_view where TABLE_NAME = 'tbl_1';
+GO
+
+exec sys.sp_tables @table_name = 'tbl_1';
+GO
+
+USE master
+GO
+
+select count(*) from sys.sp_tables_view where TABLE_NAME = 'tbl_1';
+GO
+
+exec sys.sp_tables @table_name = 'tbl_1';
+GO
+
+create table tbl_2 (a int)
+GO
+
+select count(*) from sys.sp_tables_view where TABLE_NAME = 'tbl_2';
+GO
+
+exec sys.sp_tables @table_name = 'tbl_2';
+GO
+
+USE db1
+GO
+
+select count(*) from sys.sp_tables_view where TABLE_NAME = 'tbl_2';
+GO
+
+exec sys.sp_tables @table_name = 'tbl_2';
+GO
+
+drop table tbl_1;
+GO
+
+USE master
+GO
+
+drop table tbl_2;
+GO
+
+DROP DATABASE db1
+GO
