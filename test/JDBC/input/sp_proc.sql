69ac4797ba5bcc222bc74a920b65b994b94dfa9e
Pranav Jain <pranavjc@amazon.com>
2024-08-13 16:40:53 +0000

    Update sp_proc test files
    Signed-off-by: Pranav Jain <pranavjc@amazon.com>

--- test/JDBC/input/sp_proc.sql
+++ test/JDBC/input/sp_proc.sql
@@ -101,9 +101,100 @@ go
 use master
 go
 
+drop database db1
+go
+
+-- Using Collation BBF_Unicode_CP1_CI_AI
+create database db1 collate BBF_Unicode_CP1_CI_AI
+go
+
+use db1
+go
+
+exec master.dbo.sp_hello
+go
+
+exec master..sp_hello
+go
+
+exec sp_hello
+go
+
+exec .sp_hello
+go
+
+exec ..sp_hello
+go
+
+exec dbo.sp_hello
+go
+
+exec .dbo.sp_hello
+go
+
+exec ..dbo.sp_hello
+go
+
+sp_hello
+go
+
+.sp_hello
+go
+
+..sp_hello
+go
+
+dbo.sp_hello
+go
+
+.dbo.sp_hello
+go
+
+..dbo.sp_hello
+go
+
+create proc call_sp_helllo as exec sp_hello
+go
+
+exec call_sp_helllo
+go
+
+create proc sp_hello as select 2
+go
+
+--Executes the sp_hello in db1
+exec sp_hello
+go
+
+exec dbo.sp_hello
+go
+
+exec call_sp_helllo
+go
+
+drop proc sp_hello
+go
+
+drop proc call_sp_helllo
+go
+
+create proc sp_hello as select 1/0;
+go
+
+exec sp_hello;
+go
+
+exec @a;
+go
+
+drop proc sp_hello;
+go
+
+use master
+go
+
 drop proc sp_hello
 go
 
 drop database db1
 go
-
