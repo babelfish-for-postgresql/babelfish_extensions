83836c4df0b099e66545a91478bde2a79e3905af
Sharath BP <sharatbp@amazon.com>
2024-08-22 07:53:39 +0000

    Add updated sys.configurations view to upgrade script

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.3.0--4.4.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.3.0--4.4.0.sql
@@ -11,6 +11,43 @@ SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false)
  * final behaviour.
  */
 
+CREATE OR REPLACE VIEW sys.configurations
+AS
+SELECT  configuration_id, 
+        name, 
+        value, 
+        minimum, 
+        maximum, 
+        value_in_use, 
+        description, 
+        is_dynamic, 
+        is_advanced 
+FROM sys.babelfish_configurations
+UNION ALL
+SELECT 
+CAST(103 as INT) AS configuration_id,
+CAST('user connections' AS SYS.NVARCHAR(35)) AS name,
+CAST(s.setting AS sys.sql_variant) AS value,
+CAST(s.min_val AS sys.sql_variant) AS minimum,
+CAST(s.max_val AS sys.sql_variant) AS maximum,
+CAST(s.setting AS sys.sql_variant) AS value_in_use,
+CAST(s.short_desc AS sys.nvarchar(255)) AS description,
+CAST(0 AS sys.BIT) AS is_dynamic,
+CAST(1 AS sys.BIT) AS is_advanced
+FROM pg_catalog.pg_settings s where name = 'max_connections'
+UNION ALL
+SELECT 
+CAST(505 as INT) AS configuration_id,
+CAST('network packet size (B)' AS SYS.NVARCHAR(35)) AS name,
+CAST(s.setting AS sys.sql_variant) AS value,
+CAST(s.min_val AS sys.sql_variant) AS minimum,
+CAST(s.max_val AS sys.sql_variant) AS maximum,
+CAST(s.setting AS sys.sql_variant) AS value_in_use,
+CAST(s.short_desc AS sys.nvarchar(255)) AS description,
+CAST(1 AS sys.BIT) AS is_dynamic,
+CAST(1 AS sys.BIT) AS is_advanced
+FROM pg_catalog.pg_settings s where name = 'babelfishpg_tds.tds_default_packet_size';
+GRANT SELECT ON sys.configurations TO PUBLIC;
 
 -- After upgrade, always run analyze for all babelfish catalogs.
 CALL sys.analyze_babelfish_catalogs();
