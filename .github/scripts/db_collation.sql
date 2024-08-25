dd1670cc8be9fb8c803f15ab3c9cd8c8d6047efe
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2024-08-25 18:51:41 +0000

    update readme and delete redundant script
    Signed-off-by: Shameem Ahmed <shameemahmed20apr2000@gmail.com>

--- .github/scripts/db_collation.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE test_db_collation COLLATE bbf_unicode_cp1_ci_ai;
-GO
-USE test_db_collation;
-GO
-SELECT name, collation_name from sys.databases where name = db_name();
-GO
-GRANT CONNECT TO GUEST;
-GO
