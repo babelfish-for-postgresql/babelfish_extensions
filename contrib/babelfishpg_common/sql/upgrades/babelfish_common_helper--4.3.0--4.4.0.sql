6a42aa00c2ee42a1aac4a7163a0a3a00e4373de6
yashneet vinayak <yashneet@amazon.com>
2024-08-26 12:28:10 +0000

    depricated the function
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

---
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--4.3.0--4.4.0.sql
@@ -0,0 +1,75 @@
+------------------------------------------------------------------------------
+---- Include changes related to other datatypes except spatial types here ----
+------------------------------------------------------------------------------
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO "4.4.0"" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+-- Drops an object if it does not have any dependent objects.
+-- Is a temporary procedure for use by the upgrade script. Will be dropped at the end of the upgrade.
+-- Please have this be one of the first statements executed in this upgrade script. 
+CREATE OR REPLACE PROCEDURE babelfish_drop_deprecated_object(object_type varchar, schema_name varchar, object_name varchar) AS
+$$
+DECLARE
+    error_msg text;
+    query1 text;
+    query2 text;
+BEGIN
+
+    query1 := pg_catalog.format('alter extension babelfishpg_common drop %s %s.%s', object_type, schema_name, object_name);
+    query2 := pg_catalog.format('drop %s %s.%s', object_type, schema_name, object_name);
+
+    execute query1;
+    execute query2;
+EXCEPTION
+    when object_not_in_prerequisite_state then --if 'alter extension' statement fails
+        GET STACKED DIAGNOSTICS error_msg = MESSAGE_TEXT;
+        raise warning '%', error_msg;
+    when dependent_objects_still_exist then --if 'drop view' statement fails
+        GET STACKED DIAGNOSTICS error_msg = MESSAGE_TEXT;
+        raise warning '%', error_msg;
+end
+$$
+LANGUAGE plpgsql;
+
+-- (sys.VARCHAR AS pg_catalog.TIME)
+DROP CAST IF EXISTS(sys.VARCHAR AS pg_catalog.TIME);
+
+DO $$
+DECLARE
+    exception_message text;
+BEGIN
+    ALTER FUNCTION sys.varchar2time(sys.VARCHAR) RENAME TO varchar2time_deprecated_4_4_0;
+
+EXCEPTION WHEN OTHERS THEN
+    GET STACKED DIAGNOSTICS
+    exception_message = MESSAGE_TEXT;
+    RAISE WARNING '%', exception_message;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION sys.varchar2time(sys.VARCHAR, INT4)
+RETURNS pg_catalog.TIME
+AS 'babelfishpg_common', 'varchar2time'
+LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE CAST (sys.VARCHAR AS pg_catalog.TIME)
+WITH FUNCTION sys.varchar2time(sys.VARCHAR, INT4) AS IMPLICIT;
+
+DO $$
+DECLARE
+    exception_message text;
+BEGIN
+    CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'varchar2time_deprecated_4_4_0');
+
+EXCEPTION WHEN OTHERS THEN
+    GET STACKED DIAGNOSTICS
+    exception_message = MESSAGE_TEXT;
+    RAISE WARNING '%', exception_message;
+END;
+$$;
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
--- contrib/babelfishpg_common/sql/upgrades/spatial_types--4.3.0--4.4.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/spatial_types--4.3.0--4.4.0.sql
@@ -1,11 +1,3 @@
 -------------------------------------------------------
----- Include changes related to varchar cast here ----
+---- Include changes related to spatial types here ----
 -------------------------------------------------------
-
-CREATE OR REPLACE FUNCTION sys.varchar2time(sys.VARCHAR, INT4)
-RETURNS pg_catalog.TIME
-AS 'babelfishpg_common', 'varchar2time'
-LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
- 
-CREATE CAST (sys.VARCHAR AS pg_catalog.TIME)
-WITH FUNCTION sys.varchar2time(sys.VARCHAR, INT4) AS IMPLICIT;
