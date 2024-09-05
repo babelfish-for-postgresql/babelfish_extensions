ba1055d6dc6ec40757a667c4a44b5d8f2282dd48
Jake Owen <jakeowen1@gmail.com>
2024-09-05 15:58:34 -0400

    Add ALTER FUNCTION support in Babelfish (#2914)
    This change adds support for ALTER FUNCTION syntax in Babelfish for functions and inline table valued functions. This commit does not include support for ALTER FUNCTION on multi-statement table valued functions. This commit builds off of the logic of ALTER PROCEDURE by adding support for return types and general function handling. AlterFunctionStmt are built in the parser, then we extract the relevent metadata to construct a new function. The OID and ACL of the original function remain the same.
    
    Task: BABEL-4937
    Signed-off-by: Jake Owen <owjco@amazon.com>

---
+++ test/JDBC/input/alter/alter-function-vu-cleanup.sql
@@ -0,0 +1,22 @@
+drop function alter_func_f1
+go
+
+drop function alter_func_f2
+go
+
+drop function alter_func_f3
+go
+
+drop function alter_func_f4
+go
+
+drop function alter_func_f5;
+go
+
+drop function alter_func_f6;
+go
+
+drop table alter_func_users
+go
+drop table alter_func_orders
+go
