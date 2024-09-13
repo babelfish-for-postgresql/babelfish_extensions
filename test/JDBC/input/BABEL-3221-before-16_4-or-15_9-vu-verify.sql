07ee9629576164d16fa2919ad12e1d17f7e9e441
yashneet vinayak <yashneet@amazon.com>
2024-09-13 09:40:49 +0000

    Creating before file for BABEl-3221
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

---
+++ test/JDBC/input/BABEL-3221-before-16_4-or-15_9-vu-verify.sql
@@ -0,0 +1,17 @@
+SELECT * FROM babel_3221_view;
+go
+
+SELECT * FROM babel_3221_view_2;
+go
+
+DROP VIEW babel_3221_view;
+go
+
+DROP VIEW babel_3221_view_2;
+go
+
+DROP TABLE babel_3221_table_1;
+go
+
+DROP TABLE babel_3221_table_2;
+go
