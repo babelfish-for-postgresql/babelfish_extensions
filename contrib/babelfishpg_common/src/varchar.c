e88e0bd9b55786d146934c0ad6b95ee3c86e95fe
manisha-deshpande <mmdeshp@amazon.com>
2024-09-18 17:45:46 +0000

    Fix uninitialised variable issue related to varchar2numeric function
    Task: BABEL-5129
    Signed-off-by: manisha-deshpande <mmdeshp@amazon.com>

--- contrib/babelfishpg_common/src/varchar.c
+++ contrib/babelfishpg_common/src/varchar.c
@@ -922,7 +922,11 @@ varchar2numeric(PG_FUNCTION_ARGS)
 	char	   *str;
 
 	str = varchar2cstring(source);
-	result = DatumGetNumeric(DirectFunctionCall1(numeric_in, CStringGetDatum(str)));
+	/* Updating numeric_in call with additional expected arguments `Elem type Oid` and `typmod` - BABEL-5129 */
+	result = DatumGetNumeric(DirectFunctionCall3(numeric_in, 
+        CStringGetDatum(str), 
+        ObjectIdGetDatum(InvalidOid),
+        Int32GetDatum(-1)));
 	pfree(str);
 	PG_RETURN_NUMERIC(result);
 }
