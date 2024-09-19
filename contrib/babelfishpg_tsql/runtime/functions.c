5616dc9588fe25be1dbaa6aa75d86df4c3b1ed1b
Anikait Agrawal <54908236+Anikait143@users.noreply.github.com>
2024-09-19 13:46:26 +0530

    Fix date functions to take into account the timezone setting (#2947)
    Issues -
    
    When the timezone parameter group setting is set to something other than the default UTC, Babelfish date
    related functions like SYSDATETIME(), SYSDATETIMEOFFSET(), GETDATE() and CURRENT_TIMESTAMP don't take it
    into account, while they should. The same issue occurs with a session-level timezone parameter.
    SYSDATETIMEOFFSET() function returns datetimeoffset which does not store timezone due to incorrect casting
    function.
    
    Changes made to fix the issues -
    
    Added appropriate CAST from TIMESTAMP to respective return types of date functions to ensure proper accounting for
    timezone setting. Added timestamptz_datetimeoffset function for converting timestamp with time zone to
    datetimeoffset. It ensures to store timezone property similar to timestamptz_datetime2 and timestamptz_datetime
    functions.
    
    Task: BABEL-5069
    Signed-off-by: Anikait Agrawal <agraani@amazon.com>

--- contrib/babelfishpg_tsql/runtime/functions.c
+++ contrib/babelfishpg_tsql/runtime/functions.c
@@ -942,21 +942,21 @@ Datum getutcdate(PG_FUNCTION_ARGS)
 
 Datum getdate_internal(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_DATUM(DirectFunctionCall2(timestamp_trunc,CStringGetTextDatum("millisecond"),
-						TimestampTzGetDatum(GetCurrentStatementStartTimestamp())));
+	PG_RETURN_DATUM(DirectFunctionCall1(common_utility_plugin_ptr->timestamptz_datetime, 
+						DirectFunctionCall2(timestamptz_trunc,CStringGetTextDatum("millisecond"),
+											TimestampTzGetDatum(GetCurrentStatementStartTimestamp()))));
 	
 }
 
 Datum sysdatetime(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_TIMESTAMPTZ(GetCurrentStatementStartTimestamp());
+	PG_RETURN_DATUM(DirectFunctionCall1(common_utility_plugin_ptr->timestamptz_datetime2, 
+							TimestampTzGetDatum(GetCurrentStatementStartTimestamp())));
 }
 
 Datum sysdatetimeoffset(PG_FUNCTION_ARGS)
 {
-	
-
-	PG_RETURN_DATUM(DirectFunctionCall1(common_utility_plugin_ptr->timestamp_datetimeoffset,
+	PG_RETURN_DATUM(DirectFunctionCall1(common_utility_plugin_ptr->timestamptz_datetimeoffset,
 							TimestampTzGetDatum(GetCurrentStatementStartTimestamp())));
 }
 
