50d5764f822bef4c42ac5cd534c6ca3a0f84a036
Yanjie Xu <yanjiex@amazon.com>
2024-09-27 00:43:41 +0800

    Support View usage for PIVOT operator (#2962)
    Support create/select/drop view on stmt with pivot operator
    
    We switched to deparsed psql-text approach for VIEW/JOIN/CTE, all
    pivot metadata will be passed to bbf_pivot function arguments, so we
    removed all funcCall/funcExpr context field related code.
    
    Task: BABEL-4673
    Signed-off-by: Yanjie Xu <yanjiex@amazon.com>

--- contrib/babelfishpg_tsql/sql/sys_functions.sql
+++ contrib/babelfishpg_tsql/sql/sys_functions.sql
@@ -5201,7 +5201,7 @@ END;
 $body$
 LANGUAGE plpgsql STABLE;
 
-CREATE OR REPLACE FUNCTION sys.bbf_pivot()
+CREATE OR REPLACE FUNCTION sys.bbf_pivot(IN src_sql TEXT, IN cat_sql TEXT, IN agg_func TEXT)
 RETURNS setof record
 AS 'babelfishpg_tsql', 'bbf_pivot'
 LANGUAGE C STABLE;
