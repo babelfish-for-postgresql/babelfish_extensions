97418470072eb50e79cd72aa49cacfc259a3381b
Sharath BP <sharatbp@amazon.com>
2024-09-27 11:07:32 +0000

    fix database property updatability

--- contrib/babelfishpg_tsql/src/databasepropertyex.c
+++ contrib/babelfishpg_tsql/src/databasepropertyex.c
@@ -28,11 +28,8 @@ databasepropertyex(PG_FUNCTION_ARGS)
 	int64_t		intVal = 0;
 	const char *dbname = text_to_cstring(PG_GETARG_TEXT_P(0));
 	const char *property = text_to_cstring(PG_GETARG_TEXT_P(1));
-	Oid			dboid = 0;
-	if(strcmp(dbname, "rdsadmin") == 0)
-	{
-		dbname = "master";
-	}
+	Oid 		dboid = get_db_id(dbname);
+	
 	dboid = get_db_id(dbname);
 
 	if (dboid == InvalidOid)
