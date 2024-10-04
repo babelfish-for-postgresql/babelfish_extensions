13f54b674c951e02d331d41400a171a041e7cd1d
Sharath BP <sharatbp@amazon.com>
2024-10-04 07:22:48 +0000

    Fix the update script sql

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.7.0--3.8.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.7.0--3.8.0.sql
@@ -9258,12 +9258,12 @@ CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'sp_tables_internal
 -- Please have this be one of the last statements executed in this upgrade script.
 DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar, varchar);
 
+CREATE OR REPLACE PROCEDURE sys.sp_reset_connection()
+AS 'babelfishpg_tsql', 'sp_reset_connection_internal' LANGUAGE C;
+GRANT EXECUTE ON PROCEDURE sys.sp_reset_connection() TO PUBLIC;
+
 -- After upgrade, always run analyze for all babelfish catalogs.
 CALL sys.analyze_babelfish_catalogs();
 
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
-
-CREATE OR REPLACE PROCEDURE sys.sp_reset_connection()
-AS 'babelfishpg_tsql', 'sp_reset_connection_internal' LANGUAGE C;
-GRANT EXECUTE ON PROCEDURE sys.sp_reset_connection() TO PUBLIC;
