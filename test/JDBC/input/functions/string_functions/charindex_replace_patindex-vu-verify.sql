be081c3af1b7ce6d8d7caa269a27218c676a3401
Tanzeel Khan <tzlkhan@amazon.com>
2024-10-18 10:30:28 +0000

    fix
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- test/JDBC/input/functions/string_functions/charindex_replace_patindex-vu-verify.sql
+++ test/JDBC/input/functions/string_functions/charindex_replace_patindex-vu-verify.sql
@@ -24,25 +24,32 @@ GO
 SELECT * FROM babel_5144_t2
 GO
 
+-- need to store output of charindex other wise explain analyze prints the entire function text
+-- which could vary for version upgrade tests
+CREATE TABLE #table_5144_result_store (id INT);
+GO
+INSERT INTO #table_5144_result_store VALUES (sys.charindex('de', 'abcḍèĎÈdedEDEabcd'))
+GO
+
 SELECT set_config('babelfishpg_tsql.explain_verbose', 'off', false)
 SELECT set_config('babelfishpg_tsql.explain_costs', 'off', false)
 SELECT set_config('babelfishpg_tsql.explain_timing', 'off', false)
 SELECT set_config('babelfishpg_tsql.explain_summary', 'off', false)
-GO
-SET BABELFISH_STATISTICS PROFILE ON;
 SELECT set_config('enable_seqscan', 'off', false);
 SELECT set_config('enable_bitmapscan', 'off', false);
+GO
+SET BABELFISH_STATISTICS PROFILE ON;
 GO
 
 SELECT * FROM babel_5144_t1 WHERE [replaced] = sys.replace('abcḍèĎÈdedEDEabcd', 'de', '##')
 GO
-SELECT * FROM babel_5144_t1 WHERE [charIndex] = sys.charindex('de', 'abcḍèĎÈdedEDEabcd');
+SELECT * FROM babel_5144_t1 WHERE [charIndex] = (SELECT id FROM #table_5144_result_store);
 GO
 SELECT * FROM babel_5144_t1 WHERE [patindex] = sys.patindex('%de%', 'abcḍèĎÈdedEDEabcd');
 GO
 SELECT * FROM babel_5144_t3 WHERE [replaced] = sys.replace('abcḍèĎÈdedEDEabcd', 'de', '##');
 GO
-SELECT * FROM babel_5144_t3 WHERE [charIndex] = sys.charindex('de', 'abcḍèĎÈdedEDEabcd');
+SELECT * FROM babel_5144_t3 WHERE [charIndex] = (SELECT id FROM #table_5144_result_store);
 GO
 SELECT * FROM babel_5144_t3 WHERE [patindex] = sys.patindex('%de%', 'abcḍèĎÈdedEDEabcd');
 GO
