2360465ac50fbc4fc2b87d595126acbe470834c6
Tanzeel Khan <tzlkhan@amazon.com>
2024-10-18 08:40:00 +0000

    fix
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.2.0--4.3.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--4.2.0--4.3.0.sql
@@ -357,14 +357,14 @@ CASE
 WHEN expressionToFind = '' THEN
     0
 WHEN start_location <= 0 THEN
-	strpos(expressionToSearch, expressionToFind)
+    strpos(expressionToSearch, expressionToFind)
 ELSE
-	CASE
-	WHEN strpos(substr(expressionToSearch, start_location), expressionToFind) = 0 THEN
-		0
-	ELSE
-		strpos(substr(expressionToSearch, start_location), expressionToFind) + start_location - 1
-	END
+    CASE
+    WHEN strpos(substr(expressionToSearch, start_location), expressionToFind) = 0 THEN
+        0
+    ELSE
+        strpos(substr(expressionToSearch, start_location), expressionToFind) + start_location - 1
+    END
 END;
 $BODY$
 STRICT
