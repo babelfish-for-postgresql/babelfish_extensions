753da8a872e3e48e8bfc25a79d3e45a636631a92
Pranav Jain <107626638+pranavJ23@users.noreply.github.com>
2024-10-22 17:53:47 +0530

    fix for storing the correct object definition in catalog (#3017)
    If a procedure contains datatype with brackets around like 'DECLARE @Sql [varchar] (8000)' , the babelfish catalog stores it as 'DECLARE @Sql varchar (8000)' i.e the brackets are not stored properly in catalog.
    Also if we add a single line comment before a query(like alter, create query) then while logging the definition of that query from catalog we do not get the single line comment, which is not correct.
    
    RCA: while parsing the query we mutate the parse tree in such a way that it modifies various things from the original string like single line comment, brackets, multi line comments.
    
    Fix: store the original query string before mutation and convert alter to create in case or alter procedure/function( batch level statement) and pass that query string.
    
    Issues Resolved: BABEL-5237, BABEL-5236, BABEL-5140
    
    Signed-off-by: Pranav Jain pranavjc@amazon.com

---
+++ test/JDBC/input/routines_definition_before_16_5-vu-prepare.sql
@@ -0,0 +1,56 @@
+create database db_routines_vu_prepare;
+go
+
+use db_routines_vu_prepare;
+go
+
+create procedure routines_vu_prepare_test_nvar(@test_nvar_a nvarchar , @test_nvar_b int = 8)
+AS
+BEGIN
+        SELECT @test_nvar_b=8;
+END
+go
+
+create schema routines_vu_prepare_sc1;
+go
+
+CREATE FUNCTION routines_vu_prepare_sc1.routines_vu_prepare_test_dec(
+    @test_dec_a INT,
+    @test_dec_b DEC(10,2),
+    @test_dec_c DEC(4,2)
+)
+RETURNS DEC(10,2)
+AS
+BEGIN
+    RETURN @test_dec_a * @test_dec_b * (1 - @test_dec_c);
+END;
+go
+
+create function routines_vu_prepare_fc1(@fc1_a nvarchar) RETURNS nvarchar AS BEGIN return @fc1_a END;
+go
+
+CREATE FUNCTION routines_vu_prepare_test_func_opt (@test_func_opt_a varchar(10))
+RETURNS INT
+ WITH RETURNS NULL ON NULL INPUT
+AS
+BEGIN
+        RETURN 2;
+END;
+go
+
+-- Multi statement Table valued Function
+CREATE FUNCTION routines_vu_prepare_test_func_tvf ()
+RETURNS @testFuncTvf table (tvf int PRIMARY KEY)
+AS
+BEGIN
+INSERT INTO @testFuncTvf VALUES (1)
+RETURN
+END;
+go
+
+-- Inline Table valued Function
+CREATE FUNCTION routines_vu_prepare_test_func_itvf ()
+RETURNS table
+AS
+RETURN (SELECT 42 AS VALUE)
+go
