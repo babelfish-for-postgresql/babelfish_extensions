7baee18ff63c0efecdc1c3c59da0e38c8a67911a
Sharu Goel <goelshar@amazon.com>
2024-10-24 14:07:20 +0000

    Initial commit for Babelfish v3.9
    This commit contains the following changes:
    - Add version info and upgrade test for 14.14 and 15.9.
    - Add upgrade schedule files for 14.15 and 15.9.
    - Update T-SQL extension version to 3.9.0 and add following upgrade scripts:
      - babelfishpg_tsql--3.8.0--3.9.0.sql.
      - babelfishpg_tsql--2.12.0--3.0.0.sql (Duplicate of babelfishpg_tsql--2.11.0--3.0.0.sql).
    
    Signed-off-by: Sharu Goel <goelshar@amazon.com>

---
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.8.0--3.9.0.sql
@@ -0,0 +1,19 @@
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION ""babelfishpg_tsql"" UPDATE TO '3.9.0'" to load this file. \quit
+
+-- add 'sys' to search path for the convenience
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+-- Please add your SQLs here
+/*
+ * Note: These SQL statements may get executed multiple times specially when some features get backpatched.
+ * So make sure that any SQL statement (DDL/DML) being added here can be executed multiple times without affecting
+ * final behaviour.
+ */
+
+
+-- After upgrade, always run analyze for all babelfish catalogs.
+CALL sys.analyze_babelfish_catalogs();
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
