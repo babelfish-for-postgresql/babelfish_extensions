97a0fb8668f88eecc63c17aab0e5ea67ac1ece98
Shard Gupta <shardga@amazon.com>
2024-11-08 15:39:06 +0000

    Update Extension version Build babelfishpg_common extension
    Signed-off-by: Shard Gupta <shardga@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "4.5.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.6.0.0"
+#define BABELFISH_VERSION_STR "5.0.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 17.0.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
