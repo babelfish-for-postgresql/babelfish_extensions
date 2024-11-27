cf22b333536556702e1ded9a86fb12df9ff0755d
Anikait Agrawal <agraani@dev-dsk-agraani-1c-be0732c3.eu-west-1.amazon.com>
2024-11-27 15:14:51 +0000

    Fix timeoffset in case of non-default timezone
    Signed-off-by: Anikait Agrawal <agraani@dev-dsk-agraani-1c-be0732c3.eu-west-1.amazon.com>

--- test/JDBC/input/ATTIMEZONE-dep-vu-verify.sql
+++ test/JDBC/input/ATTIMEZONE-dep-vu-verify.sql
@@ -15,3 +15,33 @@ GO
 
 SELECT ATTIMEZONE_dep_vu_prepare_f2()
 GO
+
+Select convert(datetime2,'2002-01-01 02:01:00.000 +00:00') AT TIME ZONE 'eastern standard time';
+GO
+
+Select convert(datetime2,'9999-12-31 15:59:59.000 +00:00') AT TIME ZONE 'Central Europe Standard Time';
+GO
+
+Select convert(datetimeoffset,'2002-01-01 02:01:00.000 +00:00') AT TIME ZONE 'eastern standard time';
+GO
+
+Select convert(datetimeoffset,'9999-12-31 15:59:59.000 +00:00') AT TIME ZONE 'Central Europe Standard Time';
+GO
+
+select set_config('timezone', 'Asia/Kolkata', false);
+GO
+
+Select convert(datetime2,'2002-01-01 02:01:00.000 +00:00') AT TIME ZONE 'eastern standard time';
+GO
+
+Select convert(datetime2,'9999-12-31 15:59:59.000 +00:00') AT TIME ZONE 'Central Europe Standard Time';
+GO
+
+Select convert(datetimeoffset,'2002-01-01 02:01:00.000 +00:00') AT TIME ZONE 'eastern standard time';
+GO
+
+Select convert(datetimeoffset,'9999-12-31 15:59:59.000 +00:00') AT TIME ZONE 'Central Europe Standard Time';
+GO
+
+select set_config('timezone', 'UTC', false);
+GO
