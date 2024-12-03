79e0995b2892d359ca1736cbe60c981b7950567d
robverschoor <rcv@amazon.com>
2024-12-03 15:00:14 +0000

    upgrade test

--- test/JDBC/input/declare_atatglobalvars_upgrade-before-16_5-vu-verify.sql
+++ test/JDBC/input/declare_atatglobalvars_upgrade-before-16_5-vu-verify.sql
@@ -1,12 +1,6 @@
--- check catalogs
-select * from pg_catalog.pg_proc where proname = 'p1_declare_atatglobalvars_upgrade'
+insert into t1_declare_atatglobalvars_upgrade values(4)
 go
-select * from sysobjects where name = 'p1_declare_atatglobalvars_upgrade'
-go
-
 exec p1_declare_atatglobalvars_upgrade
 go
 select dbo.f1_declare_atatglobalvars_upgrade() 
-go
-insert into t1_declare_atatglobalvars_upgrade values(4)
 go
