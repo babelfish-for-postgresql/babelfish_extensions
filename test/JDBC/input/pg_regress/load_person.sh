2b00091dd086dcc521839366df0aa81ebf855173
ANJU BHARTI <abanju@dev-dsk-abanju-1c-ea0b53a0.eu-west-1.amazon.com>
2024-12-12 10:45:31 +0000

    Update physical database name from jdbc_testdb to babelfish_db
    Signed-off-by: ANJU BHARTI <abanju@dev-dsk-abanju-1c-ea0b53a0.eu-west-1.amazon.com>

--- test/JDBC/input/pg_regress/load_person.sh
+++ test/JDBC/input/pg_regress/load_person.sh
@@ -1,5 +1,5 @@
 echo "-----copy person table from /data/person.data-----"
-PGPASSWORD=12345678 psql -U jdbc_user -d jdbc_testdb -a << EOF
+PGPASSWORD=12345678 psql -U jdbc_user -d babelfish_db -a << EOF
 \getenv pg_src PG_SRC
 \set filename :pg_src '/src/test/regress/data/person.data'
 COPY master_dbo.person FROM :'filename';
