45255d81422b55d2c775bf438f6040196a287bdc
Chenxiao Wang <chxwang@amazon.com>
2024-12-14 22:22:21 +0000

    Complete the test cases for json limit
    Signed-off-by: Chenxiao Wang <chxwang@amazon.com>

---
+++ test/JDBC/input/openjson/BABEL-5054-vu-verify.sql
@@ -0,0 +1,41 @@
+-- Intermediate JSON result exceeds 4000 characters in length
+-- Expect no error
+EXEC BABEL_5054_vu_prepare_p1
+GO
+DROP PROCEDURE BABEL_5054_vu_prepare_p1
+GO
+
+-- Intermediate JSON result exceeds 4000 characters in length
+-- Expect no error
+EXEC BABEL_5054_vu_prepare_p2
+GO
+DROP PROCEDURE BABEL_5054_vu_prepare_p2
+GO
+
+-- Intermediate JSON result exceeds 4000 characters in length
+-- Expect no error
+EXEC BABEL_5054_vu_prepare_p3
+GO
+DROP PROCEDURE BABEL_5054_vu_prepare_p3
+GO
+
+-- JSON in a single result cell exceeds 4000 characters in length
+-- Expect no error
+EXEC BABEL_5054_vu_prepare_p4
+GO
+DROP PROCEDURE BABEL_5054_vu_prepare_p4
+GO
+
+-- JSON in a single result cell exceeds 4000 characters in length
+-- Expect no error
+EXEC BABEL_5054_vu_prepare_p5
+GO
+DROP PROCEDURE BABEL_5054_vu_prepare_p5
+GO
+
+-- JSON in a single result cell exceeds 4000 characters in length
+-- Expect no error
+EXEC BABEL_5054_vu_prepare_p6
+GO
+DROP PROCEDURE BABEL_5054_vu_prepare_p6
+GO
