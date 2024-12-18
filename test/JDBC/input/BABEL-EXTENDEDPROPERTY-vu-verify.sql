eea3f9fc1b41c663d6469881cc20681caf84fbf8
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2024-12-18 16:24:14 +0530

    fix crash in fn_listextendedproperty (#3238) (#3275)
    Properly handle nullable column when fetching tuple from sys.babelfish_extended_properties catalog.
    
    Task : BABEL-5087
    
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- test/JDBC/input/BABEL-EXTENDEDPROPERTY-vu-verify.sql
+++ test/JDBC/input/BABEL-EXTENDEDPROPERTY-vu-verify.sql
@@ -25,6 +25,18 @@ GO
 SELECT * FROM fn_listextendedproperty(NULL, 'schema', 'babel_extended_property_v3_schema', 'type', NULL, NULL, NULL) ORDER BY objtype, objname, name, value;
 GO
 
+-- BABEL-5087
+EXEC sp_addextendedproperty
+    @name = N'MS_Description',
+    @level0type = N'SCHEMA',
+    @level0name = N'babel_extended_property_v3_schema',
+    @level1type = N'table',
+    @level1name = N'babel_extended_property_v3_table';
+GO
+
+SELECT  * FROM fn_listextendedproperty(NULL, 'SCHEMA', N'babel_extended_property_v3_schema', 'TABLE', N'babel_extended_property_v3_table', NULL, NULL);
+GO
+
 -- list all extended properties
 SELECT class, class_desc, IIF(major_id > 0, 1, 0) AS major_id, minor_id, name, value FROM sys.extended_properties ORDER BY class, class_desc, name, value;
 GO
