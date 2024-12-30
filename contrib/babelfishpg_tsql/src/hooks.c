a6ee8b97d81be1b6fc0541f6cba4fe9ae35f3759
“manisha-deshpande” <dmanisha.work@gmail.com>
2024-12-30 21:15:29 +0000

    Added recursive dfs logic to update unpivot node
    Signed-off-by: “manisha-deshpande” <“dmanisha.work@gmail.com”>

--- contrib/babelfishpg_tsql/src/hooks.c
+++ contrib/babelfishpg_tsql/src/hooks.c
@@ -139,6 +139,7 @@ static SortByNulls unique_constraint_nulls_ordering(ConstrType constraint_type,
 													SortByDir ordering);
 static void transform_pivot_clause(ParseState *pstate, SelectStmt *stmt);
 static void transform_unpivot_clause(ParseState *pstate, SelectStmt *stmt);
+static void transform_unpivot_clause_recursive(Node **node, Node **where_clause);
 /*****************************************
  * 			Commands Hooks
  *****************************************/
@@ -5327,69 +5328,79 @@ transform_pivot_clause(ParseState *pstate, SelectStmt *stmt)
 static void 
 transform_unpivot_clause(ParseState *pstate, SelectStmt *stmt)
 {
-	NullTest *null_test;
-	ColumnRef *measure_ref;
-	Node *new_where_clause;
-	List *from_clause = stmt->fromClause;
+	Node *where_clause = stmt->whereClause;
+    ListCell *lc;
 
-    if (from_clause != NULL && IsA(from_clause, List) && 
-        list_length(from_clause) > 0 && IsA(linitial(from_clause), List))
+    foreach(lc, stmt->fromClause)
     {
-        List *from_info = (List *)linitial(from_clause);
-        
-        /* Check if this is an UNPIVOT info list (should have 6 elements) */
-        if (list_length(from_info) == 6 && 
-            IsA(linitial(from_info), String) &&
-            strcmp(strVal(linitial(from_info)), "UNPIVOT") == 0)
+        transform_unpivot_clause_recursive((Node**)&(lc->ptr_value), &where_clause);
+    }
+
+    stmt->whereClause = where_clause;
+}
+
+static void transform_unpivot_clause_recursive(Node **node_ptr, Node **where_clause)
+{
+    JoinExpr *join;
+    List *unpivot_info;
+    char *unpivot_alias;
+    char *measure_col;
+    Node *transformed_node;
+    ColumnRef *measure_ref;
+    NullTest *null_test;
+    BoolExpr *bool_expr;
+
+    if (node_ptr == NULL || *node_ptr == NULL)
+        return;
+
+    if (IsA(*node_ptr, JoinExpr))
+    {
+        join = (JoinExpr *)*node_ptr;
+        transform_unpivot_clause_recursive(&join->larg, where_clause);
+        transform_unpivot_clause_recursive(&join->rarg, where_clause);
+    }
+    else if (IsA(*node_ptr, List))
+    {
+        unpivot_info = (List *)*node_ptr;
+        if (list_length(unpivot_info) == 6 &&
+            IsA(linitial(unpivot_info), String) &&
+            strcmp(strVal(linitial(unpivot_info)), "UNPIVOT") == 0)
         {
-            char *unpivot_alias;
-            char *measure_col;
-            Node *transformed_node;
+            unpivot_alias = strVal(list_nth(unpivot_info, 1));
+            measure_col = strVal(list_nth(unpivot_info, 3));
+            transformed_node = list_nth(unpivot_info, 5);
 
-            /* Extract information */
-            unpivot_alias = strVal(list_nth(from_info, 1));
-            measure_col = strVal(list_nth(from_info, 3));
-            transformed_node = list_nth(from_info, 5);
+            /* Create IS NOT NULL condition */
+            measure_ref = makeNode(ColumnRef);
+            measure_ref->fields = list_make2(makeString(unpivot_alias), makeString(measure_col));
+            measure_ref->location = -1;
 
-            /* Add IS NOT NULL condition */
-			/* Create the measure column reference */
-			measure_ref = makeNode(ColumnRef);
-			measure_ref->fields = list_make2(
-				makeString(unpivot_alias),
-				makeString(measure_col)
-			);    
-			measure_ref->location = -1;
+            null_test = makeNode(NullTest);
+            null_test->arg = (Expr *)measure_ref;
+            null_test->nulltesttype = IS_NOT_NULL;
+            null_test->argisrow = false;
+            null_test->location = -1;
 
-			/* Create IS NOT NULL test */
-			null_test = makeNode(NullTest);
-			null_test->arg = (Expr *)measure_ref;
-			null_test->nulltesttype = IS_NOT_NULL;
-			null_test->argisrow = false;
-			null_test->location = -1;
+            /* Add to WHERE clause */
+            if (*where_clause)
+            {
+                bool_expr = makeNode(BoolExpr);
+                bool_expr->boolop = AND_EXPR;
+                bool_expr->args = list_make2(*where_clause, null_test);
+                bool_expr->location = -1;
+                *where_clause = (Node *)bool_expr;
+            }
+            else
+            {
+                *where_clause = (Node *)null_test;
+            }
 
-			/* Create new where clause */
-			if (stmt->whereClause)
-			{
-				BoolExpr *bool_expr = makeNode(BoolExpr);
-				bool_expr->boolop = AND_EXPR;
-				//TODO: check if copyObject needed here?
-				bool_expr->args = list_make2(stmt->whereClause, null_test);
-				bool_expr->location = -1;
-				new_where_clause = (Node *)bool_expr;
-			}
-			else
-			{
-				new_where_clause = (Node *)null_test;
-			}
-
-			stmt->whereClause = new_where_clause;
-
-            /* Replace from_clause with transformed node */
-            stmt->fromClause = list_make1(transformed_node);
+            /* Replace UNPIVOT info with transformed node and recurse on it */
+            *node_ptr = transformed_node;
+            transform_unpivot_clause_recursive(node_ptr, where_clause);
         }
     }
 }
-
 
 static inline bool
 is_integer_type(Oid type)
