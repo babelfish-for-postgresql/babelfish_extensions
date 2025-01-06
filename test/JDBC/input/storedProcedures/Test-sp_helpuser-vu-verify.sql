660702009751cf50812786128d0f42976ebab9a8
Sharu Goel <goelshar@amazon.com>
2025-01-06 10:27:07 +0000

    print memberships

--- test/JDBC/input/storedProcedures/Test-sp_helpuser-vu-verify.sql
+++ test/JDBC/input/storedProcedures/Test-sp_helpuser-vu-verify.sql
@@ -1,6 +1,9 @@
 select * from sys.babelfish_authid_user_ext
 GO
 
+SELECT r.rolname AS role_name FROM pg_auth_members m JOIN pg_roles r ON m.roleid = r.oid JOIN pg_roles u ON m.member = u.oid WHERE u.rolname IN ('test_sp_helpuser_vu_prepare_db_dbo', 'master_dbo') ORDER BY r.rolname;
+GO
+
 -- verify
 EXEC Test_sp_helpuser_vu_prepare_check_helpuser 'dbo';
 GO
