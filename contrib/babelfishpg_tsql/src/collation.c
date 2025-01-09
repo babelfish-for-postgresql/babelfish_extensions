f0c5321a1d73db1b4e4e9a07f65b5dbe08c65086
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2025-01-09 12:47:43 +0000

    Revert "Fix cppcheck failures"
    This reverts commit fe6a88cf6b4d5961f0cf1a65a1fb1258e2ea4f04.

--- contrib/babelfishpg_tsql/src/collation.c
+++ contrib/babelfishpg_tsql/src/collation.c
@@ -327,7 +327,7 @@ transform_from_ci_as_for_likenode(Node *node, OpExpr *op, like_ilike_info_t like
 	Oid			rtypeId = exprType(rightop);
 	char	   *op_str;
 	Node	   *ret;
-	Const	   *patt = NULL;
+	Const	   *patt;
 	Const	   *prefix;
 	Operator	optup;
 	Pattern_Prefix_Status pstatus;
