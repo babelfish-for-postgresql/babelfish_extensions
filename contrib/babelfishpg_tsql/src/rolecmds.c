993742ba71c8922b620459e029b0ec46c9979d78
pranav jain <pranav23iitd@gmail.com>
2025-01-21 10:10:27 +0000

    fix
    Signed-off-by: pranav jain <pranav23iitd@gmail.com>

--- contrib/babelfishpg_tsql/src/rolecmds.c
+++ contrib/babelfishpg_tsql/src/rolecmds.c
@@ -978,8 +978,6 @@ suser_name(PG_FUNCTION_ARGS)
 
 	server_user_id = PG_ARGISNULL(0) ? InvalidOid : PG_GETARG_OID(0);
 
-	// if (server_user_id == InvalidOid)
-	// 	server_user_id = GetSessionUserId();
 	if (server_user_id == InvalidOid)
 		PG_RETURN_NULL();
 
