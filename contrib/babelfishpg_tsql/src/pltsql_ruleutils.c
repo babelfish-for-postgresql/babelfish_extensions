be421c395f5d518c6424a02ea0f71fbd7c3b42a1
pranav jain <pranav23iitd@gmail.com>
2025-01-22 18:03:10 +0000

    fixing warning in routines definition
    Signed-off-by: pranav jain <pranav23iitd@gmail.com>

--- contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
+++ contrib/babelfishpg_tsql/src/pltsql_ruleutils.c
@@ -537,7 +537,10 @@ tsql_get_functiondef(PG_FUNCTION_ARGS)
 	(void) tsql_print_function_arguments(&buf, proctup, false, true, &typmod_arr, &has_tvp);
 	/* TODO: In case of Table Valued Functions, return NULL. */
 	if (has_tvp)
+	{
+		ReleaseSysCache(proctup);
 		PG_RETURN_NULL();
+	}
 
 	if (isfunction || proc->pronargs > 0)
 		appendStringInfoString(&buf, ")");
