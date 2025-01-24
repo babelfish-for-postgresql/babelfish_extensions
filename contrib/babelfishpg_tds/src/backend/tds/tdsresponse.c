74afd33a8f2e2ba5ab22d220aa15d6494dc25e0a
Tanya Gupta <tanyagp@amazon.com>
2025-01-24 08:56:15 +0000

    updating condition Signed-off-by: Tanya Gupta <tanyagp@amazon.com>

--- contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
@@ -705,7 +705,7 @@ resolve_numeric_typmod_from_exp(Plan *plan, Node *expr)
 				outerplan = outerPlan(plan);
 
 				/* If this var referes to tuple returned by its outer plan then find the original tle from it */
-				if (plan != NULL && var->varno == OUTER_VAR)
+				if (plan && outerplan && (IsA(outerplan, Gather) || IsA(plan, Gather)))
 				{
 					Assert(plan);
 					Assert(outerplan);
