ef096c03c2bc3315e74f3c3853232019db1f9150
Jaspal Singh <ijaspals@amazon.com>
2025-01-31 11:17:22 +0000

    Support column_list in CONTAINS clause
    Previously in Babelfish, the feature of fulltext search for simple terms in CONTAINS clause was not supported for multiple columns. The TSQL syntax of Fulltext search for simple terms in CONTAINS clause over multiple columns was unidentifiable over TDS endpoint of Babelfish and would throw syntax error. To fix this, we have created a new grammar to support the TSQL multiple column Fulltext search syntax for simple terms in CONTAINS clause over TDS endpoint of Bablefish. Added a few more test cases
    
    TASK: BABEL-4239
    Signed-off-by: Jaspal Singh ijaspals@amazon.com

--- contrib/babelfishpg_tsql/src/backend_parser/gram-tsql-epilogue.y.c
+++ contrib/babelfishpg_tsql/src/backend_parser/gram-tsql-epilogue.y.c
@@ -811,26 +811,34 @@ TsqlExpressionContains(List *colId, Node *search_expr, core_yyscan_t yyscanner)
 static Node *
 makeToTSVectorFuncCall(List *colId, core_yyscan_t yyscanner, Node *pgconfig)
 {
-    List	*args;
-    Node	*replaceSpecialCharsFunc;
-    List	*replaceSpecialCharsArgs;
+	List	*args;
+	Node	*replaceSpecialCharsFunc;
+	List	*replaceSpecialCharsArgs;
 
 	/* length of list of columns passed as colId */
 	int 	len = colId->length; 	
 
 	/* inititalize the list of columns as null as we are using lappend function to append column(s)
-	 * so its necessary to have a null list to start with  
-	 */
+		* so its necessary to have a null list to start with  
+		*/
 	replaceSpecialCharsArgs = NIL;
 
 	for(int i = 0; i < len; i++)
 	{
-		Node 		*col;
-		char 		*colName = (colId->elements[i]).ptr_value;
-		char 		*schemaName = NULL;
-		char 		*tableName = NULL;
-		char 		*columnName = NULL;
-		char 		*dot1, *dot2;
+		/* Node to store the passed column as a column reference */
+		Node 	*col = NULL;
+
+		char 	*colName = (colId->elements[i]).ptr_value;
+		char 	*schemaName = NULL;
+		char 	*tableName = NULL;
+		char 	*columnName = NULL;
+		char 	*dot1, *dot2;
+
+		/* Check if the column name is NULL or empty */
+		if(colName == NULL)
+		{
+			continue;
+		}
 
 		/* Find the first and second dots in the column identifier */
 		dot1 = strchr(colName, '.');
@@ -890,8 +898,8 @@ makeToTSVectorFuncCall(List *colId, core_yyscan_t yyscanner, Node *pgconfig)
 		replaceSpecialCharsArgs = lappend(replaceSpecialCharsArgs, col);
 	}
 	
-    /* Create a function call for replace_special_chars_fts(column_list) */
-    replaceSpecialCharsFunc = (Node *) makeFuncCall(TsqlSystemFuncName("replace_special_chars_fts"), replaceSpecialCharsArgs, COERCE_EXPLICIT_CALL, -1);
+	/* Create a function call for replace_special_chars_fts(column_list) */
+	replaceSpecialCharsFunc = (Node *) makeFuncCall(TsqlSystemFuncName("replace_special_chars_fts"), replaceSpecialCharsArgs, COERCE_EXPLICIT_CALL, -1);
 
 	/* Create the final function call to_tsvector(pgconfig, replace_special_chars_fts(column_name)) */
 	args = list_make2(pgconfig, replaceSpecialCharsFunc);
--- test/JDBC/input/full_text_search/fts-contains-multicol-vu-verify.sql
+++ test/JDBC/input/full_text_search/fts-contains-multicol-vu-verify.sql
@@ -34,7 +34,11 @@ GO
 SELECT * FROM test_tb WHERE contains(txt1, '"human or quantum')
 GO
 
-CREATE FULLTEXT INDEX ON test_tb(txt1, txt2, txt3, txt4) KEY INDEX uid
+CREATE FULLTEXT INDEX ON test_tb(
+        txt1, 
+        txt2, 
+        txt3, 
+        txt4) KEY INDEX uid
 GO
 
 select * from test_tb where contains((txt1, txt2, txt4), '"Artificial Intelligence"' )
@@ -43,20 +47,21 @@ GO
 DROP FULLTEXT INDEX ON test_tb
 GO
 
-CREATE FULLTEXT INDEX ON test_tb(txt1, txt2) KEY INDEX uid
+CREATE FULLTEXT INDEX ON test_tb(
+        txt1, 
+        txt2) KEY INDEX uid
 GO
 
 SELECT * FROM test_tb WHERE contains((txt1, txt2), '"The human genome"' )
 GO
 
 -- negative test
--- should throw an error as the column txt3 is not fulltext indexed, BUT DOES NOT
-SELECT * FROM test_tb WHERE contains((txt1, txt3), '"The Industrial Revolution"')
+-- should throw an error as there exists no column with the name txt5
+SELECT * FROM test_tb WHERE contains((txt1, txt5), '"The Industrial Revolution"')
 GO
 
 -- negative test
--- should throw an error as the columns txt3 and txt4 are not fulltext indexed, BUT DOES NOT
-SELECT * FROM test_tb WHERE contains((txt4, txt3), '"The Industrial Revolution"')
+SELECT * FROM test_tb WHERE contains((txt1, txt2), '')
 GO
 
 
@@ -67,28 +72,108 @@ SELECT * FROM fts_schema.test_tb1 WHERE contains((txt, val), 'Product-A123')
 GO
 
 -- should throw an error as the previous full text index is not dropped
-CREATE FULLTEXT INDEX ON fts_schema.test_tb1(val, chr) KEY INDEX usr
-GO
-
--- negative test
--- should throw an error as chr is not fulltext indexed, BUT DOES NOT
-SELECT * FROM fts_schema.test_tb1 WHERE contains((val, chr), 'four')
+CREATE FULLTEXT INDEX ON fts_schema.test_tb1(
+        val, 
+        chr) KEY INDEX usr
 GO
 
 DROP FULLTEXT INDEX ON fts_schema.test_tb1
 GO
 
-CREATE FULLTEXT INDEX ON fts_schema.test_tb1(txt, val, chr) KEY INDEX usr
+CREATE FULLTEXT INDEX ON fts_schema.test_tb1(
+        txt, 
+        val, 
+        chr) KEY INDEX usr
 GO
 
-SELECT * FROM fts_schema.test_tb1 WHERE contains((test_tb1.txt, val), '"The quick brown fox"')
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        test_tb1.txt, 
+        val), '"brown fox"')
 GO
 
-SELECT * FROM fts_schema.test_tb1 WHERE contains((test_tb1.val, test_tb1.chr), 'TEST-999')
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        test_tb1.val, 
+        test_tb1.chr), 'TEST-999')
 GO
 
-SELECT * FROM fts_schema.test_tb1 WHERE contains((fts_schema.test_tb1.txt, test_tb1.chr), 'three')
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        fts_schema.test_tb1.txt, 
+        test_tb1.chr), 'three')
 GO
 
-SELECT * FROM fts_schema.test_tb1 WHERE contains((fts_schema.test_tb1.txt, fts_schema.test_tb1.val, chr), 'Invoice/001')
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        fts_schema.test_tb1.txt, 
+        fts_schema.test_tb1.val, 
+        chr), 'Invoice/001')
+GO
+
+-- negative test
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        txt, 
+        val, 
+        chr), '')
+GO
+
+-- A stop word used as a predicate, should give warning that only a stop word is used in predicate, but does not give the warning
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        txt, 
+        val, 
+        chr), 'The')
+GO
+
+-- Phrase made of only stop words is used as a predicate, should give warning and no result, but gives ambiguous result result
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        txt,
+        val, 
+        chr), '"To be"')
+GO
+
+-- Throws error as the column name is not valid
+SELECT * FROM fts_schema.test_tb1 WHERE contains(( 
+        ,
+        val, 
+        chr), '"and no play"')
+GO
+
+-- Throws error as NULL is passed as a column
+SELECT * FROM fts_schema.test_tb1 WHERE contains((
+        txt, 
+        NULL, 
+        chr), 'Order_789')
+GO
+
+
+-- Test cases for content_table
+CREATE UNIQUE INDEX ucid ON content_table(id)
+GO
+
+CREATE FULLTEXT INDEX ON content_table(
+        text_column,
+        char_column,
+        varchar_column,
+        nvarchar_column,
+        ntext_column,
+        nchar_column) KEY INDEX ucid
+GO
+
+SELECT * FROM content_table WHERE contains((
+        text_column, 
+        char_column, 
+        varchar_column), '"Planning phase"')
+GO
+
+-- Case sensitivity of column names
+SELECT * FROM content_table WHERE contains((
+        TEXT_COLUMN, 
+        char_column, 
+        Varchar_Column), '"Planning phase"')
+GO
+
+SELECT * FROM content_table WHERE contains((
+        text_column,
+        char_column,
+        varchar_column,
+        nvarchar_column,
+        ntext_column,
+        nchar_column), 'documentation')
 GO
