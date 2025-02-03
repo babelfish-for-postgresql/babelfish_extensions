8a1cec81262fe038d0d326d5cdf8596733675b53
“manisha-deshpande” <dmanisha.work@gmail.com>
2025-02-03 22:57:13 +0000

    Minor fixes
    Signed-off-by: “manisha-deshpande” <“dmanisha.work@gmail.com”>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,8 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "2.12.0" 
-#bump to 2.13?
+#define BABELFISH_VERSION_STR "2.12.0"
 #define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.17.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
