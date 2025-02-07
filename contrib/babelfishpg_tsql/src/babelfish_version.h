27f6f955fe6298a8f7307f340f8d792444e538eb
Manisha Malhar Rao Deshpande <125510312+manisha-deshpande@users.noreply.github.com>
2025-02-07 01:30:55 -0800

    Initial commit for Babelfish v2.13 (#3449)
    This commit contains the following changes:
    - Add version info and upgrade test for 14.16.
    - Add upgrade schedule files for 14.16.
    - Bump babelfish_version from 2.12.0 to 2.13.0
    
    Signed-off-by: manisha-deshpande <dmanisha.work@gmail.com>
    Co-authored-by: Rishabh Tanwar <ritanwar@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "2.12.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.16.0.0"
+#define BABELFISH_VERSION_STR "2.13.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.17.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
