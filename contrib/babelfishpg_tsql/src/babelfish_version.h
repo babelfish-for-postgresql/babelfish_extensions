1579c6ad81db4fb840f80a3e795818e2bd9afe93
Manisha Malhar Rao Deshpande <125510312+manisha-deshpande@users.noreply.github.com>
2025-02-07 07:18:10 -0800

    Initial commit for Babelfish v4.6 (#3454)
    This commit contains the following changes:
    - Add version info and upgrade test for 14.16, 14.17, 15.11, 15.12 and 16.7.
    - Add upgrade schedule files for 14.17, 15.12 and 16.7.
    - Bump babelfish_version from 4.5.0 to 4.6.0
    - Update T-SQL extension version to 4.6.0 and add following upgrade scripts:
      - babelfishpg_tsql--4.5.0--4.6.0.sql.
      - babelfishpg_tsql--2.13.0--3.0.0.sql (Duplicate of babelfishpg_tsql--2.12.0--3.0.0.sql).
      - babelfishpg_tsql--3.10.0--4.0.0.sql (Duplicate of babelfishpg_tsql--3.9.0--4.0.0.sql).
    - Update common extension version to 4.6.0 and add following upgrade scripts:
      - babelfishpg_common_helper--4.5.0--4.6.0.sql.
      - spatial_types--4.5.0--4.6.0.sql.
      - babelfishpg_common--3.9.0--4.0.0.sql (Duplicate of babelfishpg_common--3.8.0--4.0.0.sql).
    
    Signed-off-by: manisha-deshpande <dmanisha.work@gmail.com>
    Co-authored-by: Rishabh Tanwar <ritanwar@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "4.5.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.7.0.0"
+#define BABELFISH_VERSION_STR "4.6.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.8.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
