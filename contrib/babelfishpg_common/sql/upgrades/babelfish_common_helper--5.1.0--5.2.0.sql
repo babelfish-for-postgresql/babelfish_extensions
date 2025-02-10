b22e3aa8bfc813686752bcafeedf08010223e368
yashneet vinayak <yashneet@amazon.com>
2025-02-10 09:50:05 +0000

    adding exception handling in upgrade script
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
@@ -8,12 +8,25 @@
 SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
 
 CREATE OR REPLACE FUNCTION sys.varbinary2datetime(sys.BBF_VARBINARY)
-RETURNS DATETIME
+RETURNS sys.DATETIME
 AS 'babelfishpg_common', 'varbinary_datetime'
 LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE CAST (sys.BBF_VARBINARY AS DATETIME)
-WITH FUNCTION sys.varbinary2datetime(sys.BBF_VARBINARY) AS IMPLICIT;
+DO $$
+DECLARE 
+	sys_oid Oid;
+	pg_catalog_oid Oid;
+	bbf_varbinary_oid Oid;
+	datetime_oid Oid;
+BEGIN
+	sys_oid := (SELECT oid FROM pg_namespace WHERE pg_namespace.nspname ='sys');
+	bbf_varbinary_oid := (SELECT oid FROM pg_type WHERE pg_type.typname ='bbf_varbinary' AND pg_type.typnamespace = sys_oid);
+	datetime_oid := (SELECT oid FROM pg_type WHERE pg_type.typname ='datetime' AND pg_type.typnamespace = sys_oid);
+	IF (SELECT COUNT(*) FROM pg_cast WHERE pg_cast.castsource = bbf_varbinary_oid AND pg_cast.casttarget = datetime_oid) = 0 THEN
+		CREATE CAST (sys.BBF_VARBINARY AS sys.DATETIME)
+		WITH FUNCTION sys.varbinary2datetime(sys.BBF_VARBINARY) AS IMPLICIT;
+	END IF;
+END $$;
 
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
