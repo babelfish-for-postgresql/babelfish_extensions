1500bc8aa0831314a4078fcb10f22cc0ef8eeacc
Harsh Dubey <harshdu@amazon.com>
2025-02-14 15:22:07 +0000

    Support for sp_xml_preparedocument and sp_xml_removedocument procedures
    Signed-off-by: Harsh Dubey <harshdu@amazon.com>

--- test/JDBC/input/xml/sp_xml_preparedocument-vu-verify.sql
+++ test/JDBC/input/xml/sp_xml_preparedocument-vu-verify.sql
@@ -102,8 +102,7 @@ END;
 SELECT @hdoc as handle;
 GO
 
-
---When database is changed
+--When database is changed in the session
 CREATE TABLE sp_xml_preparedocument_HandleStore (handle_value int);
 GO
 
@@ -143,4 +142,128 @@ GO
 DROP TABLE sp_xml_preparedocument_HandleStore;
 GO
 
+-- Variable declaration and initialisation
+DECLARE @hdoc INT;
+DECLARE @xml_text varchar(100) = '<root/>';
+DECLARE @xpath_namespaces varchar(100) = '<root xmlns:ns1="http://example.com/ns1"/>';
+EXEC sp_xml_preparedocument @hdoc OUTPUT, @xml_text, @xpath_namespaces;
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+--For complex xml texts 1
+DECLARE @hdoc INT;
+EXEC sp_xml_preparedocument @hdoc OUTPUT, 
+'<organization>
+    <department id="D1">
+        <name>IT</name>
+        <employees>
+            <employee id="E1">
+                <name>John Doe</name>
+                <title>Developer</title>
+                <skills>
+                    <skill level="expert">SQL</skill>
+                    <skill level="intermediate">Java</skill>
+                </skills>
+            </employee>
+        </employees>
+    </department>
+</organization>';
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+-- For complex xml text2
+DECLARE @hdoc INT;
+DECLARE @doc VARCHAR(1000);
+SET @doc = '
+<ROOT>
+<Customer CustomerID="VINET" ContactName="Paul Henriot">
+   <Order CustomerID="VINET" EmployeeID="5" OrderDate="1996-07-04T00:00:00">
+      <OrderDetail OrderID="10248" ProductID="11" Quantity="12"/>
+      <OrderDetail OrderID="10248" ProductID="42" Quantity="10"/>
+   </Order>
+</Customer>
+<Customer CustomerID="LILAS" ContactName="Carlos Gonzlez">
+   <Order CustomerID="LILAS" EmployeeID="3" OrderDate="1996-08-16T00:00:00">
+      <OrderDetail OrderID="10283" ProductID="72" Quantity="3"/>
+   </Order>
+</Customer>
+</ROOT>';
+EXEC sp_xml_preparedocument @hdoc OUTPUT, @doc;
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+-- For complex namespaces
+DECLARE @hdoc INT;
+EXEC sp_xml_preparedocument @hdoc OUTPUT, '',
+'<root 
+    xmlns:ns1="http://example.com/ns1" 
+    xmlns:ns2="http://example.com/ns2" 
+    xmlns:ns3="http://example.com/ns3">
+    <ns1:element>
+        <ns2:subelement>
+            <ns3:data>value</ns3:data>
+        </ns2:subelement>
+    </ns1:element>
+</root>';
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+--When given xml text length is too large (above max limit)
+DECLARE @hdoc int;
+DECLARE @xml_text varchar(max) = '<root>' + repeat('<child></child>', 200000000) + '</root>';
+EXEC sp_xml_preparedocument @hdoc output, @xml_text;
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+-- When namespace length is too large (above max limit)
+DECLARE @hdoc int;
+DECLARE @namespace_text varchar(max) = '<root ' + repeat('xmlns:ns1="http://example.com/namespace/1" ', 200000000) + '><element/></root>';
+ EXEC sp_xml_preparedocument @hdoc output, NULL, @namespace_text;
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+--If we try to create the same table name and then run the query
+CREATE TABLE sys.babelfish_xml_handles (
+    handle int NOT NULL PRIMARY KEY,
+    doc_id int NOT NULL,
+    doc_text xml
+);
+INSERT INTO sys.babelfish_xml_handles (handle, doc_id, doc_text) VALUES
+(1, 101, '<a>1</a>'),
+(2, 102, '<b>2</b>');
+GO
+
+DECLARE @hdoc INT;
+EXEC sp_xml_preparedocument @hdoc OUTPUT,'<root/>';
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+--Test with parameter by name syntax
+DECLARE @z int;
+EXEC sp_xml_preparedocument @hdoc = @z OUTPUT, @xmltext = '<root><child>value</child></root>', @xpath_namespaces = '';
+SELECT @z as handle;
+EXEC sp_xml_removedocument @z;
+GO
+
+DECLARE @hdoc int;
+EXEC sp_xml_preparedocument @hdoc OUTPUT, 
+                           @xmltext = '<root><child>value</child></root>', 
+                           @xpath_namespaces = '';
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
+
+-- Double quoted string 
+DECLARE @hdoc INT;
+EXEC sp_xml_preparedocument @hdoc OUTPUT, "<root><child>value</child></root>";
+SELECT @hdoc as handle;
+EXEC sp_xml_removedocument @hdoc;
+GO
 
