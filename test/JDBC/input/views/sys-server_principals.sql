59a26c74718a42fbf05eb4a4c39dd52406db839a
pranav jain <pranav23iitd@gmail.com>
2025-02-19 09:06:08 +0000

    ading tests
    Signed-off-by: pranav jain <pranav23iitd@gmail.com>

--- test/JDBC/input/views/sys-server_principals.sql
+++ test/JDBC/input/views/sys-server_principals.sql
@@ -50,3 +50,15 @@ GO
 
 select suser_sid('public')
 GO
+
+select name, principal_id, sid, suser_name(principal_id) as suser_name, suser_sid(name) as suser_sid, suser_sname(sid) as suser_sname, suser_sname(suser_sid(name)) as name2 from sys.server_principals where name = 'pUbLiC'
+GO
+
+select suser_name(002)
+GO
+
+select suser_name(0000002)
+GO
+
+select suser_sid('PuBlIC')
+GO
