86682204170037db7b05834042d825014cfe64b0
Zhibai Song <szh@amazon.com>
2025-03-11 19:41:21 +0000

    Update

--- test/JDBC/input/alter/alter-mvu-test-vu-cleanup.sql
+++ test/JDBC/input/alter/alter-mvu-test-vu-cleanup.sql
@@ -1,16 +1,22 @@
 use master;
 go
 
+SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_func_mvu%' and nspname = 'alter_func_db_dbo'
+go
+
+SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_proc%' and nspname = 'alter_proc_db_dbo'
+go
+
 drop database alter_proc_db
 go
 
 drop database alter_func_db
 go
 
-SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_func_mvu%'
+SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_func_mvu%' and nspname = 'alter_func_db_dbo'
 go
 
-SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_proc%'
+SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_proc%' and nspname = 'alter_proc_db_dbo'
 go
 
 -- psql currentSchema=master_dbo,public
