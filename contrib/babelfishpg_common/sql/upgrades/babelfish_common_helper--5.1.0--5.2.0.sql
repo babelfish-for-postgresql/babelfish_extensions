407cc2b40ea21639cea531e641bf03f6a63bde35
Herambh Shah <herambhs@amazon.com>
2025-03-11 19:38:16 +0000

    Select "for json" output contains Backsla- Resolve improper oid handling in FOR JSON logic
    Fix: JSON_QUERY with FOR JSON quote escaping issue
    
    - Resolve improper oid handling in FOR JSON logic
    - Fix double escaping of quotes escaping issues
    - Introduce new domain "nvarchar_json" for all json function output
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Updated "For Json" logic for json type and test files
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Updated minor verison upgrade scripts and failing test case file
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    drop sys_functions to update return type to nvarchar_json
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Alter return type for tsql_query_to_json_ffunc
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Corrected Alter command
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing upgrade script
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing Upgrade script
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing MVU issue
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing MVU script
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
     Updated case_insensitive_collation files
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing MVU upgrade
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
     Testing if changing return type of only json_query works
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
     Testing MVU test failure
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
     Testing MVU failure
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Removed Nvarchar_json return from "for JSON" sys function
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing PR check failure
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Added collation for nvarchar_json in ISC-domain-verify
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing leak wearing by pushing empty commit
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    Testing LEAKAGE_FAILURE for PR checks
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
@@ -4,3 +4,13 @@
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '5.2.0'" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+-- For JSON Functions
+SET enable_domain_typmod = TRUE;
+CREATE DOMAIN sys.NVARCHAR_JSON AS sys.NVARCHAR;
+RESET enable_domain_typmod;
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
