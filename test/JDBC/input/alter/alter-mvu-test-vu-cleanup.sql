857602a333e53cdc1711879d289cfd8e7dab6e3c
Zhibai Song <szh@amazon.com>
2025-03-11 18:27:10 +0000

    Add test case for checking dangling data

--- test/JDBC/input/alter/alter-mvu-test-vu-cleanup.sql
+++ test/JDBC/input/alter/alter-mvu-test-vu-cleanup.sql
@@ -6,3 +6,9 @@ go
 
 drop database alter_func_db
 go
+
+SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_func_mvu%'
+go
+
+SELECT funcname, nspname FROM sys.babelfish_function_ext WHERE funcname LIKE 'alter_proc%'
+go
