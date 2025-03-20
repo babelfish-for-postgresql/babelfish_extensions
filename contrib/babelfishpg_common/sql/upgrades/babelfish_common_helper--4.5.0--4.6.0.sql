fc53a214ffbdd51d1a857d61c670886ab9af4bc6
yashneet vinayak <yashneet@amazon.com>
2025-03-20 15:34:56 +0000

    adding before test files to schedule files
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--4.5.0--4.6.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--4.5.0--4.6.0.sql
@@ -12,5 +12,13 @@ SET enable_domain_typmod = TRUE;
 CREATE DOMAIN sys.NVARCHAR_JSON AS sys.NVARCHAR;
 RESET enable_domain_typmod;
 
+CREATE OR REPLACE FUNCTION sys.varbinary2datetime(sys.BBF_VARBINARY)
+RETURNS sys.DATETIME
+AS 'babelfishpg_common', 'varbinary_datetime'
+LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE CAST (sys.BBF_VARBINARY AS sys.DATETIME)
+WITH FUNCTION sys.varbinary2datetime(sys.BBF_VARBINARY) AS IMPLICIT;
+
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
