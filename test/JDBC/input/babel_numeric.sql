733db4a93898b350ff2842586d3984fd6d482859
Rohit Bhagat <rohitbgt@amazon.com>
2025-03-20 03:50:59 +0000

    moved tests to babel_numeric test
    Signed-off-by: Rohit Bhagat <rohitbgt@amazon.com>

--- test/JDBC/input/babel_numeric.sql
+++ test/JDBC/input/babel_numeric.sql
@@ -254,3 +254,25 @@ drop type decimal_7_2;
 drop type numeric_18_2;
 drop type numeric_7_2;
 go
+
+-- Tests for BABEL-5655
+select 0 as a, 0.0 as b, 0.00 as c, 0.000 as d into babel_5655_t1
+go
+
+select TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, NUMERIC_PRECISION, NUMERIC_PRECISION_RADIX, NUMERIC_SCALE
+from information_schema.columns 
+where TABLE_NAME = 'babel_5655_t1' order by COLUMN_NAME
+go
+
+select 0.00 * cast(1.23 as numeric(10, 3))
+go
+
+select 0.00 * cast(1.23 as numeric(10, 2))
+go
+
+select 0.0000 * cast(1.23 as numeric(10, 2))
+go
+
+drop table babel_5655_t1
+go
+
