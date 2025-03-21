3d24555d15500ad76b1fee285b4c3cd22019ae34
Shreya Rai <shreyaxr@amazon.com>
2025-03-21 06:16:54 +0000

    added changes to upgrade script
    Signed-off-by: Shreya Rai <shreyaxr@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
@@ -4,3 +4,9 @@
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '5.2.0'" to load this file. \quit
+
+CREATE CAST (INT4 AS sys.BBF_VARBINARY)
+WITH FUNCTION sys.int4varbinary (INT4, integer, boolean) AS IMPLICIT;
+
+CREATE CAST (REAL AS sys.BBF_VARBINARY)
+WITH FUNCTION sys.float4varbinary (REAL, integer, boolean) AS IMPLICIT;
