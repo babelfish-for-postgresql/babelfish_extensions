612fb947778ea006b4dd0c174e401956e24436ba
Shreya Rai <shreyaxr@amazon.com>
2025-03-21 07:49:55 +0000

    upgrade script
    Signed-off-by: Shreya Rai <shreyaxr@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.1.0--5.2.0.sql
@@ -4,14 +4,6 @@
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '5.2.0'" to load this file. \quit
-
-CREATE CAST (INT4 AS sys.BBF_VARBINARY)
-WITH FUNCTION sys.int4varbinary (INT4, integer, boolean) AS IMPLICIT;
-
-CREATE CAST (REAL AS sys.BBF_VARBINARY)
-WITH FUNCTION sys.float4varbinary (REAL, integer, boolean) AS IMPLICIT;
-SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
-
 -- For JSON Functions
 DO
 $body$
@@ -27,6 +19,12 @@ BEGIN
     END IF;
 END
 $body$;
+
+CREATE CAST (INT4 AS sys.BBF_VARBINARY)
+WITH FUNCTION sys.int4varbinary (INT4, integer, boolean) AS IMPLICIT;
+
+CREATE CAST (REAL AS sys.BBF_VARBINARY)
+WITH FUNCTION sys.float4varbinary (REAL, integer, boolean) AS IMPLICIT;
 
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
