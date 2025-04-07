0b11daf41ef79c5d495a7d07124bf2005eb53450
Dipesh Dhameliya <dddhamel@amazon.com>
2025-04-07 13:00:46 +0000

    Use relpersisrence to determine temp table

--- contrib/babelfishpg_tsql/src/bbf_parallel_query.c
+++ contrib/babelfishpg_tsql/src/bbf_parallel_query.c
@@ -82,7 +82,7 @@ bbf_ExecInitParallelPlan(EState *estate, ParallelContext *pcxt, bool estimate)
 			RangeTblEntry *rte = (RangeTblEntry *) lfirst(lc);
 			if (rte->rtekind == RTE_RELATION &&
 				OidIsValid(rte->relid) &&
-				get_ENR_withoid(currentQueryEnv, rte->relid, ENR_TSQL_TEMP) != NULL)
+				get_rel_persistence(rte->relid) == RELPERSISTENCE_TEMP)
 			{
 				/* probably (re)do perm check */
 				RTEPermissionInfo *perminfo = getRTEPermissionInfo(estate->es_plannedstmt->permInfos, rte);
