c645df44debba885628b6d8ea4fb7672be5a075f
Anikait Agrawal <54908236+Anikait143@users.noreply.github.com>
2025-04-15 11:18:16 +0530

    Fix TSQL upgrade scripts (#3687)
    This commit contains the following changes:
    
    Fix content of babelfishpg_tsql--2.14.0--3.0.0.sql (Duplicate of babelfishpg_tsql--2.13.0--3.0.0.sql)
    Signed-off-by: Anikait Agrawal agraani@amazon.com

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.10.0--3.11.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--3.10.0--3.11.0.sql
@@ -5,6 +5,15 @@
 SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
 
 -- please add your SQL here
+/*
+ * Note: These SQL statements may get executed multiple times specially when some features get backpatched.
+ * So make sure that any SQL statement (DDL/DML) being added here can be executed multiple times without affecting
+ * final behaviour.
+ */
+
+
+-- After upgrade, always run analyze for all babelfish catalogs.
+CALL sys.analyze_babelfish_catalogs();
 
 -- Reset search_path to not affect any subsequent scripts
 SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
