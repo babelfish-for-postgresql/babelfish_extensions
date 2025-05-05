a4ba90d3aa21afc7aa2af2835d28978309ae04ec
yashneet vinayak <yashneet@amazon.com>
2025-05-05 11:28:12 +0000

    adding test cases
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

--- test/JDBC/input/numericOverflow.sql
+++ test/JDBC/input/numericOverflow.sql
@@ -71,3 +71,84 @@ go
 
 drop table num_zero;
 go
+
+-- Create test table with different integer types
+CREATE TABLE variable_test (
+    size_int INT,
+    size_smallint SMALLINT,
+    size_bigint BIGINT
+);
+GO
+
+-- Test data insertion
+INSERT INTO variable_test VALUES 
+(707072, 32767, 9223372036854775807);
+GO
+
+-- Basic multiplication
+SELECT 
+    size_int * 8.00 as int_mult,
+    size_smallint * 8.00 as smallint_mult,
+    size_bigint * 8.00 as bigint_mult
+FROM variable_test;
+GO
+
+-- Decimal multiplication
+SELECT 
+    size_int * 1.23 as int_mult,
+    size_smallint * 1.23 as smallint_mult,
+    size_bigint * 1.23 as bigint_mult
+FROM variable_test;
+GO
+
+-- Small decimal multiplication
+SELECT 
+    size_int * 0.01 as int_mult,
+    size_smallint * 0.01 as smallint_mult,
+    size_bigint * 0.01 as bigint_mult
+FROM variable_test;
+GO
+
+-- Large decimal multiplication
+SELECT 
+    size_int * 999999.99 as int_mult,
+    size_smallint * 999999.99 as smallint_mult,
+    size_bigint * 999999.99 as bigint_mult
+FROM variable_test;
+GO
+
+-- Basic division
+SELECT 
+    size_int / 2.00 as int_div,
+    size_smallint / 2.00 as smallint_div,
+    size_bigint / 2.00 as bigint_div
+FROM variable_test;
+GO
+
+-- Decimal division
+SELECT 
+    size_int / 1.23 as int_div,
+    size_smallint / 1.23 as smallint_div,
+    size_bigint / 1.23 as bigint_div
+FROM variable_test;
+GO
+
+-- Small decimal division
+SELECT 
+    size_int / 0.01 as int_div,
+    size_smallint / 0.01 as smallint_div,
+    size_bigint / 0.01 as bigint_div
+FROM variable_test;
+GO
+
+-- Complex calculations
+SELECT 
+    size_int * 1.23 / 4.56 as int_complex,
+    size_smallint * 1.23 / 4.56 as smallint_complex,
+    size_bigint * 1.23 / 4.56 as bigint_complex
+FROM variable_test;
+GO
+
+-- Cleanup
+DROP TABLE variable_test;
+GO
