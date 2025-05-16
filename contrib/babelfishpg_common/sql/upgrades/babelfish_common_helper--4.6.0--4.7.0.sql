c7f34f03e355fc518bac340abb68a69a6acefa29
Sumit Jaiswal <54075285+sumitj824@users.noreply.github.com>
2025-05-16 14:51:32 +0530

    Add operator class between int and numeric to support index scan (#3764)
    Earlier, index scan would not be chosen by the optimizer when filtering between
    int and numeric types if the index was created on a numeric column. While index
    scans worked when the index was on an integer column (due to existing operator
    class as part of int operator family), the reverse case wasn't supported.
    
    This commit fixes this issue by adding a new operator class as part of the numeric
    operator family which allows the optimizer to utilize indexes on numeric columns when
    filtering with integer values, improving query performance in these scenarios.
    
    This changes also requires engine changes (babelfish-for-postgresql/postgresql_modified_for_babelfish#578)
    in order to dump these Babelfish defined operator class correctly during pg_upgrade.
    
    Task: BABEL-5648
    Signed-off-by: Sumit Jaiswal <sumiji@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--4.6.0--4.7.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--4.6.0--4.7.0.sql
@@ -4,3 +4,116 @@
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '4.7.0'" to load this file. \quit
+
+SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
+
+-- Operator class for numeric_ops to incorporate various operator between numeric and int4 for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'numeric_int4_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.numeric_int4_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (numeric, int4),
+            OPERATOR 2 sys.<= (numeric, int4),
+            OPERATOR 3 sys.= (numeric, int4),
+            OPERATOR 4 sys.>= (numeric, int4),
+            OPERATOR 5 sys.> (numeric, int4),
+            FUNCTION 1 sys.numeric_int4_cmp(numeric, int4);
+    END IF;
+END $$;
+
+-- Operator class for numeric_ops to incorporate various operator between int4 and numeric for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'int4_numeric_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.int4_numeric_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (int4, numeric),
+            OPERATOR 2 sys.<= (int4, numeric),
+            OPERATOR 3 sys.= (int4, numeric),
+            OPERATOR 4 sys.>= (int4, numeric),
+            OPERATOR 5 sys.> (int4, numeric),
+            FUNCTION 1 sys.int4_numeric_cmp(int4, numeric);
+    END IF;
+END $$;
+
+-- Operator class for numeric_ops to incorporate various operator between numeric and int2 for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'numeric_int2_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.numeric_int2_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (numeric, int2),
+            OPERATOR 2 sys.<= (numeric, int2),
+            OPERATOR 3 sys.= (numeric, int2),
+            OPERATOR 4 sys.>= (numeric, int2),
+            OPERATOR 5 sys.> (numeric, int2),
+            FUNCTION 1 sys.numeric_int2_cmp(numeric, int2);
+    END IF;
+END $$;
+
+-- Operator class for numeric_ops to incorporate various operator between int2 and numeric for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'int2_numeric_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.int2_numeric_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (int2, numeric),
+            OPERATOR 2 sys.<= (int2, numeric),
+            OPERATOR 3 sys.= (int2, numeric),
+            OPERATOR 4 sys.>= (int2, numeric),
+            OPERATOR 5 sys.> (int2, numeric),
+            FUNCTION 1 sys.int2_numeric_cmp(int2, numeric);
+    END IF;
+END $$;
+
+-- Operator class for numeric_ops to incorporate various operator int8 and numeric for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'int8_numeric_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.int8_numeric_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (int8, numeric),
+            OPERATOR 2 sys.<= (int8, numeric),
+            OPERATOR 3 sys.= (int8, numeric),
+            OPERATOR 4 sys.>= (int8, numeric),
+            OPERATOR 5 sys.> (int8, numeric),
+            FUNCTION 1 sys.int8_numeric_cmp(int8, numeric);
+    END IF;
+END $$;
+
+-- Operator class for numeric_ops to incorporate various operator between numeric and int8 for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'numeric_int8_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.numeric_int8_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (numeric, int8),
+            OPERATOR 2 sys.<= (numeric, int8),
+            OPERATOR 3 sys.= (numeric, int8),
+            OPERATOR 4 sys.>= (numeric, int8),
+            OPERATOR 5 sys.> (numeric, int8),
+            FUNCTION 1 sys.numeric_int8_cmp(numeric, int8);
+    END IF;
+END $$;
+
+-- Reset search_path to not affect any subsequent scripts
+SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
