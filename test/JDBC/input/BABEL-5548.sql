54cae6b6516c88ae5414df667dadec25c4078890
Zhibai Song <szh@amazon.com>
2025-06-13 02:33:43 +0000

    Update test cases

--- test/JDBC/input/BABEL-5548.sql
+++ test/JDBC/input/BABEL-5548.sql
@@ -11,8 +11,11 @@ go
 select dbo..a from t5548
 go
 
-select ...a from .t5548
+select ...a from t5548
 GO
+
+select ..a from t5548
+go
 
 select .t5548.a from t5548;
 GO
@@ -21,6 +24,21 @@ select ..t5548.a from t5548;
 go
 
 select .dbo.t5548.a from t5548;
+GO
+
+CREATE TABLE GEOSPATIALPOINTEMPTYdt (geom geometry, geog geography);
+GO
+
+select .GEOSPATIALPOINTEMPTYdt.geom.STAsText() from GEOSPATIALPOINTEMPTYdt
+GO
+
+select ..geom.STAsText() from GEOSPATIALPOINTEMPTYdt
+GO
+
+select dbo.GEOSPATIALPOINTEMPTYdt.geom.STAsText() from GEOSPATIALPOINTEMPTYdt
+GO
+
+drop table GEOSPATIALPOINTEMPTYdt
 GO
 
 drop table t5548;
