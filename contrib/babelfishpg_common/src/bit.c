2f59e35cdf3da2afd56ab942e1ccdbd760b473c7
Ayush Shah <ayushdsh@amazon.com>
2025-06-19 09:59:27 +0000

    Handling overflow and incorrect outputs in case of certain money and smallmoney arithmetic and math functions.
    There were certain issues found both w.r.t overflow handling and certain functions and operations giving incorrect result for money and smallmoney datatype. This commit addresses those issues.
    
    Issue #1: Overflow of smallmoney arithmetic leading to TDS hang.
    Issue #2: Unhandled overflow cases leading to incorrect output.
    Issue #3: Fixeddecimal operators being resolved for smallmoney functions and operations leading to incorrect results.
    
    Task: BABEL-5745, BABEL-5757, BABEL-5756, BABEL-5747, BABEL-5754
    
    Signed-off-by: Ayush Shah <ayushdsh@amazon.com>

--- contrib/babelfishpg_common/src/bit.c
+++ contrib/babelfishpg_common/src/bit.c
@@ -75,12 +75,6 @@ PG_FUNCTION_INFO_V1(bitsmallmoneymi);
 PG_FUNCTION_INFO_V1(bitsmallmoneymul);
 PG_FUNCTION_INFO_V1(bitsmallmoneydiv);
 
-/* Arithmetic operations bit */
-PG_FUNCTION_INFO_V1(bitpl);
-PG_FUNCTION_INFO_V1(bitmi);
-PG_FUNCTION_INFO_V1(bitmul);
-PG_FUNCTION_INFO_V1(bitdiv);
-
 /* Arithmetic operations float, bit */
 PG_FUNCTION_INFO_V1(floatbitpl);
 PG_FUNCTION_INFO_V1(floatbitmi);
@@ -760,51 +754,6 @@ bitsmallmoneydiv(PG_FUNCTION_ARGS)
 				 errmsg("smallmoney out of range")));
 
 	PG_RETURN_INT64(result);
-}
-
-Datum
-bitpl(PG_FUNCTION_ARGS)
-{
-	bool        arg1 = PG_GETARG_BOOL(0);
-	bool        arg2 = PG_GETARG_BOOL(1);
-
-	PG_RETURN_BOOL(arg1 || arg2);
-}
-
-Datum
-bitmi(PG_FUNCTION_ARGS)
-{
-	bool        arg1 = PG_GETARG_BOOL(0);
-	bool        arg2 = PG_GETARG_BOOL(1);
-
-	PG_RETURN_BOOL(arg1 - arg2);
-}
-
-Datum
-bitmul(PG_FUNCTION_ARGS)
-{
-	bool        arg1 = PG_GETARG_BOOL(0);
-	bool        arg2 = PG_GETARG_BOOL(1);
-
-	PG_RETURN_BOOL(arg1 && arg2);
-}
-
-Datum
-bitdiv(PG_FUNCTION_ARGS)
-{
-	bool        arg1 = PG_GETARG_BOOL(0);
-	bool        arg2 = PG_GETARG_BOOL(1);
-	
-	if (!arg2)
-	{
-		ereport(ERROR,
-				(errcode(ERRCODE_DIVISION_BY_ZERO),
-				 errmsg("division by zero")));
-		/* ensure compiler realizes we mustn't reach the division (gcc bug) */
-		PG_RETURN_NULL();
-	}
-
-	PG_RETURN_BOOL(arg1);
 }
 
 Datum
