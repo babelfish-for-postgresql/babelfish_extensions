ea977eacf7499b064b3b81d2aa7cdd6bc82446a2
Yvinayak07 <100944709+Yvinayak07@users.noreply.github.com>
2025-07-04 05:04:45 +0530

    Initial commit for Babelfish v2.15 (#3916)
    * Update Github action scripts
    
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>
    
    * Create upgrade test folders (new schedule files)
    
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>
    
    * Bump Babelfish version string
    
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>
    
    * update Upgrade verison in upgrade-test-configuration.yml
    
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    
    ---------
    
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>
    Signed-off-by: Herambh Shah <herambhs@amazon.com>
    Co-authored-by: yashneet vinayak <yashneet@amazon.com>
    Co-authored-by: Herambh Shah <herambhs@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "2.14.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.18.0.0"
+#define BABELFISH_VERSION_STR "2.15.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.19.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
