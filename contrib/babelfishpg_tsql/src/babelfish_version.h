1885800bae0efe2c29ecc5f9a73cc019e261dab5
yashneet vinayak <yashneet@amazon.com>
2025-07-03 11:00:57 +0000

    Bump Babelfish version string
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "5.3.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 17.5.0.0"
+#define BABELFISH_VERSION_STR "5.4.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 17.6.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
