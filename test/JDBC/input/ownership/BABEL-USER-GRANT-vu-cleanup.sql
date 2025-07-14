ad38ed3d38fb0593de84dbf19b9641ee0ea2ee13
HarshLunagariya <40052763+HarshLunagariya@users.noreply.github.com>
2025-07-14 19:47:24 +0530

    [OSS-ONLY] Refactor BABEL-USER test to separate the internal grant testcases (#3954) (#3961)
    Refactor BABEL-USER test to separate the internal grant testcases because there can be scenarios where grantor on internal role membership can be different based on Babelfish initialization.
    
    Signed-off-by: Harsh Lunagariya <lunharsh@amazon.com>

---
+++ test/JDBC/input/ownership/BABEL-USER-GRANT-vu-cleanup.sql
@@ -0,0 +1,20 @@
+USE babel_5890_db
+GO
+
+DROP PROC babel_role_vu_prepare_role_mapping
+GO
+
+DROP USER babel_5890_role_vu_prepare_r2;
+GO
+
+USE master
+GO
+
+DROP LOGIN babel_5890_role_vu_prepare_r1;
+GO
+
+DROP LOGIN babel_5890_role_vu_prepare_r2;
+GO
+
+DROP DATABASE babel_5890_db
+GO
