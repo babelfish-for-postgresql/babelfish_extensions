270ada001ad88b4de17805ad64af024f5c6864a2
Tanya Gupta <59756970+tanyagupta17@users.noreply.github.com>
2025-07-25 06:31:19 +0530

    Adding operator class between numeric and fixeddecimal to support index (#3988)
    Earlier in Babelfish, index scan would not be chosen by the optimizer when filtering between numeric and money/smallmoney/fixeddecimal types if the index was created on a numeric column.
    
    This commit fixes this issue by adding a new operator class as part of the numeric operator family which allows the optimizer to utilize indexes on numeric columns when filtering with money/smallmoney/fixeddecimal values, improving query performance in these scenarios.
    
    Cherry-pick PR (5X): #3970
    Engine PR : babelfish-for-postgresql/postgresql_modified_for_babelfish#609
    
    Issues Resolved
    BABEL-5959
    
    Signed-off-by: Tanya Gupta tanyagp@amazon.com

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--3.10.0--3.11.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--3.10.0--3.11.0.sql
@@ -1,3 +1,41 @@
 ------------------------------------------------------------------------------
 ---- Include changes related to other datatypes except spatial types here ----
 ------------------------------------------------------------------------------
+
+-- Operator class for numeric_ops to incorporate various operator between numeric and fixeddecimal for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'numeric_fixeddecimal_cmp_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.numeric_fixeddecimal_cmp_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (numeric, sys.fixeddecimal),
+            OPERATOR 2 sys.<= (numeric, sys.fixeddecimal),
+            OPERATOR 3 sys.= (numeric, sys.fixeddecimal),
+            OPERATOR 4 sys.>= (numeric, sys.fixeddecimal),
+            OPERATOR 5 sys.> (numeric, sys.fixeddecimal),
+            FUNCTION 1 sys.numeric_fixeddecimal_cmp(numeric, sys.fixeddecimal);
+    END IF;
+END $$;
+
+
+-- Operator class for numeric_ops to incorporate various operator between fixeddecimal and numeric for Index scan
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_opclass opc JOIN pg_opfamily opf ON opc.opcfamily = opf.oid 
+        WHERE opc.opcname = 'fixeddecimal_numeric_cmp_ops' AND opc.opcnamespace = 'sys'::regnamespace
+        AND opf.opfname = 'numeric_ops') THEN
+
+        CREATE OPERATOR CLASS sys.fixeddecimal_numeric_cmp_ops FOR TYPE numeric
+          USING btree FAMILY numeric_ops AS
+            OPERATOR 1 sys.< (sys.fixeddecimal, numeric),
+            OPERATOR 2 sys.<= (sys.fixeddecimal, numeric),
+            OPERATOR 3 sys.= (sys.fixeddecimal, numeric),
+            OPERATOR 4 sys.>= (sys.fixeddecimal, numeric),
+            OPERATOR 5 sys.> (sys.fixeddecimal, numeric),
+            FUNCTION 1 sys.fixeddecimal_numeric_cmp(sys.fixeddecimal, numeric);
+    END IF;
+END $$;
+
