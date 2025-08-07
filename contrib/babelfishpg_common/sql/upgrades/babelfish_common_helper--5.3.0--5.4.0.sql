155068cece3bebd5d63b339c34b3344ec55c6f5d
Yashneet Vinayak <>
2025-08-07 03:09:33 +0000

    version correction
    Signed-off-by: Yashneet Vinayak <>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.3.0--5.4.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.3.0--5.4.0.sql
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------------
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '5.3.0'" to load this file. \quit
+\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO '5.4.0'" to load this file. \quit
 
 SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
 
