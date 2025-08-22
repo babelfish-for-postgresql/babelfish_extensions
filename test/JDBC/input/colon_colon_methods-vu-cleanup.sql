49e1ea2dd70dc95cd208c9b93f6a198243b35fa2
Rohit bhagat <65351720+rohit01010@users.noreply.github.com>
2025-08-22 11:57:43 +0530

    Fixed issues with datatype colon_colon methods syntax (#4035)
    * Fixed issues with datatype colon_colon methods syntax (#3991)
    
    This commit fixes issues with datatype colon_colon methods, due to this earlier datatype colon_colon methods were resulting in unexpected results and unexpected errors. Updated the syntax rules and added handling to fix these issues with datatype colon_colon methods.
    
    Task: BABEL-5987
    Signed-off-by: Rohit Bhagat <rohitbgt@amazon.com>

---
+++ test/JDBC/input/colon_colon_methods-vu-cleanup.sql
@@ -0,0 +1,46 @@
+DROP VIEW babel_5987_colon_colon_dep_view1
+GO
+
+DROP VIEW babel_5987_colon_colon_dep_view2
+GO
+
+DROP VIEW babel_5987_colon_colon_dep_view3
+GO
+
+DROP FUNCTION babel_5987_colon_colon_dep_func_5
+GO
+
+DROP FUNCTION babel_5987_colon_colon_dep_func_4
+GO
+
+DROP FUNCTION babel_5987_colon_colon_dep_func_3
+GO
+
+DROP FUNCTION babel_5987_colon_colon_dep_func_2
+GO
+
+DROP FUNCTION babel_5987_colon_colon_dep_func_1
+GO
+
+DROP PROCEDURE babel_5987_colon_colon_dep_proc3
+GO
+
+DROP PROCEDURE babel_5987_colon_colon_dep_proc2
+GO
+
+DROP PROCEDURE babel_5987_colon_colon_dep_proc1
+GO
+
+DROP TABLE babel_5987_colon_colon_t1
+GO
+
+DROP TYPE dbo.babel_5987_xmlUDT;
+GO
+
+DROP TYPE dbo.babel_5987_geometryUDT;
+GO
+
+DROP TYPE dbo.babel_5987_geographyUDT;
+GO
+
+
--- test/JDBC/upgrade/14_3/preparation/sys-assembly_types-vu-prepare.sql
+++ test/JDBC/upgrade/14_3/preparation/sys-assembly_types-vu-prepare.sql
@@ -3,13 +3,13 @@ SELECT * FROM sys.assembly_types
 GO
 
 CREATE PROC sys_assembly_types_proc_vu_prepare AS
-SELECT name, principal_id, max_length, precision, scale, collation_name, is_nullable, is_user_defined, is_assembly_type, default_object_id, rule_object_id, assembly_id, assembly_class, is_binary_ordered, is_fixed_length, prog_id, assembly_qualified_name, is_table_type FROM sys.assembly_types ORDER BY name DESC
+SELECT name, principal_id, max_length, precision, scale, collation_name, is_nullable, is_user_defined, is_assembly_type, default_object_id, rule_object_id, assembly_id, assembly_class, is_binary_ordered, is_fixed_length, prog_id, assembly_qualified_name, is_table_type FROM sys.assembly_types WHERE is_user_defined=0 ORDER BY name DESC
 GO
 
 CREATE FUNCTION sys_assembly_types_func_vu_prepare()
 RETURNS INT
 AS
 BEGIN
-RETURN (SELECT COUNT(*) FROM sys.assembly_types)
+RETURN (SELECT COUNT(*) FROM sys.assembly_types WHERE is_user_defined=0)
 END
 GO
