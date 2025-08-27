feee2e87d1bbddf5d1fba76ae17ad28b94c3acd1
Shameem Ahmed <54461265+ahmed-shameem@users.noreply.github.com>
2025-08-27 18:15:05 +0530

    Fix datetime roundoff mechanism during storing/insertion of data (#4048)
    * Fix datetime roundoff mechanism during storing/insertion of data (#4045)
    
    Datetime datatype in TSQL has a precision of 3 ms and this behaviour is persisted in all operations i.e., qual conditions, operators, cast etc. However, Babelfish only follows this rule during returning rows. In all other operations, it does not apply any round off mechanism (relies on PG’s handling).
    
    It is a day one issue in babelfish where we internally store the original value for datetime datatype and not the one rounded to increments of .000, .003, or .007 seconds.
    For Example in this case:
    
    CREATE TABLE dbo.DateTimeTest (
        id BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
        lastmodifieddate DATETIME NULL
    );
    INSERT INTO dbo.DateTimeTest (lastmodifieddate)
    VALUES (CAST('2021-09-20T04:00:49.299' AS DATETIME2(3)));
    
    The value stored in lastmodifieddate column in babelfish would actually be '2021-09-20T04:00:49.299' and not '2021-09-20T04:00:49.300'
    
    So, when we select the datetime column:
    
    SELECT * FROM dbo.DateTimeTest;
    
    Then the TDS Sender function for datetime kick in and rounds off the value to '2021-09-20T04:00:49.300'
    
    For other cases, the original value is used for any operations performed!
    Example in the second case:
    
    SELECT * FROM dbo.DateTimeTest WHERE lastmodifieddate = '2021-09-20T04:00:49.300';
    
    The = operator would compare with original value of lastmodifieddate column i.e. '2021-09-20T04:00:49.299' and hence no rows are returned.
    
    To solve this issue, rather than rounding off the values while sending the rows to user, we should store the values in rounded off format.
    
    Tasks: BABEL-5265, BABEL-1081
    
    Signed-of-by: Shameem Ahmed shmeeh@amazon.com
    (cherry picked from commit 6f86e105e16c82abcaa95f250f90a66b963561a7)

--- contrib/babelfishpg_common/src/babelfishpg_common.h
+++ contrib/babelfishpg_common/src/babelfishpg_common.h
@@ -98,4 +98,6 @@ typedef struct common_utility_plugin
 	int32_t		(*tsql_numeric_get_typmod) (Numeric num);
 	DateADT		(*initializeToDefaultDate) (void);
 	TimeADT		(*initializeToDefaultTime) (int32 typmod);
+	Timestamp	(*roundoff_datetime) (Timestamp timestamp);
+	void		(*UpdateToNextDayHelper) (struct pg_tm *tm);
 } common_utility_plugin;
