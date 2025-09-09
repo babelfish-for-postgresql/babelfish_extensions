aa5427291dac9e31fbad270b4172c4dba100a04b
Mohit Raj <90141011+Mohitraj-g@users.noreply.github.com>
2025-09-09 17:31:34 +0530

    Guard spatial type functions with ENABLE_SPATIAL_TYPES (#4066)
    Wrap spatial type function assignments with ENABLE_SPATIAL_TYPES macro guards to ensure they are only compiled
    when spatial types support is enabled.
    
    This change adds conditional compilation around bytea_from_geometry and bytea_from_geography functions
    
    This prevents compilation errors when spatial types are not available and maintains consistency with other conditional
    features in the codebase.
    
    Signed-off-by: Mohit Raj <mrxmohit@amazon.com>

--- contrib/babelfishpg_common/src/spatialtypes.h
+++ contrib/babelfishpg_common/src/spatialtypes.h
@@ -12,7 +12,9 @@
 #include "postgres.h"
 #include "fmgr.h"
 
+#ifdef ENABLE_SPATIAL_TYPES
 extern Datum bytea_from_geometry(PG_FUNCTION_ARGS);
 extern Datum bytea_from_geography(PG_FUNCTION_ARGS);
+#endif
 
 #endif  /* SPATIALTYPES_H */
