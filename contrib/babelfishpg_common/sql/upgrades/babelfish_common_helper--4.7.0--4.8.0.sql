5c5a49f335b0e8acf6574c4b0d13fb378195e61e
Rucha Kulkarni <ruchask@amazon.com>
2025-09-11 09:54:31 +0000

    Fix concatenation of binaries by adding '+' operator for sys.bbf_binary
    Signed-off-by: Rucha Kulkarni <ruchask@amazon.com>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--4.7.0--4.8.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--4.7.0--4.8.0.sql
@@ -96,3 +96,19 @@ BEGIN
       WITH FUNCTION sys.datetime2binary(SYS.DATETIME, integer, boolean) AS ASSIGNMENT;
   END IF;
 END $$;
+
+CREATE OR REPLACE FUNCTION sys.binaryadd(leftarg sys.BBF_BINARY, rightarg sys.BBF_BINARY)
+RETURNS sys.BBF_BINARY
+AS 'byteacat'
+LANGUAGE INTERNAL IMMUTABLE STRICT PARALLEL SAFE;
+
+DO $$
+BEGIN
+IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_operator WHERE oprleft = 'sys.BBF_BINARY'::pg_catalog.regtype and oprright = 'sys.BBF_BINARY'::pg_catalog.regtype and oprnamespace = 'sys'::regnamespace and oprname = '+' and oprresult != 0) THEN
+CREATE OPERATOR sys.+ (
+	LEFTARG    = sys.BBF_BINARY,
+	RIGHTARG   = sys.BBF_BINARY,
+	PROCEDURE  = sys.binaryadd
+);
+END IF;
+END $$;
