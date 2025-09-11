f2506d0d8df23d40e90e5684979d8f42c1a976e0
Yashneet Vinayak <>
2025-09-11 09:36:07 +0000

    Revert "correcting comment"
    This reverts commit 3215d3fcc9be11e2ac31d342e4c8882bc3688107.

--- contrib/babelfishpg_tds/src/backend/tds/tdsprotocol.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsprotocol.c
@@ -108,7 +108,7 @@ TdsDiscardAll()
 
 	/*
 	 * ResetTempTableNamespace might involve TOAST table access,
-	 * so we need to ensure that there is a valid snapshot.
+	 * so ensure we to have a valid snapshot.
 	 */
 	PushActiveSnapshot(GetTransactionSnapshot());
 	ResetTempTableNamespace();
