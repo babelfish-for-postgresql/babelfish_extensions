14de27f3b7262654f75cf0b0187052fac9961795
Yashneet Vinayak <>
2025-09-12 11:12:57 +0000

    adding correct bool condition for ActiveSnapshotSet
    Signed-off-by: Yashneet Vinayak <>

--- contrib/babelfishpg_tds/src/backend/tds/tdsprotocol.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsprotocol.c
@@ -89,6 +89,7 @@ static
 void
 TdsDiscardAll()
 {
+	bool		need_snapshot = !ActiveSnapshotSet();
 	/*
 	 * Disallow DISCARD ALL in a transaction block. This is arguably
 	 * inconsistent (we don't make a similar check in the command sequence
@@ -110,12 +111,12 @@ TdsDiscardAll()
 	 * ResetTempTableNamespace might involve TOAST table access,
 	 * so we need to ensure that there is a valid snapshot.
 	 */
-	if (!ActiveSnapshotSet())
+	if (need_snapshot)
 		PushActiveSnapshot(GetTransactionSnapshot());
 
 	ResetTempTableNamespace();
 
-	if (!ActiveSnapshotSet())
+	if (need_snapshot)
 		PopActiveSnapshot();
 	
 	ResetSequenceCaches();
