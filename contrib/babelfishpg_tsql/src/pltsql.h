9c2b2fdbb55ec89f57b2a8aa38f65ba9dec26a5f
Tanya Gupta <59756970+tanyagupta17@users.noreply.github.com>
2025-10-06 09:04:05 +0530

    Datalength() fixed for various datatypes  (#4144)
    * Datalength() fixed for various datatypes  (#4124)
    
    This PR fixes the datalength() for the following datatypes along with their UDTs, to align the output with TSQL:
    
    Tinyint
    Date
    Smalldatetime
    Time
    Smallmoney
    Numeric/decimal
    
    Issues Resolved
    BABEL-6120
    ---------
    
    Signed-off-by: Tanya Gupta <tanyagp@amazon.com>
    Co-authored-by: Tanya Gupta <tanyagp@amazon.com>

--- contrib/babelfishpg_tsql/src/pltsql.h
+++ contrib/babelfishpg_tsql/src/pltsql.h
@@ -2359,6 +2359,8 @@ extern void	exec_alter_role_cmd(char *query_str, RoleSpec *role);
  */
 extern bool validate_special_function(char *proc_nsname, char *proc_name, int nargs, bool num_args_match);
 extern int32	resolve_numeric_typmod_from_exp(Plan *plan, Node *expr, bool *found);
+extern Oid      get_immediate_base_type_of_UDT_internal(Oid oid);
+extern bool 	is_numeric_datatype(Oid typid);
 
 /*
  * Function in pltsql_ruleutils.c
