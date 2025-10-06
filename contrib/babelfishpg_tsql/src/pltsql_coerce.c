2e7a2a6670f463d71e2ffd7b8a1dc0c0cc1ecf07
Tanya Gupta <59756970+tanyagupta17@users.noreply.github.com>
2025-10-06 09:04:05 +0530

    Datalength() fixed for various datatypes  (#4144)
    * Datalength() fixed for various datatypes  (#4124)
    
    This PR fixes the datalength() for the following datatypes along with their UDTs, to align the output with TSQL:
    
    Tinyint
    Date
    Smalldatetime
    Time
    Smallmoney
    Numeric/decimal
    
    Issues Resolved
    BABEL-6120
    ---------
    
    Signed-off-by: Tanya Gupta <tanyagp@amazon.com>
    Co-authored-by: Tanya Gupta <tanyagp@amazon.com>

--- contrib/babelfishpg_tsql/src/pltsql_coerce.c
+++ contrib/babelfishpg_tsql/src/pltsql_coerce.c
@@ -76,7 +76,6 @@ PG_FUNCTION_INFO_V1(get_immediate_base_type_of_UDT);
 static Oid select_common_type_setop(ParseState *pstate, List *exprs, Node **which_expr, const char *context);
 static Oid select_common_type_for_isnull(ParseState *pstate, List *exprs);
 static Oid select_common_type_for_coalesce_function(ParseState *pstate, List *exprs);
-static Oid get_immediate_base_type_of_UDT_internal(Oid typeid);
 static Oid LookupCastFuncName(Oid castsource, Oid casttarget);
 static bool is_numeric_cast(Oid func_oid);
 static bool is_tsql_fixeddecimal_numeric(Oid oid);
@@ -1061,7 +1060,7 @@ run_tsql_best_match_heuristics(int nargs, Oid *input_typeids, FuncCandidateList 
  * This function returns the Immediate base type for UDT.
  * Returns InvalidOid if given type is not an UDT
  */
-static Oid
+Oid
 get_immediate_base_type_of_UDT_internal(Oid typeid)
 {
 	HeapTuple					tuple;
@@ -1200,7 +1199,7 @@ is_numeric_cast(Oid func_oid)
 /*
  * is_numeric_datatype - returns bool if given datatype is numeric, decimal, UDT on numeric or decimal.
  */
-static bool
+bool
 is_numeric_datatype(Oid typid)
 {
 	if (OidIsValid(typid) && getBaseType(typid) == NUMERICOID)
