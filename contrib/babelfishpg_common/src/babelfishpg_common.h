b471f9529612a89b29c8ec74b1309ffc31a4820b
Yvinayak07 <100944709+Yvinayak07@users.noreply.github.com>
2025-10-15 14:53:47 +0530

    Fix typmod for sys.round() function (#4185)
    This commit fixes the typmod for round function, where the return type of round function is decimal. Typmod of round function is same as the Typmod of its first argument.
    
    
    Task: BABEL-5666
    cherry-picked from #4161
    Signed-off-by: Yashneet Vinayak <yashneet@amazon.com>

--- contrib/babelfishpg_common/src/babelfishpg_common.h
+++ contrib/babelfishpg_common/src/babelfishpg_common.h
@@ -70,6 +70,10 @@ typedef struct common_utility_plugin
 	bool		(*is_tsql_money_datatype) (Oid oid);
 	bool		(*is_tsql_smallmoney_datatype) (Oid oid);
 	bool		(*isEmptyOrWhitespace) (const char *str);
+
+	/* FIX ME: Remove is_tsql_int_datatype and is_tsql_bigint_datatype once BABEL-5955 is fixed */
+	bool		(*is_tsql_int_datatype) (Oid oid);
+	bool		(*is_tsql_bigint_datatype) (Oid oid);
 	
 	Datum		(*datetime_in_str) (char *str, Node *escontext);
 	Datum		(*datetime2sqlvariant) (PG_FUNCTION_ARGS);
