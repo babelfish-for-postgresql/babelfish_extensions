3b6a50d5723b0f3affbdfa5e52d3f31753313dbb
Harsh Dubey <100467917+harshdubey166@users.noreply.github.com>
2025-10-16 18:52:40 +0530

    Fixed the issue in xml type argument in sp_xml_preparedocument procedure (#4134)
    Currently in Babelfish, sp_xml_preparedocument procedure supports only string datatypes (char, nchar, varchar, nvarchar, text, ntext) for argument xmltext whereas TSQL support xmltext argument of type char, nchar, varchar, nvarchar, text, ntext or xml.
    
    Added support for xml datatype arguments .
    
    Issues Resolved
    BABEL-6129
    Authored by : harshdu@amazon.com
    Signed-off by: harshdu@amazon.com

--- test/JDBC/input/xml/sp_xml_preparedocument-vu-cleanup.sql
+++ test/JDBC/input/xml/sp_xml_preparedocument-vu-cleanup.sql
@@ -9,3 +9,89 @@ GO
 
 DROP PROCEDURE test_xml_proc;
 GO
+
+DROP TYPE IF EXISTS XMLCharType
+GO
+
+DROP TYPE IF EXISTS XMLVarcharType
+GO
+
+DROP TYPE IF EXISTS XMLNCharType
+GO
+
+DROP TYPE IF EXISTS XMLNVarcharType
+GO
+
+DROP TYPE IF EXISTS XMLTextType
+GO
+
+DROP TYPE IF EXISTS XMLNTextType
+GO
+
+DROP PROCEDURE IF EXISTS TestXMLPrepareDocument_Char
+GO
+
+DROP PROCEDURE IF EXISTS TestXMLPrepareDocument_Varchar
+GO
+
+DROP PROCEDURE IF EXISTS TestXMLPrepareDocument_NChar
+GO
+
+DROP PROCEDURE IF EXISTS TestXMLPrepareDocument_NVarchar
+GO
+
+DROP PROCEDURE IF EXISTS TestXMLPrepareDocument_Text
+GO
+
+DROP PROCEDURE IF EXISTS TestXMLPrepareDocument_NText
+GO
+
+-- Cleanup procedures
+DROP PROCEDURE IF EXISTS ProcessXMLFromView_babel_1168
+GO
+
+DROP PROCEDURE IF EXISTS ProcessComplexXMLView_babel_1168
+GO
+
+-- Cleanup function
+DROP FUNCTION IF EXISTS GetXMLByType_babel_1168
+GO
+
+-- Cleanup views
+DROP VIEW IF EXISTS TransformedXMLView_babel_1168
+GO
+
+DROP VIEW IF EXISTS NVarcharXMLView_babel_1168
+GO
+
+DROP VIEW IF EXISTS XMLWithMetadataView_babel_1168
+GO
+
+DROP VIEW IF EXISTS ActiveXMLHandlesView_babel_1168
+GO
+
+DROP VIEW IF EXISTS NestedXMLView_babel_1168
+GO
+
+DROP VIEW IF EXISTS XMLErrorHandlingView_babel_1168
+GO
+
+DROP VIEW IF EXISTS XMLDataView_babel_1168
+GO
+
+-- Cleanup tables
+DROP TABLE IF EXISTS NVarcharXMLSource_babel_1168
+GO
+
+DROP TABLE IF EXISTS XMLMetadata_babel_1168
+GO
+
+DROP TABLE IF EXISTS XMLDataSource_babel_1168
+GO
+
+-- Cleanup casting test objects
+DROP PROCEDURE IF EXISTS TestExplicitCasting_babel_1168;
+GO
+
+DROP TABLE IF EXISTS CastingTestData_babel_1168;
+GO
