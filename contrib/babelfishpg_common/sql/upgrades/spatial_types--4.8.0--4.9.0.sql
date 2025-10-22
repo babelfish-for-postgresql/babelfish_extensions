52e291a874fb529ce0609bbeb0f43cc89f2a1216
Mohit Raj <90141011+Mohitraj-g@users.noreply.github.com>
2025-10-22 12:39:43 +0530

    Fixed geometry/geography bugs_4X (#4195)
    Key Changes:
        Added a check to disallow non-printable ASCII charaters in the input of geospatial parser.
        Removed incorrect support of Multiple Decimal Points for Geometry/Geography Data Type
    
    Tasks: BABEL-6126, BABEL-6149
    
    Signed-off by: Mohit Raj mrxmohit@amazon.com

--- contrib/babelfishpg_common/sql/upgrades/spatial_types--4.8.0--4.9.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/spatial_types--4.8.0--4.9.0.sql
@@ -1,3 +1,50 @@
 -------------------------------------------------------
 ---- Include changes related to spatial types here ----
 -------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION sys.Geometry__STPointFromText(sys.NVARCHAR,srid integer)
+	RETURNS sys.GEOMETRY
+	AS $$
+	DECLARE
+		Geomtype text;
+		geom sys.GEOMETRY;
+	BEGIN
+		IF $2 IS NULL THEN
+			RAISE EXCEPTION '''geometry::STPointFromText'' failed because parameter 2 is not allowed to be null.';
+		ELSIF $1 IS NULL THEN
+			RETURN NULL;
+		END IF;
+		geom = (SELECT sys.geomfromtext_helper($1::text, $2));
+		Geomtype = (SELECT sys.ST_GeometryType(geom));
+
+		IF Geomtype = 'ST_Point' THEN
+				RETURN geom;
+		ELSE
+			RAISE EXCEPTION 'Expected "POINT" at Position 1. The input has %', $1;
+		END IF;
+	END;
+	$$ LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION sys.Geography__STPointFromText(sys.NVARCHAR,srid integer)
+	RETURNS sys.GEOGRAPHY
+	AS $$
+	DECLARE
+		Geomtype text;
+		geom sys.GEOGRAPHY;
+	BEGIN
+		IF $2 IS NULL THEN
+			RAISE EXCEPTION '''geography::STPointFromText'' failed because parameter 2 is not allowed to be null.';
+		ELSIF $1 IS NULL THEN
+			RETURN NULL;
+		END IF;
+		geom = (SELECT sys.geogfromtext_helper($1::text, $2));
+		Geomtype = (SELECT sys.ST_GeometryType(geom));
+
+		IF Geomtype = 'ST_Point' THEN
+			RETURN geom;
+		ELSE
+			RAISE EXCEPTION 'Expected "POINT" at Position 1. The input has %', $1;
+		END IF;
+	END;
+	$$ LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE;
+	
