e21ab4989f263aec9b0d048bbbec6f9dc0c995d3
Mohit Raj <90141011+Mohitraj-g@users.noreply.github.com>
2025-10-22 12:38:50 +0530

    Fixed geometry/geography bugs_5X (#4175)
    Key Changes:
        Added a check to disallow non-printable ASCII charaters in the input of geospatial parser.
        Added check and error handling for antipodal points in linestring for geography datatype.
        Introduced NULL output for non-point instances in .M and .Z method
        Removed incorrect support of Multiple Decimal Points for Geometry/Geography Data Type
    
    Tasks: BABEL-6126, BABEL-6130, BABEL-6134, BABEL-6149
    
    Signed-off by: Mohit Raj mrxmohit@amazon.com

--- test/JDBC/input/datatypes/Test-Linestring-vu-verify.sql
+++ test/JDBC/input/datatypes/Test-Linestring-vu-verify.sql
@@ -195,6 +195,38 @@ GO
 SELECT geometry::STGeomFromText('LINESTRING(1 2 , 1 2, 1 2 5 5 )', 4326).STIntersects(geometry::STGeomFromText('LINESTRING(3 4, 3 4, 3 4 5 5 )', 4326));
 GO
 
+-- Test for antipodal points
+SELECT geography::STLineFromText('LINESTRING(0 0, 180 0)', 4326)
+go
+
+SELECT CAST(geography::STLineFromText('LINESTRING(0 0, 180 0)', 4326) AS binary(38));
+GO
+
+SELECT CAST(geography::STLineFromText('LINESTRING(0 -90, 0 90)', 4326) AS binary(38));
+GO
+
+SELECT CAST(geography::STLineFromText('LINESTRING(0 0, 180 0)', 4326) AS varbinary(38));
+GO
+
+SELECT CAST(geography::STLineFromText('LINESTRING(0 -90, 0 90)', 4326) AS varbinary(38));
+GO
+
+SELECT CAST(geography::STLineFromText('LINESTRING(0 0, 180 0)', 4326) AS char(38));
+GO
+
+-- Test M and Z functions with Linestring
+SELECT geometry::STGeomFromText('LINESTRING(0 0 0 1, 1 1 1 2)', 4326).M;
+GO
+
+SELECT geometry::STGeomFromText('LINESTRING(0 0 0 1, 1 1 1 2)', 4326).Z;
+GO
+
+SELECT geography::STGeomFromText('LINESTRING(0 0 0 1, 1 1 1 2)', 4326).M;
+GO
+
+SELECT geography::STGeomFromText('LINESTRING(0 0 0 1, 1 1 1 2)', 4326).Z;
+GO
+
 Select * from GEOSPATIALLINEGEOM_INVALID_dt;
 GO
 
