96299d4a90fa22fce09a73005bebd64891226721
Tanya Gupta <59756970+tanyagupta17@users.noreply.github.com>
2025-10-30 21:13:57 +0530

    [OSS-ONLY] Updated the ubuntu version to ubuntu-latest and updated the repo config (#3548) (#4230)
    Updated the ubuntu version to ubuntu-latest and updated the repo config.
    Temporarily disable odbc test Table_Unique_Constraints as it has started failing
    with unknown reason.
    
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>

--- test/dotnet/src/PrepareExecBinding.cs
+++ test/dotnet/src/PrepareExecBinding.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.OleDb;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.IO;
 using System.Text;
