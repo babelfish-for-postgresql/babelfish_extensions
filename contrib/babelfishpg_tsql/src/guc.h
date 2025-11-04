068c5e04c8f319b905b02465c83f2df587144b47
Ahmad Hasan <ahmadhasan87homs@gmail.com>
2025-11-04 21:58:31 +0300

    Removing extra blank line from guc.h

--- contrib/babelfishpg_tsql/src/guc.h
+++ contrib/babelfishpg_tsql/src/guc.h
@@ -15,6 +15,7 @@ typedef enum IsolationOptions
 	ISOLATION_OFF, PG_ISOLATION
 } IsolationOptions;
 
+extern bool	pltsql_disable_unmapped_error_termination;
 extern bool pltsql_fmtonly;
 extern bool pltsql_enable_create_alter_view_from_pg;
 extern bool pltsql_enable_linked_servers;
@@ -27,7 +28,6 @@ extern int  pltsql_isolation_level_repeatable_read;
 extern int  pltsql_isolation_level_serializable;
 extern int escape_hatch_identity_function;
 extern int escape_hatch_insert_bulk_options;
-extern bool	pltsql_disable_unmapped_error_termination; 					
 
 extern void define_custom_variables(void);
 extern void pltsql_validate_set_config_function(char *name, char *value);
