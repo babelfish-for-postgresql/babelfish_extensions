065f4889cd221a18e141a549338f9ec9371ba1d7
Yashneet Vinayak <>
2025-11-13 09:50:01 +0000

    adding opertor to correct schedule file
    Signed-off-by: Yashneet Vinayak <>

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.3.0--5.4.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.3.0--5.4.0.sql
@@ -57,22 +57,6 @@ BEGIN
   END IF;
 END $$;
 
-CREATE OR REPLACE FUNCTION sys.binaryadd(leftarg sys.BBF_BINARY, rightarg sys.BBF_BINARY)
-RETURNS sys.BBF_BINARY
-AS 'byteacat'
-LANGUAGE INTERNAL IMMUTABLE STRICT PARALLEL SAFE;
-
-DO $$
-BEGIN
-IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_operator WHERE oprleft = 'sys.BBF_BINARY'::pg_catalog.regtype and oprright = 'sys.BBF_BINARY'::pg_catalog.regtype and oprnamespace = 'sys'::regnamespace and oprname = '+' and oprresult != 0) THEN
-CREATE OPERATOR sys.+ (
-	LEFTARG    = sys.BBF_BINARY,
-	RIGHTARG   = sys.BBF_BINARY,
-	PROCEDURE  = sys.binaryadd
-);
-END IF;
-END $$;
-
 CREATE OR REPLACE FUNCTION sys.datetime2varbinary(SYS.DATETIME, integer, boolean)
 RETURNS sys.bbf_varbinary
 AS 'babelfishpg_common', 'datetime_varbinary'
