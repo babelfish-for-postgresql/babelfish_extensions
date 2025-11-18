8accafdb168aecd8ee0ae71665061ab81f72fc61
Bilal Kahraman <kahramannbilal@gmail.com>
2025-11-18 08:16:53 +0000

    Cover the casts with exception block in upgrade script to avoid conflicts

--- contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.4.0--5.5.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/babelfish_common_helper--5.4.0--5.5.0.sql
@@ -11,17 +11,37 @@ CREATE OR REPLACE FUNCTION sys.varbinarysysbpchar(sys.BBF_VARBINARY, integer, bo
 RETURNS sys.BPCHAR
 AS 'babelfishpg_common', 'varbinarybpchar'
 LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
- 
-CREATE CAST (sys.BBF_VARBINARY AS sys.BPCHAR)
-WITH FUNCTION sys.varbinarysysbpchar (sys.BBF_VARBINARY, integer, boolean) AS IMPLICIT;
+
+DO $$
+DECLARE
+    exception_message text;
+BEGIN
+    CREATE CAST (sys.BBF_VARBINARY AS sys.BPCHAR)
+    WITH FUNCTION sys.varbinarysysbpchar (sys.BBF_VARBINARY, integer, boolean) AS IMPLICIT;
+EXCEPTION WHEN duplicate_object THEN
+    GET STACKED DIAGNOSTICS
+    exception_message = MESSAGE_TEXT;
+    RAISE WARNING '%', exception_message;
+END;
+$$;
 
 CREATE OR REPLACE FUNCTION sys.varbinarybpchar(sys.BBF_VARBINARY, integer, boolean)
 RETURNS pg_catalog.BPCHAR
 AS 'babelfishpg_common', 'varbinarybpchar'
 LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
- 
-CREATE CAST (sys.BBF_VARBINARY AS pg_catalog.BPCHAR)
-WITH FUNCTION sys.varbinarybpchar (sys.BBF_VARBINARY, integer, boolean) AS IMPLICIT;
+
+DO $$
+DECLARE
+    exception_message text;
+BEGIN
+    CREATE CAST (sys.BBF_VARBINARY AS pg_catalog.BPCHAR)
+    WITH FUNCTION sys.varbinarybpchar (sys.BBF_VARBINARY, integer, boolean) AS IMPLICIT;
+EXCEPTION WHEN duplicate_object THEN
+    GET STACKED DIAGNOSTICS
+    exception_message = MESSAGE_TEXT;
+    RAISE WARNING '%', exception_message;
+END;
+$$;
 
 CREATE OR REPLACE FUNCTION sys.varbinarysysnchar(sys.BBF_VARBINARY, integer, boolean)
 RETURNS sys.NCHAR
@@ -38,16 +58,36 @@ RETURNS sys.BPCHAR
 AS 'babelfishpg_common', 'varbinarybpchar'
 LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE CAST (sys.BBF_BINARY AS sys.BPCHAR)
-WITH FUNCTION sys.binarysysbpchar (sys.BBF_BINARY, integer, boolean) AS IMPLICIT;
+DO $$
+DECLARE
+    exception_message text;
+BEGIN
+    CREATE CAST (sys.BBF_BINARY AS sys.BPCHAR)
+    WITH FUNCTION sys.binarysysbpchar (sys.BBF_BINARY, integer, boolean) AS IMPLICIT;
+EXCEPTION WHEN duplicate_object THEN
+    GET STACKED DIAGNOSTICS
+    exception_message = MESSAGE_TEXT;
+    RAISE WARNING '%', exception_message;
+END;
+$$;
 
 CREATE OR REPLACE FUNCTION sys.binarybpchar(sys.BBF_BINARY, integer, boolean)
 RETURNS pg_catalog.BPCHAR
 AS 'babelfishpg_common', 'varbinarybpchar'
 LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE CAST (sys.BBF_BINARY AS pg_catalog.BPCHAR)
-WITH FUNCTION sys.binarybpchar (sys.BBF_BINARY, integer, boolean) AS IMPLICIT;
+DO $$
+DECLARE
+    exception_message text;
+BEGIN
+    CREATE CAST (sys.BBF_BINARY AS pg_catalog.BPCHAR)
+    WITH FUNCTION sys.binarybpchar (sys.BBF_BINARY, integer, boolean) AS IMPLICIT;
+EXCEPTION WHEN duplicate_object THEN
+    GET STACKED DIAGNOSTICS
+    exception_message = MESSAGE_TEXT;
+    RAISE WARNING '%', exception_message;
+END;
+$$;
 
 CREATE OR REPLACE FUNCTION sys.binarysysnchar(sys.BBF_BINARY, integer, boolean)
 RETURNS sys.NCHAR
