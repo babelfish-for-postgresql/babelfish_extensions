9473bd401a8c6de28eb9e32a37956d2c75c0e90a
Rohit bhagat <65351720+rohit01010@users.noreply.github.com>
2025-12-23 11:37:04 +0530

    Fix crash in pre_transform_target_entry (#4331)
    Currently when target list has keyword "Character" a crash happens in pre_transform_target_entry. This is because Character identifier is replaced with "bpchar" during lexer stage. This case was not handled in pre_transform_target_entry which was causing this crash. Handled this case to fix the crash.
    
    Task: BABEL-5788
    
    Signed-off-by: Rohit Bhagat rohitbgt@amazon.com

---
+++ test/JDBC/input/BABEL-5788-vu-verify.sql
@@ -0,0 +1,17 @@
+select 1 as Character
+GO
+
+select 1 as Char
+GO
+
+select 1 as CHAR
+GO
+
+select 1 as CHARACTER
+GO
+
+select 1 as Long_Identifier12345678901234567890123456789012345678901234567890
+GO
+
+select 1 as Short_Identifier
+GO
