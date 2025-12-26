43eebe53827f6d3c01a87aafdd5fc7af31aa1aba
raghavbhutra <bhutra12raghav@gmail.com>
2025-12-26 16:56:32 +0530

    fix tests
    Signed-off-by: raghavbhutra <bhutra12raghav@gmail.com>

--- test/JDBC/input/functions/datetime2fromparts-vu-prepare.sql
+++ test/JDBC/input/functions/datetime2fromparts-vu-prepare.sql
@@ -1,70 +1,35 @@
 -- [BABEL-1627, 2827] Support DATETIME2FROMPARTS Transact-SQL function
--- Tests for NEW behavior (version 17.7+) - specifically test fractions > 32767 (smallint bug fix)
--- These test the updated behavior where fractions parameter supports values up to 9999999
-
-CREATE VIEW datetime2fromparts_vu_prepare_v1 AS 
-SELECT DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 32768, 5) AS just_above_smallint;
+CREATE VIEW datetime2fromparts_vu_prepare_v1 AS (select DATETIME2FROMPARTS(1962, 16, 1, 14, 64, 48, 59, 5 ));
 GO
 
-CREATE VIEW datetime2fromparts_vu_prepare_v2 AS
-SELECT DATETIME2FROMPARTS(2024, 2, 29, 12, 30, 45, 100000, 6) AS high_fraction;
+CREATE VIEW datetime2fromparts_vu_prepare_v2 AS (select DATETIME2FROMPARTS(20008, 8, 32, 34, 23, 78, 45, 8 ));
 GO
 
-CREATE VIEW datetime2fromparts_vu_prepare_v3 AS
-SELECT DATETIME2FROMPARTS(2023, 2, 28, 23, 59, 59, 9999999, 7) AS max_fraction;
+CREATE VIEW datetime2fromparts_vu_prepare_v3 AS (select DATETIME2FROMPARTS(1980, 11, 25, 22, 09, 58, 671, 4 ));
 GO
 
-CREATE VIEW datetime2fromparts_vu_prepare_v4 AS
-SELECT DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 1234567, 7) AS high_precision_fraction;
+CREATE PROCEDURE datetime2fromparts_vu_prepare_p1 AS (select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 1 ));
 GO
 
-CREATE VIEW datetime2fromparts_vu_prepare_v5 AS
-SELECT DATETIME2FROMPARTS(2023, 6, 15, 10, 20, 30, 500000, 6) AS mid_range_fraction;
+CREATE PROCEDURE datetime2fromparts_vu_prepare_p2 AS (select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 2 ));
 GO
 
-CREATE PROCEDURE datetime2fromparts_vu_prepare_p1 AS
-SELECT 
-    DATETIME2FROMPARTS(2020, 1, 1, 9, 0, 0, 50000, 5) AS frac_50000,
-    DATETIME2FROMPARTS(2021, 6, 15, 12, 30, 45, 999999, 6) AS frac_999999,
-    DATETIME2FROMPARTS(2022, 12, 31, 23, 59, 59, 9999999, 7) AS frac_9999999;
+CREATE PROCEDURE datetime2fromparts_vu_prepare_p3 AS (select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 3 ));
 GO
 
-CREATE PROCEDURE datetime2fromparts_vu_prepare_p2 AS
-SELECT DATETIME2FROMPARTS(2023, 1, 1, 12, 0, 0, 32768, 5) AS result;
+CREATE PROCEDURE datetime2fromparts_vu_prepare_p4 AS (select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 4 ));
 GO
 
 CREATE FUNCTION datetime2fromparts_vu_prepare_f1()
 RETURNS DATETIME2 AS
 BEGIN
-    RETURN (SELECT DATETIME2FROMPARTS(2023, 1, 15, 14, 35, 42, 7654321, 7));
+RETURN (select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 5 ));
 END
 GO
 
 CREATE FUNCTION datetime2fromparts_vu_prepare_f2()
 RETURNS DATETIME2 AS
 BEGIN
-    RETURN (SELECT DATETIME2FROMPARTS(2023, 6, 30, 18, 45, 12, 888888, 6));
+RETURN (select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 6 ));
 END
-GO
-
--- Test table with high fraction values
-CREATE TABLE datetime2fromparts_vu_prepare_t1 (
-    ID INT PRIMARY KEY,
-    Year INT,
-    Month INT,
-    Day INT,
-    Hour INT,
-    Minute INT,
-    Second INT,
-    Fraction INT,
-    Precision INT
-);
-GO
-
-INSERT INTO datetime2fromparts_vu_prepare_t1 VALUES
-(1, 2023, 1, 1, 12, 0, 0, 32767, 5),   -- max smallint
-(2, 2023, 1, 1, 12, 0, 0, 32768, 5),   -- above smallint
-(3, 2023, 1, 1, 12, 0, 0, 100000, 6),  -- 100k
-(4, 2023, 1, 1, 12, 0, 0, 999999, 6),  -- max precision 6
-(5, 2023, 1, 1, 12, 0, 0, 9999999, 7); -- max precision 7
 GO
--- test/JDBC/input/functions/datetime2fromparts-vu-verify.sql
+++ test/JDBC/input/functions/datetime2fromparts-vu-verify.sql
@@ -1,69 +1,57 @@
 -- [BABEL-1627, 2827] Support DATETIME2FROMPARTS Transact-SQL function
--- Verify NEW behavior - test high fraction values (>32767) work correctly
-
-SELECT * FROM datetime2fromparts_vu_prepare_v1;
+select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 0 );
 GO
 
-SELECT * FROM datetime2fromparts_vu_prepare_v2;
+select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 7 );
 GO
 
-SELECT * FROM datetime2fromparts_vu_prepare_v3;
+select DATETIME2FROMPARTS(2011, 8, 15, 14, 23, 44, 5, 8 );
 GO
 
-SELECT * FROM datetime2fromparts_vu_prepare_v4;
+SELECT DATETIME2FROMPARTS ( 2010, 12, 31, 23, 59, 59, 0, 0 ) AS Result;
 GO
 
-SELECT * FROM datetime2fromparts_vu_prepare_v5;
+SELECT * FROM datetime2fromparts_vu_prepare_v1
+GO
+DROP VIEW datetime2fromparts_vu_prepare_v1
 GO
 
-EXEC datetime2fromparts_vu_prepare_p1;
+SELECT * FROM datetime2fromparts_vu_prepare_v2
+GO
+DROP VIEW datetime2fromparts_vu_prepare_v2
 GO
 
-EXEC datetime2fromparts_vu_prepare_p2;
+SELECT * FROM datetime2fromparts_vu_prepare_v3
+GO
+DROP VIEW datetime2fromparts_vu_prepare_v3
 GO
 
-SELECT datetime2fromparts_vu_prepare_f1();
+EXEC datetime2fromparts_vu_prepare_p1
+GO
+DROP PROCEDURE datetime2fromparts_vu_prepare_p1
 GO
 
-SELECT datetime2fromparts_vu_prepare_f2();
+EXEC datetime2fromparts_vu_prepare_p2
+GO
+DROP PROCEDURE datetime2fromparts_vu_prepare_p2
 GO
 
--- Verify high fraction values from table
-SELECT 
-    ID,
-    Fraction,
-    DATETIME2FROMPARTS(Year, Month, Day, Hour, Minute, Second, Fraction, Precision) AS ConstructedDateTime,
-    Precision AS ExpectedPrecision
-FROM datetime2fromparts_vu_prepare_t1
-ORDER BY ID;
+EXEC datetime2fromparts_vu_prepare_p3
+GO
+DROP PROCEDURE datetime2fromparts_vu_prepare_p3
 GO
 
--- Direct tests for the bug fix - fractions > 32767
-SELECT DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 32768, 5) AS test_32768;
+EXEC datetime2fromparts_vu_prepare_p4
+GO
+DROP PROCEDURE datetime2fromparts_vu_prepare_p4
 GO
 
-SELECT DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 100000, 6) AS test_100000;
+SELECT datetime2fromparts_vu_prepare_f1()
+GO
+DROP FUNCTION datetime2fromparts_vu_prepare_f1()
 GO
 
-SELECT DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 1234567, 7) AS test_1234567;
+SELECT datetime2fromparts_vu_prepare_f2()
 GO
-
-SELECT DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 9999999, 7) AS test_9999999_max;
-GO
-
--- Test various precision levels with high fractions
-SELECT 
-    DATETIME2FROMPARTS(2023, 1, 1, 12, 0, 0, 99999, 5) AS precision_5_frac_99999,
-    DATETIME2FROMPARTS(2023, 1, 1, 12, 0, 0, 999999, 6) AS precision_6_frac_999999,
-    DATETIME2FROMPARTS(2023, 1, 1, 12, 0, 0, 9999999, 7) AS precision_7_frac_9999999;
-GO
-
--- Test that result string representation is correct
-SELECT 
-    DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 9999999, 7) AS dt,
-    CAST(DATETIME2FROMPARTS(2023, 1, 1, 12, 30, 45, 9999999, 7) AS VARCHAR(30)) AS dt_string;
-GO
-
--- Test edge case: precision 7 with max fractions
-SELECT DATETIME2FROMPARTS(2023, 12, 31, 23, 59, 59, 9999999, 7) AS max_datetime_max_fractions;
+DROP FUNCTION datetime2fromparts_vu_prepare_f2()
 GO
