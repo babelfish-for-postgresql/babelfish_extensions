d642c88282245f3064561498bd923329ca3105a7
Sujan Mujawar <sujanmjr@amazon.com>
2025-12-30 17:49:56 +0000

    change in upgrade script
    Signed-off-by: Sujan Mujawar <sujanmjr@amazon.com>

--- contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--5.4.0--5.5.0.sql
+++ contrib/babelfishpg_tsql/sql/upgrades/babelfishpg_tsql--5.4.0--5.5.0.sql
@@ -408,16 +408,10 @@ AS
 $BODY$
 DECLARE
 	v_style SMALLINT;
-    v_type regtype;
 BEGIN
 	v_style := floor(p_style)::SMALLINT;
-    v_type := pg_typeof(arg);
 
-	IF v_type = 'text'::regtype OR v_type = 'sys.ntext'::regtype THEN
-        RAISE USING MESSAGE := 'Explicit conversion from data type text/ntext to varchar is not allowed.';
-    END IF;
-
-    CASE v_type
+    CASE pg_typeof(arg)
 	WHEN 'date'::regtype THEN
 		IF v_style = -1 THEN
 			RETURN sys.babelfish_try_conv_date_to_string(typename, arg);
@@ -1402,10 +1396,16 @@ AS
 $BODY$
 DECLARE
 	v_style SMALLINT;
+    v_type regtype;
 BEGIN
 	v_style := floor(p_style)::SMALLINT;
+    v_type := pg_typeof(arg);
 
-	CASE pg_typeof(arg)
+	IF v_type = 'text'::regtype OR v_type = 'sys.ntext'::regtype THEN
+        RAISE USING MESSAGE := 'Explicit conversion from data type text/ntext to varchar is not allowed.';
+    END IF;
+
+    CASE v_type
 	WHEN 'date'::regtype THEN
 		IF NOT p_style_specified AND v_style = -1 THEN
 			RETURN sys.babelfish_try_conv_date_to_string(typename, arg);
