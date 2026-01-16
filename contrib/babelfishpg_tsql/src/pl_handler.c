36b1eecc59b637b1e15eda9d50640eec525e52b6
Ayush Shah <ayushdsh@amazon.com>
2026-01-16 19:05:27 +0530

    Fix: Crash when dropping table variable related temp tables due to being in PG dialect (#4410)
    In BABEL-6227, nested calls corrupted the dialect to 'postgres' causing
    table variable temp table cleanup to fail. In BABEL-6239, nested calls left
    the dialect as 'postgres' causing subsequent TSQL queries to be parsed
    with the wrong parser.
    
    This is because of using both direct assignment and set_config_option()
    function together in a nested call stack. The set_config_option() function
    is PGs GUC setting infrastructure which internally maintains a prev value
    stack. This does not work well, if caller functions have used direct
    assignment instead because it would mean that the value does not get
    registered in the internal GUC stack, leading to restoration of incorrect
    value for sql_dialect.
    
    The fix for this is to use direct assignment in the culprit function which
    is using set_config_option. Previously, we've made the decision to use
    direct assignment instead of set_config because of perf related issues.
    
    Task - BABEL-6227 & BABEL-6239
    
    Signed-off-by: Ayush Shah <ayushdsh@amazon.com>
    (cherry picked from commit 67248ef01169e9d30cd5ff70680fa9f57459b5bc)
    (cherry picked from commit 539592ca8fd83f699bd938f4fb977aa2c56a064c)

--- contrib/babelfishpg_tsql/src/pl_handler.c
+++ contrib/babelfishpg_tsql/src/pl_handler.c
@@ -5891,27 +5891,19 @@ pltsql_truncate_identifier_func(PG_FUNCTION_ARGS)
 {
 	char	   *name = text_to_cstring(PG_GETARG_TEXT_PP(0));
 	int			len = strlen(name);
-	const char *saved_dialect = GetConfigOption("babelfishpg_tsql.sql_dialect", true, true);
+	int			saved_dialect = sql_dialect;
 
 	PG_TRY();
 	{
 		/* this is BBF help function. use BBF truncation logic */
-		set_config_option("babelfishpg_tsql.sql_dialect", "tsql",
-						  GUC_CONTEXT_CONFIG,
-						  PGC_S_SESSION, GUC_ACTION_SAVE, true, 0, false);
+		sql_dialect = SQL_DIALECT_TSQL;
 		truncate_identifier(name, len, false);
 	}
-	PG_CATCH();
+	PG_FINALLY();
 	{
-		set_config_option("babelfishpg_tsql.sql_dialect", saved_dialect,
-						  GUC_CONTEXT_CONFIG,
-						  PGC_S_SESSION, GUC_ACTION_SAVE, true, 0, false);
-		PG_RE_THROW();
+		sql_dialect = saved_dialect;
 	}
 	PG_END_TRY();
-	set_config_option("babelfishpg_tsql.sql_dialect", saved_dialect,
-					  GUC_CONTEXT_CONFIG,
-					  PGC_S_SESSION, GUC_ACTION_SAVE, true, 0, false);
 
 	PG_RETURN_TEXT_P(cstring_to_text(name));
 }
