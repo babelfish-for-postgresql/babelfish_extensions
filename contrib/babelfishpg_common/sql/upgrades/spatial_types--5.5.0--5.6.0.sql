4efb61771cd0f457177adffff5340e1b124562dd
Gopal Verma <gopalgv@amazon.com>
2026-02-13 08:40:40 +0000

    update spatial_types file

--- contrib/babelfishpg_common/sql/upgrades/spatial_types--5.5.0--5.6.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/spatial_types--5.5.0--5.6.0.sql
@@ -10,6 +10,11 @@ CREATE OR REPLACE FUNCTION sys.Geometry__Parse(geometry_tagged_text sys.NVARCHAR
 	     IF UPPER(geometry_tagged_text COLLATE "default") = 'NULL' THEN
             RETURN NULL;
         END IF;
+
+        IF geometry_tagged_text COLLATE "default" ~* '\s+ZM?\s*\(' THEN
+            RAISE EXCEPTION 'parse error - invalid geometry';
+        END IF;
+        
         RETURN sys.geomfromtext_helper(geometry_tagged_text::text, 0);
     END;
     $$ LANGUAGE plpgsql STRICT IMMUTABLE PARALLEL SAFE;
@@ -23,6 +28,11 @@ CREATE OR REPLACE FUNCTION sys.Geography__Parse(geography_tagged_text sys.NVARCH
 	    IF UPPER(geography_tagged_text COLLATE "default") = 'NULL' THEN
             RETURN NULL;
         END IF;
+         -- Reject Z/ZM dimension qualifier
+        IF geography_tagged_text COLLATE "default" ~* '\s+ZM?\s*\(' THEN
+            RAISE EXCEPTION 'parse error - invalid geometry';
+        END IF;
+        
         RETURN sys.geogfromtext_helper(geography_tagged_text::text, 4326);
     END;
     $$ LANGUAGE plpgsql STRICT IMMUTABLE PARALLEL SAFE;
