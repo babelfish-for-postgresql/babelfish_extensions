8fadcb9a6074a31cbc8f4c20f8a47466b107b62b
Japleen Kaur <amjj@amazon.com>
2026-02-19 15:50:17 +0000

    update leak warning

--- .github/scripts/scan-warnings.sh
+++ .github/scripts/scan-warnings.sh
@@ -49,8 +49,8 @@ if [[ "$SNAPSHOT_ACTIVE_COUNT" -ne 44 ]]; then
     ERROR_FOUND=true
 fi
 
-if [[ "$LEAK_COUNT" -ne 376 ]]; then
-    echo "Error: Expected 376 leak warnings, but found $LEAK_COUNT"
+if [[ "$LEAK_COUNT" -ne 416 ]]; then
+    echo "Error: Expected 416 leak warnings, but found $LEAK_COUNT"
     ERROR_FOUND=true
 fi
 
