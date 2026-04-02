fa2702065c0a1240a9778188faf4937f6b5e6318
Harsh Dubey <harshdu@amazon.com>
2026-04-02 07:04:51 +0000

    Resolving conflicts
    Signed-off-by: Harsh Dubey <harshdu@amazon.com>

--- contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
+++ contrib/babelfishpg_tds/src/backend/tds/tdsresponse.c
@@ -1636,12 +1636,7 @@ PrepareRowDescription(TupleDesc typeinfo, PlannedStmt *plannedstmt, List *target
 				 * schema name only assuming its shared schema.
 				 */
 				if (relMetaDataInfo->partName[1] == NULL)
-				{
-					if (physical_schema_name != NULL)
-						relMetaDataInfo->partName[1] = pstrdup(physical_schema_name);
-					else
-						relMetaDataInfo->partName[1] = NULL;
-				}
+					relMetaDataInfo->partName[1] = pstrdup(physical_schema_name);
 				
 				if (physical_schema_name)
 					pfree(physical_schema_name);
