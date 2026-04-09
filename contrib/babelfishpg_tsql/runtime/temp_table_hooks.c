9fc94012cc76be8c68b335672f3cd7a92ce0f336
Ayush-061 <ayushhx@amazon.com>
2026-04-09 10:31:49 +0000

    Moved validation to beginning and only kept connection check

--- contrib/babelfishpg_tsql/runtime/temp_table_hooks.c
+++ contrib/babelfishpg_tsql/runtime/temp_table_hooks.c
@@ -1,7 +1,7 @@
 /*-------------------------------------------------------------------------
  *
  * temp_table_hooks.c
- *	  Functions for handling temp table attribute lookups for BCP support
+ *	  Functions for handling temp table attribute lookups
  *
  *-------------------------------------------------------------------------
  */
@@ -38,8 +38,8 @@ PG_FUNCTION_INFO_V1(is_temp_table_name);
  * Uses object_id() to resolve table name to OID, then retrieves attributes
  * from ENR cache or pg_attribute catalog.
  *
- * This function only works in T-SQL dialect context. Returns empty result
- * set when called from PG dialect.
+ * This function only works on TDS connections (Babelfish context).
+ * Returns empty result set when called from non-TDS connections.
  */
 Datum
 get_tsql_temp_table_attributes(PG_FUNCTION_ARGS)
@@ -47,6 +47,29 @@ get_tsql_temp_table_attributes(PG_FUNCTION_ARGS)
 	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
 	Relation	attrel;
 	Oid			relid = InvalidOid;
+	Datum		tablename;
+	bool		tablename_isnull;
+	FmgrInfo	flinfo;
+
+	/* Extract arguments into local variables */
+	tablename = PG_GETARG_DATUM(0);
+	tablename_isnull = PG_ARGISNULL(0);
+
+	/* Initialize FmgrInfo for object_id call */
+	MemSet(&flinfo, 0, sizeof(flinfo));
+	flinfo.fn_addr = object_id;
+	flinfo.fn_nargs = 2;
+	flinfo.fn_mcxt = CurrentMemoryContext;
+
+	/* Only process on TDS connections. Return empty result set otherwise. */
+	if (!(is_bbf_tds_connection_hook && is_bbf_tds_connection_hook()))
+	{
+		attrel = table_open(AttributeRelationId, AccessShareLock);
+		rsinfo->expectedDesc = RelationGetDescr(attrel);
+		InitMaterializedSRF(fcinfo, MAT_SRF_USE_EXPECTED_DESC | MAT_SRF_BLESS);
+		table_close(attrel, AccessShareLock);
+		return (Datum) 0;
+	}
 
 	attrel = table_open(AttributeRelationId, AccessShareLock);
 
@@ -56,27 +79,13 @@ get_tsql_temp_table_attributes(PG_FUNCTION_ARGS)
 		rsinfo->expectedDesc = RelationGetDescr(attrel);
 		InitMaterializedSRF(fcinfo, MAT_SRF_USE_EXPECTED_DESC | MAT_SRF_BLESS);
 
-		/*
-		 * Only process in T-SQL dialect or TDS connection. In PG dialect from
-		 * non-TDS connection, return empty result set.
-		 */
-		if (sql_dialect != SQL_DIALECT_TSQL &&
-			!(is_bbf_tds_connection_hook && is_bbf_tds_connection_hook()))
-			goto done;
-
 		/* Resolve table name to OID via object_id() */
-		if (!PG_ARGISNULL(0))
+		if (!tablename_isnull)
 		{
 			LOCAL_FCINFO(locfcinfo, 2);
-			FmgrInfo	flinfo;
-
-			MemSet(&flinfo, 0, sizeof(flinfo));
-			flinfo.fn_addr = object_id;
-			flinfo.fn_nargs = 2;
-			flinfo.fn_mcxt = CurrentMemoryContext;
 
 			InitFunctionCallInfoData(*locfcinfo, &flinfo, 2, InvalidOid, NULL, NULL);
-			locfcinfo->args[0].value = PG_GETARG_DATUM(0);
+			locfcinfo->args[0].value = tablename;
 			locfcinfo->args[0].isnull = false;
 			locfcinfo->args[1].value = (Datum) 0;
 			locfcinfo->args[1].isnull = true;
@@ -115,9 +124,6 @@ get_tsql_temp_table_attributes(PG_FUNCTION_ARGS)
 				systable_endscan(scan);
 			}
 		}
-
-done:
-		;	/* empty statement for label */
 	}
 	PG_FINALLY();
 	{
@@ -141,10 +147,13 @@ is_temp_table_name(PG_FUNCTION_ARGS)
 	char   *object_name;
 	bool	result;
 
+	/* Validate argument at the start */
 	if (PG_ARGISNULL(0))
 		PG_RETURN_BOOL(false);
 
+	/* Extract argument into local variable */
 	input = text_to_cstring(PG_GETARG_VARCHAR_PP(0));
+
 	downcase_truncate_split_object_name(input, NULL, NULL, NULL, &object_name);
 	pfree(input);
 
