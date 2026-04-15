4f04c5759ed3c210c37fcce446e72c1cad3255bc
Mohit Raj <90141011+Mohitraj-g@users.noreply.github.com>
2026-04-15 16:43:14 +0530

    Initial commit for Babelfish v2.18
    This commit contains the following changes:
    
    1. Add version info and upgrade test for 14.23
    2. Add upgrade schedule files for 14.23
    3. Bump babelfish_version from 2.17.0 to 2.18.0
    
    Signed-off-by: Mohitraj G <mrxmohit@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "2.17.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.23.0.0"
+#define BABELFISH_VERSION_STR "2.18.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.24.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
