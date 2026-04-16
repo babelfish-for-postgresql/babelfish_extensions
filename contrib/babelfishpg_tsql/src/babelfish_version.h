67fbdc4e1c0a4da806f028a692c8a9025a9fbd8c
Mohit Raj <90141011+Mohitraj-g@users.noreply.github.com>
2026-04-16 23:24:22 +0530

    Initial commit for Babelfish v4.11 (#4736)
    This commit contains the following changes:
    
    1. Bump babelfish_version from 4.10.0 to 4.11.0
    2. Update T-SQL extension version to 4.11.0 and add upgrade scripts
    3. Update common extension version to 4.11.0 and add upgrade scripts
    
    Signed-off-by: Mohitraj G <mrxmohit@amazon.com>
    Assisted by Kiro

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "4.10.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.14.0.0"
+#define BABELFISH_VERSION_STR "4.11.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 16.15.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
