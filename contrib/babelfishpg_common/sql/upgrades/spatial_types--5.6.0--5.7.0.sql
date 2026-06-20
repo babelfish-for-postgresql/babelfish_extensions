774464044ca270f7b709e81d3e4dcde49205fc2b
Gopal Verma <gopalgv@amazon.com>
2026-06-20 11:37:27 +0000

    Define temporary babelfish_drop_deprecated_object proc in spatial upgrade
    The 5.6.0->5.7.0 spatial upgrade called sys.babelfish_drop_deprecated_object
    to drop the removed *_helper functions, but that procedure is a temporary
    upgrade-only helper that each script must define at its top and drop at its
    end. Without the definition, pg_upgrade failed with:
      ERROR: procedure sys.babelfish_drop_deprecated_object(unknown,unknown,unknown) does not exist
    
    Add the CREATE OR REPLACE PROCEDURE at the start of the deprecation section
    and DROP PROCEDURE as the last statement, matching the existing
    spatial_types--5.3.0--5.4.0 convention.

--- contrib/babelfishpg_common/sql/upgrades/spatial_types--5.6.0--5.7.0.sql
+++ contrib/babelfishpg_common/sql/upgrades/spatial_types--5.6.0--5.7.0.sql
@@ -1122,6 +1122,36 @@ CREATE OPERATOR CLASS sys.gist_geography_ops_2d
 -- install (otherwise pg_dump and upgrade dependency checks diverge).
 -- =============================================================
 
+-- Drops an object if it does not have any dependent objects.
+-- Is a temporary procedure for use by the upgrade script. Will be dropped at the end of the upgrade.
+-- Please have this be one of the first statements executed in this upgrade script.
+CREATE OR REPLACE PROCEDURE babelfish_drop_deprecated_object(object_type varchar, schema_name varchar, object_name varchar) AS
+$$
+DECLARE
+    error_msg text;
+    query1 text;
+    query2 text;
+BEGIN
+
+    query1 := pg_catalog.format('alter extension babelfishpg_common drop %s %s.%s', object_type, schema_name, object_name);
+    query2 := pg_catalog.format('drop %s %s.%s', object_type, schema_name, object_name);
+
+    execute query1;
+    execute query2;
+EXCEPTION
+    when object_not_in_prerequisite_state then --if 'alter extension' statement fails
+        GET STACKED DIAGNOSTICS error_msg = MESSAGE_TEXT;
+        raise warning '%', error_msg;
+    when dependent_objects_still_exist then --if 'drop view' statement fails
+        GET STACKED DIAGNOSTICS error_msg = MESSAGE_TEXT;
+        raise warning '%', error_msg;
+    when undefined_function then --if 'Deprecated function does not exist'
+        GET STACKED DIAGNOSTICS error_msg = MESSAGE_TEXT;
+        raise warning '%', error_msg;
+end
+$$
+LANGUAGE plpgsql;
+
 DO $$
 DECLARE
     exception_message text;
@@ -1646,3 +1676,7 @@ END;
 $$;
 
 CALL sys.babelfish_drop_deprecated_object('function', 'sys', 'z_helper_geog_deprecated_5_7_0');
+
+-- Drops the temporary procedure used by the upgrade script.
+-- Please have this be one of the last statements executed in this upgrade script.
+DROP PROCEDURE sys.babelfish_drop_deprecated_object(varchar, varchar, varchar);
