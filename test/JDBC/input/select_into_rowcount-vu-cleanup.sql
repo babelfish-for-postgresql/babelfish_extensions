47b29f39fc9c2f3444f6af94cfd2de67368fbfaf
Rucha Kulkarni <ruchask@amazon.com>
2026-06-28 07:50:13 +0000

    Fix rowcount not reported for SELECT INTO
    Signed-off-by: Rucha Kulkarni <ruchask@amazon.com>

---
+++ test/JDBC/input/select_into_rowcount-vu-cleanup.sql
@@ -0,0 +1,12 @@
+DROP TABLE select_into_rowcount_vu_verify_t1;
+GO
+DROP TABLE select_into_rowcount_vu_verify_t2;
+GO
+DROP TABLE select_into_rowcount_vu_verify_t3;
+GO
+DROP TABLE select_into_rowcount_vu_verify_t7;
+GO
+DROP TABLE select_into_rowcount_vu_verify_t9;
+GO
+DROP TABLE select_into_rowcount_vu_prepare_src;
+GO
