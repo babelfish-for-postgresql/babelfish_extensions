7c6fe51a1e988397de2ca34d3f10af771c09ceef
Anju Bharti <66729219+anju15bharti@users.noreply.github.com>
2026-07-09 21:00:32 +0530

    Initial commit for Babelfish v3.16 (#4888)
    This commit contains the following changes:
    
    1. Add version info and upgrade test for 15.19 and 14.25
    2. Add upgrade schedule files for 15.19 and 14.25
    3. Bump babelfish_version from 3.15.0 to 3.16.0
    
    Signed-off-by: anju15bharti <abanju@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "3.15.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 15.19.0.0"
+#define BABELFISH_VERSION_STR "3.16.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 15.20.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
