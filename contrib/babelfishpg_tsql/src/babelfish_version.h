bd7edb0d0fdbfe6bad48a5a43d7c3e35507d4ab5
Anju Bharti <66729219+anju15bharti@users.noreply.github.com>
2026-07-09 20:59:19 +0530

    Initial commit for Babelfish v2.19 (#4889)
    This commit contains the following changes:
    
    1. Add version info and upgrade test for 14.24
    2. Add upgrade schedule files for 14.24
    3. Bump babelfish_version from 2.18.0 to 2.19.0
    
    Signed-off-by: anju15bharti <abanju@amazon.com>

--- contrib/babelfishpg_tsql/src/babelfish_version.h
+++ contrib/babelfishpg_tsql/src/babelfish_version.h
@@ -8,7 +8,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define BABELFISH_VERSION_STR "2.18.0"
-#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.24.0.0"
+#define BABELFISH_VERSION_STR "2.19.0"
+#define BABELFISH_INTERNAL_VERSION_STR "Babelfish 14.25.0.0"
 #define BABEL_COMPATIBILITY_VERSION "12.0.2000.8"
 #define BABEL_COMPATIBILITY_MAJOR_VERSION "12"
