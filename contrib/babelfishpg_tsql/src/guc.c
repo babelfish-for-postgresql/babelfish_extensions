ab820bddaffa540af82f2f689c3e535f82f14d89
Harsh Dubey <100467917+harshdubey166@users.noreply.github.com>
2026-08-26 11:07:39 +0530

    [OSS-ONLY] Minor fixes and improvements in TDS and PL/tsql handling (#4955)
    Minor fixes and improvements in TDS and PL/tsql handling
    
    Authored by : harshdu@amazon.com
    Signed-off by : harshdu@amazon.com

--- contrib/babelfishpg_tsql/src/guc.c
+++ contrib/babelfishpg_tsql/src/guc.c
@@ -1124,7 +1124,7 @@ define_custom_variables(void)
 							 NULL,
 							 &pltsql_enable_create_alter_view_from_pg,
 							 false,
-							 PGC_USERSET,
+							 PGC_SUSET,
 							 GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_FILE | GUC_DISALLOW_IN_AUTO_FILE,
 							 NULL, NULL, NULL);
 	/*
@@ -1135,7 +1135,7 @@ define_custom_variables(void)
 							 NULL,
 							 &pltsql_enable_alter_owner_from_pg,
 							 false,
-							 PGC_USERSET,
+							 PGC_SUSET,
 							 GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_FILE | GUC_DISALLOW_IN_AUTO_FILE,
 							 NULL, NULL, NULL);
 
@@ -1145,7 +1145,7 @@ define_custom_variables(void)
 							 NULL,
 							 &babelfish_dump_restore,
 							 false,
-							 PGC_USERSET,
+							 PGC_SUSET,
 							 GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_FILE | GUC_DISALLOW_IN_AUTO_FILE,
 							 NULL, NULL, NULL);
 
@@ -1154,7 +1154,7 @@ define_custom_variables(void)
 							 NULL,
 							 &restore_tsql_tabletype,
 							 false,
-							 PGC_USERSET,
+							 PGC_SUSET,
 							 GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_FILE | GUC_DISALLOW_IN_AUTO_FILE,
 							 NULL, NULL, NULL);
 
@@ -1163,7 +1163,7 @@ define_custom_variables(void)
 							   NULL,
 							   &babelfish_dump_restore_min_oid,
 							   NULL,
-							   PGC_USERSET,
+							   PGC_SUSET,
 							   GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_FILE | GUC_DISALLOW_IN_AUTO_FILE,
 							   check_babelfish_dump_restore_min_oid, NULL, NULL);
 
