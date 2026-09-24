553b3ee85ba3f7756cd52e00e3d56192fee9fa3c
Ayush Shah <ayushdsh@amazon.com>
2026-09-24 14:51:58 +0000

    increased sla

--- test/JDBC/input/BABEL-SP_COLUMNS_MANAGED-dep-vu-verify.sql
+++ test/JDBC/input/BABEL-SP_COLUMNS_MANAGED-dep-vu-verify.sql
@@ -1,4 +1,4 @@
--- sla 25000
+-- sla 40000
 SET NOCOUNT ON
 DECLARE @sp_columns_managed_var table(a nvarchar(128), b nvarchar(128), c nvarchar(128), d nvarchar(128), e int, f nvarchar(4000), g nvarchar(3), h nvarchar(128), i int, j int, k int, l int, m int, n int, o nvarchar(128), p nvarchar(128), q nvarchar(128), r nvarchar(128), s int, t int, u int)
 INSERT INTO @sp_columns_managed_var EXEC babel_sp_columns_managed_dep_vu_prepare_p1 "master", "dbo", "sysdatabases"
